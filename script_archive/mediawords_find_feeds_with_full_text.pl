c8ffa30be83b8896e490f3fa5b6a909591abb5c4
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-05-24 20:00:52 +0000

    Split up query into seperate variables.

--- script_archive/mediawords_find_feeds_with_full_text.pl
+++ script_archive/mediawords_find_feeds_with_full_text.pl
@@ -11,18 +11,22 @@ BEGIN
 
 use DBIx::Simple::MediaWords;
 use MediaWords::DB;
+use Readonly;
 
 # create a media source from a feed url
 sub main
 {
     my $db = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info );
 
-    my $res =
-      $db->query( "select * from media natural join  " .
-          "(select media_id, max(similarity) as max_similarity, avg(similarity) as avg_similarity, " .
+
+    Readonly my $aggregate_stats => " max(similarity) as max_similarity, avg(similarity) as avg_similarity, " .
           "   min(similarity) as min_similarity,  avg(length(extracted_text)) as avg_extracted_length," .
           "   avg(length(html_strip(title || description))) as avg_rss_length,  " .
-          "   avg(length(html_strip(description))) as avg_rss_discription, count(*) from "
+          "   avg(length(html_strip(description))) as avg_rss_discription ";
+
+    my $res =
+      $db->query( "select * from media natural join  " .
+          "(select media_id, $aggregate_stats, count(*) from " .
           "     (select *, similarity(extracted_text, html_strip(title || description) ) " .
           "       from stories, (select stories_id,  array_to_string(array_agg(download_text), ' ') as extracted_text   " .
 "from (select * from downloads natural join stories natural join download_texts where publish_date > now() - interval ' 1 week'  order by downloads_id) "
