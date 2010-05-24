e10ce93cdcdda0d12e6cd867dbaadf5f62ab8067
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-05-24 20:59:47 +0000

    Further split query into seperate variables also restrict to media source with 2 or less feeds.

--- script_archive/mediawords_find_feeds_with_full_text.pl
+++ script_archive/mediawords_find_feeds_with_full_text.pl
@@ -24,18 +24,26 @@ sub main
           "   avg(length(html_strip(title || description))) as avg_rss_length,  " .
           "   avg(length(html_strip(description))) as avg_rss_discription ";
 
+    Readonly my $story_restrictions => "publish_date > now() - interval '1 week' and media_id in (select media_id from media_feed_counts where feed_count <= 2)";
+
+    Readonly my $story_extracted_text => "select stories_id,  array_to_string(array_agg(download_text), ' ') as extracted_text   " .
+"from (select * from downloads natural join stories natural join download_texts where $story_restrictions  order by downloads_id) "
+          . "            as downloads group by stories_id ";
+
     my $res =
       $db->query( "select * from media natural join  " .
           "(select media_id, $aggregate_stats, count(*) from " .
           "     (select *, similarity(extracted_text, html_strip(title || description) ) " .
-          "       from stories, (select stories_id,  array_to_string(array_agg(download_text), ' ') as extracted_text   " .
-"from (select * from downloads natural join stories natural join download_texts where publish_date > now() - interval ' 1 week'  order by downloads_id) "
-          . "            as downloads group by stories_id) as story_extracted_texts where stories.stories_id = story_extracted_texts.stories_id and publish_date > now() - interval '1 week' ) as media_extraction_text_similarity group by media_id order by media_id ) as foo"
-      );
+          "       from stories, ( $story_extracted_text ) as story_extracted_texts " .
+	  " where stories.stories_id = story_extracted_texts.stories_id and $story_restrictions ) as media_extraction_text_similarity group by media_id order by media_id ) as foo"
+		);
+
+    my $arrays = $res->arrays;
 
     print "Output:\n";
-    print $res->flat;
+    print join "\n" , @{[map { join ", ", @{$_} } @$arrays]};
     print "\n";
 }
 
 main();
+
