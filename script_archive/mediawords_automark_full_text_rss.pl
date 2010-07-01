67df0089f23620b03cebb2a06367517bad90ac69
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-07-01 22:11:55 +0000

    Initial script to automatically mark the most obvious media as being full text rss.

---
+++ script_archive/mediawords_automark_full_text_rss.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+
+BEGIN
+{
+    use FindBin;
+    use lib "$FindBin::Bin/../lib";
+}
+
+use Encode;
+use MediaWords::DB;
+use DBIx::Simple::MediaWords;
+use MediaWords::Tagger;
+use MediaWords::DBI::Downloads;
+use List::Uniq ':all';
+
+sub main
+{
+
+    my $dbs = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info )
+      || die DBIx::Simple::MediaWords->error;
+
+    $dbs->query(
+'update media set full_text_rss = true where media_id in (select media_id from media_rss_full_text_detection_data where  avg_similarity >= 0.97 and avg_rss_length > 1000) '
+    );
+
+}
+
