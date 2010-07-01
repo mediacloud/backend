e5a935db7329bcb933eabf8a32bcbbdc039e119f
Hal Roberts <hroberts@cyber.law.harvard.edu>
2010-07-01 13:57:11 +0000

    formatting

--- lib/MediaWords/Util/HTML.pm
+++ lib/MediaWords/Util/HTML.pm
@@ -15,8 +15,8 @@ use HTML::Entities qw( decode_entities  );
 use Devel::Peek qw(Dump);
 use Encode;
 
-#provide a procedural interface to HTML::Strip
-# cpan the module should probably include this but it doesn't
+# provide a procedural interface to HTML::Strip
+# use HTML::StripPP instead of HTML::Strip b/c HTML::Strip mucks up the encoding
 sub html_strip
 {
 
@@ -29,6 +29,7 @@ sub html_strip
     return HTML::StripPP::strip( $_[ 0 ] ) || '';
 }
 
+# old version of html_strip that tries to work around HTML::Strip encoding problem
 sub html_strip_encoding_fix
 {
     my $html_text = shift;
