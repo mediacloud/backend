575d9ce88cc3d4aa234dec518591718e54d8059b
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-07-01 22:30:19 +0000

    Add db commit.

--- script_archive/mediawords_automark_full_text_rss.pl
+++ script_archive/mediawords_automark_full_text_rss.pl
@@ -18,12 +18,12 @@ use List::Uniq ':all';
 sub main
 {
 
-    my $dbs = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info )
-      || die DBIx::Simple::MediaWords->error;
+    my $dbs = MediaWords::DB::connect_to_db;
 
     $dbs->query(
 'update media set full_text_rss = true where media_id in (select media.media_id from media_rss_full_text_detection_data natural join media where  avg_similarity >= 0.97 and avg_rss_length > 1000 and full_text_rss is null)'
-    );
+    ) || die;
 
+    $dbs->commit;
 }
 
