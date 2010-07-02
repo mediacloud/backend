281624be9aa00c4346fd2a5bf7e2ed670b98499b
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-07-02 17:47:52 +0000

    Fix bug in script.

--- script_archive/mediawords_automark_full_text_rss.pl
+++ script_archive/mediawords_automark_full_text_rss.pl
@@ -26,18 +26,21 @@ sub main
 'select media.media_id from media_rss_full_text_detection_data natural join media where  avg_similarity >= 0.97 and avg_rss_length > 1000 and full_text_rss is null'
     )->flat;
 
-    say "updating " . scalar( @media_ids_to_update ) . " media ids";
+    say "initially updating " . scalar( @media_ids_to_update ) . " media ids";
 
-    return if scalar( @media_ids_to_update ) == 0;
+    if (scalar( @media_ids_to_update ) > 0)
+      {
 
     $dbs->query( 'update media set full_text_rss = true where media_id in (??)', @media_ids_to_update ) || die;
 
+  }
     @media_ids_to_update = $dbs->query(
 'select media_id from (select media_id, avg_rss_length, avg_extracted_length, (abs(avg_rss_length-avg_extracted_length)/avg_rss_length) as length_proportion  from media_rss_full_text_detection_data where avg_rss_length > 400 ) as foo where length_proportion < 0.03'
     )->flat;
 
+    say "Running: update media set full_text_rss = true where full_text_rss is null and url like '\%livejournal\%' and media_id in (??)";
     $dbs->query(
-"update media set full_text_rss = true where full_text_rss is null and url like '%livejournal%' and media_id in (??)",
+"update media set full_text_rss = true where full_text_rss is null and url like '\%livejournal\%' and media_id in (??)",
         @media_ids_to_update
     );
 
