8fa0b374106f187cec0863a7db0405b7eec10f85
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-07-21 21:25:31 +0000

    Quick query bug fix
    replace and with or

--- script_archive/mediawords_automark_full_text_rss.pl
+++ script_archive/mediawords_automark_full_text_rss.pl
@@ -79,8 +79,8 @@ sub main
         @media_ids_to_update
     );
 
-    my @media_ids_to_update = $dbs->query(
-"select media_id from media_rss_full_text_detection_data natural join media where avg_similarity >= 0.95 and min_similarity >= 0.80 and (url like '%blogspot%' or url like '%livejournal%' and url like '%liveinternet.ru%' ) and full_text_rss is null "
+    @media_ids_to_update = $dbs->query(
+"select media_id from media_rss_full_text_detection_data natural join media where avg_similarity >= 0.95 and min_similarity >= 0.80 and (url like '%blogspot%' or url like '%livejournal%' or url like '%liveinternet.ru%' ) and full_text_rss is null "
     )->flat;
 
     say "Updating " . scalar( @media_ids_to_update );
