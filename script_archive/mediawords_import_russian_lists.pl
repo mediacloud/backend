c8ba35e382b81cae0aa10e41645e45e849f2dee3
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-07-30 19:20:10 +0000

    Add scrapper for top100.rambler.ru

--- script_archive/mediawords_import_russian_lists.pl
+++ script_archive/mediawords_import_russian_lists.pl
@@ -23,6 +23,7 @@ use MediaWords::DB;
 use Data::Dumper;
 use Perl6::Say;
 use Class::CSV;
+use utf8;
 use List::MoreUtils qw(any all none notall true false firstidx first_index 
                            lastidx last_index insert_after insert_after_string 
                            apply after after_incl before before_incl indexes 
@@ -102,16 +103,129 @@ sub get_liveinternet_ru_rankings
     $csv->print();
 }
 
+sub get_top100_rambler_rankings
+{
+   my $ua = LWP::UserAgent->new;
+
+    my $fields = [qw/ rank name link unique_visitors page_views /];
+
+    my $csv = Class::CSV->new(
+    fields         => $fields,
+			     );
+
+    # add header line.
+    $csv->add_line($fields);
+
+    my $foo;
+
+    for my $i ( 1 .. 35 )
+    {
+        my $url = "http://top100.rambler.ru/navi/?theme=440&page=$i&view=full";
+        print STDERR "fetching $url \n";
+        my $html = $ua->get( $url )->decoded_content;
+
+	my $tree = HTML::TreeBuilder::XPath->new; # empty tree
+	$tree->parse_content($html);
+
+	my @p= $tree->findnodes( "//table[\@id='stat-top100']/tr");
+
+	foreach my $p (@p)
+	  {
+	    #say STDERR "Here's the node dumped";
+	    
+	    my @children = $p->content_list();
+
+	    #say $p->dump;
+
+	    unless( scalar(@children) == 3)
+	      {
+		say "skipping wrong count";
+		say $p->dump;
+		next;
+	      }
+
+	    unless (all {$_->tag eq 'td' } @children)
+	      {
+		say "skipping ";
+		say $p->dump;
+		next;
+	      }
+
+	    my $rank     = $children[0]->as_text;
+	    my $visitors = $children[2]->as_text;
+
+	    $rank  =~ s/.*? ?(\d+)\..*/\1/;
+
+
+	    #say "dumping 2dn child";
+
+	    #$children[1]->dump;
+
+	    my @a_elements = $children[1]->look_down( 
+						"_tag", "a",
+						"class", "rt"
+						) ;
+
+
+	    die unless scalar(@a_elements) == 1;
+
+	    my $a_element = $a_elements[0];
+
+	    #say $a_element->dump;
+
+	    #say "Passes test continuing";
+
+
+	    my $link = $a_element->attr('href');
+
+	    my $name = $a_element->as_text;
+
+	    #say "Link  $link";
+	    #say "Link_text  $name";
+
+	    my @div_class_links_list = $children[1]->look_down( 
+						"_tag", "div",
+						"class", "links"
+						) ;
+
+	    #say "divclass links element count:";
+
+	    #say  scalar( @div_class_links_list ) . "elements";
+
+	    my $div_class_links = shift @div_class_links_list;
+
+	    #say $div_class_links->dump;
+
+	    die unless $div_class_links->content_list == 2;
+
+	    my $text_child = ($div_class_links->content_list)[1];
+
+	    die unless scalar($text_child);
+
+	    my $rankings = $text_child;
+
+	    $rankings =~ s/ — Индекс популярности: //;
+	    
+	    my ($rank1, $rank2) = split /\s+/, $rankings;
+
+	    #NOTE: Assuming the 2 ranks are “number of unique visitors for specific period of time”; “number of page views for specific time”.
+
+	    $csv->add_line( { rank => $rank, name => $name, link => $link, unique_visitors => $rank1, page_views => $rank2 } );
+	  }
+
+	# Now that we're done with it, we must destroy it.
+	$tree = $tree->delete;
+    }
+
+    $csv->print();
+}
+
 sub main
 {
     binmode STDOUT, ":utf8";
     binmode STDERR, ":utf8";
 
-    get_liveinternet_ru_rankings
+    get_top100_rambler_rankings;
 }
 
 main();
-
-__END__
-
-"></a><a href="http://unab0mber.livejournal.com/" title="Abandon all hope ye who enter here">
