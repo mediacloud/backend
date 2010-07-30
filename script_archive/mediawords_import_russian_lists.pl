5cf5a132a546596905d0d32522efb168bda5bd13
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-07-30 19:23:21 +0000

    Reformat code

--- script_archive/mediawords_import_russian_lists.pl
+++ script_archive/mediawords_import_russian_lists.pl
@@ -24,26 +24,23 @@ use Data::Dumper;
 use Perl6::Say;
 use Class::CSV;
 use utf8;
-use List::MoreUtils qw(any all none notall true false firstidx first_index 
-                           lastidx last_index insert_after insert_after_string 
-                           apply after after_incl before before_incl indexes 
-                           firstval first_value lastval last_value each_array
-                           each_arrayref pairwise natatime mesh zip uniq minmax);
+use List::MoreUtils qw(any all none notall true false firstidx first_index
+  lastidx last_index insert_after insert_after_string
+  apply after after_incl before before_incl indexes
+  firstval first_value lastval last_value each_array
+  each_arrayref pairwise natatime mesh zip uniq minmax);
 use constant COLLECTION_TAG => 'russian_yandex_20100316';
 
 sub get_liveinternet_ru_rankings
 {
-   my $ua = LWP::UserAgent->new;
+    my $ua = LWP::UserAgent->new;
 
-    my $fields = [qw/rank name link visitors/];
+    my $fields = [ qw/rank name link visitors/ ];
 
-    my $csv = Class::CSV->new(
-    fields         => $fields,
-			     );
+    my $csv = Class::CSV->new( fields => $fields, );
 
     # add header line.
-    $csv->add_line($fields);
-
+    $csv->add_line( $fields );
 
     for my $i ( 1 .. 30 )
     {
@@ -51,53 +48,56 @@ sub get_liveinternet_ru_rankings
         print STDERR "fetching $url \n";
         my $html = $ua->get( $url )->decoded_content;
 
-	my $tree = HTML::TreeBuilder::XPath->new; # empty tree
-	$tree->parse_content($html);
+        my $tree = HTML::TreeBuilder::XPath->new;    # empty tree
+        $tree->parse_content( $html );
 
-	my @p= $tree->findnodes( '//center/form/center/table/tr/td/table/tr');
+        my @p = $tree->findnodes( '//center/form/center/table/tr/td/table/tr' );
 
-	foreach my $p (@p)
-	  {
-	    #say "Here's the node dumped \n";
-	    
-	    my @children = $p->content_list();
-	    
-	    unless( scalar(@children) == 4)
-	      {
-		#say "skipping wrong count";
-		#say $p->dump;
-		next;
-	      }
+        foreach my $p ( @p )
+        {
 
-	    unless (all {$_->tag eq 'td' } @children)
-	      {
-		#say "skipping ";
-		#say $p->dump;
-		next;
-	      }
+            #say "Here's the node dumped \n";
 
-	    my $rank     = $children[0]->as_text;
-	    my $name     = $children[1]->as_text;
-	    my $visitors = $children[2]->as_text;
+            my @children = $p->content_list();
 
-	    $rank  =~ s/.*? ?(\d+)\..*/\1/;
+            unless ( scalar( @children ) == 4 )
+            {
 
-	    $visitors =~ s/\D//g;
+                #say "skipping wrong count";
+                #say $p->dump;
+                next;
+            }
 
-	    my @a_elements = $children[1]->find('a');
+            unless ( all { $_->tag eq 'td' } @children )
+            {
 
-	    die unless scalar(@a_elements) == 1;
+                #say "skipping ";
+                #say $p->dump;
+                next;
+            }
 
-	    my $a_element = $a_elements[0];
-	    my $link = $a_element->attr('href');
+            my $rank     = $children[ 0 ]->as_text;
+            my $name     = $children[ 1 ]->as_text;
+            my $visitors = $children[ 2 ]->as_text;
 
-	    #say "$rank : $name : $link : $visitors";
-	    
-	    $csv->add_line( { rank => $rank, name => $name, link => $link, visitors => $visitors } );
-	  }
+            $rank =~ s/.*? ?(\d+)\..*/\1/;
 
-	# Now that we're done with it, we must destroy it.
-	$tree = $tree->delete;
+            $visitors =~ s/\D//g;
+
+            my @a_elements = $children[ 1 ]->find( 'a' );
+
+            die unless scalar( @a_elements ) == 1;
+
+            my $a_element = $a_elements[ 0 ];
+            my $link      = $a_element->attr( 'href' );
+
+            #say "$rank : $name : $link : $visitors";
+
+            $csv->add_line( { rank => $rank, name => $name, link => $link, visitors => $visitors } );
+        }
+
+        # Now that we're done with it, we must destroy it.
+        $tree = $tree->delete;
     }
 
     $csv->print();
@@ -105,16 +105,14 @@ sub get_liveinternet_ru_rankings
 
 sub get_top100_rambler_rankings
 {
-   my $ua = LWP::UserAgent->new;
+    my $ua = LWP::UserAgent->new;
 
-    my $fields = [qw/ rank name link unique_visitors page_views /];
+    my $fields = [ qw/ rank name link unique_visitors page_views / ];
 
-    my $csv = Class::CSV->new(
-    fields         => $fields,
-			     );
+    my $csv = Class::CSV->new( fields => $fields, );
 
     # add header line.
-    $csv->add_line($fields);
+    $csv->add_line( $fields );
 
     my $foo;
 
@@ -124,97 +122,90 @@ sub get_top100_rambler_rankings
         print STDERR "fetching $url \n";
         my $html = $ua->get( $url )->decoded_content;
 
-	my $tree = HTML::TreeBuilder::XPath->new; # empty tree
-	$tree->parse_content($html);
+        my $tree = HTML::TreeBuilder::XPath->new;    # empty tree
+        $tree->parse_content( $html );
 
-	my @p= $tree->findnodes( "//table[\@id='stat-top100']/tr");
+        my @p = $tree->findnodes( "//table[\@id='stat-top100']/tr" );
 
-	foreach my $p (@p)
-	  {
-	    #say STDERR "Here's the node dumped";
-	    
-	    my @children = $p->content_list();
+        foreach my $p ( @p )
+        {
 
-	    #say $p->dump;
+            #say STDERR "Here's the node dumped";
 
-	    unless( scalar(@children) == 3)
-	      {
-		say "skipping wrong count";
-		say $p->dump;
-		next;
-	      }
+            my @children = $p->content_list();
 
-	    unless (all {$_->tag eq 'td' } @children)
-	      {
-		say "skipping ";
-		say $p->dump;
-		next;
-	      }
+            #say $p->dump;
 
-	    my $rank     = $children[0]->as_text;
-	    my $visitors = $children[2]->as_text;
+            unless ( scalar( @children ) == 3 )
+            {
+                say "skipping wrong count";
+                say $p->dump;
+                next;
+            }
 
-	    $rank  =~ s/.*? ?(\d+)\..*/\1/;
+            unless ( all { $_->tag eq 'td' } @children )
+            {
+                say "skipping ";
+                say $p->dump;
+                next;
+            }
 
+            my $rank     = $children[ 0 ]->as_text;
+            my $visitors = $children[ 2 ]->as_text;
 
-	    #say "dumping 2dn child";
+            $rank =~ s/.*? ?(\d+)\..*/\1/;
 
-	    #$children[1]->dump;
+            #say "dumping 2dn child";
 
-	    my @a_elements = $children[1]->look_down( 
-						"_tag", "a",
-						"class", "rt"
-						) ;
+            #$children[1]->dump;
 
+            my @a_elements = $children[ 1 ]->look_down( "_tag", "a", "class", "rt" );
 
-	    die unless scalar(@a_elements) == 1;
+            die unless scalar( @a_elements ) == 1;
 
-	    my $a_element = $a_elements[0];
+            my $a_element = $a_elements[ 0 ];
 
-	    #say $a_element->dump;
+            #say $a_element->dump;
 
-	    #say "Passes test continuing";
+            #say "Passes test continuing";
 
+            my $link = $a_element->attr( 'href' );
 
-	    my $link = $a_element->attr('href');
+            my $name = $a_element->as_text;
 
-	    my $name = $a_element->as_text;
+            #say "Link  $link";
+            #say "Link_text  $name";
 
-	    #say "Link  $link";
-	    #say "Link_text  $name";
+            my @div_class_links_list = $children[ 1 ]->look_down( "_tag", "div", "class", "links" );
 
-	    my @div_class_links_list = $children[1]->look_down( 
-						"_tag", "div",
-						"class", "links"
-						) ;
+            #say "divclass links element count:";
 
-	    #say "divclass links element count:";
+            #say  scalar( @div_class_links_list ) . "elements";
 
-	    #say  scalar( @div_class_links_list ) . "elements";
+            my $div_class_links = shift @div_class_links_list;
 
-	    my $div_class_links = shift @div_class_links_list;
+            #say $div_class_links->dump;
 
-	    #say $div_class_links->dump;
+            die unless $div_class_links->content_list == 2;
 
-	    die unless $div_class_links->content_list == 2;
+            my $text_child = ( $div_class_links->content_list )[ 1 ];
 
-	    my $text_child = ($div_class_links->content_list)[1];
+            die unless scalar( $text_child );
 
-	    die unless scalar($text_child);
+            my $rankings = $text_child;
 
-	    my $rankings = $text_child;
+            $rankings =~ s/ — Индекс популярности: //;
 
-	    $rankings =~ s/ — Индекс популярности: //;
-	    
-	    my ($rank1, $rank2) = split /\s+/, $rankings;
+            my ( $rank1, $rank2 ) = split /\s+/, $rankings;
 
-	    #NOTE: Assuming the 2 ranks are “number of unique visitors for specific period of time”; “number of page views for specific time”.
+#NOTE: Assuming the 2 ranks are “number of unique visitors for specific period of time”; “number of page views for specific time”.
 
-	    $csv->add_line( { rank => $rank, name => $name, link => $link, unique_visitors => $rank1, page_views => $rank2 } );
-	  }
+            $csv->add_line(
+                { rank => $rank, name => $name, link => $link, unique_visitors => $rank1, page_views => $rank2 } );
+        }
 
-	# Now that we're done with it, we must destroy it.
-	$tree = $tree->delete;
+        # Now that we're done with it, we must destroy it.
+        $tree = $tree->delete;
     }
 
     $csv->print();
