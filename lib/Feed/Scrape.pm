653b08d672ac84b9fd02ed8c52fb0959648360a4
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-09-09 21:56:24 +0000

    Fix bug with atom feeds that have content elements that are not CDATA encoded.

--- lib/Feed/Scrape.pm
+++ lib/Feed/Scrape.pm
@@ -11,6 +11,12 @@ use List::Util;
 use Regexp::Common qw /URI/;
 use URI::URL;
 #use XML::FeedPP;
+use XML::LibXML;
+use List::MoreUtils qw(any all none notall true false firstidx first_index 
+                           lastidx last_index insert_after insert_after_string 
+                           apply after after_incl before before_incl indexes 
+                           firstval first_value lastval last_value each_array
+                           each_arrayref pairwise natatime mesh zip uniq minmax);
 
 use MediaWords::Util::Web;
 
@@ -150,6 +156,81 @@ sub _is_valid_url
 
 # METHODS
 
+sub _fix_atom_content_element_encoding
+{
+    my $xml_string = shift @_;
+
+    my $parser      = XML::LibXML->new;
+    my $doc         = $parser->parse_string( $xml_string );
+    my $doc_element = $doc->documentElement() || die;
+
+    my $xpc = XML::LibXML::XPathContext->new;
+    $xpc->registerNs( 'x', 'http://www.w3.org/2005/Atom' );
+    my @content_nodes = $xpc->findnodes( '//x:entry/x:content', $doc_element )->get_nodelist;
+
+    #either the feed is RSS or there is no content
+    return $xml_string if scalar( @content_nodes ) == 0;
+
+    my $fixed_content_element = 0;
+
+    foreach my $content_node ( @content_nodes )
+    {
+        next if ( !$content_node->hasChildNodes() );
+
+	my $child_nodes = $content_node->childNodes();
+
+        my $child_node_count = $child_nodes->size;
+
+        if ( $child_node_count == 1 )
+        {
+            my $first_child = $content_node->firstChild();
+
+            next if ( $first_child->nodeType == XML_CDATA_SECTION_NODE );
+            next if ( $first_child->nodeType == XML_TEXT_NODE );
+        }
+
+	my @content_node_child_list = $child_nodes->get_nodelist();
+
+        # allow white space before CDATA_SECTION
+	if (any { $_->nodeType == XML_CDATA_SECTION_NODE} @content_node_child_list )
+	{
+	    my @non_cdata_children = grep {$_->nodeType !=  XML_CDATA_SECTION_NODE} @content_node_child_list;
+
+	    if (all {$_->nodeType == XML_TEXT_NODE }  @non_cdata_children )
+	      {
+		if (all {$_->data =~ /\s+/ }  @non_cdata_children )
+		  {
+		    #say STDERR "Skipping CDATA and white space only description ";
+		    #exit;
+		    next;
+		  }
+	      }
+	}
+
+        $fixed_content_element = 1;
+
+        # say STDERR "fixing content_node: " . $content_node->toString;
+        # say Dumper ( [ $child_nodes->get_nodelist() ] );
+        # say Dumper ( [ map { $_->toString } $child_nodes->get_nodelist() ] );
+
+        my $child_nodes_string = join '', ( map { $_->toString() } ( $child_nodes->get_nodelist() ) );
+
+        $content_node->removeChildNodes();
+
+        my $cdata_node = XML::LibXML::CDATASection->new( $child_nodes_string );
+        $content_node->appendChild( $cdata_node );
+        #say STDERR "fixed content_node: " . $content_node->toString;
+    }
+
+    #just return the original string if we didn't need to fix anything...
+    return $xml_string if !$fixed_content_element;
+
+    my $ret = $doc->toString;
+
+    #say "Returning :'$ret'";
+    return $ret;
+}
+
 # parse feed with XML::FeedPP after some simple munging to correct feed formatting.
 # return the XML::FeedPP feed object or undef if the parse failed.
 sub parse_feed
@@ -175,6 +256,8 @@ sub parse_feed
 
     # parser doesn't like files that start with comments
     $content =~ s/^<!--[^>]*-->\s*<\?/<\?/;
+
+    $content = _fix_atom_content_element_encoding( $content );
 
     my $feed;
     eval { $feed = XML::FeedPP::MediaWords->new( { content => $content, type => 'string' } ) };
