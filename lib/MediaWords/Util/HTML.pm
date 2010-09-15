8a6bdf2d616af93020826d58f590bfeb71992b08
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-09-15 19:42:17 +0000

    Add hackish work around to prevent a Perl segfault crash.
    Not sure why things are crashing my only guess is that it's a Perl 5.8 specific bug since I can't 
    reproduce on 5.10.

--- lib/MediaWords/Util/HTML.pm
+++ lib/MediaWords/Util/HTML.pm
@@ -29,6 +29,15 @@ sub html_strip
     #
     # return $text;
 
+    #
+    # TODO HACK to prevent StripPP from segfaulting.
+    # This appears to be necessary on Perl 5.8 but not on 5.10.
+    #
+    if (length ($_[ 0 ] ) > 14000)
+    {
+	return (HTML::StripPP::strip( substr ($_[ 0 ], 0, 14000) ) . HTML::StripPP::strip( substr ($_[ 0 ], 14000) ) ) || '';
+    }
+
     return HTML::StripPP::strip( $_[ 0 ] ) || '';
 }
 
