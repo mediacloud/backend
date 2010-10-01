b27d698f50f1e7c58fcb2beaabf24ec4d6aa53eb
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2010-10-01 20:12:09 +0000

    Rename additional index so that all indexes on the downloads table start with downloads_.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -317,7 +317,7 @@ CREATE INDEX downloads_stories_to_be_extracted on downloads (stories_id) where e
 
 CREATE INDEX downloads_extracted_stories on downloads (stories_id) where type='content' and state='success';
 CREATE INDEX downloads_spider_urls on downloads(url) where type = 'spider_blog_home' or type = 'spider_posting' or type = 'spider_rss' or type = 'spider_blog_friends_list';
-CREATE INDEX spider_download_errors_to_clear on downloads(state,type,error_message) where state='error' and type in ('spider_blog_home','spider_posting','spider_rss','spider_blog_friends_list') and (error_message like '50%' or error_message= 'Download timed out by Fetcher::_timeout_stale_downloads') ;
+CREATE INDEX downloads_spider_download_errors_to_clear on downloads(state,type,error_message) where state='error' and type in ('spider_blog_home','spider_posting','spider_rss','spider_blog_friends_list') and (error_message like '50%' or error_message= 'Download timed out by Fetcher::_timeout_stale_downloads') ;
 CREATE INDEX downloads_state_queued on downloads(state) where state='queued' or state='fetching';
 
 create view downloads_media as select d.*, f.media_id as _media_id from downloads d, feeds f where d.feeds_id = f.feeds_id;
