2b7d5e32f915da332c4122eca914813835f0c467
Hal Roberts <hroberts@cyber.law.harvard.edu>
2010-10-07 04:17:05 +0000

    uncomment media_set update and add date range to media_set update

--- script/mediawords_update_aggregate_vectors.pl
+++ script/mediawords_update_aggregate_vectors.pl
@@ -26,18 +26,21 @@ sub run_daemon
 
     while ( 1 )
     {
-        my ( $yesterday ) = $db->query( "select date_trunc( 'day', now() - interval '28 hours' )" )->flat;
+        my ( $yesterday ) = $db->query( "select date_trunc( 'day', now() - interval '36 hours' )" )->flat;
         my ( $one_month_ago ) = $db->query( "select date_trunc( 'day', now() - interval '1 month' )" )->flat;
         MediaWords::StoryVectors::update_aggregate_words( $db, $one_month_ago, $yesterday );
 
-     # this is almost as slow as just revectoring everthing, so I'm commenting out for now
-     # my $media_sets = $db->query( "select * from media_sets where vectors_added = false" )->hashes;
-     # for my $media_set ( @{ $media_sets } )
-     # {
-     #     print STDERR "update_aggregate_vectors: media_set $media_set->{ media_sets_id }\n";
-     #     MediaWords::StoryVectors::update_aggregate_words( $db, undef, undef, 0, undef, $media_set->{ media_sets_id } );
-     #     $db->query( "update media_sets set vectors_added = true where media_sets_id = ?", $media_set->{ media_sets_id } );
-     # }
+        # this is almost as slow as just revectoring everthing, so I'm commenting out for now
+        my $media_sets = $db->query( "select ms.* from media_sets ms where ms.vectors_added = false" )->hashes;
+        for my $media_set ( @{ $media_sets } )
+        {
+            my ( $start_date, $end_date ) = $db->query( 
+                "select min(start_date), max(end_date) from dashboard_media_sets " . 
+                "  where media_sets_id = ?", $media_set->{ media_sets_id } )->flat;
+            print STDERR "update_aggregate_vectors: media_set $media_set->{ media_sets_id }\n";
+            MediaWords::StoryVectors::update_aggregate_words( $db, $start_date, $end_date, 0, undef, $media_set->{ media_sets_id } );
+            $db->query( "update media_sets set vectors_added = true where media_sets_id = ?", $media_set->{ media_sets_id } );
+        }
 
         my $dashboard_topics = $db->query( "select * from dashboard_topics where vectors_added = false" )->hashes;
         for my $dashboard_topic ( @{ $dashboard_topics } )
