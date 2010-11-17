f54f33c13e1dca3825f199b50a10f99fe2c22db4
Hal Roberts <hroberts@cyber.law.harvard.edu>
2010-11-17 12:01:55 +0000

    update to check for existing medium

--- script_archive/mediawords_import_jk_feeds.pl
+++ script_archive/mediawords_import_jk_feeds.pl
@@ -33,75 +33,69 @@ sub add_tag
 # create a media source from a blog url and a 
 sub create_medium
 {
-    my ( $blog_url, $cluster_num ) = @_;
+    my ( $db, $blog_url ) = @_;
 
-    eval {        
-
-        my $db = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info );
-
-        if ( $db->query( "select * from media where url = ?", $blog_url )->hash )
-        {
-            print STDERR "medium '$blog_url' already exists\n";
-            return;
-        }
-
-        my $response = LWP::UserAgent->new->request( HTTP::Request->new( GET => $blog_url ) );
-
-        if ( !$response->is_success )
-        {
-            print STDERR "Unable to fetch '$blog_url': " . $response->status_line . "\n";
-            return;
-        }
-
-        my $html = $response->decoded_content;
-        
-        my $medium_name;
-        if ( $html =~ /<title>(.*)<\/title>/i )
-        {
-            $medium_name = $1;
-        }
-        else {
-            $medium_name = $blog_url;
-        }
-
-        my $feed_url;
-        if ( $blog_url =~ /livejournal.com/ )
-        {
-            $feed_url = "$blog_url/data/atom";
-        }
-        elsif ($blog_url =~ /liveinternet.ru/ )
-        {
-            $feed_url = "$blog_url/rss";
-        }
-        
-        if ( $db->query( "select * from media where name = ?", $medium_name )->hash )
-        {
-            print STDERR "medium '$medium_name' ($blog_url) already exists\n";
-            return;
-        }
-
-        my $medium;
-        if ( $feed_url )
-        {
-            $medium = $db->create
-                ( 'media', { name => $medium_name, url => $blog_url, moderated => 'true', feeds_added => 'true' } );
-            $db->create( 'feeds', { name => $medium_name, url => $feed_url, media_id => $medium->{ media_id } } );
-        }
-        else {
-            $medium = $db->create
-                ( 'media', { name => $medium_name, url => $blog_url, moderated => 'false', feeds_added => 'false' } );
-        }
-
-        add_tag( $db, $medium->{ media_id }, 'collection', 'morning_analytics_russia_20100915' );
-        add_tag( $db, $medium->{ media_id }, 'morning_analytics_russia_20100915_cluster', $cluster_num );
-
-        print STDERR "added $blog_url, $medium_name, $feed_url\n";
-    };
-    if ( $@ )
+    if ( $blog_url !~ /^http/ ) 
     {
-        print STDERR "Error adding $blog_url: $@\n";
+        $blog_url = "http://$blog_url";
     }
-    return 1;
+
+    if ( my ( $media_id ) = $db->query( "select * from media where url = ?", $blog_url )->flat )
+    {
+        print STDERR "medium '$blog_url' already exists ( $media_id )\n";
+        return $media_id;
+    }
+
+    my $response = LWP::UserAgent->new->request( HTTP::Request->new( GET => $blog_url ) );
+
+    if ( !$response->is_success )
+    {
+        print STDERR "Unable to fetch '$blog_url': " . $response->status_line . "\n";
+        return undef;
+    }
+
+    my $html = $response->decoded_content;
+    
+    my $medium_name;
+    if ( $html =~ /<title>(.*)<\/title>/i )
+    {
+        $medium_name = $1;
+    }
+    else {
+        $medium_name = $blog_url;
+    }
+
+    my $feed_url;
+    if ( $blog_url =~ /livejournal.com/ )
+    {
+        $feed_url = "$blog_url/data/atom";
+    }
+    elsif ($blog_url =~ /liveinternet.ru/ )
+    {
+        $feed_url = "$blog_url/rss";
+    }
+    
+    if ( my ( $media_id ) = $db->query( "select media_id from media where name = ?", $medium_name )->flat )
+    {
+        print STDERR "medium '$medium_name' ($blog_url) already exists\n";
+        return $media_id;
+    }
+
+    my $medium;
+    if ( $feed_url )
+    {
+        $medium = $db->create
+            ( 'media', { name => $medium_name, url => $blog_url, moderated => 'true', feeds_added => 'true' } );
+        $db->create( 'feeds', { name => $medium_name, url => $feed_url, media_id => $medium->{ media_id } } );
+    }
+    else {
+        $medium = $db->create
+            ( 'media', { name => $medium_name, url => $blog_url, moderated => 'false', feeds_added => 'false' } );
+    }
+        
+    print STDERR "added $blog_url, $medium_name, $feed_url\n";
+        
+    return $medium->{ media_id };
 }
 
 sub main
@@ -120,15 +114,24 @@ sub main
     my $csv = Text::CSV_XS->new( { binary => 1 } ) || die "Cannot use CSV: " . Text::CSV_XS->error_diag();
 
     open my $fh, "<:encoding(utf8)", $file || die "Unable to open file $file: $!\n";
-
     $csv->column_names( $csv->getline( $fh ) );
 
     my $media_added = 0;
     while ( my $row = $csv->getline_hr( $fh ) )
     {
-        if ( create_medium( $row->{ url }, $row->{ cluster } ) )
+        eval {
+            my $db = MediaWords::DB::connect_to_db;
+            
+            if ( my $media_id = create_medium( $db, $row->{ url } ) )
+            {
+                add_tag( $db, $media_id, 'collection', 'morning_analytics_russia_full_20101109' );
+                add_tag( $db, $media_id, 'morning_analytics_russia_full_20100911_cluster', $row->{ cluster } );
+                print STDERR "BLOGS ADDED: " . ++$media_added . "\n";
+            }
+        };
+        if ( $@ )
         {
-            print STDERR "BLOGS ADDED: " . ++$media_added . "\n";
+            print STDERR "Error adding $row->{ url }: $@\n";
         }
     }
 }
