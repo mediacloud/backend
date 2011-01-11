15b8cdbcda2bcd73bbe8f5bc793671e85b5ee339
Hal Roberts <hroberts@cyber.law.harvard.edu>
2011-01-11 06:04:06 +0000

    very simple json writing module

---
+++ lib/MediaWords/Util/JSON.pm
@@ -0,0 +1,34 @@
+package MediaWords::Util::JSON;
+
+# very simple module for generating from perl data
+
+use strict;
+
+# return a json string representing a perl data structre
+sub get_json_from_perl
+{
+    my ( $data ) = @_;
+    
+    if ( ref( $data ) eq 'ARRAY' )
+    {
+        return "[\n" . join( ",\n", map { get_json_from_perl( $_ ) } @{ $data } ) . "]\n";
+    }
+    elsif( ref( $data ) eq 'HASH' )
+    {
+        return "{\n" . join( ",\n", map { "$_: " . get_json_from_perl( $data->{ $_ } ) } keys( %{ $data } ) ) . "}\n";
+    }
+    else {
+        return "''" if ( !defined( $data ) );
+        
+        if ( $data =~ /^[0-9]+$/ )
+        {
+            return $data;
+        }
+        else {
+            $data =~ s/'/\\'/g;
+            return "'$data'";
+        }
+    }
+}
+
+1;
