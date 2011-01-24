7bee1f184911c14f67463238cb909749c7b62f28
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-01-24 23:06:16 +0000

    Don't over write user submitted values for the second media source.

--- root/include/media_cloud_rough.js
+++ root/include/media_cloud_rough.js
@@ -2,6 +2,9 @@ $(document).ready(function()
     {
 
  wordcloud = true; //We start off showing the word cloud
+
+ first_display_div_call = true;
+
  visible = $('#compare_media_sets').val();
 
  if (visible == "true")
@@ -16,8 +19,11 @@ function DisplayDIV(d) {
 		 $('#compare').html("Use single Data Source");
 		visible = true;
 		$('#compare_media_sets').val("true");
-		$('#date2').val($('#date1').val());
-		$('#dashboard_topics_id2').val($('#dashboard_topics_id1').val());
+		//Hack to prevent us from overwritting the query values for the second source.
+		if ( first_display_div_call == false) {
+		    $('#date2').val($('#date1').val());
+		    $('#dashboard_topics_id2').val($('#dashboard_topics_id1').val());
+		}
 	}
 	else {  //if we're allready viewing both data sets
 	    document.getElementById(d).style.display = "none";
@@ -26,6 +32,7 @@ function DisplayDIV(d) {
 		//this is where you want to set all of Data Set #2's information to NULL again, just in case they filled anything in
 		$('#compare_media_sets').val("false");
 	}
+	first_display_div_call = false;
 }//end function
 function swapDIV(s) {
 	if (s == 'CMcontentarea') { //if we want to view the Coverage Map
