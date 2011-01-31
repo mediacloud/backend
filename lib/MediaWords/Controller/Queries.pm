178a086c448a585e19e83ff2cfd01eb54f2fb1e6
Hal Roberts <hroberts@cyber.law.harvard.edu>
2011-01-31 22:23:42 +0000

    various feature updates, including comparison word clouds and max term ratios

--- lib/MediaWords/Controller/Queries.pm
+++ lib/MediaWords/Controller/Queries.pm
@@ -30,18 +30,12 @@ sub list : Local
     $c->stash->{ template } = 'queries/list.tt2';
 }
 
-# create a new dashboard
-sub create : Local
+# get a query form and set the media sets and dashboard topics option values in the form
+sub get_query_form
 {
-    my ( $self, $c ) = @_;
-
-    my $form = $c->create_form(
-        {
-            load_config_file => $c->path_to() . '/root/forms/query.yml',
-            method           => 'post',
-            action           => $c->uri_for( '/queries/create' ),
-        }
-    );
+    my ( $self, $c, $query ) = @_;
+    
+    my $form = $c->create_form( { load_config_file => $c->path_to() . '/root/forms/query.yml' } );
 
     my $media_sets = $c->dbis->query( 
         "select ms.*, d.name as dashboard_name " . 
@@ -50,40 +44,46 @@ sub create : Local
         "    dms.dashboards_id = d.dashboards_id " .
         "  order by d.name, ms.name" )->hashes;
     my $media_set_options = [ map { [ $_->{ media_sets_id }, "$_->{ name } ($_->{ dashboard_name })" ] } @{ $media_sets } ];
-    my $media_set_field = $form->get_fields( { name => 'media_sets_ids' } )->[ 0 ];
-    $media_set_field->options( $media_set_options );
+    $form->get_field( 'media_sets_ids' )->options( $media_set_options );
 
     my $dashboard_topics = $c->dbis->query(
-        "select dt.*, d.name as dashboard_name " . 
-        "  from dashboard_topics dt, dashboards d " . 
-        "  where dt.dashboards_id = d.dashboards_id " .
-        "  order by d.name, dt.name" )->hashes;
+        "select dt.*, d.name as dashboard_name from dashboard_topics dt, dashboards d " . 
+        "  where dt.dashboards_id = d.dashboards_id order by d.name, dt.name" )->hashes;
     my $dashboard_topic_options = [ map { [ $_->{ dashboard_topics_id }, "$_->{ name } ($_->{ dashboard_name })" ] } @{ $dashboard_topics } ];
-    my $dashboard_topic_field = $form->get_fields( { name => 'dashboard_topics_ids' } )->[ 0 ];
-    $dashboard_topic_field->options( $dashboard_topic_options );
+    $form->get_field( 'dashboard_topics_ids' )->options( $dashboard_topic_options );
+    
+    if ( $query )
+    {
+        map { $form->get_field( $_ )->default( $query->{ $_ } ) } ( qw/start_date end_date media_sets_ids dashboard_topics_ids/ );
+    }
+    
+    return $form;
+}
 
+# create a new query
+sub create : Local
+{
+    my ( $self, $c ) = @_;
+
+    my $form = $self->get_query_form( $c );
+    
     $form->process( $c->request );
 
     if ( !$form->submitted_and_valid() )
     {
-        $c->stash->{ form }     = $form;
+        $c->stash->{ form }     = $form;        
         $c->stash->{ template } = 'queries/create.tt2';
         return;
     }
 
-    my $query = MediaWords::DBI::Queries::find_or_create_query_by_params( 
-        $c->dbis, 
-        { start_date => $c->req->param( 'start_date' ), 
-          end_date => $c->req->param( 'end_date' ),
-          media_sets_ids => [ $c->req->param( 'media_sets_ids') ], 
-          dashboard_topics_ids => [ $c->req->param( 'dashboard_topics_ids' ) ] } );
+    my $query = MediaWords::DBI::Queries::find_or_create_query_by_request( $c->dbis, $c->req );
           
     $c->dbis->query( "update queries set generate_page = 't' where queries_id = ?", $query->{ queries_id } );
 
     $c->response->redirect( $c->uri_for( "/queries/view/$query->{ queries_id }", { status_msg => 'Query created.' } ) );
 }
 
-# return url for a chart of the daily terms in the dashboard topics vs. total top 500 words for each day in the date range
+# return url for a chart of the daily terms in the dashboard topics vs. all words for each day in the date range
 sub _get_topic_chart_url
 {
     my ( $self, $c, $query ) = @_;
@@ -93,20 +93,24 @@ sub _get_topic_chart_url
         return undef;
     }
     
+    my $media_sets_ids_list = join( ',', @{ $query->{ media_sets_ids } } );
+    
     my $dashboard_topics_ids_list = join( ',', @{ $query->{ dashboard_topics_ids } } );
     
     my $date_term_counts = [ $c->dbis->query(
-        "select topic_words.publish_day, min( dt.query ), " . 
+        "select topic_words.publish_day, min( dt.query ) || ' - ' || min( ms.name ) as term, " . 
         "    sum( topic_words.total_count::float / all_words.total_count::float )::float as term_count " .
-        "  from total_daily_words topic_words, total_daily_words all_words, dashboard_topics dt " .
-        "  where topic_words.dashboard_topics_id in ( $dashboard_topics_ids_list ) " . 
+        "  from total_daily_words topic_words, total_daily_words all_words, dashboard_topics dt, media_sets ms " .
+        "  where topic_words.media_sets_id in ( $media_sets_ids_list ) " . 
+        "    and topic_words.dashboard_topics_id in ( $dashboard_topics_ids_list ) " . 
         "    and topic_words.publish_day = all_words.publish_day and topic_words.media_sets_id = all_words.media_sets_id " . 
         "    and all_words.dashboard_topics_id is null and dt.dashboard_topics_id = topic_words.dashboard_topics_id " .
         "    and topic_words.publish_day between date_trunc( 'week', '$query->{ start_date }'::date ) " . 
-        "    and date_trunc( 'week', '$query->{ end_date }'::date)  + interval '6 days'" .
-        "  group by topic_words.publish_day, dt.dashboard_topics_id " )->arrays ];
+        "    and date_trunc( 'week', '$query->{ end_date }'::date)  + interval '6 days' " .
+        "    and ms.media_sets_id = topic_words.media_sets_id " .
+        "  group by topic_words.publish_day, dt.dashboard_topics_id, ms.media_sets_id " )->arrays ];
         
-    return MediaWords::Util::Chart::generate_line_chart_url_from_dates( $date_term_counts );
+    return MediaWords::Util::Chart::generate_line_chart_url_from_dates( $date_term_counts, $query->{ start_date }, $query->{ end_date } );
 }
 
 # view a single query
@@ -122,20 +126,27 @@ sub view : Local
     
     my $dashboard = $c->dbis->query( "select * from dashboards where dashboards_id = 1" )->hash;
         
-    my $word_cloud_base_url = "/queries/sentences/$query->{ queries_id }";
-    my $word_cloud = MediaWords::Util::WordCloud::get_word_cloud( $c, $word_cloud_base_url, $words, $query );  
+    my $word_cloud = MediaWords::Util::WordCloud::get_word_cloud( $c, '/queries/sentences', $words, $query );  
     
     my $sentences_form = $c->create_form( {
         load_config_file => $c->path_to() . '/root/forms/term.yml',
         method           => 'get',
-        action           => $c->uri_for( '/queries/sentences/' . $query->{ queries_id } ) } );
+        action           => $c->uri_for( '/queries/sentences/' ) } );
+    $sentences_form->get_fields( { name => 'queries_ids' } )->[ 0 ]->value( $query->{ queries_id } );
         
     my $terms_form = $c->create_form( {
         load_config_file => $c->path_to() . '/root/forms/terms.yml',
         method           => 'get',
         action           => $c->uri_for( '/queries/terms/' . $query->{ queries_id } ) } );
         
-    my $topic_chart_url = $self->_get_topic_chart_url( $c, $query );
+    
+    my ( $topic_chart_url, $max_topic_term_ratios ) = @_;
+    if ( @{ $query->{ dashboard_topics } } )
+    {
+        $topic_chart_url = $self->_get_topic_chart_url( $c, $query );
+        my $topic_terms = [ map { $_->{ query } } @{ $query->{ dashboard_topics } } ];
+        $max_topic_term_ratios = MediaWords::DBI::Queries::get_max_term_ratios( $c->dbis, $query, $topic_terms, 1 );
+    }
     
     $c->stash->{ query } = $query;
     $c->stash->{ word_cloud } = $word_cloud;
@@ -143,6 +154,8 @@ sub view : Local
     $c->stash->{ sentences_form } = $sentences_form;
     $c->stash->{ terms_form } = $terms_form;
     $c->stash->{ topic_chart_url } = $topic_chart_url;
+    $c->stash->{ max_topic_term_ratios } = $max_topic_term_ratios;
+    $c->stash->{ max_sentences } = MediaWords::DBI::Queries::MAX_QUERY_SENTENCES;
     
     $c->stash->{ template } = 'queries/view.tt2';
 }
@@ -162,6 +175,7 @@ sub view_media : Local
     $c->stash->{ template } = 'queries/view_media.tt2';
 }
 
+# generate page with google chart of term frequencies for the given terms
 sub terms : Local
 {
     my ( $self, $c, $queries_id ) = @_;
@@ -173,13 +187,15 @@ sub terms : Local
     my $query = MediaWords::DBI::Queries::find_query_by_id( $c->dbis, $queries_id );
 
     my $date_term_counts = MediaWords::DBI::Queries::get_term_counts( $c->dbis, $query, $terms );
-    
+        
     if ( $c->req->param( 'csv' ) )
     {
         my $csv_hashes = [ map { { day => $_->[ 0 ], term => $_->[ 1 ], count => $_->[ 2 ] } } @{ $date_term_counts } ];
         MediaWords::Util::CSV::send_hashes_as_csv_page( $c, $csv_hashes, "term_counts.csv" );
         return;
     }
+
+    my $max_term_ratios = MediaWords::DBI::Queries::get_max_term_ratios( $c->dbis, $query, $terms );
 
     my $term_chart_url = MediaWords::Util::Chart::generate_line_chart_url_from_dates( 
         $date_term_counts, $query->{ start_date }, MediaWords::Util::SQL::increment_day( $query->{ end_date }, 6 ) );
@@ -188,14 +204,19 @@ sub terms : Local
     $c->stash->{ term_chart_url } = $term_chart_url;
     $c->stash->{ terms } = $terms;
     $c->stash->{ terms_string } = $terms_string;
+    $c->stash->{ max_term_ratios } = $max_term_ratios;
     $c->stash->{ template } = 'queries/terms.tt2';
 }
 
 sub sentences : Local
 {
-    my ( $self, $c, $queries_id ) = @_;
+    my ( $self, $c ) = @_;
     
-    my $query = MediaWords::DBI::Queries::find_query_by_id( $c->dbis, $queries_id ) || die( "query '$queries_id' not found" );
+    my $queries_ids = [ $c->req->param( 'queries_ids' ) ];
+    
+    die( "no queries_ids" ) if ( !defined( $queries_ids->[ 0 ] ) );
+    
+    my $queries = [ map { MediaWords::DBI::Queries::find_query_by_id( $c->dbis, $_ ) || die( "no query $_" ) } @{ $queries_ids } ];
     
     my $stem = $c->req->param( 'stem' );
     my $term = $c->req->param( 'term' );
@@ -208,23 +229,65 @@ sub sentences : Local
     my $stories;
     if ( $stem )
     {
-        $stories = MediaWords::DBI::Queries::get_stem_stories_with_sentences( $c->dbis, $stem, [ $query ] );
+        $stories = MediaWords::DBI::Queries::get_stem_stories_with_sentences( $c->dbis, $stem, $queries );
     } 
     else {
-        $stories = MediaWords::DBI::Queries::get_stories_with_sentences( $c->dbis, [ $query ] );
+        $stories = MediaWords::DBI::Queries::get_stories_with_sentences( $c->dbis, $queries );
     }   
     
     map { ( $_->{ medium } ) = $c->dbis->find_by_id( 'media', $_->{ media_id } ) } @{ $stories };
     
-    # my $queries_description = join( " or ", map { $_->{ description } } @{ $queries } );
-    my $queries_description = $query->{ description };
+    my $queries_description = join( " or ", map { $_->{ description } } @{ $queries } );
     
-    $c->stash->{ query } = $query;
+    my $num_sentences = 0;
+    map { map { $num_sentences++ } @{ $_->{ sentences } } } @{ $stories };
+    
+    $c->stash->{ queries } = $queries;
+    $c->stash->{ queries_uids } = $queries_ids;
     $c->stash->{ queries_description } = $queries_description;
     $c->stash->{ stem } = $stem;
     $c->stash->{ term } = $term;
     $c->stash->{ stories } = $stories;
     $c->stash->{ template } = 'queries/sentences.tt2';
+    $c->stash->{ max_sentences } = MediaWords::DBI::Queries::MAX_QUERY_SENTENCES;
+    $c->stash->{ num_sentences } = $num_sentences;
+}
+
+# generate a word cloud comparing one word cloud to another
+sub compare : Local
+{
+    my ( $self, $c ) = @_;
+    
+    my $queries_id = $c->req->param( 'queries_id' ) || die( 'no queries_id' );
+    my $query_a = MediaWords::DBI::Queries::find_query_by_id( $c->dbis, $queries_id )
+        || die( "Unable to find query $queries_id" );
+    
+    my $form = $self->get_query_form( $c, $query_a );
+    
+    $form->process( $c->request );
+
+    if ( !$form->submitted_and_valid() )
+    {
+        $c->stash->{ query_a } = $query_a;
+        $c->stash->{ form } = $form;        
+        $c->stash->{ template } = 'queries/compare_select.tt2';
+        return;
+    }
+
+    my $query_b = MediaWords::DBI::Queries::find_or_create_query_by_request( $c->dbis, $c->req );
+    
+    my $words_a = MediaWords::DBI::Queries::get_top_500_weekly_words( $c->dbis, $query_a );
+    my $words_b = MediaWords::DBI::Queries::get_top_500_weekly_words( $c->dbis, $query_b );
+    
+    my $word_cloud = MediaWords::Util::WordCloud::get_multi_set_word_cloud( 
+        $c, '/queries/sentences', [ $words_a, $words_b ], [ $query_a, $query_b ] );
+    
+    MediaWords::DBI::Queries::add_cos_similarities( $c->dbis, [ $query_a, $query_b ] );
+    
+    $c->stash->{ word_cloud } = $word_cloud;
+    $c->stash->{ query_a } = $query_a;
+    $c->stash->{ query_b } = $query_b;
+    $c->stash->{ template } = 'queries/compare.tt2';
 }
 
 1;
