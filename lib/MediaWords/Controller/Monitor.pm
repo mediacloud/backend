3392bce318385f34c01e7d56725c2a859917822c
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-05-02 15:28:55 +0000

    Update ajax json method to support more advanced google visualizations.

--- lib/MediaWords/Controller/Monitor.pm
+++ lib/MediaWords/Controller/Monitor.pm
@@ -38,7 +38,9 @@ use Dir::Self;
 use Readonly;
 use CHI;
 use File::stat;
-use Data::Google::Visualization::DataTable;
+use Data::Google::Visualization::DataTable::MediaWords;
+use Date::Parse;
+use DateTime::Format::Pg;
 
 # statics for state between print_time() calls
 my $_start_time;
@@ -53,20 +55,33 @@ sub index : Path : Args(0)
     #    $self->_redirect_to_default_page( $c, $dashboards_id );
 }
 
-sub crawler_google_data_table : Local
+sub crawler_google_data_table : Local : FormConfig
 {
     my ( $self, $c ) = @_;
 
+    my $form = $c->stash->{ form };
+
     say STDERR "crawler_google_data_table";
 
+    my $start_date = $c->req->param( 'start_date' );
+    my $end_date   =  $c->req->param( 'end_date' );
+
+    say STDERR "start_date: $start_date end_date: $end_date";
+
     my $download_day_counts = $c->dbis->query(
-"select date_trunc('day', download_time) as day, count(*) from downloads where download_time > now() - interval '10 weeks' group by date_trunc('day', download_time);"
+#"select date_trunc('day', download_time) as day, count(*) from downloads where download_time >now() - interval '10 weeks' group by date_trunc('day', download_time);"
+"select date_trunc('day', download_time) as day, count(*) from downloads where date_trunc('day', download_time) >= ? and  date_trunc('day', download_time) <= ? group by date_trunc('day', download_time);", $start_date, $end_date
     )->hashes();
 
-    my $datatable = Data::Google::Visualization::DataTable->new();
+    foreach my $download_day_count (@$download_day_counts)
+    {
+       $download_day_count->{day} = DateTime::Format::Pg->parse_datetime ($download_day_count->{day});
+    }
+
+    my $datatable = Data::Google::Visualization::DataTable::MediaWords->new();
 
     $datatable->add_columns(
-        { id => 'day',   label => 'Date',  type => 'string' },
+        { id => 'day',   label => 'Date',  type => 'date' },
         { id => 'count', label => 'Count', type => 'number' },
     );
     say STDERR Dumper( $download_day_counts );
@@ -76,11 +91,11 @@ sub crawler_google_data_table : Local
     my $json_output = $datatable->output_json(
 
         #columns => ['date','number','string' ],
-        # pretty  => 1,
+        pretty  => 1,
     );
 
     $c->res->body( $json_output );
-    $c->res->content_type( "application/json" );
+    #$c->res->content_type( "application/json" );
 
     $c->stash->{ download_day_counts } = $download_day_counts;
 
@@ -88,7 +103,7 @@ sub crawler_google_data_table : Local
 }
 
 # generate main dashboard page
-sub view : Local
+sub view : Local : FormConfig
 {
     my ( $self, $c ) = @_;
 
