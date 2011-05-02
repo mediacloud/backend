8bd619ae5546a51e933803970e9b82b21156e9e2
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-05-02 22:18:31 +0000

    Update script with new Catalyst version.

--- script/mediawords_test.pl
+++ script/mediawords_test.pl
@@ -1,20 +1,7 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'MediaWords';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage( 1 ) if ( $help || !$ARGV[ 0 ] );
-
-print request( $ARGV[ 0 ] )->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('MediaWords', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ mediawords_test.pl - Catalyst Test
 mediawords_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    mediawords_test.pl http://localhost/some_action
@@ -41,14 +28,13 @@ mediawords_test.pl [options] uri
 
 Run a Catalyst action from the command line.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Sebastian Riedel, C<sri@oook.de>
-Maintained by the Catalyst Core Team.
+Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
