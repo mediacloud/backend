5d2b740b1107b2b01e20509a847a9d1cf794ffa9
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-05-02 22:15:21 +0000

    Update script mediawords_create.pl with new Catalyst version.

--- script/mediawords_create.pl
+++ script/mediawords_create.pl
@@ -1,26 +1,10 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use Catalyst::Helper;
 
-my $force = 0;
-my $mech  = 0;
-my $help  = 0;
-
-GetOptions(
-    'nonew|force'    => \$force,
-    'mech|mechanize' => \$mech,
-    'help|?'         => \$help
-);
-
-pod2usage( 1 ) if ( $help || !$ARGV[ 0 ] );
-
-my $helper = Catalyst::Helper->new( { '.newfiles' => !$force, mech => $mech } );
-
-pod2usage( 1 ) unless $helper->mk_component( 'MediaWords', @ARGV );
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('MediaWords', 'Create');
 
 1;
 
@@ -33,26 +17,28 @@ mediawords_create.pl - Create a new Catalyst Component
 mediawords_create.pl [options] model|view|controller name [helper] [options]
 
  Options:
-   -force        don't create a .new file where a file to be created exists
-   -mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
-   -help         display this help and exits
+   --force        don't create a .new file where a file to be created exists
+   --mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
+   --help         display this help and exits
 
  Examples:
    mediawords_create.pl controller My::Controller
-   mediawords_create.pl controller My::Controller BindLex
    mediawords_create.pl -mechanize controller My::Controller
    mediawords_create.pl view My::View
-   mediawords_create.pl view MyView TT
-   mediawords_create.pl view TT TT
+   mediawords_create.pl view HTML TT
    mediawords_create.pl model My::Model
    mediawords_create.pl model SomeDB DBIC::Schema MyApp::Schema create=dynamic\
    dbi:SQLite:/tmp/my.db
    mediawords_create.pl model AnotherDB DBIC::Schema MyApp::Schema create=static\
-   dbi:Pg:dbname=foo root 4321
+   [Loader opts like db_schema, naming] dbi:Pg:dbname=foo root 4321
+   [connect_info opts like quote_char, name_sep]
 
  See also:
    perldoc Catalyst::Manual
    perldoc Catalyst::Manual::Intro
+   perldoc Catalyst::Helper::Model::DBIC::Schema
+   perldoc Catalyst::Model::DBIC::Schema
+   perldoc Catalyst::View::TT
 
 =head1 DESCRIPTION
 
@@ -62,14 +48,13 @@ Existing component files are not overwritten.  If any of the component files
 to be created already exist the file will be written with a '.new' suffix.
 This behavior can be suppressed with the C<-force> option.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Sebastian Riedel, C<sri@oook.de>
-Maintained by the Catalyst Core Team.
+Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
