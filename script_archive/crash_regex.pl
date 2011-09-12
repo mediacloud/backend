5cd3f27a7ef77e42b559229f748d85d7ec2d504f
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-09-12 18:50:01 +0000

    *** empty log message ***

--- script_archive/crash_regex.pl
+++ script_archive/crash_regex.pl
@@ -37,11 +37,40 @@ sub hang_regex
 {
     my ( $text ) = @_;
 
-    print STDERR "starting hang_regex\n";
+    print "starting _apply_dangerous_regex\n";
+    eval {
+      #utf8::upgrade( $text );
+      print Dumper( $text );
+      print "\n";
+
+    };
+
+    my $temp = $text;
+    #print Dumper( $temp );
+    #utf8::upgrade( $temp );
+    my $temp_base64 = encode_base64( encode("UTF-8", $temp ) );
+
+    eval {
+      print "Based64 encoded: '$temp_base64'";
+      print "\n";
+    };
+
+    utf8::upgrade( $temp_base64 );
+
+    print Dumper ($temp_base64);
+    print "\n";
+
+    #$text =~ s/([^-\w]\w[\.!?])\001/$1/sgo; 
+
+    print "starting _apply_dangerous_regex part 1\n";
 
     $text =~ s/([^-\w]\w\.)\001/$1/sgo; 
 
-    print STDERR "finished hang_regex\n";
+    print "starting _apply_dangerous_regex part 2\n";
+    $text =~ s/([^-\w]\w\!)\001/$1/sgo; 
+    print "starting _apply_dangerous_regex part 3\n";
+    $text =~ s/([^-\w]\w\?)\001/$1/sgo; 
+    print "Finished _apply_dangerous_regex\n\n";
 
     return $text;
 }
