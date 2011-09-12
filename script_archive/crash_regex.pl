de06f81db256a905266adbc90a1009dc03111df8
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-09-12 18:43:28 +0000

    *** empty log message ***

--- script_archive/crash_regex.pl
+++ script_archive/crash_regex.pl
@@ -18,6 +18,7 @@ use Data::Dumper;
 use Perl6::Say;
 use Encode;
 use MIME::Base64;
+use Lingua::EN::Sentence::MediaWords;
 
 sub hang_regex
 {
@@ -34,7 +35,7 @@ my $var2_base64_decoded = decode ( "utf8", decode_base64 ( $VAR2 ) );
 
 say STDERR Dumper( $VAR2);
 say STDERR Dumper( $var2_base64_decoded );
-
+Lingua::EN::Sentence::MediaWords::_apply_dangerous_regex ( $var2_base64_decoded );
 hang_regex ( $var2_base64_decoded );
 
 say STDERR "Survived dangerous regular expression";
