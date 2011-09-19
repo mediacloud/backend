1c7832d6cc2b6a090a86b10d5e9daaea307ca83c
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-09-19 21:01:31 +0000

    Disable md5 output in Lingua::EN::Sentence::ApplyRegexWithoutLocale

--- lib/Lingua/EN/Sentence/ApplyRegexWithoutLocale.pm
+++ lib/Lingua/EN/Sentence/ApplyRegexWithoutLocale.pm
@@ -1,49 +1,57 @@
 package Lingua::EN::Sentence::ApplyRegexWithoutLocale;
 
 use strict;
+
 #use POSIX qw(locale_h);
 use utf8;
 use Data::Dumper;
 use MIME::Base64;
 use Encode;
 
+my $output_detailed_debug_info = 0;
+
 sub _apply_dangerous_regex
 {
     my ( $text ) = @_;
 
-    print "starting _apply_dangerous_regex\n";
-    eval {
-      #utf8::upgrade( $text );
-      print Dumper( $text );
-      print "\n";
+    if ( $output_detailed_debug_info )
+    {
+        print "starting _apply_dangerous_regex\n";
+        eval {
 
-    };
+            #utf8::upgrade( $text );
+            print Dumper( $text );
+            print "\n";
 
-    my $temp = $text;
-    #print Dumper( $temp );
-    #utf8::upgrade( $temp );
-    my $temp_base64 = encode_base64( encode("UTF-8", $temp ) );
+        };
 
-    eval {
-      print "Based64 encoded: '$temp_base64'";
-      print "\n";
-    };
+        my $temp = $text;
 
-    utf8::upgrade( $temp_base64 );
+        #print Dumper( $temp );
+        #utf8::upgrade( $temp );
+        my $temp_base64 = encode_base64( encode( "UTF-8", $temp ) );
 
-    print Dumper ($temp_base64);
-    print "\n";
+        eval {
+            print "Based64 encoded: '$temp_base64'";
+            print "\n";
+        };
 
-    #$text =~ s/([^-\w]\w[\.!?])\001/$1/sgo; 
+        utf8::upgrade( $temp_base64 );
+
+        print Dumper ( $temp_base64 );
+        print "\n";
+    }
+
+    #$text =~ s/([^-\w]\w[\.!?])\001/$1/sgo;
 
     print "starting _apply_dangerous_regex part 1\n";
 
-    $text =~ s/([^-\w]\w\.)\001/$1/sgo; 
+    $text =~ s/([^-\w]\w\.)\001/$1/sgo;
 
     print "starting _apply_dangerous_regex part 2\n";
-    $text =~ s/([^-\w]\w\!)\001/$1/sgo; 
+    $text =~ s/([^-\w]\w\!)\001/$1/sgo;
     print "starting _apply_dangerous_regex part 3\n";
-    $text =~ s/([^-\w]\w\?)\001/$1/sgo; 
+    $text =~ s/([^-\w]\w\?)\001/$1/sgo;
     print "Finished _apply_dangerous_regex\n\n";
 
     return $text;
