5f4d294e9faeec7986cc5003c55e503a0d33e132
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-10-17 18:37:20 +0000

    Add purge_story_sentences_data_for_unretained_dates routine

--- lib/MediaWords/StoryVectors.pm
+++ lib/MediaWords/StoryVectors.pm
@@ -373,6 +373,18 @@ sub purge_story_words_data_for_unretained_dates
     return;
 }
 
+sub purge_story_sentences_data_for_unretained_dates
+{
+    my ( $db ) = @_;
+
+    my $default_story_words_start_date = get_default_story_words_start_date();
+    my $default_story_words_end_date   = get_default_story_words_end_date();
+
+    $db->query( " SELECT purge_story_sentences( ?::date , ?::date )", $default_story_words_start_date, $default_story_words_end_date );
+
+    return;
+}
+
 # update story vectors for the given story, updating story_sentences and story_sentence_words
 # if no_delete is true, do not try to delete existing entries in the above table before creating new ones (useful for optimization
 # if you are very sure no story vectors exist for this story).
