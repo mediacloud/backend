ecd51a23996d692c5787f671eee8615fa21e6919
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-11-18 23:19:40 +0000

    Add common include file

---
+++ lib/MediaWords/CommonLibs.pm
@@ -0,0 +1,44 @@
+package MediaWords::CommonLibs;
+
+use 5.8.8;
+
+
+require Exporter;
+@ISA = qw(Exporter);
+
+use strict;
+use warnings;
+
+use Perl6::Say;
+
+use mro     ();
+use feature ();
+use Data::Dumper;
+
+use Exporter;
+use List::Util;
+use List::MoreUtils;
+
+sub import {
+    use Data::Dumper();
+    require Readonly;
+
+    warnings->import();
+    strict->import();
+    Data::Dumper->export_to_level(1, , @Data::Dumper::Export );
+
+    Readonly->export_to_level( 1, , @Readonly::Export);
+
+    List::Util->export_to_level( 1, , @List::Util::EXPORT_OK);
+    List::MoreUtils->export_to_level( 1, , @List::MoreUtils::EXPORT_OK);
+
+     MediaWords::CommonLibs->export_to_level( 1, , qw ( say ) );
+     MediaWords::CommonLibs->export_to_level( 1, , qw ( Readonly ) );
+    {
+    no strict;
+    *{caller().'::say'} = \&say;
+    }
+}
+
+
+1;
