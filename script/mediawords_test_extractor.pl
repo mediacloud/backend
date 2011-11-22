e8f6dbf6151a3758a43ba31dac59d81fe656cbf0
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2011-11-22 16:45:05 +0000

    Refactor -- move functions into ExtractTest.pm

--- script/mediawords_test_extractor.pl
+++ script/mediawords_test_extractor.pl
@@ -24,198 +24,10 @@ use Lingua::EN::Sentence::MediaWords;
 use Perl6::Say;
 use Data::Dumper;
 use MediaWords::Util::HTML;
+use MediaWords::Util::ExtractorTest;
 
 my $_re_generate_cache = 0;
 
-sub text_length
-{
-    my $html_text = shift;
-
-    if ( !$html_text )
-    {
-        return 0;
-    }
-
-    my $hs = HTML::Strip->new();
-
-    my $tmp = ( $hs->parse( $html_text ) );
-    $hs->eof();
-
-    my $ret = length( $tmp );
-
-    return $ret;
-}
-
-sub get_lines_that_should_be_in_story
-{
-    ( my $download, my $dbs ) = @_;
-
-    my @story_lines = $dbs->query(
-        "select * from extractor_training_lines where extractor_training_lines.downloads_id = ? order by line_number ",
-        $download->{ downloads_id } )->hashes;
-
-    my $line_should_be_in_story = {};
-
-    for my $story_line ( @story_lines )
-    {
-        $line_should_be_in_story->{ $story_line->{ line_number } } = $story_line->{ required } ? 'required' : 'optional';
-    }
-
-    return $line_should_be_in_story;
-}
-
-sub get_cached_extractor_line_scores
-{
-    ( my $download, my $dbs ) = @_;
-
-    return $dbs->query( " SELECT  * from extractor_results_cache where downloads_id = ? order by line_number asc ",
-        $download->{ downloads_id } )->hashes;
-}
-
-sub store_extractor_line_scores
-{
-    ( my $scores, my $lines, my $download, my $dbs ) = @_;
-
-    $dbs->begin_work;
-
-    $dbs->query( 'DELETE FROM extractor_results_cache where downloads_id = ?', $download->{ downloads_id } );
-
-    my $line_number = 0;
-    for my $score ( @{ $scores } )
-    {
-
-        #print (keys %{$score}) . "\n";
-        $dbs->insert(
-            'extractor_results_cache',
-            {
-                is_story                => $score->{ is_story },
-                explanation             => $score->{ explanation },
-                discounted_html_density => $score->{ discounted_html_density },
-                html_density            => $score->{ html_density },
-                downloads_id            => $download->{ downloads_id },
-                line_number             => $line_number,
-            }
-        );
-
-        $line_number++;
-    }
-
-    $dbs->commit;
-}
-
-sub get_extractor_scores_for_lines
-{
-    ( my $lines, my $story_title, my $story_description, my $download, my $dbs ) = @_;
-
-    my $ret;
-
-    if ( !$_re_generate_cache )
-    {
-        $ret = get_cached_extractor_line_scores( $download, $dbs );
-    }
-
-    if ( !defined( $ret ) || !@{ $ret } )
-    {
-        $ret = MediaWords::Crawler::Extractor::score_lines( $lines, $story_title, $story_description, );
-        store_extractor_line_scores( $ret, $lines, $download, $dbs );
-    }
-    return $ret;
-}
-
-#returns the sum of the string length for each line the training table says should be in the story.
-sub get_character_count_for_story
-{
-    ( my $download, my $line_should_be_in_story ) = @_;
-    my $lines = get_preprocessed_content_lines_for_download( $download );
-    my $story_characters = sum( map { text_length( $lines->[ $_ ] ) } keys %{ $line_should_be_in_story } );
-
-    return $story_characters;
-}
-
-my $_processed_lines_cache = {};
-
-sub get_preprocessed_content_lines_for_download
-{
-    ( my $download ) = @_;
-
-    if ( defined( $_processed_lines_cache->{ $download->{ downloads_id } } ) )
-    {
-        return $_processed_lines_cache->{ $download->{ downloads_id } };
-    }
-
-    my $preprocessed_lines = MediaWords::DBI::Downloads::fetch_preprocessed_content_lines( $download );
-
-    $_processed_lines_cache = { $download->{ downloads_id } => $preprocessed_lines };
-
-    return $preprocessed_lines;
-}
-
-sub _get_sentence_info_for_lines
-{
-    my ( $line_numbers, $preprocessed_lines, $story, $dbs ) = @_;
-
-    my $sentences_total        = 0;
-    my $sentences_dedupped     = 0;
-    my $sentences_not_dedupped = 0;
-    my $sentences_missing      = 0;
-
-    for my $line_number ( @{ $line_numbers } )
-    {
-        my $line_text = $preprocessed_lines->[ $line_number ];
-
-        say "Line text: $line_text";
-
-        $line_text = html_strip( $line_text );
-
-        say "Line text no html: $line_text";
-
-        my $sentences = Lingua::EN::Sentence::MediaWords::get_sentences( $line_text );
-
-        foreach my $sentence ( @{ $sentences } )
-        {
-
-            $sentence = html_strip( $sentence );
-
-            #say "Sentence: '$sentence'";
-
-            my $dup_sentence = $dbs->query(
-                "select * from story_sentence_counts " .
-                  "  where sentence_md5 = md5( ? ) and media_id = ? and publish_week = date_trunc( 'week', ?::date )" .
-                  "  order by story_sentence_counts_id limit 1",
-                $sentence,
-                $story->{ media_id },
-                $story->{ publish_date }
-            )->hash;
-
-            $sentences_total++;
-
-            if ( $dup_sentence )
-            {
-                if ( $dup_sentence->{ sentence_count } <= 1 )
-                {
-                    $sentences_not_dedupped++;
-                }
-                else
-                {
-                    $sentences_dedupped++;
-                }
-            }
-            else
-            {
-                $sentences_missing++;
-            }
-        }
-
-    }
-
-    my $ret = {
-        sentences_total      => $sentences_total,
-        sentences_not_dupped => $sentences_not_dedupped,
-        sentences_dupped     => $sentences_dedupped,
-        sentences_missing    => $sentences_missing,
-    };
-
-}
 
 sub processDownload
 {
@@ -223,12 +35,12 @@ sub processDownload
 
     my $errors = 0;
 
-    my $line_should_be_in_story = get_lines_that_should_be_in_story( $download, $dbs );
+    my $line_should_be_in_story = MediaWords::Util::ExtractorTest::get_lines_that_should_be_in_story( $download, $dbs );
 
     my @required_lines = grep { $line_should_be_in_story->{ $_ } eq 'required' } keys %{ $line_should_be_in_story };
     my @optional_lines = grep { $line_should_be_in_story->{ $_ } eq 'optional' } keys %{ $line_should_be_in_story };
 
-    my $preprocessed_lines = get_preprocessed_content_lines_for_download( $download );
+    my $preprocessed_lines = MediaWords::Util::ExtractorTest::get_preprocessed_content_lines_for_download( $download );
 
     my $story_line_count = scalar( keys %{ $line_should_be_in_story } );
 
@@ -239,7 +51,7 @@ sub processDownload
 
     my $scores = [];
 
-    $scores = get_extractor_scores_for_lines( $preprocessed_lines, $story_title, $story_description, $download, $dbs );
+    $scores =  MediaWords::Util::ExtractorTest::get_extractor_scores_for_lines( $preprocessed_lines, $story_title, $story_description, $download, $dbs, ! $_re_generate_cache );
 
     my @extracted_lines = map { $_->{ line_number } } grep { $_->{ is_story } } @{ $scores };
 
@@ -248,7 +60,7 @@ sub processDownload
 
     my @correctly_included_lines = get_unique( [ \@extracted_lines, get_union_ref( [ \@missing_lines, \@extra_lines ] ) ] );
 
-    my $story_characters = get_character_count_for_story( $download, $line_should_be_in_story );
+    my $story_characters = MediaWords::Util::ExtractorTest::get_character_count_for_story( $download, $line_should_be_in_story );
 
     my $download_errors;
 
@@ -256,7 +68,7 @@ sub processDownload
 
     for my $missing_line_number ( @missing_lines )
     {
-        $missing_characters += text_length( $preprocessed_lines->[ $missing_line_number ] );
+        $missing_characters += MediaWords::Util::ExtractorTest::html_stripped_text_length( $preprocessed_lines->[ $missing_line_number ] );
 
         $download_errors .= "missing line $missing_line_number: " . $preprocessed_lines->[ $missing_line_number ] . "\n";
     }
@@ -264,7 +76,7 @@ sub processDownload
 
     for my $extra_line_number ( @extra_lines )
     {
-        $extra_characters += text_length( $preprocessed_lines->[ $extra_line_number ] );
+        $extra_characters += MediaWords::Util::ExtractorTest::html_stripped_text_length( $preprocessed_lines->[ $extra_line_number ] );
         $download_errors .= "extra line $extra_line_number: " . $preprocessed_lines->[ $extra_line_number ] . "\n";
     }
 
@@ -272,7 +84,7 @@ sub processDownload
 
     #say Dumper( $story );
 
-    my $extra_line_sentence_info = _get_sentence_info_for_lines( [ @extra_lines ], $preprocessed_lines, $story, $dbs );
+    my $extra_line_sentence_info = MediaWords::Util::ExtractorTest::get_sentence_info_for_lines( [ @extra_lines ], $preprocessed_lines, $story, $dbs );
 
     my $extra_sentences_dedupped     = $extra_line_sentence_info->{ sentences_dupped };
     my $extra_sentences_not_dedupped = $extra_line_sentence_info->{ sentences_not_dupped };
@@ -281,7 +93,7 @@ sub processDownload
     my $extra_sentences_total = $extra_line_sentence_info->{ sentences_total };
 
     my $correctly_included_line_sentence_info =
-      _get_sentence_info_for_lines( [ @correctly_included_lines ], $preprocessed_lines, $story, $dbs );
+      MediaWords::Util::ExtractorTest::get_sentence_info_for_lines( [ @correctly_included_lines ], $preprocessed_lines, $story, $dbs );
 
     my $correctly_included_sentences_dedupped     = $correctly_included_line_sentence_info->{ sentences_dupped };
     my $correctly_included_sentences_not_dedupped = $correctly_included_line_sentence_info->{ sentences_not_dupped };
@@ -289,7 +101,7 @@ sub processDownload
 
     my $correctly_included_sentences_total = $correctly_included_line_sentence_info->{ sentences_total };
 
-    my $missing_line_sentence_info = _get_sentence_info_for_lines( [ @missing_lines ], $preprocessed_lines, $story, $dbs );
+    my $missing_line_sentence_info = MediaWords::Util::ExtractorTest::get_sentence_info_for_lines( [ @missing_lines ], $preprocessed_lines, $story, $dbs );
 
     my $missing_sentences_dedupped     = $missing_line_sentence_info->{ sentences_dupped };
     my $missing_sentences_not_dedupped = $missing_line_sentence_info->{ sentences_not_dupped };
