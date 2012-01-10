a2dcb5db040a26e9a6a254b8014a000fe3400fb5
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-01-10 19:35:47 +0000

    Refactor for testing.

--- lib/MediaWords/Crawler/FeedHandler.pm
+++ lib/MediaWords/Crawler/FeedHandler.pm
@@ -17,6 +17,7 @@ use Switch;
 use Carp;
 use Perl6::Say;
 use List::Util qw (max maxstr);
+use Try::Tiny;
 
 use Feed::Scrape::MediaWords;
 use MediaWords::Crawler::BlogSpiderBlogHomeHandler;
@@ -43,17 +44,32 @@ sub _get_stories_from_feed_contents
 {
     my ( $dbs, $download, $decoded_content ) = @_;
 
+    my $media_id = MediaWords::DBI::Downloads::get_media_id( $dbs, $download );
+    my $download_time = $download->{ download_time };
+
+    return try
+    {
+        return _get_stories_from_feed_contents_impl( $decoded_content, $media_id, $download_time );
+    }
+    catch
+    {
+        die( "Error processing feed for $download->{ url } " );
+    };
+}
+
+sub _get_stories_from_feed_contents_impl
+{
+    my ( $decoded_content, $media_id, $download_time ) = @_;
+
     my $feed = Feed::Scrape::MediaWords->parse_feed( $decoded_content );
 
-    die( "Unable to parse feed for $download->{ url }" ) unless $feed;
+    die( "Unable to parse feed" ) unless $feed;
 
     my $items = [ $feed->get_item ];
 
     my $num_new_stories = 0;
 
     my $ret = [];
-
-    my $media_id = MediaWords::DBI::Downloads::get_media_id( $dbs, $download );
 
   ITEM:
     for my $item ( @{ $items } )
@@ -68,7 +84,8 @@ sub _get_stories_from_feed_contents
 
         $url =~ s/[\n\r\s]//g;
 
-        my $date = DateTime->from_epoch( epoch => Date::Parse::str2time( $item->pubDate() ) ) || DateTime->from_epoch( epoch => Date::Parse::str2time( $download->{ download_time} ) );
+        my $date = DateTime->from_epoch( epoch => Date::Parse::str2time( $item->pubDate() ) )
+          || DateTime->from_epoch( epoch => Date::Parse::str2time( $download_time ) );
 
         # if ( !$story )
         $num_new_stories++;
@@ -118,17 +135,17 @@ sub _story_is_new
         # in perl's internal representation -- not a byte-string that contains UTF-8
         # data
 
-         if ( Encode::is_utf8( $story->{title} ) )
-         {
-             $title = $story->{title};
-         }
-         else
-         {
+        if ( Encode::is_utf8( $story->{ title } ) )
+        {
+            $title = $story->{ title };
+        }
+        else
+        {
 
-             # TODO: A utf-8 byte string is only highly likely... we should actually examine the HTTP
-             #   header or the XML pragma so this doesn't explode when given another encoding.
-             $title = decode( 'utf-8', $story->{title} );
-         }
+            # TODO: A utf-8 byte string is only highly likely... we should actually examine the HTTP
+            #   header or the XML pragma so this doesn't explode when given another encoding.
+            $title = decode( 'utf-8', $story->{ title } );
+        }
 
         #say STDERR "Searching for story by title";
 
@@ -388,6 +405,7 @@ sub handle_feed_content
     }
     else
     {
+
         #say STDERR "No stories found";
         $content_ref = \"(redundant feed)";
     }
