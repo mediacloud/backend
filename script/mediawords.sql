e7cbdd7eb0559f0855ac85e41a52e0bde1ec40ea
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-02-23 15:36:05 +0000

    Add foreign key constraint linking story_sentences to stories.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -735,6 +735,7 @@ create table story_sentences (
 create index story_sentences_story on story_sentences (stories_id, sentence_number);
 create index story_sentences_publish_day on story_sentences( date_trunc( 'day', publish_date ), media_id );
 ALTER TABLE  story_sentences ADD CONSTRAINT story_sentences_media_id_fkey FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE;
+ALTER TABLE  story_sentences ADD CONSTRAINT story_sentences_stories_id_fkey FOREIGN KEY (stories_id) REFERENCES stories(stories_id) ON DELETE CASCADE;
     
 create table story_sentence_counts (
        story_sentence_counts_id     bigserial       primary key,
