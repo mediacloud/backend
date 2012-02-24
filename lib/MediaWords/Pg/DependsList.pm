75171944ba74601ad5264040adb52feec8463e50
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-02-24 23:40:05 +0000

    Add package to track dependencies of Pg/PL perl modules.

---
+++ lib/MediaWords/Pg/DependsList.pm
@@ -0,0 +1,13 @@
+package MediaWords::Pg::DependsList;
+
+#A list of all modules needed by Pg/PL Perl stored procedures
+
+use HTML::StripPP;
+use HTML::Entities qw( decode_entities  );
+use Devel::Peek qw(Dump);
+use Encode;
+use List::Util qw(min);
+use Lingua::StopWords;
+use Lingua::Stem::Snowball;
+use Data::Dumper;
+use Time::Local;
