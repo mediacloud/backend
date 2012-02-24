6af9fef1919453d27aca01c74ef04f70614ac972
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-02-24 23:43:31 +0000

    Pg/PL dependency cleanup.

--- lib/MediaWords/Pg/Cleanup.pm
+++ lib/MediaWords/Pg/Cleanup.pm
@@ -1,6 +1,4 @@
 package MediaWords::Pg::Cleanup;
-use MediaWords::CommonLibs;
-
 
 # various cleanup functions
 
@@ -82,41 +80,5 @@ sub remove_duplicate_stories
     }
 }
 
-# # cleanup stories assocaited with feeds from media different from stories.media_id by creating a copy of each
-# # story for each assocaited medium
-# sub cleanup_story_media {
-#
-#     my $s_sth = query("select s.stories_id, s.media_id, min(fsm.feeds_id) as feeds_id " .
-#                       "  from stories s, feeds_stories_map fsm, feeds f " .
-#                       "  where s.feeds_id = fsm.feeds_id and fsm.feeds_id = f.feeds_id and " .
-#                       "    f.media_id <> s.media_id group by s.stories_id, f.media_id");
-#
-#     while (my $story = fetchrow($s_sth)) {
-#
-#         my ($new_story) = execute_prepared("insert into stories (media_id, url, guid, title, " .
-#                                            "    description, publish_date, collect_date) " .
-#                                            "  select $1, url, uid, title, description, publish_date, collect_date " .
-#                                            "    from stories where stories_id = $2 returning stories_id",
-#                                            [ qw(INT INT) ], [ $story->{media_id}, $story->{stories_id} ]);
-#
-#         my $d_sth = query_prepare("select * from downloads where stories_id = $1 order by downloads_id", $story->{stories_id});
-#         my $parent_downloads_id;
-#         while (my $d = fetchrow($d_sth)) {
-#             my $new_download =
-#                 query("insert into downloads (feeds_id, stories_id, parent, url, host, download_time, type, state, path, " .
-#                       "    error_message, priority, sequence, extracted) " .
-#                       " values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) return downloads_id",
-#                       [ qw(INT INT INT TEXT TEXT TIMESTAMP TEXT TEXT TEXT TEXT INT INT BOOLEAN) ],
-#                       [ $story->{feeds_id}, $new_story->{stories_id}, $parent_downloads_id, $d->{url}, $d->{host},
-#                         $d->{download_time}, $d->{type}, $d->{state}, $d->{path}, $d->{error_message}, $d->{priority},
-#                         $d->{sequence}, $d->{extracted} ]);
-#
-#             $parent_download_id = $new_download->{downloads_id};
-#
-#
-#         }
-#     }
-#
-# }
 
 1;
