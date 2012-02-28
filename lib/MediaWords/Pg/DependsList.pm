4443a1f1af8b48719190f994b3918ac4ab2e3b3d
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-02-28 16:29:39 +0000

    Add use strict.

--- lib/MediaWords/Pg/DependsList.pm
+++ lib/MediaWords/Pg/DependsList.pm
@@ -2,6 +2,8 @@ package MediaWords::Pg::DependsList;
 
 #A list of all modules needed by Pg/PL Perl stored procedures
 
+use strict;
+
 use HTML::StripPP;
 use HTML::Entities qw( decode_entities  );
 use Devel::Peek qw(Dump);
