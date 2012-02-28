6f32d0c5b17c1c947a6adeb9f3cba2edc958b8a4
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-02-28 21:49:50 +0000

    Remove one level of braces for increased code readibility.

--- lib/MediaWords/Crawler/HeuristicLineScoring.pm
+++ lib/MediaWords/Crawler/HeuristicLineScoring.pm
@@ -20,7 +20,6 @@ use HTML::CruftText;
 use Carp qw (confess);
 use Lingua::ZH::MediaWords;
 
-
 # lines with less than this discounted html density are extracted
 use constant MAX_HTML_DENSITY => .1;
 
@@ -28,7 +27,6 @@ use constant MAX_HTML_DENSITY => .1;
 # given a minimum score
 use constant MINIMUM_CHARACTERS       => 32;
 use constant MINIMUM_CHARACTERS_SCORE => MAX_HTML_DENSITY;
-
 
 # discounts -- if a given line matches any of the following measures,
 # the html density is multiplied by the given factor
@@ -83,7 +81,6 @@ use constant REQUIRE_NON_BLANK => 1004;
 # we don't care about lines with only html and no text
 use constant REQUIRE_NON_HTML => 1005;
 
-
 sub _score_line_with_line_info
 {
     my ( $info_for_lines, $skip_title_search ) = @_;
@@ -92,163 +89,160 @@ sub _score_line_with_line_info
 
     my $found_article_title = 0;
 
+    my $comment_addition;
+
+    my $last_story_line = 0;
+
+    for ( my $i = 0 ; $i < @{ $info_for_lines } ; $i++ )
     {
-        my $comment_addition;
+        my $score;
 
-        my $last_story_line = 0;
+        my ( $html_density, $discounted_html_density, $explanation );
 
-        for ( my $i = 0 ; $i < @{ $info_for_lines } ; $i++ )
+        my $line_info = $info_for_lines->[ $i ];
+
+        if ( $line_info->{ has_comment } )
         {
-            my $score;
-
-            my ( $html_density, $discounted_html_density, $explanation );
-
-            my $line_info = $info_for_lines->[ $i ];
-
-            if ( $line_info->{ has_comment } )
-            {
-                $comment_addition += COMMENT_ADDITION;
-            }
-            else
-            {
-                $comment_addition -= COMMENT_ADDITION / 40;
-            }
-
-            if ( $comment_addition < 0 )
-            {
-                $comment_addition = 0;
-            }
-
-            if ( $line_info->{ auto_excluded } )
-            {
-                my $auto_exclude_explanation = $line_info->{ auto_exclude_explanation };
-                my $explanation_codes        = {
-                    "require body"       => REQUIRE_BODY,
-                    "require non-blank"  => REQUIRE_NON_BLANK,
-                    "require non-html"   => REQUIRE_NON_HTML,
-                    "require word"       => REQUIRE_WORD,
-                    "require clickprint" => REQUIRE_CLICKPRINT,
-                    "require sphereit"   => REQUIRE_SPHEREIT
-                };
-
-                $discounted_html_density = $explanation_codes->{ $auto_exclude_explanation }
-                  || confess "Invalid explanation: $auto_exclude_explanation";
-
-                $explanation .= "$auto_exclude_explanation\n";
-            }
-            else
-            {
-
-                $html_density = $line_info->{ html_density };
-
-                if (   ( $line_info->{ line_length } < MINIMUM_CHARACTERS )
-                    && ( $line_info->{ html_density } < MINIMUM_CHARACTERS_SCORE ) )
-                {
-                    $explanation .= "minimum characters score: " . MINIMUM_CHARACTERS_SCORE . "\n";
-                    $line_info->{ html_density } = MINIMUM_CHARACTERS_SCORE;
-                }
-
-                $discounted_html_density = $line_info->{ html_density };
-
-                if ( !$skip_title_search )
-                {
-                    if ( $line_info->{ line_starts_with_title_text } )
-                    {
-                        $found_article_title = 1;
-                        $explanation .= "title match discount" . "\n";
-                        $discounted_html_density *= TITLE_MATCH_DISCOUNT;
-                    }
-
-                    if ( !$found_article_title )
-                    {
-                        $explanation .= "per-title addition \n";
-                        $discounted_html_density += .1;
-                    }
-                }
-
-                if ( $comment_addition )
-                {
-                    $explanation .= "comment addition: $comment_addition\n";
-                    $discounted_html_density += $comment_addition;
-                }
-
-                if ( $line_info->{ line_length } > LENGTH_DISCOUNT_LENGTH )
-                {
-                    $explanation .= "length discount: " . LENGTH_DISCOUNT . "\n";
-                    $discounted_html_density *= LENGTH_DISCOUNT;
-                }
-                if ( $line_info->{ line_length } > ( 4 * LENGTH_DISCOUNT_LENGTH ) )
-                {
-                    $explanation .= "super length discount: " . LENGTH_DISCOUNT . "\n";
-                    $discounted_html_density *= LENGTH_DISCOUNT;
-                }
-
-                for ( my $j = 0 ; $j < $line_info->{ copyright_copy } ; $j++ )
-                {
-                    $explanation .= "copyright discount: " . COPYRIGHT_DISCOUNT . "\n";
-                    $discounted_html_density *= COPYRIGHT_DISCOUNT;
-                }
-
-                if ( $line_info->{ article_has_clickprint } )
-                {
-                    $explanation .= "clickprint discount: " . CLICKPRINT_DISCOUNT . "\n";
-                    $discounted_html_density *= CLICKPRINT_DISCOUNT;
-                }
-
-                if ( $line_info->{ article_has_sphereit_map } && $line_info->{ sphereit_map_includes_line } )
-                {
-                    $explanation .= "sphereit discount: " . SPHEREIT_DISCOUNT . "\n";
-                    $discounted_html_density *= SPHEREIT_DISCOUNT;
-                }
-
-                if ( $last_story_line )
-                {
-                    my $distance_to_last_story_line = $i - $last_story_line;
-                    if ( $distance_to_last_story_line
-                        && ( $distance_to_last_story_line <= PROXIMITY_LINES ) )
-                    {
-                        $explanation .= "proximity discount: " . PROXIMITY_DISCOUNT . "\n";
-                        $discounted_html_density *= PROXIMITY_DISCOUNT;
-                    }
-                    else
-                    {
-                        my $a = ( $distance_to_last_story_line * DISTANCE_ADDITION );
-                        $explanation .= "distance addition: $a\n";
-                        $discounted_html_density += $a;
-                    }
-                }
-
-                if (   ( $discounted_html_density > MAX_HTML_DENSITY )
-                    && ( $discounted_html_density < ( MAX_HTML_DENSITY * 3 ) ) )
-                {
-                    my $d = $line_info->{ description_similarity_discount };
-                    if ( $d < 1 )
-                    {
-                        $explanation .= "similarity discount: $d\n";
-                    }
-                    $discounted_html_density *= $d;
-                }
-            }
-
-            $score->{ html_density }            = $html_density                                   || 0;
-            $score->{ discounted_html_density } = $discounted_html_density                        || 0;
-            $score->{ explanation }             = $explanation                                    || '';
-            $score->{ is_story }                = ( $discounted_html_density < MAX_HTML_DENSITY ) || 0;
-            $score->{ line_number }             = $i;
-
-            if ( $score->{ is_story } )
-            {
-                $last_story_line = $i;
-            }
-
-            # print "score: [" . $score->{is_story} . " / " . $score->{html_density} . "] $line\n";
-
-            push( @{ $scores }, $score );
+            $comment_addition += COMMENT_ADDITION;
+        }
+        else
+        {
+            $comment_addition -= COMMENT_ADDITION / 40;
         }
 
-        MediaWords::Crawler::Extractor::print_time( "loop_lines" );
+        if ( $comment_addition < 0 )
+        {
+            $comment_addition = 0;
+        }
 
+        if ( $line_info->{ auto_excluded } )
+        {
+            my $auto_exclude_explanation = $line_info->{ auto_exclude_explanation };
+            my $explanation_codes        = {
+                "require body"       => REQUIRE_BODY,
+                "require non-blank"  => REQUIRE_NON_BLANK,
+                "require non-html"   => REQUIRE_NON_HTML,
+                "require word"       => REQUIRE_WORD,
+                "require clickprint" => REQUIRE_CLICKPRINT,
+                "require sphereit"   => REQUIRE_SPHEREIT
+            };
+
+            $discounted_html_density = $explanation_codes->{ $auto_exclude_explanation }
+              || confess "Invalid explanation: $auto_exclude_explanation";
+
+            $explanation .= "$auto_exclude_explanation\n";
+        }
+        else
+        {
+
+            $html_density = $line_info->{ html_density };
+
+            if (   ( $line_info->{ line_length } < MINIMUM_CHARACTERS )
+                && ( $line_info->{ html_density } < MINIMUM_CHARACTERS_SCORE ) )
+            {
+                $explanation .= "minimum characters score: " . MINIMUM_CHARACTERS_SCORE . "\n";
+                $line_info->{ html_density } = MINIMUM_CHARACTERS_SCORE;
+            }
+
+            $discounted_html_density = $line_info->{ html_density };
+
+            if ( !$skip_title_search )
+            {
+                if ( $line_info->{ line_starts_with_title_text } )
+                {
+                    $found_article_title = 1;
+                    $explanation .= "title match discount" . "\n";
+                    $discounted_html_density *= TITLE_MATCH_DISCOUNT;
+                }
+
+                if ( !$found_article_title )
+                {
+                    $explanation .= "per-title addition \n";
+                    $discounted_html_density += .1;
+                }
+            }
+
+            if ( $comment_addition )
+            {
+                $explanation .= "comment addition: $comment_addition\n";
+                $discounted_html_density += $comment_addition;
+            }
+
+            if ( $line_info->{ line_length } > LENGTH_DISCOUNT_LENGTH )
+            {
+                $explanation .= "length discount: " . LENGTH_DISCOUNT . "\n";
+                $discounted_html_density *= LENGTH_DISCOUNT;
+            }
+            if ( $line_info->{ line_length } > ( 4 * LENGTH_DISCOUNT_LENGTH ) )
+            {
+                $explanation .= "super length discount: " . LENGTH_DISCOUNT . "\n";
+                $discounted_html_density *= LENGTH_DISCOUNT;
+            }
+
+            for ( my $j = 0 ; $j < $line_info->{ copyright_copy } ; $j++ )
+            {
+                $explanation .= "copyright discount: " . COPYRIGHT_DISCOUNT . "\n";
+                $discounted_html_density *= COPYRIGHT_DISCOUNT;
+            }
+
+            if ( $line_info->{ article_has_clickprint } )
+            {
+                $explanation .= "clickprint discount: " . CLICKPRINT_DISCOUNT . "\n";
+                $discounted_html_density *= CLICKPRINT_DISCOUNT;
+            }
+
+            if ( $line_info->{ article_has_sphereit_map } && $line_info->{ sphereit_map_includes_line } )
+            {
+                $explanation .= "sphereit discount: " . SPHEREIT_DISCOUNT . "\n";
+                $discounted_html_density *= SPHEREIT_DISCOUNT;
+            }
+
+            if ( $last_story_line )
+            {
+                my $distance_to_last_story_line = $i - $last_story_line;
+                if ( $distance_to_last_story_line
+                    && ( $distance_to_last_story_line <= PROXIMITY_LINES ) )
+                {
+                    $explanation .= "proximity discount: " . PROXIMITY_DISCOUNT . "\n";
+                    $discounted_html_density *= PROXIMITY_DISCOUNT;
+                }
+                else
+                {
+                    my $a = ( $distance_to_last_story_line * DISTANCE_ADDITION );
+                    $explanation .= "distance addition: $a\n";
+                    $discounted_html_density += $a;
+                }
+            }
+
+            if (   ( $discounted_html_density > MAX_HTML_DENSITY )
+                && ( $discounted_html_density < ( MAX_HTML_DENSITY * 3 ) ) )
+            {
+                my $d = $line_info->{ description_similarity_discount };
+                if ( $d < 1 )
+                {
+                    $explanation .= "similarity discount: $d\n";
+                }
+                $discounted_html_density *= $d;
+            }
+        }
+
+        $score->{ html_density }            = $html_density                                   || 0;
+        $score->{ discounted_html_density } = $discounted_html_density                        || 0;
+        $score->{ explanation }             = $explanation                                    || '';
+        $score->{ is_story }                = ( $discounted_html_density < MAX_HTML_DENSITY ) || 0;
+        $score->{ line_number }             = $i;
+
+        if ( $score->{ is_story } )
+        {
+            $last_story_line = $i;
+        }
+
+        # print "score: [" . $score->{is_story} . " / " . $score->{html_density} . "] $line\n";
+
+        push( @{ $scores }, $score );
     }
+
+    MediaWords::Crawler::Extractor::print_time( "loop_lines" );
 
     #In rare cases we won't match the article title and we'll discount all lines
     #we rescore the article without looking for the title to fix this
