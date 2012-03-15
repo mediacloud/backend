530ce544b3245c0e4a8deb3c05199fe904aee6a0
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-03-15 19:55:43 +0000

    Disable set u before sourcing ~/perl5/perlbrew/etc/bashrc

--- install_mc_perlbrew_and_modules.sh
+++ install_mc_perlbrew_and_modules.sh
@@ -8,7 +8,9 @@ set -u
 set -o  errexit
 
 TMPDIR="." ./foreign_modules/perlbrew_install.sh
+set +u
 source ~/perl5/perlbrew/etc/bashrc
+set -u
 perlbrew init
 nice perlbrew install perl-5.14.2 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN
 perlbrew switch perl-5.14.2
