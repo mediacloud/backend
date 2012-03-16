4001cf13a4b9a916667c3ed9b0ce7f37e0739f63
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-03-16 17:31:47 +0000

    make sure that the script doesn't die on carton installs of modules in the foreign module directory.

--- install_mc_perlbrew_and_modules.sh
+++ install_mc_perlbrew_and_modules.sh
@@ -19,8 +19,10 @@ perlbrew lib create mediacloud
 perlbrew switch perl-5.14.2@mediacloud
 cpanm foreign_modules/carton-v0.9.3.tar.gz
 cpanm foreign_modules/List-MoreUtils-0.33.tgz
-carton install --deploy
-carton install --deploy
-carton install foreign_modules/YAML-Syck-1.20.tar.gz
-carton install foreign_modules/carton-v0.9.3.tar.gz
 cpanm foreign_modules/Devel-NYTProf-4.06.tar.gz 
+carton install --deploy
+carton install --deploy
+carton install foreign_modules/YAML-Syck-1.20.tar.gz || echo " Yaml localy installed"
+echo "starting install of carton within carton"
+carton install foreign_modules/carton-v0.9.3.tar.gz || carton install foreign_modules/carton-v0.9.3.tar.gz  || echo " carton installed"
+echo "Successfully installed Perl and modules for MediaCloud"
