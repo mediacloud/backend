bc4d6c5b6b8dd8e0427e89406a06ba4099f05631
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-03-22 22:59:51 +0000

    Reenable missing use.

--- script/mediawords_rewrite_downloads.pl
+++ script/mediawords_rewrite_downloads.pl
@@ -12,7 +12,8 @@ BEGIN
 }
 
 # use MediaWords::Crawler::Extractor;
-# use Getopt::Long;
+use Getopt::Long;
+
 # use HTML::Strip;
 use DBIx::Simple::MediaWords;
 use MediaWords::DB;
@@ -23,6 +24,7 @@ use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 use List::Compare::Functional qw (get_unique get_complement get_union_ref );
 use Perl6::Say;
 use Data::Dumper;
+
 # use MediaWords::Util::HTML;
 # use MediaWords::Util::ExtractorTest;
 # use Data::Compare;
@@ -127,10 +129,11 @@ sub main
                 my $thread_db = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info );
                 while ( my $download = $q->dequeue() )
                 {
-		    last if $download == -1;
-		    #die "test";
+                    last if $download == -1;
+
+                    #die "test";
                     say STDERR "rewriting download: " . $download->{ downloads_id };
-		    say STDERR "old download path: " . $download->{ path };
+                    say STDERR "old download path: " . $download->{ path };
                     MediaWords::DBI::Downloads::rewrite_downloads_content( $thread_db, $download );
                 }
                 say "Thread returning ";
@@ -159,7 +162,7 @@ sub main
         say STDERR "Joining thread";
         foreach my $thr ( threads->list() )
         {
-	    $q->enqueue( -1);
+            $q->enqueue( -1 );
             $thr->join();
             say STDERR "joined thread";
         }
