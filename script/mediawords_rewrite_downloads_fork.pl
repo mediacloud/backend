752440ba24856bcde65ccaf576e2ede00973d7d9
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-03-26 19:28:02 +0000

    Fix indentation

--- script/mediawords_rewrite_downloads_fork.pl
+++ script/mediawords_rewrite_downloads_fork.pl
@@ -48,21 +48,20 @@ sub extract_text
 
     Readonly my $max_iterations => 1;
 
-  ##  while ( 1 )
+    ##  while ( 1 )
     {
-	
-	my $num_downloads = 0;
+
+        my $num_downloads = 0;
         print STDERR "[$process_num, $job_process_num] find new downloads ($num_downloads remaining) ...\n";
 
         my $downloads = $db->query(
             "SELECT d.* from downloads d " . "  where d.extracted='f' and d.type='content' and d.state='success' " .
-              "    and  (( ( d.feeds_id + $job_process_num ) % $num_total_processes ) = 0 ) " .  "order by stories_id asc ".
+              "    and  (( ( d.feeds_id + $job_process_num ) % $num_total_processes ) = 0 ) " . "order by stories_id asc " .
               "  limit " . PROCESS_SIZE );
 
-	$downloads = $db->query(
-	    "select * from downloads d where state = 'success' and path like 'content/%' and  (( ( d.feeds_id + $job_process_num ) % $num_total_processes ) = 0 ) ORDER BY downloads_id asc limit $download_batch_size; "
-            );
-
+        $downloads = $db->query(
+"select * from downloads d where state = 'success' and path like 'content/%' and  (( ( d.feeds_id + $job_process_num ) % $num_total_processes ) = 0 ) ORDER BY downloads_id asc limit $download_batch_size; "
+        );
 
         # my $downloads = $db->query( "select * from downloads where stories_id = 418981" );
         my $download_found;
@@ -71,17 +70,18 @@ sub extract_text
             $download_found = 1;
 
             eval {
-		say STDERR "Job $process_num rewriting download: " . $download->{ downloads_id };
-		say STDERR "Job $process_num old download path: " . $download->{ path };
-		MediaWords::DBI::Downloads::rewrite_downloads_content( $db, $download );
-                #MediaWords::DBI::Downloads::process_download_for_extractor( $db, $download, "$process_num, $job_process_num" );
+                say STDERR "Job $process_num rewriting download: " . $download->{ downloads_id };
+                say STDERR "Job $process_num old download path: " . $download->{ path };
+                MediaWords::DBI::Downloads::rewrite_downloads_content( $db, $download );
+
+             #MediaWords::DBI::Downloads::process_download_for_extractor( $db, $download, "$process_num, $job_process_num" );
 
             };
 
             if ( $@ )
             {
                 say STDERR "[$process_num] extractor error processing download " . $download->{ downloads_id } . ": $@";
-		$db->rollback;
+                $db->rollback;
             }
         }
 
@@ -90,6 +90,7 @@ sub extract_text
         if ( !$download_found )
         {
             print STDERR "[$process_num] no downloads found. sleeping ...\n";
+
             #sleep 60;
         }
 
@@ -105,13 +106,13 @@ sub main
     binmode STDERR, ":utf8";
 
     $num_total_processes ||= 1;
-	$num_total_jobs ||= 1;
-	$job_number ||= 1;
+    $num_total_jobs      ||= 1;
+    $job_number          ||= 1;
 
     # turn off buffering so processes don't write over each other as much
     $| = 1;
 
-	my $num_processes = int( $num_total_processes / $num_total_jobs );
+    my $num_processes = int( $num_total_processes / $num_total_jobs );
 
     for ( my $i = 0 ; $i < $num_processes ; $i++ )
     {
