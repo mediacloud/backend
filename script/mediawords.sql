65920f12261f28e837f8ff70a33d73ef17c0e3d1
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-03-26 15:21:25 +0000

    Allow this file to run as a single transaction.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -573,7 +573,7 @@ create view downloads_non_media as select d.* from downloads d where d.feeds_id 
 CREATE INDEX downloads_sites_index on downloads (regexp_replace(host, $q$^(.)*?([^.]+)\.([^.]+)$$q$ ,E'\\2.\\3'));
 CREATE INDEX downloads_sites_pending on downloads (regexp_replace(host, $q$^(.)*?([^.]+)\.([^.]+)$$q$ ,E'\\2.\\3')) where state='pending';
 
-CREATE INDEX CONCURRENTLY downloads_queued_spider ON downloads(downloads_id) where state = 'queued' and  type in  ('spider_blog_home','spider_posting','spider_rss','spider_blog_friends_list','spider_validation_blog_home','spider_validation_rss');
+CREATE INDEX downloads_queued_spider ON downloads(downloads_id) where state = 'queued' and  type in  ('spider_blog_home','spider_posting','spider_rss','spider_blog_friends_list','spider_validation_blog_home','spider_validation_rss');
 
 /*
 CREATE INDEX downloads_sites_downloads_id_pending on downloads (regexp_replace(host, $q$^(.)*?([^.]+)\.([^.]+)$$q$ ,E'\\2.\\3'), downloads_id) where state='pending';
@@ -772,8 +772,8 @@ create index story_sentence_words_story on story_sentence_words (stories_id, sen
 create index story_sentence_words_dsm on story_sentence_words (publish_day, stem, media_id);
 create index story_sentence_words_day on story_sentence_words(publish_day);
 create index story_sentence_words_media_day on story_sentence_words (media_id, publish_day);
-#ALTER TABLE  story_sentence_words ADD CONSTRAINT story_sentence_words_media_id_fkey FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE;
-#ALTER TABLE  story_sentence_words ADD CONSTRAINT story_sentence_words_stories_id_fkey FOREIGN KEY (stories_id) REFERENCES stories(stories_id) ON DELETE CASCADE;
+--ALTER TABLE  story_sentence_words ADD CONSTRAINT story_sentence_words_media_id_fkey FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE;
+--ALTER TABLE  story_sentence_words ADD CONSTRAINT story_sentence_words_stories_id_fkey FOREIGN KEY (stories_id) REFERENCES stories(stories_id) ON DELETE CASCADE;
 
 create table daily_words (
        daily_words_id               serial          primary key,
