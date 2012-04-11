7f4d6f7cc7ce627a2eeea1cf96169da34885d54e
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-04-11 22:02:14 +0000

    Don't install from the foriegn modules directory just rely on Carton.

--- install_modules_with_carton.sh
+++ install_modules_with_carton.sh
@@ -8,7 +8,7 @@ source ./script/set_perl_brew_environment.sh
 
 carton install --deployment || echo "initial carton run "
 carton install --deployment
-carton install foreign_modules/YAML-Syck-1.20.tar.gz || echo " Yaml locally installed"
-echo "starting install of carton within carton"
-carton install foreign_modules/carton-v0.9.4.tar.gz || carton install foreign_modules/carton-v0.9.4.tar.gz  || echo " carton installed"
+#carton install foreign_modules/YAML-Syck-1.20.tar.gz || echo " Yaml locally installed"
+#echo "starting install of carton within carton"
+#carton install foreign_modules/carton-v0.9.4.tar.gz || carton install foreign_modules/carton-v0.9.4.tar.gz  || echo " carton installed"
 echo "Successfully installed Perl and modules for MediaCloud"
