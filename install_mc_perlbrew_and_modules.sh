5a6154fa2547dcfc3211b58d6b1f17deb98ce5f1
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-04-12 14:44:01 +0000

    output status messages

--- install_mc_perlbrew_and_modules.sh
+++ install_mc_perlbrew_and_modules.sh
@@ -7,7 +7,10 @@ cd $working_dir
 set -u
 set -o  errexit
 
+echo "starting perlbrew install "
 ./install_mc_perlbrew.sh
+echo "starting non-carton-based modules install "
 ./install_modules_outside_of_carton.sh
+echo "starting carton-based modules install "
 ./install_modules_with_carton.sh
-
+echo "Install complete"
