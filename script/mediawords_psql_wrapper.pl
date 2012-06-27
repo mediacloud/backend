df4ba380c32885114bcc81f639d9c89836c6c249
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-06-27 21:11:37 +0000

    Add instructions.

--- script/mediawords_psql_wrapper.pl
+++ script/mediawords_psql_wrapper.pl
@@ -9,6 +9,33 @@ BEGIN
     use lib "$FindBin::Bin/../lib";
 }
 
+# This script sets all necessary postgresql environment variables based on the mediacloud configuration and then does an exec of psql
+#
+# For example, to connect to the default database as defined in mediawords.yml run:
+#
+#         ./script/run_with_carton.sh ./script/mediawords_psql_wrapper.pl
+#
+# This script also takes 3 special options: --db-label, --list-labels, and --dump-env_commands
+#
+# --db-label starts a psql client to connect to a particular database based on the label field of database section of mediawords.yml. For example, to connect to the test database run:
+#      ./script/run_with_carton.sh ./script/mediawords_psql_wrapper.pl --db-label test
+#
+# --list-labels outputs a list of all database labels defined in mediawords.yml
+#
+# --dump-env_commands outputs the commands that you would use to set the relevant environment variables so that you could just run psql directly and have it connect to the given database. This is most useful for running database tools other than psql such as pg_dump and pg_restore. This option can be combined with --db-label but must be given first. For example, to get the environment variable settings for the test database run:
+#
+# ./script/run_with_carton.sh ./script/mediawords_psql_wrapper.pl --dump-env-commands --db-label test
+#
+# This will output something like the following:
+#
+#    export PGPASSWORD=mediacloud
+#    export PGHOST=localhost
+#    export PGDATABASE=mediacloud_test
+#    export PGUSER=mediacloud
+#
+# Any arguments given after these options will be passed on to psql. So to list all Postgresql databases on a given Postgresql instance (as opposed to the databases defined in mediawords.yml) run the following:
+# ./script/run_with_carton.sh ./script/mediawords_psql_wrapper.pl -l
+
 use MediaWords::DB;
 use Modern::Perl "2012";
 use MediaWords::CommonLibs;
