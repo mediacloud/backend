91eac82c790f0fc3d639a679a11be8ff7a41d6b3
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-06-27 18:04:21 +0000

    Change mediawords_fastcgi.pl into a wrapper script for starting with carton and plackup.

--- script/mediawords_fastcgi.pl
+++ script/mediawords_fastcgi.pl
@@ -1,48 +1,25 @@
 #!/usr/bin/env perl
 
+# Carton and pluckup are the recommended way of starting media cloud. So this script has been reproposed to simply exec ./script/run_fcgi_with_plackup_and_carton
+
+# In most cases, this script can be in an Apache config simply by changing the $HOME environment variable for the mediacloud user below.
+
 use strict;
-use Catalyst::ScriptRunner;
-Catalyst::ScriptRunner->run( 'MediaWords', 'FastCGI' );
+#use Catalyst::ScriptRunner;
+#Catalyst::ScriptRunner->run( 'MediaWords', 'FastCGI' );
+
+use File::Spec;
+use File::Basename;
+
+
+    #ALTER THIS LINE TO THE HOME DIRECTORY OF THE MEDIACLOUD USER
+    $ENV{ HOME }= '/space/mediacloud';
+
+    #According to a question on SO, this is a the safest way to get the directory of the current script. 
+    #See http://stackoverflow.com/questions/84932/how-do-i-get-the-full-path-to-a-perl-script-that-is-executing
+
+    my $dirname = dirname(__FILE__);
+
+    exec("$dirname/run_fcgi_with_plackup_and_carton.sh");
 
 1;
-
-=head1 NAME
-
-mediawords_fastcgi.pl - Catalyst FastCGI
-
-=head1 SYNOPSIS
-
-mediawords_fastcgi.pl [options]
-
- Options:
-   -? -help      display this help and exits
-   -l --listen   Socket path to listen on
-                 (defaults to standard input)
-                 can be HOST:PORT, :PORT or a
-                 filesystem path
-   -n --nproc    specify number of processes to keep
-                 to serve requests (defaults to 1,
-                 requires -listen)
-   -p --pidfile  specify filename for pid file
-                 (requires -listen)
-   -d --daemon   daemonize (requires -listen)
-   -M --manager  specify alternate process manager
-                 (FCGI::ProcManager sub-class)
-                 or empty string to disable
-   -e --keeperr  send error messages to STDOUT, not
-                 to the webserver
-
-=head1 DESCRIPTION
-
-Run a Catalyst application as fastcgi.
-
-=head1 AUTHORS
-
-Catalyst Contributors, see Catalyst.pm
-
-=head1 COPYRIGHT
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
