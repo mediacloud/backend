ab8c06522c303f7f64f4332e46a4d04a8597710d
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-07-03 18:24:36 +0000

    ban additional tables.

--- script/mediawords_sample_db_tables.pl
+++ script/mediawords_sample_db_tables.pl
@@ -31,7 +31,7 @@ sub main
 
     my @tables = $dbs->query( $table_names_query )->flat();
 
-    my @non_id_tables = qw ( daily_country_counts daily_stats daily_words_with_totals india_million media_tag_counts queries_dashboard_topics_map queries_media_sets_map ssw_queue story_sentence_words tar_downloads_queue top_ten_tags_for_media url_discovery_counts extractor_training_lines_corrupted_download_content hr_pilot_study_stories ma_ms_queue media_rss_full_text_detection_data original_sopa_story_publish_dates);
+    my @non_id_tables = qw ( daily_country_counts daily_stats daily_words_with_totals india_million media_tag_counts queries_dashboard_topics_map queries_media_sets_map ssw_queue story_sentence_words tar_downloads_queue top_ten_tags_for_media url_discovery_counts extractor_training_lines_corrupted_download_content hr_pilot_study_stories ma_ms_queue media_rss_full_text_detection_data original_sopa_story_publish_dates pilot_story_sims pilot_story_sims_code pilot_study_stories questionable_downloads_rows sopa_58b_sentiment sopa_date_counts_dump sopa_date_counts_dump_000004 sopa_date_counts_dump_000005 sopa_date_counts_dump_000006 sopa_date_counts_dump_000007 sopa_date_counts_dump_000008 sopa_date_counts_dump_000009 sopa_date_counts_dump_000011 sopa_date_counts_dump_000012 sopa_date_counts_dump_000021 sopa_date_counts_dump_000022 sopa_date_counts_dump_000023 sopa_date_counts_dump_000024  );
 
     @tables = get_complement( [ \@non_id_tables, \@tables ] );
 
