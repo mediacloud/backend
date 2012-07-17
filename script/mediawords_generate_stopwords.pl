3516c2b112333ecee5ec0e730329c002ae8ed6df
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2012-07-17 15:05:23 +0000

    stoplist generator: IDF method

--- script/mediawords_generate_stopwords.pl
+++ script/mediawords_generate_stopwords.pl
@@ -74,13 +74,13 @@ sub gen_term_frequency
 
     my $sentences_rs =
       $db->query( "SELECT sentence FROM story_sentences ORDER BY story_sentences_id LIMIT $sentence_limit" );
-    my $count          = 0;
+    my $sentence_count = 0;
     my $terms_analysed = 0;
     while ( my $sentence = $sentences_rs->hash() )
     {
-        if ( ++$count % 1000 == 0 )
+        if ( ++$sentence_count % 1000 == 0 )
         {
-            printf STDERR "Tokenizing sentence %d out of ~%d...\n", $count, $sentence_limit;
+            printf STDERR "Tokenizing sentence %d out of ~%d...\n", $sentence_count, $sentence_limit;
         }
 
         my $terms = MediaWords::StoryVectors::tokenize( [ $sentence->{ sentence } ] );
@@ -96,7 +96,11 @@ sub gen_term_frequency
             $db->dbh->pg_putcopydata( encode_utf8( $term ) . "\n" );
 
             # Term limit reached
-            last if ( ++$terms_analysed >= $term_limit );
+            ++$terms_analysed;
+            if ( $term_limit != 0 )
+            {
+                last if ( $terms_analysed >= $term_limit );
+            }
         }
 
         $db->dbh->pg_putcopyend();
@@ -112,13 +116,11 @@ sub gen_term_frequency
                                   ORDER BY term_count DESC
                                   LIMIT $stoplist_threshold"
     );
-    print STDERR "\nTERM COUNT:\n\tterm\tcount\n";
     binmode( STDERR, ":utf8" );
     while ( my $term_count = $term_count_rs->hash() )
     {
-        printf STDERR "\t%s\t%d\n", $term_count->{ term }, $term_count->{ term_count };
+        printf STDERR "%s\t%d\n", $term_count->{ term }, $term_count->{ term_count };
     }
-    print STDERR "\n";
 
     # Cleanup
     $db->query( "DROP TABLE temp_term_counts" );
@@ -128,7 +130,97 @@ sub gen_term_frequency
 # Inverse Document Frequency (IDF)
 sub gen_inverse_document_frequency
 {
-    print "Not implemented.\n";    # FIXME
+    my ( $db, $term_limit, $sentence_limit, $stoplist_threshold ) = @_;
+
+    # Temp. table for storing word counts
+    $db->query( "DROP TABLE IF EXISTS temp_term_counts" );
+    $db->query(
+        "CREATE TEMPORARY TABLE temp_term_counts (
+                    term        VARCHAR(256) NOT NULL
+                 ) WITH (OIDS=FALSE)"
+    );
+
+    # Sentence count (to give a sense of progress)
+    if ( $sentence_limit == 0 )
+    {
+        die "You would be better of setting a sentence limit when using IDF.\n";
+    }
+    printf STDERR "Will go through ~%d sentences.\n", $sentence_limit;
+
+    my $stories_rs = $db->query(
+        " SELECT
+                        stories_id,
+                        ARRAY_TO_STRING(ARRAY_AGG(sentence), ' ') AS story
+                    FROM (
+                        SELECT
+                            stories_id,
+                            sentence
+                        FROM story_sentences
+                        ORDER BY stories_id, sentence_number
+                        LIMIT $sentence_limit
+                    ) AS story_sentences_limited
+                    GROUP BY stories_id
+                    ORDER BY stories_id"
+    );
+    my $story_count    = 0;
+    my $terms_analysed = 0;
+    while ( my $story = $stories_rs->hash() )    # for every document
+    {
+        my $i    = 0;
+        my $term = '';
+
+        if ( ++$story_count % 10 == 0 )
+        {
+            printf STDERR "Tokenizing story %d...\n", $story_count;
+        }
+
+        my $terms = MediaWords::StoryVectors::tokenize( [ $story->{ story } ] );
+        my %unique_terms;                        # of a document
+
+        for ( $i = 0 ; $i < $#$terms ; $i++ )
+        {
+            $term = $terms->[ $i ];
+
+            # Definitely not a stopword and probably not a word anyway.
+            next if ( length( $term ) > 256 );
+
+            $unique_terms{ $term } = 1 unless ( exists( $unique_terms{ $term } ) );
+
+            # Term limit reached
+            ++$terms_analysed;
+            if ( $term_limit != 0 )
+            {
+                last if ( $terms_analysed >= $term_limit );
+            }
+        }
+
+        $db->dbh->do( "COPY temp_term_counts (term) FROM STDIN" );
+        while ( ( $term, $i ) = each( %unique_terms ) )
+        {
+            $db->dbh->pg_putcopydata( encode_utf8( $term ) . "\n" );
+        }
+
+        $db->dbh->pg_putcopyend();
+    }
+
+    # Print term count
+    my $term_count_rs = $db->query(
+        "SELECT
+                                    term,
+                                    LOG($story_count / COUNT(term)) AS idf
+                                  FROM temp_term_counts
+                                  GROUP BY term
+                                  ORDER BY idf ASC
+                                  LIMIT $stoplist_threshold"
+    );
+    binmode( STDERR, ":utf8" );
+    while ( my $term_count = $term_count_rs->hash() )
+    {
+        printf STDERR "%s\t%f\n", $term_count->{ term }, $term_count->{ idf };
+    }
+
+    # Cleanup
+    $db->query( "DROP TABLE temp_term_counts" );
 }
 
 # Normalised Inverse Document Frequency (normalised IDF)
@@ -146,11 +238,11 @@ sub gen_term_based_sampling
 # FIXME:comment
 sub main
 {
-    my $generation_type    = 'tf';                                     # which method of stoplist generation should be used
+    my $generation_type    = 'tf';                              # which method of stoplist generation should be used
     my @valid_types        = ( 'tf', 'idf', 'nidf', 'tbrs' );
-    my $term_limit         = 0;                                        # how many terms (words) to take into account
-    my $sentence_limit     = 4000;                                     # how many sentences to take into account
-    my $stoplist_threshold = 20;                                       # how many stopwords to print
+    my $term_limit         = 0;                                 # how many terms (words) to take into account
+    my $sentence_limit     = 4000;                              # how many sentences to take into account
+    my $stoplist_threshold = 20;                                # how many stopwords to print
 
     my Readonly $usage =
       'Usage: ./mediawords_generate_stopwords.pl' . ' [--type=tf|idf|nidf|tbrs]' . ' [--term_limit=i]' .
