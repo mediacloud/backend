b964811a76b4c5ecc6471c4258d289fbcbc8fcb8
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2012-07-23 14:36:19 +0000

    Install: skip creating PostgreSQL's user / databases if they exist already

--- install_scripts/create_default_db_user_and_databases.sh
+++ install_scripts/create_default_db_user_and_databases.sh
@@ -15,10 +15,32 @@ fi
 
 PASSWORD="mediacloud"
 
-echo "Creating postgresql user 'mediaclouduser' with password '$PASSWORD'"
+echo "creating postgresql user 'mediaclouduser' with password '$PASSWORD'"
+CREATEUSER_RUN=`sudo su -l postgres -c "$PSQL -c \"CREATE USER mediaclouduser WITH SUPERUSER password '$PASSWORD' ; \" 2>&1 " || echo "Oops."`
+if [[ "$CREATEUSER_RUN" == *"already exists"* ]]; then
+    echo "postgresql user 'mediaclouduser' already exists, skipping creation."
+    echo "if you want to purge everything and start from scratch, run purge_mediacloud_databases.sh manually."
+else
+    echo "postgresql error while creating user 'mediaclouduser': $CREATEUSER_RUN"
+    exit 1
+fi
 
-sudo su -l postgres -c "$PSQL -c \"CREATE USER mediaclouduser WITH SUPERUSER password '$PASSWORD' ; \" "
-echo "creating database mediacloud"
-sudo su -l postgres -c "$CREATEDB --owner mediaclouduser mediacloud"
+echo "creating database 'mediacloud'"
+CREATEDB_MEDIACLOUD_RUN=`sudo su -l postgres -c "$CREATEDB --owner mediaclouduser mediacloud 2>&1" || echo "Oops."`
+if [[ "$CREATEDB_MEDIACLOUD_RUN" == *"already exists"* ]]; then
+    echo "postgresql database 'mediacloud' already exists, skipping creation."
+    echo "if you want to purge everything and start from scratch, run purge_mediacloud_databases.sh manually."
+else
+    echo "postgresql error while creating database 'mediacloud': $CREATEDB_MEDIACLOUD_RUN"
+    exit 1
+fi
+
 echo "creating database mediacloud_test"
-sudo su -l postgres -c "$CREATEDB --owner mediaclouduser mediacloud_test"
+CREATEDB_MEDIACLOUDTEST_RUN=`sudo su -l postgres -c "$CREATEDB --owner mediaclouduser mediacloud_test 2>&1" || echo "Oops."`
+if [[ "$CREATEDB_MEDIACLOUDTEST_RUN" == *"already exists"* ]]; then
+    echo "postgresql database 'mediacloud_test' already exists, skipping creation."
+    echo "if you want to purge everything and start from scratch, run purge_mediacloud_databases.sh manually."
+else
+    echo "postgresql error while creating database 'mediacloud_test': $CREATEDB_MEDIACLOUDTEST_RUN"
+    exit 1
+fi
