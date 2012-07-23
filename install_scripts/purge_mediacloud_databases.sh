1c03b3df1d9b4ddb575fdb36252dc9991c07900a
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-07-23 18:42:15 +0000

    Purge the mediaclouduser from psql even if the databases don't exist.

--- install_scripts/purge_mediacloud_databases.sh
+++ install_scripts/purge_mediacloud_databases.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -u
-set -o errexit
-
 if [ `uname` == 'Darwin' ]; then
     # Mac OS X
     PSQL=/opt/local/lib/postgresql84/bin/psql
@@ -13,6 +10,12 @@ else
     DROPDB="dropdb"
 fi
 
+echo "DROPPING db mediacloud"
 sudo su -l postgres -c "$DROPDB mediacloud"
+echo "DROPPING db mediacloud_test"
 sudo su -l postgres -c "$DROPDB mediacloud_test"
+
+set -u
+set -o errexit
+
 sudo su -l postgres -c "$PSQL -c \"DROP USER IF EXISTS mediaclouduser \" "
