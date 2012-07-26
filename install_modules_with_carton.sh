32c9f4e456605067c7d0b5b88dac4dc2875ada51
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2012-07-26 14:23:16 +0000

    Install: on OS X, install BerkeleyDB before anything else

--- install_modules_with_carton.sh
+++ install_modules_with_carton.sh
@@ -7,15 +7,15 @@ working_dir=`dirname $0`
 
 cd $working_dir
 
-./script/run_carton.sh install --deployment || echo "initial carton run "
-./script/run_carton.sh install --deployment
-
-# On OS X, these modules get lost in the process for some reason
 if [ `uname` == 'Darwin' ]; then
+	# On OS X, these modules get lost in the process for some reason
 	./script/run_carton.sh install YAML::Syck
 
-	# INSTALL orders to install 'db44' on OS X, thus a hardcoded path
+	# Install BerkeleyDB correctly before installing the remaining modules from carton.lock
 	BERKELEYDB_INCLUDE=/opt/local/include/db44 BERKELEYDB_LIB=/opt/local/lib/db44 ./script/run_carton.sh install BerkeleyDB
 fi
+
+./script/run_carton.sh install --deployment || echo "initial carton run "
+./script/run_carton.sh install --deployment
 
 echo "Successfully installed Perl and modules for MediaCloud"
