d3faf27c9894741ee3cf196070b66afc8f950a5b
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-08-15 18:27:18 +0000

    Run 8 instances in parallel to speed things up.

--- script/mediawords_reformat_all_code.sh
+++ script/mediawords_reformat_all_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find script script_archive/ t lib -iname '*.pm' -print0  -or -iname '*.pl' -print0 -or -iname '*.t'  -print0 | xargs -0 -n1 ./script/run_with_carton.sh ./script/mediawords_reformat_code.pl
+find script script_archive/ t lib -iname '*.pm' -print0  -or -iname '*.pl' -print0 -or -iname '*.t'  -print0 | xargs -0 -n 1 -P 8 ./script/run_with_carton.sh ./script/mediawords_reformat_code.pl
 find script script_archive/ t lib -iname '*.pm.bak' -print0  -or -iname '*.pl.bak' -print0  -or -iname '*.t.bak'  -print0 | xargs -0 rm
