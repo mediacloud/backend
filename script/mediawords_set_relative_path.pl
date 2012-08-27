5d92cb41a6a8ae43ff03990c27ee83d09aa2f7da
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-08-27 20:10:29 +0000

    Increase batch size from 100 to 1000

--- script/mediawords_set_relative_path.pl
+++ script/mediawords_set_relative_path.pl
@@ -69,7 +69,7 @@ sub set_relative_path_all_downloads
     #Make sure the file start and end ranges are multiples of 1000
     my $start_downloads_id = int( $min_downloads_id / 1000 ) * 1000;
 
-    Readonly my $download_batch_size => 100;
+    Readonly my $download_batch_size => 1000;
 
     my $batch_number = 0;
 
