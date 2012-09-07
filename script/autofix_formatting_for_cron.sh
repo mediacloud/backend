4cc303b8ab72cce37012418b4f70d8d352b3118d
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-07 23:11:40 +0000

    CD to the correct directory at start of script.

--- script/autofix_formatting_for_cron.sh
+++ script/autofix_formatting_for_cron.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+working_dir=`dirname $0`
+
+cd $working_dir
+
+set -u
+set -o  errexit
+
 svn up > /dev/null
 svn status | grep '^M' && echo "Unchecked in changes aborting" && exit
 ./script/mediawords_reformat_all_code.sh
