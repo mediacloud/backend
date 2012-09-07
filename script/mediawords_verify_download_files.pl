68a1a7aad861b85c410eb60e8f84cf581e886cc6
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-07 19:20:20 +0000

    Update for test deployment.

--- script/mediawords_verify_download_files.pl
+++ script/mediawords_verify_download_files.pl
@@ -22,8 +22,6 @@ sub verify_downloads_files
 {
     my $db = MediaWords::DB::connect_to_db;
 
-    $db->dbh->{ AutoCommit } = 0;
-
     my $config = MediaWords::Util::Config::get_config;
     my $data_dir = $config->{ mediawords }->{ data_content_dir } || $config->{ mediawords }->{ data_dir };
 
@@ -33,7 +31,8 @@ sub verify_downloads_files
         my $num_downloads = 0;
 
         my $relative_file_paths = $db->query(
-"select distinct(relative_file_path) from downloads where file_status = 'tbd' and relative_file_path <> 'inline' and relative_file_path <> 'na' and relative_file_path <> 'tbd' limit 10 "
+	    " select distinct( relative_file_path) from downloads where " . 
+	    " file_status = 'tbd'::download_file_status AND relative_file_path <> 'tbd'::text AND relative_file_path <> 'error'::text limit 1;"
         );
 
         while ( my $relative_file_path_hash = $relative_file_paths->hash() )
@@ -47,18 +46,16 @@ sub verify_downloads_files
             if ( -f $file_path )
             {
                 say "$file_path exists";
-                $db->query( "UPDATE downloads set file_status = 'present' where relative_file_path = ? ",
-                    $relative_file_path );
+                #$db->query( "UPDATE downloads set file_status = 'present' where relative_file_path = ? ",
+                #    $relative_file_path );
             }
             else
             {
                 say "$file_path doesn't exist";
-                $db->query( "UPDATE downloads set file_status = 'missing' where relative_file_path = ? ",
-                    $relative_file_path );
+                #$db->query( "UPDATE downloads set file_status = 'missing' where relative_file_path = ? ",
+                #    $relative_file_path );
             }
         }
-
-        $db->commit;
 
         return;
     }
