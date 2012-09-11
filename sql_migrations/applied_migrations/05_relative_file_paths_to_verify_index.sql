f4c7b3a02d1249ca731c8d70648f41a24f0eb8e3
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-11 22:01:00 +0000

    Remove concurrent from create index.

--- sql_migrations/applied_migrations/05_relative_file_paths_to_verify_index.sql
+++ sql_migrations/applied_migrations/05_relative_file_paths_to_verify_index.sql
@@ -1,1 +1,1 @@
-CREATE INDEX CONCURRENTLY relative_file_paths_to_verify on downloads( relative_file_path ) where file_status = 'tbd' and relative_file_path <> 'tbd' and relative_file_path <> 'error';
+CREATE INDEX relative_file_paths_to_verify on downloads( relative_file_path ) where file_status = 'tbd' and relative_file_path <> 'tbd' and relative_file_path <> 'error';
