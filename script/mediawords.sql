a12a7ea7e02a0fc28a0f802e9022806e487b314c
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-12 14:21:07 +0000

    Appended applied migration to the schema file.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -1348,3 +1348,10 @@ LANGUAGE 'plpgsql';
 
 --DROP TRIGGER IF EXISTS download_relative_file_path_trigger on downloads CASCADE;
 --CREATE TRIGGER download_relative_file_path_trigger BEFORE INSERT OR UPDATE ON downloads FOR EACH ROW EXECUTE PROCEDURE  download_relative_file_path_trigger() ;
+
+-- Add column to allow more active feeds to be downloaded more frequently.
+ALTER TABLE feeds ADD COLUMN last_new_story_time timestamp without time zone;
+UPDATE feeds SET last_new_story_time = greatest( last_download_time, last_new_story_time );
+ALTER TABLE feeds ALTER COLUMN last_download_time TYPE timestamp with time zone;
+ALTER TABLE feeds ALTER COLUMN last_new_story_time TYPE timestamp with time zone;
+
