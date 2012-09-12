8a69cc117b7c99c6e64b58cc6e0d96093395db7a
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-12 21:09:12 +0000

    Auto formatting fix script

--- lib/MediaWords/Controller/Stories.pm
+++ lib/MediaWords/Controller/Stories.pm
@@ -243,39 +243,43 @@ sub stories_query_json : Local
 
     my $last_stories_id = $c->req->param( 'last_stories_id' );
 
-    if ( ! defined ( $last_stories_id ) )
+    if ( !defined( $last_stories_id ) )
     {
 
-      ( $last_stories_id ) = $c->dbis->query ( " select stories_id from stories where collect_date < now() - interval '1 days' order by stories_id desc limit 1 " )->flat;
-      $last_stories_id--;
+        ( $last_stories_id ) = $c->dbis->query(
+" select stories_id from stories where collect_date < now() - interval '1 days' order by stories_id desc limit 1 "
+        )->flat;
+        $last_stories_id--;
     }
 
     say STDERR "Last_stories_id is $last_stories_id";
 
-    Readonly my $stories_to_return => min (  $c->req->param( 'story_count' ) // 25 , 1000);
+    Readonly my $stories_to_return => min( $c->req->param( 'story_count' ) // 25, 1000 );
 
-    my $stories = $c->dbis->query( " SELECT * FROM stories WHERE stories_id > ? ORDER by stories_id asc LIMIT ? ", $last_stories_id, $stories_to_return )->hashes;
+    my $stories = $c->dbis->query( " SELECT * FROM stories WHERE stories_id > ? ORDER by stories_id asc LIMIT ? ",
+        $last_stories_id, $stories_to_return )->hashes;
 
-    foreach my $story ( @ { $stories } )
+    foreach my $story ( @{ $stories } )
     {
-    	my $story_text = MediaWords::DBI::Stories::get_text_for_word_counts( $c->dbis, $story );
-    	$story->{ story_text } = $story_text;
+        my $story_text = MediaWords::DBI::Stories::get_text_for_word_counts( $c->dbis, $story );
+        $story->{ story_text } = $story_text;
     }
 
-    foreach my $story ( @ { $stories } )
+    foreach my $story ( @{ $stories } )
     {
-    	my $content_ref = MediaWords::DBI::Stories::get_content_for_first_download( $c->dbis, $story );
+        my $content_ref = MediaWords::DBI::Stories::get_content_for_first_download( $c->dbis, $story );
 
-	if ( ! defined( $content_ref ) )
-	{
-	   $story->{ first_raw_download_file }->{missing} = 'true';
-	}
-	else
-	{
-	   #say STDERR "got content_ref $$content_ref";
+        if ( !defined( $content_ref ) )
+        {
+            $story->{ first_raw_download_file }->{ missing } = 'true';
+        }
+        else
+        {
 
-	  $story->{ first_raw_download_file } = $$content_ref;
-	}
+            #say STDERR "got content_ref $$content_ref";
+
+            $story->{ first_raw_download_file } = $$content_ref;
+        }
     }
 
     say STDERR "finished stories_query_json";
@@ -283,7 +287,6 @@ sub stories_query_json : Local
     $c->response->content_type( 'application/json' );
     return $c->res->body( encode_json( $stories ) );
 }
-
 
 # display regenerated tags for story
 sub retag : Local
