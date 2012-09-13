d7b13b31dd438b47fad98a2fe862efbb218076e2
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-13 15:00:52 +0000

    Auto formatting fix script

--- lib/MediaWords/Controller/Stories.pm
+++ lib/MediaWords/Controller/Stories.pm
@@ -245,11 +245,13 @@ sub stories_query_json : Local
 
     my $start_stories_id = $c->req->param( 'start_stories_id' );
 
-    die " Cannot use both last_stories_id and start_stories_id" if defined ( $last_stories_id ) and defined ( $start_stories_id );
+    die " Cannot use both last_stories_id and start_stories_id"
+      if defined( $last_stories_id )
+          and defined( $start_stories_id );
 
-    if ( defined ( $start_stories_id ) && ! ( defined ( $last_stories_id ) ) )
+    if ( defined( $start_stories_id ) && !( defined( $last_stories_id ) ) )
     {
-	$last_stories_id = $start_stories_id - 1;
+        $last_stories_id = $start_stories_id - 1;
     }
     else
     {
