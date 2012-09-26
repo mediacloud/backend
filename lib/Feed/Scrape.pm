24466f39897b948064412cda10ba6aae42c2131d
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-09-26 18:12:50 +0000

    Auto formatting fix script

--- lib/Feed/Scrape.pm
+++ lib/Feed/Scrape.pm
@@ -451,7 +451,7 @@ sub get_valid_feeds_from_single_index_url
 # TODO: Refactor and clean up this function so it's more readable. It's only called in a couple of places so changing it should be relatively safe
 #  Variables such as $recurse should have consistent type, and not be used for multiple purposes. (Currently $recurse can be either a boolean or a reference to an array of strings.
 # Ideally there would be a private implementation function that does the actual recursion that is called by a short non-recurvsive public function
-# 
+#
 #
 sub get_valid_feeds_from_index_url
 {
