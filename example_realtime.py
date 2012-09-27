2f8c0e62b330659e0b6dde1b512d8473ffb45503
Rahul Bhargava <rahulb@media.mit.edu>
2012-09-27 13:01:35 -0400

    added logging to realtime fetching

--- example_realtime.py
+++ example_realtime.py
@@ -4,6 +4,7 @@ import ConfigParser
 import json
 from pubsub import pub
 import nltk
+import logging
 
 from mediacloud.api import MediaCloud
 from mediacloud.storage import StoryDatabase
@@ -15,8 +16,15 @@ created after the latest one it has in it's db.  It saves the metadata for all t
 a 'mediacloud' CouchDB database.
 '''
 
+STORIES_TO_FETCH = 100
+
 config = ConfigParser.ConfigParser()
 config.read('mc-client.config')
+
+# setup logging
+logging.basicConfig(filename='mc-realtime.log',level=logging.DEBUG)
+log = logging.getLogger('mc-realtime')
+log.info("---------------------------------------------------------------------------")
 
 # setup a connection to a local DB
 db = StoryDatabase('mediacloud', config.get('db','host'), config.get('db','port') )
@@ -25,8 +33,8 @@ db = StoryDatabase('mediacloud', config.get('db','host'), config.get('db','port'
 mc = MediaCloud( config.get('api','user'), config.get('api','pass') )
 
 max_story_id = db.getMaxStoryId()
-results = mc.storiesSince( max_story_id, 100 )
-print "Fetched "+str(len(results))+" stories (after "+str(max_story_id)+")"
+results = mc.storiesSince( max_story_id, STORIES_TO_FETCH )
+log.info("Fetched "+str(len(results))+" stories (after "+str(max_story_id)+")")
 
 # set up my callback function that adds word count to the story
 pub.subscribe(mediacloud.examples.addWordCountToStory, StoryDatabase.EVENT_PRE_STORY_SAVE)
@@ -37,5 +45,8 @@ for story in results:
     worked = db.addStory(story)
     if worked:
       saved = saved + 1
+    else:
+      log.warning("  unable to save story "+str(story._stories_id))
 
-print "Saved "+str(saved)+" stories"
+max_story_id = db.getMaxStoryId()
+log.info("Saved "+str(saved)+" stories - new max id "+str(max_story_id))
