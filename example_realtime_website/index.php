5872a18e78d73d563a3844778ada29c414569c55
Rahul Bhargava <rahulb@media.mit.edu>
2012-09-27 09:55:27 -0400

    simple php script to show story length histogram

---
+++ example_realtime_website/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>MediaCloud API Client Examples</title>
+    <script type="text/javascript" src="js/d3.v2.min.js"></script>
+    <link href="css/mediacloud.css" rel="stylesheet" type="text/css"/>
+  </head>
+
+  <body>
+
+<?php
+require_once('./lib/CouchSimple.php');
+
+$options['host'] = "localhost"; 
+$options['port'] = 5984;
+
+$couch = new CouchSimple($options);
+
+// total article count
+$results = json_decode( $couch->send("GET", "/mediacloud/_design/examples/_view/total_articles") ); 
+$articleCount = $results->rows[0]->value;
+
+// article count by length
+$results = json_decode( $couch->send("GET", "/mediacloud/_design/examples/_view/word_counts?group=true") ); 
+
+$wordCounts = array();
+$barsToShow = 40;
+$bucketSize = 200;  // must match view
+$maxStoryLengthToShow = ($barsToShow)*$bucketSize;
+$includedStories = 0;
+$excludedStories = 0;
+// prefill array
+$i = 0;
+for($i=0;$i<$barsToShow;$i++){
+  $wordCounts[$i*$bucketSize] = 0;
+}
+// 
+$maxIncludedStoryCount = 0;
+foreach ($results->rows as $row){
+  if (array_key_exists($row->key,$wordCounts)) {
+    $wordCounts[$row->key] = $row->value;
+    $maxIncludedStoryCount = max($maxIncludedStoryCount,$row->value);
+    $includedStories+=$row->value;
+  } else {
+    $excludedStories+=$row->value;
+  }
+}
+?>
+<h1>MediaCloud API Client Examples</h1>
+
+<p>
+<?=$articleCount?> stories in the database</b>
+</p>
+
+<h2>Story Length</h2>
+<p>
+Here is a histogram of story length.  The horizontal axis is word length (0-200, 200-400, etc). 
+The vertical axis is the number of stories have that many words.  This graph includes 
+<?=$includedStories?> stories shorter than <?=$maxStoryLengthToShow?> words (excluding the
+<?=$excludedStories?> stories longer than that).
+</p>
+
+<script type="text/javascript">
+var datasetKeys = [
+<?php
+foreach ($wordCounts as $wordCount=>$storyCount){
+?> <?=$wordCount?>,
+<?php
+}
+?>
+]
+var dataset = [
+<?php
+foreach ($wordCounts as $wordCount=>$storyCount){
+?> <?=$storyCount?>,
+<?php
+}
+?>
+];
+</script>
+
+<script type="text/javascript">
+var chartHeight = 200;
+var chartWidth = 800;
+var barWidth = 20;
+var maxStoryLenth = <?=$maxStoryLengthToShow?>;
+var barsToShow = <?=$barsToShow?>;
+var maxIncludedStoryCount = <?=$maxIncludedStoryCount?>;
+var y = d3.scale.linear()
+     .domain([0, maxIncludedStoryCount])
+     .range([0, chartHeight]);
+var x = d3.scale.linear()
+     .domain([0,maxStoryLenth])
+     .range([0, chartWidth]);
+var chart = d3.select("body").append("svg")
+     .attr("class", "chart")
+     .attr("width", barWidth*barsToShow+50)
+     .attr("height", chartHeight+25)
+     .append("g")
+     .attr("transform", "translate(10,0)");
+chart.selectAll("rect")
+     .data(dataset)
+     .enter().append("rect")
+     .attr("x", function(d,i) { return i*barWidth; })
+     .attr("y", function(d) {return chartHeight - y(d);} )
+     .attr("height", y)
+     .attr("width", barWidth);
+chart.selectAll(".rule")
+     .data(x.ticks(barsToShow/4))
+     .enter().append("text")
+     .attr("class", "rule")
+     .attr("x", x)
+     .attr("y", chartHeight)
+     .attr("dy", 15)
+     .attr("text-anchor", "middle")
+     .text(String);
+chart.append("line")
+     .attr("x1", 0)
+     .attr("x2", chartWidth)
+     .attr("y1", chartHeight)
+     .attr("y2", chartHeight)
+     .style("stroke", "#666");
+</script>
+
+  </body>
+
+</html>
