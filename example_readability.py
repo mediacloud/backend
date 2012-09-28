5221af719ba8c8adc2e5392703da4998f81da04e
Rahul Bhargava <rahulb@media.mit.edu>
2012-09-28 14:30:38 -0400

    added FleshKincaidGradeLevel readability example

---
+++ example_readability.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import ConfigParser
+import json
+from pubsub import pub
+import nltk
+
+from mediacloud.api import MediaCloud
+from mediacloud.storage import StoryDatabase
+import mediacloud.examples
+
+'''
+This example file fetches the latest 25 stories from MediaCloud and saves their metadata 
+to a 'mediacloud' CouchDB database.  It adds in the extracted text readability via a 
+pre-save event subscription.
+
+To Install:
+>>> import nltk
+>>> nltk.download()
+select the corpora tab and download stopwords
+'''
+
+config = ConfigParser.ConfigParser()
+config.read('mc-client.config')
+
+# set up a connection to a local DB
+db = StoryDatabase('mediacloud', config.get('db','host'), config.get('db','port') )
+
+# connect to MC and fetch some articles
+mc = MediaCloud( config.get('api','user'), config.get('api','pass') )
+results = mc.recentStories()
+print "Fetched "+str(len(results))+" stories"
+
+# set up my callback function that adds word count to the story
+pub.subscribe(mediacloud.examples.addFleshKincaidGradeLevelToStory, StoryDatabase.EVENT_PRE_STORY_SAVE)
+
+# save all the stories in the db (this will fire the callback above)
+saved = 0
+for story in results:
+    worked = db.addStory(story)
+    if worked:
+        saved = saved + 1
+
+print "Saved "+str(saved)+" stories"
--- mediacloud/examples.py
+++ mediacloud/examples.py
@@ -2,6 +2,7 @@
 import couchdb
 from pubsub import pub
 import nltk
+from mediacloud.readability.readabilitytests import ReadabilityTool
 
 '''
 This file holds some simple example functions that process a story and add some piece
@@ -17,3 +18,19 @@ def addWordCountToStory(db_story, raw_story):
     text = nltk.Text(raw_story['story_text'].encode('utf-8'))
     word_count = len(text)
     db_story['word_count'] = word_count
+    
+def addFleshKincaidGradeLevelToStory(db_story, raw_story):
+    '''
+    Simple hook to add the Flesch-Kincaid Grade to the database.  This uses a pre-save
+    callback to add a new 'fk_grade_level' column.  This relies on patched ntlk_contrib
+    code, stored in the mediacloud.readability module (cause the published code don't
+    work!).
+    '''
+    text = raw_story['story_text'].encode('utf-8')
+    r = ReadabilityTool()
+    gradeLevel = None
+    try:
+      gradeLevel = r.FleschKincaidGradeLevel(text)
+    except KeyError:
+      pass
+    db_story['fk_grade_level'] = gradeLevel
