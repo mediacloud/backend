798747cc0b9d6e169cad280f1d5fd6c1930b1bfb
Hal Roberts <hroberts@cyber.law.harvard.edu>
2012-10-14 05:29:34 +0000

    mark some sources as not needing moderation if the feeds look like main feeds

--- script/mediawords_add_default_feeds.pl
+++ script/mediawords_add_default_feeds.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 
-# periodically check for new media sources that have not had default feeds added to them and add the default feeds
+# periodically check for new media sources that have not had default feeds added to them and add the default feeds.
+# look for feeds that are most likely to be real feeds.  If we find more than one but no more than MAX_DEFAULT_FEEDS
+# of those feeds, use the first such one and do not moderate the source.  Else, do a more expansive search
+# and mark for moderation.
 
 use strict;
 
@@ -9,6 +12,8 @@ BEGIN
     use FindBin;
     use lib "$FindBin::Bin/../lib";
 }
+
+use constant MAX_DEFAULT_FEEDS => 4;
 
 use DBIx::Simple::MediaWords;
 use Feed::Scrape::MediaWords;
@@ -31,10 +36,36 @@ sub main
         {
             my $existing_urls = [];
 
-            my $feed_links =
-              Feed::Scrape::MediaWords->get_valid_feeds_from_single_index_url( $medium->{ url }, 1, $db, [],
-                $existing_urls );
+            # first look for <link> feeds or a set of url pattern feeds that are likely to be 
+            # main feeds if present (like "$url/feed")
+            my $default_feed_links = Feed::Scrape->get_main_feed_urls_from_url( $medium->{ url } );
+            
+            # otherwise do an expansive search
+            my $feed_links;
+            my $need_to_moderate;            
+            if ( !@{ $default_feed_links } )
+            {
+                $need_to_moderate = 1;
+                $feed_links =
+                    Feed::Scrape::MediaWords->get_valid_feeds_from_index_url( $medium->{ url }, 1, $db, [], $existing_urls );
+                    
+                # look through all feeds found for those with the host name in them and if found
+                # treat them as default feeds
+                my $medium_host = lc( URI->new( $medium->{ url } )->host );
+                $default_feed_links = [ grep { lc( URI->new( $_->{ url } )->host ) eq $medium_host } @{ $feed_links } ];
+                $default_feed_links = [ grep { $_->{ url } !~ /foaf/ } @{ $default_feed_links } ];                
+            }
 
+            # if there are more than 0 default feeds, use those.  If there are no more than
+            # MAX_DEFAULT_FEEDS, use the first one and don't moderate.
+            if ( @{ $default_feed_links } )
+            {
+                $default_feed_links = [ sort { length( $a->{ url } ) <=> length ( $b->{ url } ) } @{ $default_feed_links } ];
+                $default_feed_links = [ $default_feed_links->[ 0 ] ] if ( @{ $default_feed_links } <= MAX_DEFAULT_FEEDS );
+                $feed_links = $default_feed_links;
+                $need_to_moderate = 0;
+            }
+            
             for my $feed_link ( @{ $feed_links } )
             {
                 print "ADDED $medium->{ name }: $feed_link->{ name } - $feed_link->{ url }\n";
@@ -58,11 +89,13 @@ sub main
                 print $error;
             }
 
+            my $moderated = $need_to_moderate ? 'f' : 't';
+
             $db->query(
-                "update media set feeds_added = true, moderation_notes = ? where media_id = ?",
-                $medium->{ moderation_notes },
-                $medium->{ media_id }
+                "update media set feeds_added = true, moderation_notes = ?, moderated =  ? where media_id = ?",
+                $medium->{ moderation_notes }, $moderated, $medium->{ media_id }
             );
+            
         }
 
         $db->disconnect;
