9ebc7103cb4ffb93df6e756ac2f47ec591563a01
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-10-14 05:26:49 +0000

    Auto formatting fix script

--- script/mediawords_search_stories.pl
+++ script/mediawords_search_stories.pl
@@ -20,49 +20,51 @@ use MediaWords::Util::CSV;
 sub execute_and_store_search
 {
     my ( $db, $query_story_search ) = @_;
-    
+
     my $query = MediaWords::DBI::Queries::find_query_by_id( $db, $query_story_search->{ queries_id } );
 
     my $stories = MediaWords::DBI::Queries::search_stories( $db, $query, $query_story_search );
-    
+
     # my $fields = [ qw(stories_id url title publish_date media_id media_name media_url media_set_name story_text) ];
-    # 
+    #
     # my $csv_string = MediaWords::Util::CSV::get_hashes_as_encoded_csv( $stories_with_text, $fields );
-    # 
-    # $db->query( 
-    #     "update query_story_searches set search_completed = 't', csv_text = ? where query_story_searches_id = ?", 
+    #
+    # $db->query(
+    #     "update query_story_searches set search_completed = 't', csv_text = ? where query_story_searches_id = ?",
     #     $csv_string, $query_story_search->{ query_story_searches_id } );
 
     for my $story ( @{ $stories } )
     {
-        $db->query( 
+        $db->query(
             "insert into query_story_searches_stories_map ( query_story_searches_id, stories_id ) values ( ?, ? )",
-            $query_story_search->{ query_story_searches_id }, $story->{ stories_id } );
+            $query_story_search->{ query_story_searches_id },
+            $story->{ stories_id }
+        );
     }
 
-    $db->query( 
-        "update query_story_searches set search_completed = 't' where query_story_searches_id = ?", 
+    $db->query( "update query_story_searches set search_completed = 't' where query_story_searches_id = ?",
         $query_story_search->{ query_story_searches_id } );
 }
 
-sub main 
+sub main
 {
     while ( 1 )
     {
         my $db = MediaWords::DB::connect_to_db;
-        
+
         $db->begin_work;
         my $query_story_searches = $db->query( "select * from query_story_searches where search_completed = 'f'" )->hashes;
         if ( @{ $query_story_searches } )
         {
             map { execute_and_store_search( $db, $_ ) } @{ $query_story_searches };
         }
-        else {
+        else
+        {
             sleep 60;
         }
         $db->commit;
     }
-    
+
 }
 
 main();
