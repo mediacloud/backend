c04b25b87963edf3e1ce5f0a6779d1ab86f83636
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2012-10-31 16:03:42 +0000

    further query optimization.

--- script/mediawords_verify_download_files.pl
+++ script/mediawords_verify_download_files.pl
@@ -34,7 +34,7 @@ sub verify_downloads_files
          my $relative_file_paths =
           $db->query( " select distinct(relative_file_path) from   " . 
 		      "     ( select relative_file_path from downloads where  file_status = 'tbd'::download_file_status AND relative_file_path <> 'tbd'::text AND " . 
-		      "       relative_file_path <> 'error'::text AND relative_file_path <> 'na'::text AND relative_file_path <> 'inline'::text  limit 100 ) as foo " .
+		      "       relative_file_path <> 'error'::text AND relative_file_path <> 'na'::text AND relative_file_path <> 'inline'::text  limit 1000 ) as foo " .
 		      "  limit 100; "
           );
 
