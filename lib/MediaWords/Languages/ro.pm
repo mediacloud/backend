aac38e9724f0ef6ffe34e993c2ab84c0c5721a6d
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2012-11-22 23:59:15 +0000

    Romanian language module + home-made non-breakable prefix list

---
+++ lib/MediaWords/Languages/ro.pm
@@ -0,0 +1,66 @@
+package MediaWords::Languages::ro;
+use Moose;
+with 'MediaWords::Languages::Language';
+
+#
+# Romanian
+#
+
+use strict;
+use warnings;
+
+use Modern::Perl "2012";
+use MediaWords::CommonLibs;
+
+sub get_language_code
+{
+    return 'ro';
+}
+
+sub fetch_and_return_tiny_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_from_file( 'lib/MediaWords/Languages/resources/ro_stoplist.txt' );
+}
+
+sub fetch_and_return_short_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_from_file( 'lib/MediaWords/Languages/resources/ro_stoplist.txt' );
+}
+
+sub fetch_and_return_long_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_from_file( 'lib/MediaWords/Languages/resources/ro_stoplist.txt' );
+}
+
+sub stem
+{
+    my $self = shift;
+    return $self->_stem_with_lingua_stem_snowball( 'ro', 'UTF-8', \@_ );
+}
+
+sub get_word_length_limit
+{
+    my $self = shift;
+
+    # The 44-letter word pneumonoultramicroscopicsilicovolcaniconioză is the longest word.
+    # It is a substantive referring to a disease.
+    return 44;
+}
+
+sub get_sentences
+{
+    my ( $self, $story_text ) = @_;
+    return $self->_tokenize_text_with_lingua_sentence( 'ro',
+        'lib/MediaWords/Languages/resources/ro_nonbreaking_prefixes.txt', $story_text );
+}
+
+sub tokenize
+{
+    my ( $self, $sentence ) = @_;
+    return $self->_tokenize_with_spaces( $sentence );
+}
+
+1;
