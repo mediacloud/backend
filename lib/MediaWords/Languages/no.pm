e979578680cad3ddc39ed56e3fa39960beada8f5
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2012-11-22 23:58:09 +0000

    Norwegian language module + home-made non-breakable prefix list

---
+++ lib/MediaWords/Languages/no.pm
@@ -0,0 +1,72 @@
+package MediaWords::Languages::no;
+use Moose;
+with 'MediaWords::Languages::Language';
+
+#
+# Norwegian
+#
+
+use strict;
+use warnings;
+
+use Modern::Perl "2012";
+use MediaWords::CommonLibs;
+
+sub get_language_code
+{
+    return 'no';
+}
+
+sub fetch_and_return_tiny_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_with_lingua_stopwords( 'no', 'UTF-8' );
+}
+
+sub fetch_and_return_short_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_with_lingua_stopwords( 'no', 'UTF-8' );
+}
+
+sub fetch_and_return_long_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_with_lingua_stopwords( 'no', 'UTF-8' );
+}
+
+sub stem
+{
+    my $self = shift;
+    return $self->_stem_with_lingua_stem_snowball( 'no', 'UTF-8', \@_ );
+}
+
+sub get_word_length_limit
+{
+    my $self = shift;
+
+    # The longest word in Norwegian, that is a real word in ordinary use, is
+    # menneskerettighetsorganisasjonene (33 letters).[citation needed] The
+    # meaning is "the human rights organizations". Being used mostly in
+    # statistics, the term sannsynlighetstetthetsfunksjonene (meaning “the
+    # probability density functions”) is also 33 characters long. The physics
+    # term minoritetsladningsbærerdiffusjonskoeffisientmålingsapparatur has
+    # 60 characters, but is not a common word. Its meaning is "(a) device
+    # for measuring the distance between particles in a crystal".
+    return 60;
+}
+
+sub get_sentences
+{
+    my ( $self, $story_text ) = @_;
+    return $self->_tokenize_text_with_lingua_sentence( 'no',
+        'lib/MediaWords/Languages/resources/no_nonbreaking_prefixes.txt', $story_text );
+}
+
+sub tokenize
+{
+    my ( $self, $sentence ) = @_;
+    return $self->_tokenize_with_spaces( $sentence );
+}
+
+1;
