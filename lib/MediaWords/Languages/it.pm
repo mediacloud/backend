f499a5dcf6a92a0a8710ce7ea7d06edd4ab7d8fc
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2012-11-22 14:02:54 +0000

    Italian language module

---
+++ lib/MediaWords/Languages/it.pm
@@ -0,0 +1,66 @@
+package MediaWords::Languages::it;
+use Moose;
+with 'MediaWords::Languages::Language';
+
+#
+# Italian
+#
+
+use strict;
+use warnings;
+
+use Modern::Perl "2012";
+use MediaWords::CommonLibs;
+
+sub get_language_code
+{
+    return 'it';
+}
+
+sub fetch_and_return_tiny_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_with_lingua_stopwords( 'it', 'UTF-8' );
+}
+
+sub fetch_and_return_short_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_with_lingua_stopwords( 'it', 'UTF-8' );
+}
+
+sub fetch_and_return_long_stop_words
+{
+    my $self = shift;
+    return $self->_get_stop_words_with_lingua_stopwords( 'it', 'UTF-8' );
+}
+
+sub stem
+{
+    my $self = shift;
+    return $self->_stem_with_lingua_stem_snowball( 'it', 'UTF-8', \@_ );
+}
+
+sub get_word_length_limit
+{
+    my $self = shift;
+
+    # The longest accepted neologism is psiconeuroendocrinoimmunologia (30 letters).[citation needed]
+    # (http://en.wikipedia.org/wiki/Longest_words#Italian)
+    return 30;
+}
+
+sub get_sentences
+{
+    my ( $self, $story_text ) = @_;
+    return $self->_tokenize_text_with_lingua_sentence( 'it', 'lib/MediaWords/Languages/it_nonbreaking_prefixes.txt',
+        $story_text );
+}
+
+sub tokenize
+{
+    my ( $self, $sentence ) = @_;
+    return $self->_tokenize_with_spaces( $sentence );
+}
+
+1;
