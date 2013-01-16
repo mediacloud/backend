e5f961535694efdd3c3e56cdb47e060452df69d5
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-01-16 19:30:59 +0000

    Auto formatting fix script

--- script/mediawords_extractor_test_to_features.pl
+++ script/mediawords_extractor_test_to_features.pl
@@ -277,30 +277,30 @@ sub main
     foreach my $download ( @{ $downloads } )
     {
         my $ea = each_arrayref( $download->{ line_info }, $download->{ preprocessed_lines } );
-	
-	#TODO DRY out this code 
-	my $previous_states = [ qw ( prestart start ) ];
+
+        #TODO DRY out this code
+        my $previous_states = [ qw ( prestart start ) ];
         while ( my ( $line_info, $line_text ) = $ea->() )
         {
 
-	    my $current_state = $line_info->{ class };
+            my $current_state = $line_info->{ class };
 
-	    if ( $line_info->{ auto_excluded } == 1 )
-	    {
-		$current_state = 'auto_excluded';
-	    }
+            if ( $line_info->{ auto_excluded } == 1 )
+            {
+                $current_state = 'auto_excluded';
+            }
 
-	    my $prior_state_string = join '_', @$previous_states;
-	    $line_info->{ "priors_$prior_state_string" } = 1;
+            my $prior_state_string = join '_', @$previous_states;
+            $line_info->{ "priors_$prior_state_string" } = 1;
 
-	    if ( $previous_states->[ 1] eq 'auto_excluded' )
-	    {
-		$line_info->{ previous_line_auto_excluded } = 1;
-	    }
+            if ( $previous_states->[ 1 ] eq 'auto_excluded' )
+            {
+                $line_info->{ previous_line_auto_excluded } = 1;
+            }
 
-	    shift $previous_states;
+            shift $previous_states;
 
-	    push $previous_states, $current_state;
+            push $previous_states, $current_state;
 
             next if $line_info->{ auto_excluded } == 1;
 
