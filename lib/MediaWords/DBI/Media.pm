4ec52abfb3cbe980bb8da24195887e3ee7564d47
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-01-18 22:22:47 +0000

    Fix bug causing \r to be included in media tags.

--- lib/MediaWords/DBI/Media.pm
+++ lib/MediaWords/DBI/Media.pm
@@ -237,6 +237,8 @@ sub _find_media_from_urls
     {
         my $medium;
 
+	trim( $tagged_url );
+
         my ( $url, $tags_string ) = ( $tagged_url =~ /^\r*\s*([^\s]*)(?:\s+(.*))?/ );
 
         if ( $url !~ m~^[a-z]+://~ )
