995b69a51a7e0fd5c3693c5832cee7289fb94c89
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-01-24 11:54:29 +0000

    default value for "disable_json_top_500_words_cache" config option

--- lib/MediaWords/Util/Config.pm
+++ lib/MediaWords/Util/Config.pm
@@ -68,11 +68,12 @@ sub set_defaults
 {
     my ( $config ) = @_;
 
-    $config->{ mediawords }->{ script_dir }               ||= "$_base_dir/script";
-    $config->{ mediawords }->{ data_dir }                 ||= "$_base_dir/data";
-    $config->{ mediawords }->{ language }                 ||= "en_US_and_ru_RU";
-    $config->{ mediawords }->{ always_show_stack_traces } ||= "no";
-    $config->{ session }->{ storage }                     ||= "$ENV{HOME}/tmp/mediacloud-session";
+    $config->{ mediawords }->{ script_dir }                       ||= "$_base_dir/script";
+    $config->{ mediawords }->{ data_dir }                         ||= "$_base_dir/data";
+    $config->{ mediawords }->{ language }                         ||= "en_US_and_ru_RU";
+    $config->{ mediawords }->{ always_show_stack_traces }         ||= "no";
+    $config->{ mediawords }->{ disable_json_top_500_words_cache } ||= "no";
+    $config->{ session }->{ storage }                             ||= "$ENV{HOME}/tmp/mediacloud-session";
 
     return $config;
 }
