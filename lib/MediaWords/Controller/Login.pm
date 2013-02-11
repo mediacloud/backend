e6d8c8ac2c2da2d8d1f1999b203f85993d07c370
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-02-11 02:45:50 +0000

    login form rewritten to FormFu

--- lib/MediaWords/Controller/Login.pm
+++ lib/MediaWords/Controller/Login.pm
@@ -22,28 +22,70 @@ Login
 
 =cut
 
+# Login form
 sub index : Path : Args(0)
 {
     my ( $self, $c ) = @_;
 
-    my $form_was_submitted = $c->request->params->{ submit };
-
-    if ( $form_was_submitted )
-    {
-        my $email    = $c->request->params->{ email };
-        my $password = $c->request->params->{ password };
-
-        if ( $email && $password )
+    my $form = $c->create_form(
         {
+            load_config_file => $c->path_to() . '/root/forms/auth/login.yml',
+            method           => 'POST',
+            action           => $c->uri_for( '/login' ),
+        }
+    );
 
-            # Attempt to log the user in
-            if ( $c->authenticate( { username => $email, password => $password } ) )
-            {
+    $form->process( $c->request );
 
-                # If successful, redirect to default homepage
-                my $config            = MediaWords::Util::Config::get_config;
-                my $default_home_page = $config->{ mediawords }->{ default_home_page };
-                $c->response->redirect( $c->uri_for( $default_home_page ) );
+    if ( !$form->submitted_and_valid() )
+    {
+        $c->stash->{ form } = $form;
+        $c->stash->{ c }    = $c;
+        $c->stash( template => 'auth/login.tt2' );
+        return;
+    }
+
+    my $email    = $form->param_value( 'email' );
+    my $password = $form->param_value( 'password' );
+
+    if ( !( $email && $password ) )
+    {
+        unless ( $c->user_exists )
+        {
+            $c->stash->{ form } = $form;
+            $c->stash->{ c }    = $c;
+            $c->stash( template  => 'auth/login.tt2' );
+            $c->stash( error_msg => "Empty email address and / or password." );
+            return;
+        }
+    }
+
+    # Attempt to log the user in
+    if ( !$c->authenticate( { username => $email, password => $password } ) )
+    {
+        $c->stash->{ form } = $form;
+        $c->stash->{ c }    = $c;
+        $c->stash( template  => 'auth/login.tt2' );
+        $c->stash( error_msg => "Incorrect email address and / or password." );
+        return;
+    }
+
+    # Reset the password reset token (if any)
+    $c->dbis->query(
+        <<"EOF",
+        UPDATE auth_users
+        SET password_reset_token = NULL
+        WHERE email = ?
+EOF
+        $email
+    );
+
+    # If successful, redirect to default homepage
+    my $config            = MediaWords::Util::Config::get_config;
+    my $default_home_page = $config->{ mediawords }->{ default_home_page };
+    $c->response->redirect( $c->uri_for( $default_home_page ) );
+}
+
 
                 return;
             }
