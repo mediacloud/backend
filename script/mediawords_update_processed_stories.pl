23318878bd7121fa88496e31c4020f53f3892208
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-02-28 22:20:56 +0000

    Auto formatting fix script

--- script/mediawords_update_processed_stories.pl
+++ script/mediawords_update_processed_stories.pl
@@ -28,19 +28,24 @@ sub extract_text
 
     $db->dbh->{ AutoCommit } = 0;
 
-    my $last_story_sentences_id_processed = ( $db->query(" SELECT value from database_variables where name = 'LAST_STORY_SENTENCES_ID_PROCESSED' ")->flat() ) [0];
+    my $last_story_sentences_id_processed =
+      ( $db->query( " SELECT value from database_variables where name = 'LAST_STORY_SENTENCES_ID_PROCESSED' " )->flat() )
+      [ 0 ];
 
     die unless defined( $last_story_sentences_id_processed );
 
-    my $stop_story_sentences_id = ( $db->query( " SELECT max(story_sentences_id) from story_sentences" )->flat())[0];
+    my $stop_story_sentences_id = ( $db->query( " SELECT max(story_sentences_id) from story_sentences" )->flat() )[ 0 ];
 
-    $db->query(" INSERT INTO processed_stories ( stories_id ) select distinct( stories_id ) from story_sentences where story_sentences_id >? and story_sentences_id <= ? order by stories_id" , $last_story_sentences_id_processed, $stop_story_sentences_id );
+    $db->query(
+" INSERT INTO processed_stories ( stories_id ) select distinct( stories_id ) from story_sentences where story_sentences_id >? and story_sentences_id <= ? order by stories_id",
+        $last_story_sentences_id_processed, $stop_story_sentences_id
+    );
 
     $db->query( "UPDATE database_variables SET value = ? where name =  'LAST_STORY_SENTENCES_ID_PROCESSED' ",
-		$stop_story_sentences_id );
+        $stop_story_sentences_id );
 
     $db->commit;
-    
+
 }
 
 # fork of $num_processes
