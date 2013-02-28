d1c7997748c22d037a65c778affc507cd9484909
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-02-28 12:10:16 +0000

    run_test_suite.sh checks if hostname / domainname is empty / numeric; some hosting companies set the hostnames of their virtual servers to numeric values, and the test suite then fails without explaining much

--- script/run_test_suite.sh
+++ script/run_test_suite.sh
@@ -3,6 +3,52 @@
 set -u
 set -o  errexit
 
+# Some dedicated server companies set the hostnames / domainnames of their
+# virtual servers to numeric values (such as 288066). The test suite is
+# then unable to access http://288066/
+
+function hostname_help {
+    echo "Please set the hostname to an alphanumeric value, e.g.:"
+    echo "    hostname mediacloud"
+    echo "    export HOSTNAME=mediacloud"
+    echo "and then try again."    
+}
+
+function domainname_help {
+    echo "Please set the domainname to an alphanumeric value, e.g.:"
+    echo
+    echo "    domainname local"
+    echo
+    echo "and then try again."
+}
+
+system_hostname=`hostname`
+shell_hostname="$HOSTNAME"      # internal bash variable
+system_domainname=`domainname`
+if [[ -z "$system_hostname" || "$system_hostname" == "(none)" 
+    || -z "$shell_hostname" || "$shell_hostname" == "(none)" ]]; then
+
+    echo "Unable to determine hostname or hostname is empty."
+    hostname_help
+    exit 1
+fi
+if [[ "$system_hostname" =~ ^[0-9]+$ || "$shell_hostname" =~ ^[0-9]+$ ]] ; then
+    echo "Your hostname ('$HOSTNAME') consists only of integers and thus some of the tests might fail."
+    hostname_help
+    exit 1
+fi
+if [[ -z "$system_domainname" || "$system_domainname" == "(none)" ]]; then
+    echo "Unable to determine domainname or domainname is empty."
+    domainname_help
+    exit 1
+fi
+if [[ "$system_domainname" =~ ^[0-9]+$ ]] ; then
+    echo "Your domainname ('$system_domainname') consists only of integers and thus some of the tests might fail."
+    domainname_help
+    exit 1
+fi
+
+# Run test suite
 working_dir=`dirname $0`
 
 cd $working_dir
