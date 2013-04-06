97d9b9deef2ad692f61a2fb2c31425cf25c4e72f
Hal Roberts <hroberts@cyber.law.harvard.edu>
2013-04-06 07:36:25 +0000

    fix encoding bug; add no_vector option for process_download_for_extractor

--- lib/MediaWords/DBI/Downloads.pm
+++ lib/MediaWords/DBI/Downloads.pm
@@ -521,8 +521,8 @@ sub store_content
         my $path = 'content:' . $$content_ref;
         $db->query(
             "update downloads set state = ?, path = ?, error_message = ? where downloads_id = ?",
-            $new_state, $path,
-            $download->{ error_message },
+            $new_state, encode( 'utf8', $path ),
+            encode( 'utf8', $download->{ error_message } ),
             $download->{ downloads_id }
         );
 
@@ -573,12 +573,14 @@ sub get_medium
 
 sub process_download_for_extractor
 {
-    my ( $db, $download, $process_num, $no_dedup_sentences ) = @_;
-
+    my ( $db, $download, $process_num, $no_dedup_sentences, $no_vector ) = @_;
+    
     print STDERR "[$process_num] extract: $download->{ downloads_id } $download->{ stories_id } $download->{ url }\n";
     my $download_text = MediaWords::DBI::DownloadTexts::create_from_download( $db, $download );
 
     #print STDERR "Got download_text\n";
+
+    return if ( $no_vector );
 
     my $remaining_download =
       $db->query( "select downloads_id from downloads " . "where stories_id = ? and extracted = 'f' and type = 'content' ",
