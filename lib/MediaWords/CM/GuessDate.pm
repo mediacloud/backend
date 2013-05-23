908e5aca3449f9b7f5fc58620d155a79c65c8444
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-05-23 10:02:03 +0000

    rename $xpath variable to $html_tree to better represent the contents of the variable

--- lib/MediaWords/CM/GuessDate.pm
+++ lib/MediaWords/CM/GuessDate.pm
@@ -100,9 +100,9 @@ my $_date_guess_functions = [
 # return the first in a list of nodes matching the xpath pattern
 sub _find_first_node
 {
-    my ( $tree, $xpath ) = @_;
+    my ( $html_tree, $xpath ) = @_;
 
-    my @nodes = $tree->findnodes( $xpath );
+    my @nodes = $html_tree->findnodes( $xpath );
 
     my $node = pop @nodes;
 
@@ -110,15 +110,15 @@ sub _find_first_node
 }
 
 # get HTML::TreeBuilder::LibXML object representing the html
-sub _get_xpath
+sub _get_html_tree
 {
     my ( $html ) = @_;
 
-    my $xpath = HTML::TreeBuilder::LibXML->new;
-    $xpath->ignore_unknown( 0 );
-    $xpath->parse_content( $html );
+    my $html_tree = HTML::TreeBuilder::LibXML->new;
+    $html_tree->ignore_unknown( 0 );
+    $html_tree->parse_content( $html );
 
-    return $xpath;
+    return $html_tree;
 }
 
 # return true if the args are valid date arguments.  assume a date has to be between 2000 and 2020.
@@ -151,9 +151,9 @@ sub _round_midnight_to_noon
 # <meta name="DC.date.issued" content="2011-12-16T13:56:00-08:00" />
 sub _guess_by_dc_date_issued
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//meta[@name="DC.date.issued"]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//meta[@name="DC.date.issued"]' ) )
     {
         return $node->attr( 'content' );
     }
@@ -162,9 +162,9 @@ sub _guess_by_dc_date_issued
 # <li property="dc:date dc:created" content="2012-01-17T05:51:44-07:00" datatype="xsd:dateTime" class="created">January 17, 2012</li>
 sub _guess_by_dc_created
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//li[@property="dc:date dc:created"]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//li[@property="dc:date dc:created"]' ) )
     {
         return $node->attr( 'content' );
     }
@@ -173,9 +173,9 @@ sub _guess_by_dc_created
 # <meta name="item-publish-date" content="Wed, 28 Dec 2011 17:39:00 GMT" />
 sub _guess_by_meta_publish_date
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//meta[@name="item-publish-date"]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//meta[@name="item-publish-date"]' ) )
     {
         return $node->attr( 'content' );
     }
@@ -184,9 +184,9 @@ sub _guess_by_meta_publish_date
 # <p class="storydate">Tue, Dec 6th 2011 7:28am</p>
 sub _guess_by_storydate
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//p[@class="storydate"]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//p[@class="storydate"]' ) )
     {
         return $node->as_text;
     }
@@ -195,9 +195,9 @@ sub _guess_by_storydate
 # <span class="date" data-time="1326839460">Jan 17, 2012 10:31 pm UTC</span>
 sub _guess_by_datatime
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//span[@class="date" and @data-time]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//span[@class="date" and @data-time]' ) )
     {
         return $node->attr( 'data-time' );
     }
@@ -206,9 +206,9 @@ sub _guess_by_datatime
 # <time datetime="2012-06-06" pubdate="foo" />
 sub _guess_by_datetime_pubdate
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//time[@datetime and @pubdate]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//time[@datetime and @pubdate]' ) )
     {
         return $node->attr( 'datetime' );
     }
@@ -217,7 +217,7 @@ sub _guess_by_datetime_pubdate
 # look for a date in the story url
 sub _guess_by_url
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
     my $url = $story->{ url };
     my $redirect_url = $story->{ redirect_url } || $url;
@@ -237,9 +237,9 @@ sub _guess_by_url
 # look for any element with a class='date' attribute
 sub _guess_by_class_date
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    if ( my $node = _find_first_node( $xpath, '//*[@class="date"]' ) )
+    if ( my $node = _find_first_node( $html_tree, '//*[@class="date"]' ) )
     {
         return $node->as_text;
     }
@@ -249,7 +249,7 @@ sub _guess_by_class_date
 # look for any month name followed by something that looks like a date
 sub _guess_by_date_text
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
     my $month_names = [ qw/january february march april may june july august september october november december/ ];
 
@@ -270,13 +270,13 @@ sub _guess_by_date_text
 # if _guess_by_url returns a date, use _guess_by_date_text if the days agree
 sub _guess_by_url_and_date_text
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
-    my $url_date = _guess_by_url( $story, $html, $xpath );
+    my $url_date = _guess_by_url( $story, $html, $html_tree );
 
     return if ( !$url_date );
 
-    my $text_date = _make_unix_timestamp( _guess_by_date_text( $story, $html, $xpath ) );
+    my $text_date = _make_unix_timestamp( _guess_by_date_text( $story, $html, $html_tree ) );
 
     if ( ( $text_date > $url_date ) and ( ( $text_date - $url_date ) < 86400 ) )
     {
@@ -293,7 +293,7 @@ sub _guess_by_url_and_date_text
 # track of the '_guess_by_existing_story_date' method
 sub _guess_by_existing_story_date
 {
-    my ( $story, $html, $xpath ) = @_;
+    my ( $story, $html, $html_tree ) = @_;
 
     return $story->{ publish_date };
 }
@@ -329,13 +329,13 @@ sub guess_timestamp($$$;$)
 {
     my ( $db, $story, $html, $use_threshold ) = @_;
 
-    my $xpath = _get_xpath( $html );
+    my $html_tree = _get_html_tree( $html );
 
     my $story_timestamp = _make_unix_timestamp( $story->{ publish_date } );
 
     for my $date_guess_function ( @{ $_date_guess_functions } )
     {
-        if ( my $timestamp = _make_unix_timestamp( $date_guess_function->{ function }->( $story, $html, $xpath ) ) )
+        if ( my $timestamp = _make_unix_timestamp( $date_guess_function->{ function }->( $story, $html, $html_tree ) ) )
         {
             if ( $use_threshold && ( abs( $timestamp - $story_timestamp ) < ( $_date_guess_threshhold * 86400 ) ) )
             {
@@ -372,10 +372,10 @@ sub test_date_parsers
     {
         if ( my $test = $date_guess_function->{ test } )
         {
-            my $xpath = _get_xpath( $test );
+            my $html_tree = _get_html_tree( $test );
 
             my $story = { url => $test };
-            my $timestamp = _make_unix_timestamp( $date_guess_function->{ function }->( $story, $test, $xpath ) );
+            my $timestamp = _make_unix_timestamp( $date_guess_function->{ function }->( $story, $test, $html_tree ) );
 
             if ( defined( $timestamp ) and ( $timestamp ne $date_guess_function->{ expected } ) )
             {
