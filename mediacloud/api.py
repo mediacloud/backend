1b9f3680718da330aa3b895751556b5d804c7bb1
Rahul Bhargava <rahulb@media.mit.edu>
2013-05-29 15:06:17 -0400

    prep to support multiple methods

--- mediacloud/api.py
+++ mediacloud/api.py
@@ -10,7 +10,7 @@ class MediaCloud(object):
 
     VERSION = "0.1"
 
-    API_URL = "http://amanda.law.harvard.edu/admin/stories/stories_query_json"
+    API_URL = "http://amanda.law.harvard.edu/admin/stories/"
 
     DEFAULT_STORY_COUNT = 25
 
@@ -30,7 +30,7 @@ class MediaCloud(object):
             f = open('mediacloud/test/fixtures/stories_15_since_'+str(story_id)+'.json','r');
             content = f.read()
             return self._parseJsonResults(content)
-        return self._queryJson( {'last_stories_id': story_id, 'story_count':count, 'raw_1st_download':fetch_raw_text} )
+        return self._queryJson('stories_query_json', {'last_stories_id': story_id, 'story_count':count, 'raw_1st_download':fetch_raw_text} )
         
     def recentStories(self, story_count=DEFAULT_STORY_COUNT,  fetch_raw_text=False):
         '''
@@ -40,7 +40,7 @@ class MediaCloud(object):
             f = open('mediacloud/test/fixtures/stories_30_within_last_day.json','r');
             content = f.read()
             return self._parseJsonResults(content)
-        return self._queryJson( {'story_count': story_count, 'raw_1st_download':fetch_raw_text} )
+        return self._queryJson('stories_query_json', {'story_count': story_count, 'raw_1st_download':fetch_raw_text} )
 
     def storyDetail(self, story_id,  fetch_raw_text=False):
         '''
@@ -50,9 +50,9 @@ class MediaCloud(object):
             f = open('mediacloud/test/fixtures/story_'+str(story_id)+'.json','r');
             content = f.read()
             return self._parseJsonResults(content)[0]
-        return self._queryJson( {'start_stories_id': story_id, 'story_count': 1, 'raw_1st_download':fetch_raw_text} )[0]
+        return self._queryJson('stories_query_json', {'start_stories_id': story_id, 'story_count': 1, 'raw_1st_download':fetch_raw_text} )[0]
   
-    def _queryJson(self, params):
+    def _queryJson(self, method, params):
         '''
         Call this to make a JSON query to the MC server and return a python object 
         with results
@@ -61,21 +61,22 @@ class MediaCloud(object):
             params['raw_1st_download'] = 1
         else:
             params['raw_1st_download'] = 0
-        return self._parseJsonResults( self._query(params) )
+        return self._parseJsonResults( self._query(method, params) )
   
-    def _query(self, params):
+    def _query(self, method, params):
         '''
         Make an authenticated request to MediaCloud with the URL params passed in
         '''
         auth_handler = urllib2.HTTPBasicAuthHandler()
+        uri = self.API_URL + method
         auth_handler.add_password(realm='Media Cloud Admin',
-                    uri= self.API_URL,
+                    uri= uri,
                     user= self._api_user,
                     passwd= self._api_pass)
         opener = urllib2.build_opener(auth_handler)
         urllib2.install_opener(opener)
-        url_handle = urllib2.urlopen(self.API_URL, urllib.urlencode(params))
-        #print self.API_URL + "?" + urllib.urlencode(params)
+        url_handle = urllib2.urlopen(uri, urllib.urlencode(params))
+        #print uri + "?" + urllib.urlencode(params)
         return url_handle.read()
   
     def _parseJsonResults(self, json_text):
