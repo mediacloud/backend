9342a43b095ec02f50f4bf37556660fd155292c8
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-06-05 22:59:50 +0000

    add temporary exception to not test ExtUtils::MakeMaker because of a reported bug

--- install_modules_outside_of_carton.sh
+++ install_modules_outside_of_carton.sh
@@ -21,6 +21,11 @@ else
 
 fi
 
+# FIXME Install ExtUtils::MakeMaker (a Carton dependency) separately
+# without testing it because t/meta_convert.t fails on some machines
+# (https://rt.cpan.org/Public/Bug/Display.html?id=85861)
+$CPANM --notest ExtUtils::MakeMaker
+
 $CPANM Carton~0.9.15
 $CPANM List::MoreUtils
 $CPANM Devel::NYTProf
