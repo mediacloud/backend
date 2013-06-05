3469dc6c86b1149eb6f8b44deee2bb8bf78fd599
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-06-05 20:38:29 +0000

    Add ubunut missing package: gawk.
    Which is needed by google's compact-language-detector_0.1-1_amd64.deb

--- install_scripts/install_mediacloud_system_package_dependencies.sh
+++ install_scripts/install_mediacloud_system_package_dependencies.sh
@@ -65,6 +65,7 @@ else
     # assume Ubuntu
     sudo apt-get --assume-yes install \
         expat libexpat1-dev libxml2-dev \
+        gawk \
         postgresql-server-dev-all postgresql-client libdb-dev libtest-www-mechanize-perl libtidy-dev \
         libopengl-perl libgraph-writer-graphviz-perl libgraphviz-perl graphviz graphviz-dev graphviz-doc libgraphviz-dev \
         libyaml-syck-perl liblist-allutils-perl liblist-moreutils-perl libreadonly-perl libreadonly-xs-perl curl \
