e2c025274fc514e4ac331181b8a9487c2a1fab84
Rahul Bhargava <rahulb@media.mit.edu>
2013-06-13 22:10:12 -0400

    release 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='mediacloud',
-  version='0.2',
+  version='0.3',
   description='MediaCloud API Client Library',
   author='Rahul Bhargava',
   author_email='rahulb@media.mit.edu',
