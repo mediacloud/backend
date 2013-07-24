542ee2698106099687970138fcbcc4d13ff8b035
Hal Roberts <hroberts@cyber.law.harvard.edu>
2013-07-24 05:39:49 -0700

    look in /usr/lib/jvm/java-6-sdk as well, since that's where the old ubuntu has the sdk; also print the error message regardless of platform

--- script/set_java_home.sh
+++ script/set_java_home.sh
@@ -7,21 +7,25 @@ if [ `uname` == 'Darwin' ]; then
 
     # Mac OS X
     JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/
-
-    if     [ ! -d $JAVA_HOME ] \
-        || [ ! -f $JAVA_HOME/Commands/javac ] \
-        || [ ! -f $JAVA_HOME/Headers/jni.h ] \
-        || [ ! -f $JAVA_HOME/Libraries/libjvm.dylib ]; then
-
-        echo "Proper Java deployment was not found in $JAVA_HOME."
-        echo "Please download and install Java for OS X Developer Package from"
-        echo "https://developer.apple.com/downloads/ or via the Software Update."
-        exit 1
-    fi
-
 else
 
     # Assume Ubuntu
     JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
-
+    if [ ! -d $JAVA_HOME ]; then
+        JAVA_HOME=/usr/lib/jvm/java-6-sun
+    fi
+    
 fi
+
+if     [ ! -d $JAVA_HOME ] \
+    && [ ! -f $JAVA_HOME/Commands/javac ] \
+    && [ ! -f $JAVA_HOME/Headers/jni.h ] \
+    && [ ! -f $JAVA_HOME/Libraries/libjvm.dylib ]; then
+
+    echo "Proper Java deployment was not found in $JAVA_HOME."
+    echo "Please download and install Java. "
+    echo "For ubuntu, install openjdk package.  For OS X install OS X Developer Package from"
+    echo "https://developer.apple.com/downloads/ or via the Software Update."
+    exit 1
+fi
+
