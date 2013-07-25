eff5d0ccd37d23029135e80397ec14d9041cd0df
Hal Roberts <hroberts@cyber.law.harvard.edu>
2013-07-24 20:03:19 -0700

    create new database connection for each connection to avoid connections stuck in a bad transaction

--- lib/MediaWords/Model/DBIS.pm
+++ lib/MediaWords/Model/DBIS.pm
@@ -18,19 +18,19 @@ sub new
 
     my @info = @{ $self->{ connect_info } || [] };
 
-    # TODO replace this with  MediaWords::DB::connect_to_db();
-    $self->{ dbis } = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info )
-      or die DBIx::Simple::MediaWords->error;
-
-    $self->{ dbis }->dbh->{ RaiseError } = 1;
-
     return $self;
 }
 
 sub dbis
 {
 
-    return $_[ 0 ]->{ dbis };
+    # TODO replace this with  MediaWords::DB::connect_to_db();
+    my $db = DBIx::Simple::MediaWords->connect( MediaWords::DB::connect_info )
+      or die DBIx::Simple::MediaWords->error;
+
+    $db->dbh->{ RaiseError } = 1;
+
+    return $db;
 }
 
 1;
