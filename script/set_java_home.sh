f405dcfb1649a81b1f3b1edfb671192c153aef73
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-07-25 04:02:22 +0300

    Properly check for JDK on both older and newer Ubuntus, print out a more explanatory message when not found
    This reverts commit 542ee2698106099687970138fcbcc4d13ff8b035.

--- script/set_java_home.sh
+++ script/set_java_home.sh
@@ -6,26 +6,43 @@
 if [ `uname` == 'Darwin' ]; then
 
     # Mac OS X
-    JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/
+    POSSIBLE_JDK_PATH=/System/Library/Frameworks/JavaVM.framework/
+
+    if     [ -d "$POSSIBLE_JDK_PATH" ] \
+        && [ -f "$POSSIBLE_JDK_PATH/Commands/javac" ] \
+        && [ -f "$POSSIBLE_JDK_PATH/Headers/jni.h" ] \
+        && [ -f "$POSSIBLE_JDK_PATH/Libraries/libjvm.dylib" ]; then
+
+        JAVA_HOME="$POSSIBLE_JDK_PATH"
+
+    else
+        echo "Proper Java deployment was not found in $POSSIBLE_JDK_PATH."
+        echo "Please download and install Java for OS X Developer Package from"
+        echo "https://developer.apple.com/downloads/ or via the Software Update."
+        exit 1
+    fi
+
 else
 
-    # Assume Ubuntu
-    JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
-    if [ ! -d $JAVA_HOME ]; then
-        JAVA_HOME=/usr/lib/jvm/java-6-sun
+    # Ubuntu
+    declare -a POSSIBLE_JDK_PATHS=(
+        /usr/lib/jvm/java-7-openjdk-amd64/  # newer Ubuntu
+        /usr/lib/jvm/java-6-sun             # older Ubuntu
+    )
+
+    for path in ${POSSIBLE_JDK_PATHS[@]}; do
+        if     [ -d "$path" ]
+            && [ ! -f "$path/bin/javac" ]
+            && [ ! -f "$path/include/jni.h" ]; then
+            
+            JAVA_HOME="$path"
+        fi
+    done
+    if [ -z "$JAVA_HOME" ]; then
+        echo "Proper Java deployment was not found anywhere."
+        echo "Please download and install Java for OS X Developer Package by running:"
+        echo "    apt-get install openjdk-7-jdk"
+        exit 1
     fi
-    
+
 fi
-
-if     [ ! -d $JAVA_HOME ] \
-    && [ ! -f $JAVA_HOME/Commands/javac ] \
-    && [ ! -f $JAVA_HOME/Headers/jni.h ] \
-    && [ ! -f $JAVA_HOME/Libraries/libjvm.dylib ]; then
-
-    echo "Proper Java deployment was not found in $JAVA_HOME."
-    echo "Please download and install Java. "
-    echo "For ubuntu, install openjdk package.  For OS X install OS X Developer Package from"
-    echo "https://developer.apple.com/downloads/ or via the Software Update."
-    exit 1
-fi
-
