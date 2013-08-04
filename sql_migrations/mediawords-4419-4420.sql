2f5ac7c45aff90872d094dce034ce9d82b0588b5
Hal Roberts <hroberts@cyber.law.harvard.edu>
2013-08-03 20:05:00 -0700

    formatting

--- sql_migrations/mediawords-4419-4420.sql
+++ sql_migrations/mediawords-4419-4420.sql
@@ -21,7 +21,7 @@ create table controversy_dump_tags (
     tags_id                     int not null references tags
 );
 
-alter table controversy_dump_time_slices add  tags_id int references tags;
+alter table controversy_dump_time_slices add tags_id int references tags;
 
 alter table cd.controversy_links_cross_media drop media_name;
 alter table cd.controversy_links_cross_media drop ref_media_name;
