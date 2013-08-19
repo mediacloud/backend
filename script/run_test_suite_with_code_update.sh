39a7737904f8197e3c69240f00adf549b5110616
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-08-19 04:34:46 -0400

    update all local branches

--- script/run_test_suite_with_code_update.sh
+++ script/run_test_suite_with_code_update.sh
@@ -9,5 +9,15 @@ cd $working_dir
 
 cd ..
 
-git pull
-./script/run_test_suite.sh 
+# Fetch and update all local branches
+git fetch --all
+git pull --all
+STARTING_BRANCH=$(git branch | grep '\*' | sed 's/^.//')
+for i in $(git branch | sed 's/^.//'); do
+	git checkout $i
+	git pull || exit 1
+done
+git checkout $STARTING_BRANCH
+
+./script/run_test_suite.sh
+
