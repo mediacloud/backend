69a9a9ca465d2e8f4ef68f49b1e74994ace21584
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-08-19 11:55:45 +0300

    separate code updating and test suite

--- script/run_test_suite_with_code_update.sh
+++ script/run_test_suite_with_code_update.sh
@@ -3,21 +3,7 @@
 set -u
 set -o  errexit
 
-working_dir=`dirname $0`
-
-cd $working_dir
-
-cd ..
-
-# Fetch and update all local branches
-git fetch --all
-git pull --all
-STARTING_BRANCH=$(git branch | grep '\*' | sed 's/^.//')
-for i in $(git branch | sed 's/^.//'); do
-	git checkout $i
-	git pull || exit 1
-done
-git checkout $STARTING_BRANCH
-
+cd `dirname $0`/../
+source ./script/update_code_from_git.sh
 ./script/run_test_suite.sh
 
