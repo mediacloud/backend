432d65bf3b0896ae22c8f323d9f2c2a942d4ee94
Ubuntu <ubuntu@ip-10-144-74-106.ec2.internal>
2013-08-23 15:24:16 +0000

    add python-pip

--- install_scripts/install_mediacloud_system_package_dependencies.sh
+++ install_scripts/install_mediacloud_system_package_dependencies.sh
@@ -69,7 +69,7 @@ else
         postgresql-server-dev-all postgresql-client libdb-dev libtest-www-mechanize-perl libtidy-dev \
         libopengl-perl libgraph-writer-graphviz-perl libgraphviz-perl graphviz graphviz-dev graphviz-doc libgraphviz-dev \
         libyaml-syck-perl liblist-allutils-perl liblist-moreutils-perl libreadonly-perl libreadonly-xs-perl curl \
-        python python-dev \
+        python python-dev python-pip \
         build-essential make gcc g++ cpanminus perl-doc liblocale-maketext-lexicon-perl openjdk-7-jdk
 
     # Install CLD separately
