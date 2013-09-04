2783b07e853c660feb172c4bfb58249c790e2a70
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-09-04 14:42:16 -0400

    Add support to fetch stories_ids

--- python_scripts/solr_query_immigrant.py
+++ python_scripts/solr_query_immigrant.py
@@ -6,6 +6,7 @@ import csv
 import sys
 import pysolr
 import dateutil.parser
+import copy
 
 def get_word_counts( solr, query, date_str, count=1000 ) :
     date = dateutil.parser.parse( date_str )
@@ -52,9 +53,9 @@ def counts_to_db_style( counts ) :
                       
 
 def solr_connection() :
-    return pysolr.Solr('http://localhost:8983/solr/')
+    return pysolr.Solr('http://localhost:8983/solr/', timeout=3600)
 
-def query_top25msm_for_range( solr, query_specific_fq_params ):
+def query_top25msm_for_range_title_only( solr, query_specific_fq_params ):
     common_fq_params = ['field_type:st', 'publish_date:[2012-01-01T00:00:00Z TO NOW]']
     fq_params = common_fq_params + query_specific_fq_params
 
@@ -74,35 +75,113 @@ def query_top25msm_for_range( solr, query_specific_fq_params ):
     counts = dict(zip(facet_counts[0::2],facet_counts[1::2]))
     return counts
 
-solr = solr_connection()
+def get_stories_ids_for_query( solr, query_specific_fq_params ):
+    common_fq_params = ['field_type:st', 'publish_date:[2012-01-01T00:00:00Z TO NOW]']
+    fq_params = common_fq_params + query_specific_fq_params
+
+    print fq_params
+
+    rows = 100000
+
+    result = solr.search( join_query, **{
+            # 'facet':"true",
+            # 'facet.range.start':'2012-01-01T00:00:00Z',
+            # 'facet.range':'publish_date',
+            # 'facet.range.end':['NOW','NOW'],
+            # 'facet.range.gap':'+1MONTH',
+            'fq': fq_params,
+            'fl': 'stories_id',
+            'rows': rows
+            })
+
+    assert result.hits <= rows
+
+    stories_ids = set([ doc['stories_id'] for doc in result.docs ])
+
+    return stories_ids
+
+
+def query_top25msm_for_range_body( solr, query_specific_fq_params ):
+    common_fq_params = ['field_type:ss', 'publish_date:[2012-01-01T00:00:00Z TO NOW]', "sentence:'illegal immigrant'"]
+
+    join_params = [ '{!join from=stories_id to=stories_id}' + param for param in query_specific_fq_params ]
+    
+    fq_params = common_fq_params + join_params
+
+    print fq_params
+
+    result = solr.search( join_query, **{
+            'facet':"true",
+            'facet.range.start':'2012-01-01T00:00:00Z',
+            'facet.range':'publish_date',
+            'facet.range.end':['NOW','NOW'],
+            'facet.range.gap':'+1MONTH',
+            'fq': fq_params,
+            })
+
+    facet_counts = result.facets['facet_ranges']['publish_date']['counts']
+
+    counts = dict(zip(facet_counts[0::2],facet_counts[1::2]))
+    return counts
+
+def get_stories_ids( solr, query_specific_filters ) :
+    print "start get_stories_ids"
+
+    fetch_queries = copy.deepcopy(query_specific_filters)
+
+    fetch_queries.extend([ map( lambda str: str.replace( "title:", "sentence:" ), query ) for query in fetch_queries ])
+    stories_ids = set()
+    for fetch_query  in fetch_queries:
+        stories_ids_for_query = get_stories_ids_for_query( solr, fetch_query )
+        stories_ids |= stories_ids_for_query
+
+    print "got {0} stories_id's".format( len( stories_ids) )
+
+    return stories_ids
+
+def main():
+    solr = solr_connection()
+
+    #results = query_top25msm_for_range_body( solr, [ '{!join from=stories_id to=stories_id}title:immigrants' ] )
+    #ipdb.set_trace()
+
+    #ipdb.set_trace()
+
+    query_specific_filters = [
+        [  'title:immigrants'  ],
+        [  '-title:illegal',  'title:immigrants' ],
+        [  'title:illegals'  ],
+        [  'title:aliens' , 'title:illegal'],
+        ]
+
+    results = {}
+
+    stories_ids = get_stories_ids( solr, query_specific_filters ) 
+
+    print "got stories_ids"
+
+    exit()
+
+    for query_specific_filter in query_specific_filters:
+        counts = query_top25msm_for_range_body( solr, query_specific_filter )
+        #counts['query'] = str(  query_specific_filter )
+        results[ str( query_specific_filter ) ] = counts
+
+    row_titles = sorted(results.values()[0].keys())
+    row_titles.insert(0, 'query')
+
+    for query in results.keys():
+        results[query]['query'] = query
+
+    with open("/tmp/sample.csv", 'wb') as csvfile:
+        samplewriter = csv.DictWriter(csvfile, row_titles )
+        samplewriter.writeheader()
+        samplewriter.writerows(results.values())
+
+    #print results
+
 join_query = '{!join from=media_id_inner to=media_id}media_sets_id:1'
 
-#ipdb.set_trace()
-
-query_specific_filters = [
-    [  '-title:illegal',  'title:immigrants' ],
-    [  'title:immigrants'  ],
-    [  'title:illegals'  ],
-    [  'title:alien' , 'title:illegal'],
-    ]
-
-results = {}
-for query_specific_filter in query_specific_filters:
-    counts = query_top25msm_for_range( solr, query_specific_filter )
-    #counts['query'] = str(  query_specific_filter )
-    results[ str( query_specific_filter ) ] = counts
-
-row_titles = sorted(results.values()[0].keys())
-row_titles.insert(0, 'query')
-
-for query in results.keys():
-    results[query]['query'] = query
-
-with open("/tmp/sample.csv", 'wb') as csvfile:
-    samplewriter = csv.DictWriter(csvfile, row_titles )
-    samplewriter.writeheader()
-    samplewriter.writerows(results.values())
-    
-#print results
-
+if __name__ == "__main__":
+    main()
 
