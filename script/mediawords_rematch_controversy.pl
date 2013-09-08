a451921e0e30f0bc263debf8b3ee26d4f5a7edeb
Hal Roberts <hroberts@cyber.law.harvard.edu>
2013-09-08 08:37:28 -0700

    uncomment controversy_stories deletion; add --dry option; print out csv of removed stories

--- script/mediawords_rematch_controversy.pl
+++ script/mediawords_rematch_controversy.pl
@@ -17,18 +17,22 @@ use Getopt::Long;
 use MediaWords::CM::Mine;
 use MediaWords::DB;
 use MediaWords::DBI::Controversies;
+use MediaWords::Util::CSV;
 
 sub main
 {
-    my ( $controversy_opt );
+    my ( $controversy_opt, $dry );
 
     binmode( STDOUT, 'utf8' );
     binmode( STDERR, 'utf8' );
     $| = 1;
 
-    Getopt::Long::GetOptions( "controversy=s" => \$controversy_opt ) || return;
+    Getopt::Long::GetOptions(
+        "controversy=s" => \$controversy_opt,
+        "dry!"          => \$dry,
+    ) || return;
 
-    die( "Usage: $0 --controversy < id >" ) unless ( $controversy_opt );
+    die( "Usage: $0 --controversy < id > [ --dry ]" ) unless ( $controversy_opt );
 
     my $db = MediaWords::DB::connect_to_db;
 
@@ -39,6 +43,7 @@ sub main
         my $stories = $db->query( <<END, $controversy->{ controversies_id } )->hashes;
 select s.* from cd.live_stories s where controversies_id = ? order by stories_id desc
 END
+        my $removed_stories = [];
         for my $story ( @{ $stories } )
         {
             print STDERR "rematch story: $story->{ stories_id } - $story->{ title } [ $story->{ url } ]\n";
@@ -46,13 +51,20 @@ END
             {
                 print STDERR "REMOVE\n";
 
-                #                 $db->query( <<END, $story->{ stories_id }, $controversy->{ controversies_id } );
-                # delete from controversy_stories where stories_id = ? and controversies_id = ?
-                # END
+                if ( !$dry )
+                {
+                    $db->query( <<END, $story->{ stories_id }, $controversy->{ controversies_id } );
+delete from controversy_stories where stories_id = ? and controversies_id = ?
+END
+                }
+
+                push( @{ $removed_stories }, $story );
             }
         }
-    }
 
+        print MediaWords::Util::CSV::get_hashes_as_encoded_csv( $removed_stories,
+            [ qw(stories_id title url publish_date media_id) ] );
+    }
 }
 
 main();
