484accd237aca56729672498ef981f922383b9a8
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-09-16 18:45:42 +0300

    example of getting a log path

--- script/mediawords_search_stories.pl
+++ script/mediawords_search_stories.pl
@@ -12,12 +12,17 @@ BEGIN
 use Modern::Perl "2012";
 use MediaWords::CommonLibs;
 use MediaWords::GearmanFunctions::SearchStories;
+use Gearman::JobScheduler;
 
 sub main
 {
     while ( 1 )
     {
-        MediaWords::GearmanFunctions::SearchStories->enqueue_on_gearman();
+        my $gearman_job_id = MediaWords::GearmanFunctions::SearchStories->enqueue_on_gearman();
+        say STDERR "Enqueued Gearman job with ID: $gearman_job_id";
+        my $log_path =
+          Gearman::JobScheduler::log_path_for_gearman_job( 'MediaWords::GearmanFunctions::SearchStories', $gearman_job_id );
+        say STDERR "(the job is writing / will write its log to: $log_path)";
         sleep( 60 );
     }
 }
