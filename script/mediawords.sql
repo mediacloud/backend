5b0200e470345a192c8d9c46adbf286df20a5613
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-09-20 14:10:19 -0400

    Finish merging in master branch sql schema changes.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -65,7 +65,7 @@ DECLARE
     
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4422;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4423;
     
 BEGIN
 
@@ -204,6 +204,24 @@ END;
 $$
 LANGUAGE 'plpgsql'
  ;
+
+CREATE OR REPLACE FUNCTION last_updated_trigger () RETURNS trigger AS
+$$
+   DECLARE
+      path_change boolean;
+   BEGIN
+      -- RAISE NOTICE 'BEGIN ';                                                                                                                            
+
+      IF ( TG_OP = 'UPDATE' ) OR (TG_OP = 'INSERT') then
+
+      	 NEW.db_row_last_updated = now();
+
+      END IF;
+
+      RETURN NEW;
+   END;
+$$
+LANGUAGE 'plpgsql';
 
 create table media (
     media_id            serial          primary key,
@@ -518,11 +536,16 @@ create index media_sets_vectors_added on media_sets ( vectors_added );
 create table media_sets_media_map (
     media_sets_media_map_id     serial  primary key,
     media_sets_id               int     not null references media_sets on delete cascade,    
-    media_id                    int     not null references media on delete cascade
+    media_id                    int     not null references media on delete cascade,
+    db_row_last_updated                timestamp with time zone not null
 );
+
+DROP TRIGGER IF EXISTS media_sets_media_map_last_updated_trigger on media_sets_media_map CASCADE;
+CREATE TRIGGER media_sets_media_map_last_updated_trigger BEFORE INSERT OR UPDATE ON media_sets_media_map FOR EACH ROW EXECUTE PROCEDURE last_updated_trigger() ;
 
 create index media_sets_media_map_set on media_sets_media_map ( media_sets_id );
 create index media_sets_media_map_media on media_sets_media_map ( media_id );
+CREATE index media_sets_media_map_db_row_last_updated on media_sets_media_map ( db_row_last_updated );
 
 CREATE OR REPLACE FUNCTION media_set_sw_data_retention_dates(v_media_sets_id int, default_start_day date, default_end_day date, OUT start_date date, OUT end_date date) AS
 $$
@@ -657,6 +680,7 @@ create table stories (
     publish_date                timestamp       not null,
     collect_date                timestamp       not null,
     full_text_rss               boolean         not null default 'f',
+    db_row_last_updated                timestamp with time zone,
     language                    varchar(3)      null   -- 2- or 3-character ISO 690 language code; empty if unknown, NULL if unset
 );
 
@@ -669,7 +693,10 @@ create index stories_collect_date on stories (collect_date);
 create index stories_title_pubdate on stories(title, publish_date);
 create index stories_md on stories(media_id, date_trunc('day'::text, publish_date));
 create index stories_language on stories(language);
+create index stories_db_row_last_updated on stories( db_row_last_updated );
 
+DROP TRIGGER IF EXISTS stories_last_updated_trigger on stories CASCADE;
+CREATE TRIGGER stories_last_updated_trigger BEFORE INSERT OR UPDATE ON stories FOR EACH ROW EXECUTE PROCEDURE last_updated_trigger() ;
 
 CREATE TYPE download_state AS ENUM ('error', 'fetching', 'pending', 'queued', 'success', 'feed_error');    
 CREATE TYPE download_type  AS ENUM ('Calais', 'calais', 'content', 'feed', 'spider_blog_home', 'spider_posting', 'spider_rss', 'spider_blog_friends_list', 'spider_validation_blog_home','spider_validation_rss','archival_only');    
@@ -905,6 +932,7 @@ create table story_sentences (
        sentence                     text            not null,
        media_id                     int             not null, -- references media on delete cascade,
        publish_date                 timestamp       not null,
+       db_row_last_updated          timestamp with time zone, -- time this row was last updated
        language                     varchar(3)      null      -- 2- or 3-character ISO 690 language code; empty if unknown, NULL if unset
 );
 
@@ -912,9 +940,13 @@ create index story_sentences_story on story_sentences (stories_id, sentence_numb
 create index story_sentences_publish_day on story_sentences( date_trunc( 'day', publish_date ), media_id );
 create index story_sentences_language on story_sentences(language);
 create index story_sentences_media_id    on story_sentences( media_id );
+create index story_sentences_db_row_last_updated    on story_sentences( db_row_last_updated );
 
 ALTER TABLE  story_sentences ADD CONSTRAINT story_sentences_media_id_fkey FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE;
 ALTER TABLE  story_sentences ADD CONSTRAINT story_sentences_stories_id_fkey FOREIGN KEY (stories_id) REFERENCES stories(stories_id) ON DELETE CASCADE;
+
+DROP TRIGGER IF EXISTS story_sentences_last_updated_trigger on story_sentences CASCADE;
+CREATE TRIGGER story_sentences_last_updated_trigger BEFORE INSERT OR UPDATE ON story_sentences FOR EACH ROW EXECUTE PROCEDURE last_updated_trigger() ;
     
 create table story_sentence_counts (
        story_sentence_counts_id     bigserial       primary key,
