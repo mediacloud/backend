5c538303fa068f03f02f24b8d55329b49a1e5520
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-09-26 17:32:00 -0400

    Clean up python code.

--- python_scripts/solr_query_wordcount_timer.py
+++ python_scripts/solr_query_wordcount_timer.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python
 
-#import ipdb
+import ipdb
 #import time
 #import csv
 import sys
 import pysolr
 import dateutil.parser
 
-def get_word_counts( solr, query, date_str, count=1000 ) :
-    date = dateutil.parser.parse( date_str )
+def get_word_counts( solr, query, date_str, num_words=1000 ) :
     documents = []
-    
-    date_str = date.isoformat() + 'Z'
-    date_query = "publish_date:[{0} TO {0}+7DAYS]".format(date_str)
 
-    sys.stderr.write( ' starting fetch for ' + query )
-    sys.stderr.write( "\n");
+    fq = get_fq( query )
+
+    return _get_word_counts_impl( solr, fq, num_words )
+
+def _get_word_counts_impl( solr, fq, num_words ):
 
     facet_field = "includes"
 
-    results = solr.search( query, **{ 
+    num_words = max ( num_words, 5000 )
+
+    query_params = { 
             'facet':"true",
-            "facet.limit":count,
+            "facet.limit": num_words,
             "facet.field": facet_field,
             "facet.method":"enum",
-            "fq": date_query,
-            })
+            "fq": fq,
+            }
+
+    #query_params['fq'] = " AND ".join( query_params['fq'] )
+
+    results = solr.search( '*:*', ** query_params)
+
+    #ipdb.set_trace()
 
     facets = results.facets['facet_fields']['includes']
 
@@ -33,10 +40,36 @@ def get_word_counts( solr, query, date_str, count=1000 ) :
 
     return counts
 
+
+def get_fq(  query ) :
+    start_date = dateutil.parser.parse( query['start_date'] )
+    end_date = dateutil.parser.parse( query['end_date'] )
+
+    date_str_start = start_date.isoformat() + 'Z'
+    date_str_end   = end_date.isoformat() + 'Z'
+
+    if query['start_date'] == query['end_date']:
+        date_query = "publish_date:[{0} TO {1}+7DAYS]".format(date_str_start, date_str_end)
+    else:
+        date_query = "publish_date:[{0} TO {1}]".format(date_str_start, date_str_end)
+
+    media_sets_ids = query[ 'media_sets_ids' ]
+
+    media_sets_query = 'media_sets_id:({0})'.format( " OR ".join([ "{:d}".format(id) for id in media_sets_ids ]) )
+    
+    #sys.stderr.write( media_sets_query )
+    #sys.stderr.write( date_query )
+
+    ret =  [date_query, media_sets_query]
+
+    return ret
+
 def counts_to_db_style( counts ) :
     ret = []
     total_words = sum( counts.values() )
 
+    total_words = max( total_words, 1 )
+    
     stem_count_factor = 1
 
     for word,count in counts.iteritems() :
@@ -54,9 +87,17 @@ def counts_to_db_style( counts ) :
 def solr_connection() :
     return pysolr.Solr('http://localhost:8983/solr/')
 
+def main():
 
-#results = get_word_counts(solr_connection(), 'sentence:the', '2013-08-10', count=100);
+    solr = solr_connection()
+    fq = None
+    
+    #publish_date:[2013-09-16T00:00:00Z TO 2013-09-16T00:00:00Z+7DAYS] AND media_sets_id:(5)
+    fq = 'publish_date:[2013-09-16T00:00:00Z TO *]'
 
-#print results
-#print results.facets['facet_fields']['includes']
+    counts = _get_word_counts_impl( solr, fq, 1000 )
+    print counts
 
+
+if __name__ == "__main__":
+    main()
