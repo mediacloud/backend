0879762a38712f8be8fb2a5d95296155b9ee1fd2
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-10-01 16:36:44 -0400

    Add joblib module.

--- python_scripts/pip_installs.sh
+++ python_scripts/pip_installs.sh
@@ -3,5 +3,6 @@
 sudo pip install pysolr[tomcat]==3.0.6
 sudo pip install dateutils
 sudo pip install ipdb
-sudo pip install --upgrade lxml
+sudo pip install lxml
 sudo pip install flask
+sudo pip install joblib
