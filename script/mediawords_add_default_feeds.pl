be6de6ab2d54ee35385902cd09ecaea24e620219
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-10-07 00:18:53 +0300

    note about Gearman log path

--- script/mediawords_add_default_feeds.pl
+++ script/mediawords_add_default_feeds.pl
@@ -20,9 +20,21 @@ sub main
     {
         my $gearman_job_id = MediaWords::GearmanFunction::AddDefaultFeeds->enqueue_on_gearman();
         say STDERR "Enqueued Gearman job with ID: $gearman_job_id";
-        my $log_path = Gearman::JobScheduler::log_path_for_gearman_job( MediaWords::GearmanFunction::AddDefaultFeeds->name(),
-            $gearman_job_id );
-        say STDERR "(the job is writing / will write its log to: $log_path)";
+
+        eval {
+            # The following call might fail if the job takes some time to start,
+            # so consider adding:
+            #     sleep(1);
+            # before calling log_path_for_gearman_job()
+            my $log_path =
+              Gearman::JobScheduler::log_path_for_gearman_job( MediaWords::GearmanFunction::AddDefaultFeeds->name(),
+                $gearman_job_id );
+            say STDERR "The job is writing its log to: $log_path";
+        };
+        if ( $@ )
+        {
+            say STDERR "The job probably hasn't started yet, so I don't know where does the log reside";
+        }
 
         sleep( 60 );
     }
