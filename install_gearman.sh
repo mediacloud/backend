e08c8b9ebc65e8051dca15509486e30e1c38fb30
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2013-10-09 14:37:41 -0400

    Fix typo

--- install_gearman.sh
+++ install_gearman.sh
@@ -2,4 +2,4 @@
 
 source ./script/set_perl_brew_environment.sh
 cpanm -L local Module::Install
-panm -L local git://github.com/pypt/p5-Gearman-JobScheduler.git@0.07
+cpanm -L local git://github.com/pypt/p5-Gearman-JobScheduler.git@0.07
