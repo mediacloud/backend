586a62b7bb905e5a55fc332dcb9a85e2d1b0f70c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-10-23 12:01:27 +0300

    add 10gen's key before trying to add MongoDB repository

--- script/vagrant/provision_root.sh
+++ script/vagrant/provision_root.sh
@@ -38,6 +38,7 @@ grub-install /dev/sda
 update-grub
 
 echo "Adding MongoDB 10gen repository and installing MongoDB..."
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' \
 	> /etc/apt/sources.list.d/mongodb.list
 
