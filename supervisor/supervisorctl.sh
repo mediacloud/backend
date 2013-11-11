4dbb9958a2c5151fcab2017d4bb5b18afd59fea1
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-11-11 16:18:58 +0200

    correct way to determine where the script is located

--- supervisor/supervisorctl.sh
+++ supervisor/supervisorctl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source "supervisor_is_up_to_date.inc.sh"
+PWD="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$PWD/supervisor_is_up_to_date.inc.sh"
 validate_supervisor_version
 
 supervisorctl \
--- supervisor/supervisord.sh
+++ supervisor/supervisord.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-DIR=`dirname -- "$0"`
-cd "$DIR"
-
-source "supervisor_is_up_to_date.inc.sh"
+PWD="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$PWD/supervisor_is_up_to_date.inc.sh"
 validate_supervisor_version
 
+cd "$PWD/"
 supervisord -c supervisord.conf
