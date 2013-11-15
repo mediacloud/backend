51d6a572ed49c2853b9b104e0a5bc32871c21369
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-11-15 21:09:56 +0200

    install Perl 5.18.1 (latest stable version as of Nov 15, 2013) instead of Perl 5.14.2

--- install_mc_perlbrew.sh
+++ install_mc_perlbrew.sh
@@ -17,8 +17,8 @@ set +u
 source ~/perl5/perlbrew/etc/bashrc
 set -u
 perlbrew init
-nice perlbrew install perl-5.14.2 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN
-perlbrew switch perl-5.14.2
+nice perlbrew install perl-5.18.1 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN
+perlbrew switch perl-5.18.1
 perlbrew install-cpanm
 perlbrew lib create mediacloud
-perlbrew switch perl-5.14.2@mediacloud
+perlbrew switch perl-5.18.1@mediacloud
