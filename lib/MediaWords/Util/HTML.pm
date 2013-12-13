f4a08ecbfae669003f0a99b87fa9115845f9b0f1
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-12-13 18:02:10 +0200

    Use Memoize + Tie::Cache to cache html_strip() subroutine which is likely to be called multiple times with the same data from the extractor

--- lib/MediaWords/Util/HTML.pm
+++ lib/MediaWords/Util/HTML.pm
@@ -18,8 +18,21 @@ use HTML::Entities qw( decode_entities  );
 use Devel::Peek qw(Dump);
 use Encode;
 use List::Util qw(min);
+use Memoize;
+use Tie::Cache;
 
 use constant MAX_SEGMENT_LENGTH => 14000;
+
+
+# Cache output of html_strip() because it is likely that it is going to be called multiple times from extractor
+my %_html_strip_cache;
+tie %_html_strip_cache, 'Tie::Cache', {
+    MaxCount => 1024,           # 1024 entries
+    MaxBytes => 1024 * 1024     # 1 MB of space
+};
+
+memoize 'html_strip', SCALAR_CACHE => [HASH => \%_html_strip_cache];
+
 
 # provide a procedural interface to HTML::Strip
 # use HTML::StripPP instead of HTML::Strip b/c HTML::Strip mucks up the encoding
