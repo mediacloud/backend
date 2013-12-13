e03bac02ddb40f4ea74373e09c4d329b325fe87c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-12-13 16:01:22 +0200

    Cleanup html_strip()

--- lib/MediaWords/Util/HTML.pm
+++ lib/MediaWords/Util/HTML.pm
@@ -19,65 +19,51 @@ use Devel::Peek qw(Dump);
 use Encode;
 use List::Util qw(min);
 
+use constant MAX_SEGMENT_LENGTH => 14000;
+
 # provide a procedural interface to HTML::Strip
 # use HTML::StripPP instead of HTML::Strip b/c HTML::Strip mucks up the encoding
-sub html_strip
+sub html_strip($)
 {
+    my $html = shift;
 
-    # use HTML::Strip;
-
-    # my $html_text = shift;
-    # my $hs = HTML::Strip->new();
-    # my $text = $hs->parse($html_text);
-    # $hs->eof();
-    #
-    # return $text;
-
-    #
-    # TODO HACK to prevent StripPP from segfaulting.
-    # This appears to be necessary on Perl 5.8 but not on 5.10.
-    #
-
-    if ( !defined( $_[ 0 ] ) )
+    unless ( defined( $html ) )
     {
         return '';
     }
 
-    if ( ( length( $_[ 0 ] ) > 14000 ) )
-    {
-        my $max_segment_length = 14000;
+    my $html_length = length($html);
 
+    if ( ( $html_length > MAX_SEGMENT_LENGTH ) )
+    {
         my $str_pos = 0;
 
         my $ret;
 
-        while ( $str_pos < length( $_[ 0 ] ) )
+        while ( $str_pos < $html_length )
         {
-
-            #perldoc: index STR,SUBSTR,POSITION
-
-            my $new_line_pos = index( $_[ 0 ], "\n", $str_pos + 5000 );
+            my $new_line_pos = index( $html, "\n", $str_pos + 5000 );
 
             my $new_line_segment_length;
 
             if ( $new_line_pos == -1 )
             {
-                $new_line_segment_length = $max_segment_length;
+                $new_line_segment_length = MAX_SEGMENT_LENGTH;
             }
             else
             {
                 $new_line_segment_length = $new_line_pos - $str_pos;
             }
 
-            my $segment_length = min( $new_line_segment_length, $max_segment_length );
+            my $segment_length = min( $new_line_segment_length, MAX_SEGMENT_LENGTH );
 
-            my $token_end_pos = index( $_[ 0 ], ">", $str_pos + 5000 ) + 1;
+            my $token_end_pos = index( $html, '>', $str_pos + 5000 ) + 1;
 
             my $token_boundary_segment_length;
 
             if ( $token_end_pos == 0 )
             {
-                $token_boundary_segment_length = $max_segment_length;
+                $token_boundary_segment_length = MAX_SEGMENT_LENGTH;
             }
             else
             {
@@ -86,19 +72,14 @@ sub html_strip
 
             $segment_length = min( $segment_length, $token_boundary_segment_length );
 
-            #say STDERR "segment_length $segment_length";
-            #$DB::signal = 2;
-
-            #say STDERR "segment \n" .  substr ($_[ 0 ], $str_pos, $segment_length);
-
-            $ret .= HTML::StripPP::strip( substr( $_[ 0 ], $str_pos, $segment_length ) );
+            $ret .= HTML::StripPP::strip( substr( $html, $str_pos, $segment_length ) );
             $str_pos += $segment_length;
         }
 
         return $ret;
     }
 
-    return HTML::StripPP::strip( $_[ 0 ] ) || '';
+    return HTML::StripPP::strip( $html ) || '';
 }
 
 1;
