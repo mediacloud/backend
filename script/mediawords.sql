cd54c54d39c2e62522983b08c5ecbc588776f58b
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2013-12-28 17:16:53 +0200

    Correctly handle cases when a record to "stories" / "media_sets" is being INSERTed and thus the "old record" doesn't exist

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -65,7 +65,7 @@ DECLARE
     
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4432;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4433;
     
 BEGIN
 
@@ -236,10 +236,23 @@ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION update_stories_updated_time_by_stories_id_trigger () RETURNS trigger AS
 $$
-   DECLARE
-      path_change boolean;
-   BEGIN
-	UPDATE stories set db_row_last_updated = now() where stories_id = OLD.stories_id;
+    DECLARE
+        path_change boolean;
+        reference_stories_id integer default null;
+    BEGIN
+
+        IF TG_OP = 'INSERT' THEN
+            -- The "old" record doesn't exist
+            reference_stories_id = NEW.stories_id;
+        ELSIF ( TG_OP = 'UPDATE' ) OR (TG_OP = 'DELETE') THEN
+            reference_stories_id = OLD.stories_id;
+        ELSE
+            RAISE EXCEPTION 'Unconfigured operation: %', TG_OP;
+        END IF;
+
+        UPDATE stories
+        SET db_row_last_updated = now()
+        WHERE stories_id = reference_stories_id;
 	RETURN NULL;
    END;
 $$
@@ -247,12 +260,26 @@ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION update_stories_updated_time_by_media_id_trigger () RETURNS trigger AS
 $$
-   DECLARE
-      path_change boolean;
-   BEGIN
-	UPDATE stories set db_row_last_updated = now() where media_id = OLD.media_id;
-	RETURN NULL;
-   END;
+    DECLARE
+        path_change boolean;
+        reference_media_id integer default null;
+    BEGIN
+
+        IF TG_OP = 'INSERT' THEN
+            -- The "old" record doesn't exist
+            reference_media_id = NEW.media_id;
+        ELSIF ( TG_OP = 'UPDATE' ) OR (TG_OP = 'DELETE') THEN
+            reference_media_id = OLD.media_id;
+        ELSE
+            RAISE EXCEPTION 'Unconfigured operation: %', TG_OP;
+        END IF;
+
+        UPDATE stories
+        SET db_row_last_updated = now()
+        WHERE media_id = reference_media_id;
+
+        RETURN NULL;
+    END;
 $$
 LANGUAGE 'plpgsql';
 
