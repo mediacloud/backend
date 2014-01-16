a4e28ea1a11a09e2eb6f32762ce1f6fcdfb67049
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-01-17 00:19:54 +0200

    Made the CRF "ModelRunner" Java class non-static in order to 1) isolate implementation details from Perl code, and 2) make the memory usage more isolated

--- java/CrfUtils/src/test/java/org/mediacloud/crfutils/ModelRunnerTest.java
+++ java/CrfUtils/src/test/java/org/mediacloud/crfutils/ModelRunnerTest.java
@@ -1,6 +1,5 @@
 package org.mediacloud.crfutils;
 
-import cc.mallet.fst.CRF;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import junit.framework.Assert;
@@ -10,16 +9,16 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class ModelRunnerTest extends TestCase {
-    
+
     private final String test_extractor_model_filename = "test_extractor_model";
-    
+
     private final String extractor_model_path;
-    
+
     private final String test_input;
     private final String test_output;
-    
-    private static String getResourceAsString(String filename)
-    {
+
+    private static String getResourceAsString(String filename) {
+
         String data;
 
         try {
@@ -28,42 +27,42 @@ public class ModelRunnerTest extends TestCase {
             System.err.println("File read error: " + e.getMessage());
             data = null;
         }
-        
+
         return data;
     }
 
     public ModelRunnerTest() throws IOException {
+
         this.extractor_model_path = ModelRunnerTest.class.getResource(test_extractor_model_filename).getPath();
-        
+
         this.test_input = getResourceAsString("test_input.txt");
         this.test_output = getResourceAsString("test_output.txt");
     }
-    
+
     public void testCRF() throws IOException, FileNotFoundException, ClassNotFoundException, Exception {
-        
-        CRF crf = ModelRunner.readModel( extractor_model_path );
-        
-        String[] results = ModelRunner.runModelString( this.test_input, crf );
+
+        ModelRunner mr = new ModelRunner(extractor_model_path);
+
+        String[] results = mr.runModelString(this.test_input);
         String resultsString = StringUtils.join(results, "\n");
-                
+
         Assert.assertEquals("CRF results are correct", test_output, resultsString);
-        
+
     }
 
     public void testCRFMemory() throws IOException, FileNotFoundException, ClassNotFoundException, Exception {
-        
-        CRF crf = ModelRunner.readModel( extractor_model_path );
-        
+
+        ModelRunner mr = new ModelRunner(extractor_model_path);
+
         String[] results = null;
-        
+
         for (int x = 0; x < 1000; ++x) {
-            results = ModelRunner.runModelString( this.test_input, crf );
+            results = mr.runModelString(this.test_input);
         }
-        
+
         String resultsString = StringUtils.join(results, "\n");
-                
+
         Assert.assertEquals("CRF results are correct", test_output, resultsString);
-        
     }
 
 }
