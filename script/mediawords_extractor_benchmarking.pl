2adeedae3d2b8a2157d5fd2a989bc428d19de125
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-01-27 20:19:25 +0200

    Make "mediawords_extractor_benchmarking.pl" script support (optional) vectoring too

--- script/mediawords_extractor_benchmarking.pl
+++ script/mediawords_extractor_benchmarking.pl
@@ -35,27 +35,45 @@ use MediaWords::Languages::en;
 
 #use XML::LibXML::Enhanced;
 
-sub test_extractor
+sub test_extract
 {
 
     my ( $downloads ) = @_;
 
     my @downloads = @{ $downloads };
 
-    say STDERR "Starting store_downloads";
+    say STDERR "Starting test_extract()...";
 
     @downloads = sort { $a->{ downloads_id } <=> $b->{ downloads_id } } @downloads;
 
-    my $download_results = [];
-
-    my $dbs = MediaWords::DB::connect_to_db;
+    my $db = MediaWords::DB::connect_to_db;
 
     for my $download ( @downloads )
     {
-        say STDERR "Processing download $download->{downloads_id}";
-        my $extract_results = MediaWords::DBI::Downloads::extractor_results_for_download( $dbs, $download );
+        say STDERR "Processing download $download->{downloads_id}...";
+        my $extract_results = MediaWords::DBI::Downloads::extractor_results_for_download( $db, $download );
 
         say "Got extract_results: " . Dumper( $extract_results );
+    }
+}
+
+sub test_extract_and_vector
+{
+    my ( $downloads ) = @_;
+
+    my @downloads = @{ $downloads };
+
+    say STDERR "Starting test_extract_and_vector()...";
+
+    @downloads = sort { $a->{ downloads_id } <=> $b->{ downloads_id } } @downloads;
+
+    my $db = MediaWords::DB::connect_to_db;
+
+    for my $download ( @downloads )
+    {
+        say STDERR "Processing download $download->{downloads_id}...";
+        MediaWords::DBI::Downloads::process_download_for_extractor( $db, $download, '1' );
+        say STDERR "Done.";
     }
 }
 
@@ -86,7 +104,7 @@ sub main
             @download_ids = <DOWNLOAD_ID_FILE>;
         }
 
-        say STDERR "Will extract download IDs: " . join( ', ', @download_ids );
+        say STDERR "Will process download IDs: " . join( ', ', @download_ids );
 
         $downloads = $dbs->query(
             <<EOF,
@@ -100,7 +118,7 @@ EOF
     }
     else
     {
-        say STDERR "Will extract *all* 'content' downloads";
+        say STDERR "Will process *all* 'content' downloads";
 
         $downloads = $dbs->query(
             <<EOF
@@ -115,12 +133,35 @@ EOF
 
     die 'no downloads found ' unless scalar( @{ $downloads } );
 
-    say STDERR "Will extract " . scalar( @{ $downloads } ) . " downloads.";
+    say STDERR "Will process " . scalar( @{ $downloads } ) . " downloads.";
+
+    my $extract_and_vector = 0;
+    if ( defined $ENV{ 'MEDIAWORDS_EXTRACTOR_BENCHMARKING_VECTOR' } )
+    {
+        $extract_and_vector = 1;
+    }
+
+    if ( $extract_and_vector )
+    {
+        say STDERR "Will extract *and* vector downloads.";
+    }
+    else
+    {
+        say STDERR "Will only extract downloads.";
+    }
 
     foreach my $iteration ( 1 .. $iterations )
     {
         say STDERR "iteration $iteration";
-        test_extractor( $downloads );
+
+        if ( $extract_and_vector )
+        {
+            test_extract_and_vector( $downloads );
+        }
+        else
+        {
+            test_extract( $downloads );
+        }
     }
 }
 
