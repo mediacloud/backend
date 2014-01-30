a38e194961f4f1d675b9e0397b0c8a5fc2500b08
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-01-30 15:04:14 -0500

    Create a wrapper library around Solr requests.

---
+++ python_scripts/mc_solr.py
@@ -0,0 +1,49 @@
+import requests
+import ipdb
+import mc_config
+import psycopg2
+import psycopg2.extras
+import time
+
+def get_solr_location():
+    ##TODO -- get this from the yaml file
+    return 'http://localhost:8983'
+
+def get_solr_collection_url_prefix():
+    return get_solr_location + '/solr/collection1'
+
+def solr_request( path, params):
+    url = get_solr_collection_url_prefix() + '/' + path
+    params['wt'] = 'json'
+    r = requests.get( url, params, headers = { 'Accept': 'application/json'})  
+    data = r.json()
+
+    return data
+
+def dataimport_command( command, params={}):
+    params['command'] = command
+    return solr_request( 'dataimport', params )
+
+def dataimport_status():
+    return dataimport_command( 'status' )
+
+def dataimport_delta_import():
+    params = {
+        'commit':  'true',
+        'clean':  'false',
+        }
+
+    ##Note: We're using the delta import through full import approach
+    return dataimport_command( 'full_import', params )
+
+def dataimport_full_import():
+    params = {
+        'commit':  'true',
+        'clean':  'true',
+        }
+
+    ##Note: We're using the delta import through full import approach
+    return dataimport_command( 'full_import', params )
+    
+def dataimport_reload_config():
+    return dataimport_command( 'reload' )
