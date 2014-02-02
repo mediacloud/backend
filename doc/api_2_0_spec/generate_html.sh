8750bb8b046616e1fe6c9860ac18436b6f65a6ad
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-02-02 18:17:38 +0200

    Move Pandoc’s CSS to external HTML file so that Pandoc can now create a fully self-contained file and GitHub doesn’t try to render the <style> block

--- doc/api_2_0_spec/generate_html.sh
+++ doc/api_2_0_spec/generate_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pandoc -s -S --toc --toc-depth=4 api_2_0_spec.md -o api_2_0_spec.html
+pandoc -s -S --toc --toc-depth=4 --include-in-header=api_2_0_spec-head.html api_2_0_spec.md -o api_2_0_spec.html
 
