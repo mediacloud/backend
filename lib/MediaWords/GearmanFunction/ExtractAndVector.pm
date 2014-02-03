215c55e6859650b1e9a5b60fd272e447cde25923
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-02-03 19:21:40 +0200

    Write a single Gearman extractor log because there are a lot of extraction jobs so it's impractical to log each job into a separate file

--- lib/MediaWords/GearmanFunction/ExtractAndVector.pm
+++ lib/MediaWords/GearmanFunction/ExtractAndVector.pm
@@ -64,6 +64,13 @@ sub run($$)
     return 1;
 }
 
+# write a single log because there are a lot of extraction jobs so it's
+# impractical to log each job into a separate file
+sub unify_logs()
+{
+    return 1;
+}
+
 no Moose;    # gets rid of scaffolding
 
 # Return package name instead of 1 or otherwise worker.pl won't know the name of the package it's loading
