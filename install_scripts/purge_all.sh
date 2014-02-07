af407fa2425185800a310e6d69cca45fb9b879cb
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-02-07 17:27:37 +0200

    Remove unused Bash include

--- install_scripts/purge_all.sh
+++ install_scripts/purge_all.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Include PostgreSQL path helpers
-PWD="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$PWD/postgresql_path_helpers.inc.sh"
-
 ./install_scripts/purge_carton_install.sh
 ./install_scripts/purge_mc_perl_brew.sh
 ./install_scripts/purge_mediacloud_databases.sh
