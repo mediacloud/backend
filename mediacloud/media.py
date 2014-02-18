ca8b67d1910c6ac26d854565e375a866e5822797
Edward L Platt <ed@elplatt.com>
2014-02-18 16:57:43 -0500

    Add functions to get all sets or sources, close #6.
    mediacloud.media.all_sets() returns a list of media set objects.
    mediacloud.media.all_sources() returns a list of media source objects.
    
    Unit tests have been included.

--- mediacloud/media.py
+++ mediacloud/media.py
@@ -33,6 +33,34 @@ def set(media_set_id):
             media_sets[m_id]['media_ids'].append(int(row[2]))
     return media_sets[int(media_set_id)]
 
+def all_sources():
+    '''
+    Call this to get a list of media sources
+    '''
+    global media_sources
+    if media_sources == None: 
+        MEDIA_FILE_PATH = os.path.dirname(__file__)+'/data/media_ids.csv'
+        media_sources = _loadMediaIdsCsv(MEDIA_FILE_PATH)
+    return media_sources.itervalues()
+    
+def all_sets():
+    '''
+    Call this to get a list of all media sets
+    '''
+    global media_sets
+    if media_sets == None: 
+        media_sets = {}
+        MEDIA_FILE_PATH = os.path.dirname(__file__)+'/data/media_sets.csv'
+        media_file = open(MEDIA_FILE_PATH, 'rb')
+        csv_reader = csv.reader(media_file, encoding='utf-8')
+        header = csv_reader.next() # skip header
+        for row in csv_reader:
+            m_id = int(row[0])
+            if m_id not in media_sets:
+                media_sets[m_id] = { 'id': m_id, 'name': row[1], 'media_ids': [] }
+            media_sets[m_id]['media_ids'].append(int(row[2]))
+    return media_sets.itervalues()
+
 def _loadMediaIdsCsv(file_path):
     media_list = {}
     media_file = open(file_path, 'rb')
