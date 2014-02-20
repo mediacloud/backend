7b2219018b58229c85cf8f4b9c56d9b88c634ab4
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-02-20 14:35:00 -0500

    Add API call for tags.

---
+++ lib/MediaWords/Controller/Api/V2/Tags.pm
@@ -0,0 +1,27 @@
+package MediaWords::Controller::Api::V2::Tags;
+use Modern::Perl "2013";
+use MediaWords::CommonLibs;
+
+
+use strict;
+use warnings;
+use MediaWords::DBI::StorySubsets;
+use MediaWords::Controller::Api::V2::MC_REST_SimpleObject;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'MediaWords::Controller::Api::V2::MC_REST_SimpleObject' }
+
+sub get_table_name
+{
+    return "tags";
+}
+
+sub list_query_filter_field
+{
+    return 'tag_sets_id';
+}
+
+
+1;
