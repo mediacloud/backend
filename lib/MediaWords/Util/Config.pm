7ae3d099d9e4bdd8fad021789b758b41b1fe6f54
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-02-27 16:43:39 +0200

    Add CRF model runner web service wrapper script; add required configuration in mediawords.yml; add Supervisord entry to automatically start the web service (if it's enabled); install Maven which is required to compile+run the CRF model runner web service

--- lib/MediaWords/Util/Config.pm
+++ lib/MediaWords/Util/Config.pm
@@ -102,6 +102,12 @@ sub set_defaults
     $config->{ gearmand }->{ listen }   ||= '127.0.0.1';
     $config->{ gearmand }->{ port }     ||= 4731;
 
+    # CRF model runner web service
+    $config->{ crf_web_service }->{ enabled }               ||= 'no';
+    $config->{ crf_web_service }->{ listen }                ||= '127.0.0.1';
+    $config->{ crf_web_service }->{ port }                  ||= 8441;
+    $config->{ crf_web_service }->{ number_of_threads }     ||= 32;
+
     # Supervisor
     $config->{ supervisor }->{ childlogdir } ||= 'data/supervisor_logs/';
 
