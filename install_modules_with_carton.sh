f8438af27309126985d58f580e22fd6fc6b67aa4
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-03-04 14:40:01 +0200

    When installing, execute "./script/run_carton.sh install --deployment" twice because the first attempt might skip some dependencies and fail; this is yet another attempt to fix Vagrant automated test runs

--- install_modules_with_carton.sh
+++ install_modules_with_carton.sh
@@ -23,8 +23,10 @@ mkdir -p local/
 # monkey-patched version of the module beforehand
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm -L local/ git://github.com/pypt/p5-Graph-Layout-Aesthetic.git@0.12
 
-# Install the rest of the modules
+# Install the rest of the modules; run the command twice because the first
+# attempt might fail
 source ./script/set_java_home.sh
+JAVA_HOME=$JAVA_HOME ./script/run_carton.sh install --deployment || { echo "First attempt to install CPAN modules might fail"; }
 JAVA_HOME=$JAVA_HOME ./script/run_carton.sh install --deployment
 
-echo "Successfully installed Perl and modules for MediaCloud"
+echo "Successfully installed Perl and modules for Media Cloud"
