f2232be7f164e50ea7ae9fc15129d7913c380aa4
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-03-12 18:11:58 -0400

    Add story_sentences_tags_map table.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -68,7 +68,7 @@ DECLARE
     
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4441;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4442;
     
 BEGIN
 
@@ -1062,7 +1062,25 @@ ALTER TABLE  story_sentences ADD CONSTRAINT story_sentences_stories_id_fkey FORE
 
 DROP TRIGGER IF EXISTS story_sentences_last_updated_trigger on story_sentences CASCADE;
 CREATE TRIGGER story_sentences_last_updated_trigger BEFORE INSERT OR UPDATE ON story_sentences FOR EACH ROW EXECUTE PROCEDURE last_updated_trigger() ;
-    
+
+create table story_sentences_tags_map
+(
+    story_sentences_tags_map_id     bigserial  primary key,
+    story_sentences_id              bigint     not null references story_sentences on delete cascade,
+    tags_id                 int     not null references tags on delete cascade,
+    db_row_last_updated                timestamp with time zone not null
+);
+
+DROP TRIGGER IF EXISTS story_sentences_tags_map_last_updated_trigger on story_sentences_tags_map CASCADE;
+CREATE TRIGGER story_sentences_tags_map_last_updated_trigger BEFORE INSERT OR UPDATE ON story_sentences_tags_map FOR EACH ROW EXECUTE PROCEDURE last_updated_trigger() ;
+DROP TRIGGER IF EXISTS story_sentences_tags_map_update_story_sentences_last_updated_trigger on story_sentences_tags_map;
+CREATE TRIGGER story_sentences_tags_map_update_story_sentences_last_updated_trigger AFTER INSERT OR UPDATE OR DELETE ON story_sentences_tags_map FOR EACH ROW EXECUTE PROCEDURE update_stories_updated_time_by_stories_id_trigger();
+
+CREATE index story_sentences_tags_map_db_row_last_updated on story_sentences_tags_map ( db_row_last_updated );
+create unique index story_sentences_tags_map_story on story_sentences_tags_map (story_sentences_id, tags_id);
+create index story_sentences_tags_map_tag on story_sentences_tags_map (tags_id);
+CREATE INDEX story_sentences_tags_map_story_id ON story_sentences_tags_map USING btree (story_sentences_id);
+
 create table story_sentence_counts (
        story_sentence_counts_id     bigserial       primary key,
        sentence_md5                 varchar(64)     not null,
