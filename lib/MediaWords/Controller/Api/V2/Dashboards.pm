103faeae208b4422baddc3687eae1a944fd5f389
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-03-27 14:40:26 -0400

    Fix formatting to enable merge.

--- lib/MediaWords/Controller/Api/V2/Dashboards.pm
+++ lib/MediaWords/Controller/Api/V2/Dashboards.pm
@@ -2,7 +2,6 @@ package MediaWords::Controller::Api::V2::Dashboards;
 use Modern::Perl "2013";
 use MediaWords::CommonLibs;
 
-
 use strict;
 use warnings;
 use MediaWords::DBI::StorySubsets;
@@ -35,18 +34,21 @@ sub _add_nested_data
 
     foreach my $dashboard ( @{ $dashboards } )
     {
-        my $media_sets = $db->query("select ms.media_sets_id, ms.name from media_sets ms, dashboard_media_sets dms where  " .
-				    " dms.media_sets_id=ms.media_sets_id and dms.dashboards_id  = ? ORDER by ms.media_sets_id",
+        my $media_sets = $db->query(
+            "select ms.media_sets_id, ms.name from media_sets ms, dashboard_media_sets dms where  " .
+              " dms.media_sets_id=ms.media_sets_id and dms.dashboards_id  = ? ORDER by ms.media_sets_id",
             $dashboard->{ dashboards_id }
         )->hashes;
 
-	foreach my $media_set ( @$media_sets ) 
-	{
-	    my $media = $db->query("SELECT m.name, m.url, m.media_id from media m , media_sets_media_map msmm" .
-				   " WHERE m.media_id = msmm.media_id AND msmm.media_sets_id = ? ORDER BY m.media_id ",
-				   $media_set->{ media_sets_id } )->hashes();
-	    $media_set->{ media } = $media;
-	}
+        foreach my $media_set ( @$media_sets )
+        {
+            my $media = $db->query(
+                "SELECT m.name, m.url, m.media_id from media m , media_sets_media_map msmm" .
+                  " WHERE m.media_id = msmm.media_id AND msmm.media_sets_id = ? ORDER BY m.media_id ",
+                $media_set->{ media_sets_id }
+            )->hashes();
+            $media_set->{ media } = $media;
+        }
 
         $dashboard->{ media_sets } = $media_sets;
     }
