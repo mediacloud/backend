998a29ad90e63cb340e4e0edad841983bd16748f
mediacloud role account <mediacloud@cyber.law.harvard.edu>
2014-04-10 12:33:27 -0400

    Increase number of crawlers and extractors on Chloe.

--- supervisor/supervisorctl.sh
+++ supervisor/supervisorctl.sh
@@ -4,7 +4,7 @@ PWD="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$PWD/supervisor_is_up_to_date.inc.sh"
 validate_supervisor_version
 
-supervisorctl \
+/usr/local/bin/supervisorctl \
     -c `dirname "$0"`/supervisord.conf \
     -s http://localhost:4398 \
     -u supervisord \
