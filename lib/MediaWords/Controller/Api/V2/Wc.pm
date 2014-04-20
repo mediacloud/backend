bd0fe9e3d4713f14679c6d5391cdd5a09fc419e5
Hal Roberts <hroberts@cyber.law.harvard.edu>
2014-04-19 22:24:11 -0500

    fix undef error for l param

--- lib/MediaWords/Controller/Api/V2/Wc.pm
+++ lib/MediaWords/Controller/Api/V2/Wc.pm
@@ -45,12 +45,12 @@ sub list_GET : Local : PathPrefix( '/api' )
 
     my $q  = $c->req->parameters->{ 'q' };
     my $fq = $c->req->parameters->{ 'fq' };
-    my $l  = $c->req->parameters->{ 'l' };
-    
+    my $l  = $c->req->parameters->{ 'l' } || '';
+
     # no remote parameter prevents loop of server calling itself via MediaWords::Solr::WordCounts::_get_remote_word_counts
     my $nr = $c->req->parameters->{ 'nr' };
 
-    my $languages = [ split( /\W/, $l )  ];
+    my $languages = [ split( /\W/, $l ) ];
 
     my $words = MediaWords::Solr::count_words( $q, $fq, $languages, $nr );
 
