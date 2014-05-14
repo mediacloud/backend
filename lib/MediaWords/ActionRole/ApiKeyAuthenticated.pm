aa8b5f1ff09ab610bb97b22081f6961d3efa0ff8
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-05-14 17:32:48 +0300

    Move reusable helper subroutine to MediaWords::ActionRole::AbstractAuthenticatedActionRole

--- lib/MediaWords/ActionRole/ApiKeyAuthenticated.pm
+++ lib/MediaWords/ActionRole/ApiKeyAuthenticated.pm
@@ -8,31 +8,13 @@ use strict;
 use warnings;
 
 use Moose::Role;
+with 'MediaWords::ActionRole::AbstractAuthenticatedActionRole';
 use namespace::autoclean;
 
 use Modern::Perl "2013";
 use MediaWords::CommonLibs;
 
 use MediaWords::Util::Config;
-use MediaWords::DBI::Auth;
-
-sub _api_key_is_invalid($$$)
-{
-    my ( $self, $c, $api_auth ) = @_;
-
-    # user_for_api_token_catalyst() did the check already
-    if (    defined( $api_auth )
-        and ref( $api_auth ) eq ref( {} )
-        and defined( $api_auth->{ email } )
-        and length( $api_auth->{ email } ) > 0 )
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
-}
 
 before execute => sub {
     my ( $self, $controller, $c ) = @_;
@@ -41,18 +23,13 @@ before execute => sub {
     my $allow_unauth = MediaWords::Util::Config::get_config->{ mediawords }->{ allow_unauthenticated_api_requests } || 'no';
     if ( $allow_unauth ne 'yes' )
     {
-        my $api_auth = MediaWords::DBI::Auth::user_for_api_token_catalyst( $c );
-
-        if ( $self->_api_key_is_invalid( $c, $api_auth ) )
+        my ( $user_email, $user_roles ) = $self->_user_email_and_roles( $c );
+        unless ( $user_email and $user_roles )
         {
-
             $controller->status_forbidden( $c, message => 'Invalid API key. Access denied.' );
             $c->detach();
-
-            return 0;
+            return;
         }
-
-        $c->stash->{ auth_user } = $api_auth;
     }
 };
 
--- lib/MediaWords/ActionRole/Logged.pm
+++ lib/MediaWords/ActionRole/Logged.pm
@@ -11,40 +11,15 @@ use Modern::Perl "2013";
 use MediaWords::CommonLibs;
 
 use Moose::Role;
+with 'MediaWords::ActionRole::AbstractAuthenticatedActionRole';
 use namespace::autoclean;
-
-use Data::Dumper;
-use MediaWords::DBI::Auth;
 
 use constant NUMBER_OF_REQUESTED_ITEMS_KEY => 'MediaWords::ActionRole::Logged::requested_items_count';
 
 after execute => sub {
     my ( $self, $controller, $c ) = @_;
 
-    my $user_email = undef;
-    my $user_roles = undef;
-    if ( $c->user )
-    {
-        $user_email = $c->user->username;
-        $user_roles = $c->user->roles;
-    }
-    else
-    {
-        my $api_auth = undef;
-        if ( $c->stash->{ api_auth } )
-        {
-            $api_auth = $c->stash->{ api_auth };
-        }
-        else
-        {
-            $api_auth = MediaWords::DBI::Auth::user_for_api_token_catalyst( $c );
-        }
-        if ( $api_auth )
-        {
-            $user_email = $api_auth->{ email };
-            $user_roles = $api_auth->{ roles };
-        }
-    }
+    my ( $user_email, $user_roles ) = $self->_user_email_and_roles( $c );
     unless ( $user_email and $user_roles )
     {
         die "user_email is undef (I wasn't able to authenticate either using the API key nor the normal means)";
