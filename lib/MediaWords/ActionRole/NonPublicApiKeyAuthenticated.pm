3c143baa219ee9232e6b6dcb5eb452504e46ddf5
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-05-21 17:50:09 -0400

    Check whether the user has access to the non-public api

--- lib/MediaWords/ActionRole/NonPublicApiKeyAuthenticated.pm
+++ lib/MediaWords/ActionRole/NonPublicApiKeyAuthenticated.pm
@@ -15,18 +15,36 @@ use Modern::Perl "2013";
 use MediaWords::CommonLibs;
 
 use MediaWords::Util::Config;
+use MediaWords::DBI::Auth;
 
 before execute => sub {
     my ( $self, $controller, $c ) = @_;
+
+    say STDERR "NonPublicApiKeyAuthenticated";
 
     # Check API key
     my $allow_unauth = MediaWords::Util::Config::get_config->{ mediawords }->{ allow_unauthenticated_api_requests } || 'no';
     if ( $allow_unauth ne 'yes' )
     {
         my ( $user_email, $user_roles ) = $self->_user_email_and_roles( $c );
+
+        #say STDERR Dumper( $user_roles );
+
         unless ( $user_email and $user_roles )
         {
             $controller->status_forbidden( $c, message => 'Invalid API key. Access denied.' );
+            $c->detach();
+            return;
+        }
+
+        my $user_info = MediaWords::DBI::Auth::user_info( $c->dbis, $user_email );
+
+        #say STDERR Dumper( $user_info );
+
+        if ( !$user_info->{ non_public_api } )
+        {
+            #say STDERR "non public api access denied";
+            $controller->status_forbidden( $c, message => 'Your API key does not allow access to this URL. Access denied.' );
             $c->detach();
             return;
         }
