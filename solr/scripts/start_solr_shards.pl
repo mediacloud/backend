9dbb39616f49d4afbe76cb8b54b2cc796fa9ef4d
Hal Roberts <hroberts@cyber.law.harvard.edu>
2014-05-25 11:33:24 -0500

    use simpler oracle jvm parallel gc opts

--- solr/scripts/start_solr_shards.pl
+++ solr/scripts/start_solr_shards.pl
@@ -9,8 +9,7 @@ use v5.10;
 use FindBin;
 use Getopt::Long;
 
-use constant JVM_OPTS =>
-"-XX:NewRatio=3 -XX:SurvivorRatio=4 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=8 -XX:+UseConcMarkSweepGC -XX:+CMSScavengeBeforeRemark -XX:PretenureSizeThreshold=64m -XX:CMSFullGCsBeforeCompaction=1 -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:CMSTriggerPermRatio=80 -XX:CMSMaxAbortablePrecleanTime=6000 -XX:+CMSParallelRemarkEnabled -XX:+ParallelRefProcEnabled -XX:+UseLargePages -XX:+AggressiveOpts";
+use constant JVM_OPTS => '-XX:MaxGCPauseMillis=1000';
 
 sub main
 {
