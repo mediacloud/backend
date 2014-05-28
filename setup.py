43f3cdc4d3a615c0882f510952e52822a50350dd
Rahul Bhargava <rahulb@media.mit.edu>
2014-05-28 11:26:49 -0400

    v2.6 version bump and egg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mediacloud',
-  version='2.5',
+  version='2.6',
   description='MediaCloud API Client Library',
   author='Rahul Bhargava',
   author_email='rahulb@media.mit.edu',
