b195bfbb475d79624c26be68ee47681ec1da2535
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-05-31 23:31:22 +0300

    Use travis_wait() in order to increase Travis-CI timeout from 10 minutes to 20 minutes for Perl build

--- install_mc_perlbrew.sh
+++ install_mc_perlbrew.sh
@@ -17,7 +17,16 @@ set +u
 source ~/perl5/perlbrew/etc/bashrc
 set -u
 perlbrew init
-nice perlbrew install perl-5.16.3 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN
+
+CMD_PERLBREW_INSTALL="nice perlbrew install perl-5.16.3 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN"
+if [[ -z "$USE_TRAVIS_WAIT" ]]; then
+    "$CMD_PERLBREW_INSTALL"
+else
+    # Allow Travis-CI to complete Perl install in 20 minutes (instead of the
+    # default 10)
+    travis_wait "$CMD_PERLBREW_INSTALL"
+fi
+
 perlbrew switch perl-5.16.3
 perlbrew install-cpanm
 perlbrew lib create mediacloud
