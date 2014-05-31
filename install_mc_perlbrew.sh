810c56813bdfaef0137b29fb174107dd7918c349
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-05-31 23:38:52 +0300

    Add travis_wait() to .travis.yml where it is more likely to work

--- install_mc_perlbrew.sh
+++ install_mc_perlbrew.sh
@@ -17,16 +17,7 @@ set +u
 source ~/perl5/perlbrew/etc/bashrc
 set -u
 perlbrew init
-
-CMD_PERLBREW_INSTALL="nice perlbrew install perl-5.16.3 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN"
-if [[ -z "$USE_TRAVIS_WAIT" ]]; then
-    "$CMD_PERLBREW_INSTALL"
-else
-    # Allow Travis-CI to complete Perl install in 20 minutes (instead of the
-    # default 10)
-    travis_wait "$CMD_PERLBREW_INSTALL"
-fi
-
+nice perlbrew install perl-5.16.3 -Duseithreads -Dusemultiplicity -Duse64bitint -Duse64bitall -Duseposix -Dusethreads -Duselargefiles -Dccflags=-DDEBIAN
 perlbrew switch perl-5.16.3
 perlbrew install-cpanm
 perlbrew lib create mediacloud
