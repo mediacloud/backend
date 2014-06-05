62bf0ce2a21506fee1ab730d360b7cf390f6056f
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-06-05 15:24:26 +0300

    Use a more dispersed way to install Media Cloud on Travis-CI (maybe it won't timeout that way)

--- install_mc_perlbrew_and_modules.sh
+++ install_mc_perlbrew_and_modules.sh
@@ -7,10 +7,8 @@ cd $working_dir
 set -u
 set -o  errexit
 
-if [[ -z "$TRAVIS_SKIP_INSTALL_MC_PERLBREW" ]]; then
-    echo "starting perlbrew install "
-    ./install_mc_perlbrew.sh
-fi
+echo "starting perlbrew install "
+./install_mc_perlbrew.sh
 echo "starting non-carton-based modules install "
 ./install_modules_outside_of_carton.sh
 echo "starting carton-based modules install "
