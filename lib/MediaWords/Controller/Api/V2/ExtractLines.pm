bc9c0f4e1ccefec61935f68d5d95a79db1f56407
pishkin <pamela.mishkin@gmail.com>
2014-06-09 11:31:02 -0400

    added ExtractLines.pm

---
+++ lib/MediaWords/Controller/Api/V2/ExtractLines.pm
@@ -0,0 +1,77 @@
+package MediaWords::Controller::Api::V2::MC_REST_SimpleObject;
+use Modern::Perl "2013";
+use MediaWords::CommonLibs;
+
+use MediaWords::DBI::StorySubsets;
+use strict;
+use warnings;
+use base 'Catalyst::Controller::REST';
+use JSON;
+use List::Util qw(first max maxstr min minstr reduce shuffle sum);
+use Moose;
+use namespace::autoclean;
+use List::Compare;
+use Carp;
+
+=head1 NAME
+
+MediaWords::Controller::Media - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 index 
+
+=cut
+
+BEGIN { extends 'MediaWords::Controller::Api::V2::MC_Controller_REST' }
+
+# Default authentication action roles
+__PACKAGE__->config(    #
+    action => {         #
+        single => { Does => [ qw( ~PublicApiKeyAuthenticated ~Throttled ~Logged ) ] },    #
+        list   => { Does => [ qw( ~PublicApiKeyAuthenticated ~Throttled ~Logged ) ] },    #
+      }    #
+);         #
+
+use constant ROWS_PER_PAGE => 20;
+
+use MediaWords::Tagger;
+
+sub list : Local : ActionClass('REST')    # action roles are to be set for each derivative sub-actions
+{
+}
+
+sub list_GET : Local
+{
+    my ( $self, $c ) = @_;
+
+    my $body_html = $c->req->param( 'body_html' );
+    $body_html //= ROWS_PER_PAGE;
+
+    # say STDERR "rows $rows";
+
+    my $lines = [ split( /[\n\r]+/, $body_html ) ];
+
+    $lines = MediaWords::Crawler::Extractor::preprocess( $lines );
+
+    $self->status_ok( $c, entity => $lines );
+}
+
+=head1 AUTHOR
+
+Pamela Mishkin
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
