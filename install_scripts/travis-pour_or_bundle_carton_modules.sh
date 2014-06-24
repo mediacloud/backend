0733d5e9ceb3cf85b72a99e13ff0988fedfce185
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-06-24 20:14:41 +0300

    Test for empty variables

--- install_scripts/travis-pour_or_bundle_carton_modules.sh
+++ install_scripts/travis-pour_or_bundle_carton_modules.sh
@@ -31,27 +31,51 @@ function bundle_id {
 
     # Kernel architecture, e.g. x86_64
     local OS_ARCH=`uname -m`
+    if [ -z "$OS_ARCH" ]; then
+        echo "Unable to determine kernel architecture."
+        exit 1
+    fi
 
     # Perl version, e.g. 5.016002
     local PERL_VERSION=`perl -e "print $]"`
+    if [ -z "$PERL_VERSION" ]; then
+        echo "Unable to determine Perl version."
+        exit 1
+    fi
 
     # Author date (GMT) of current version of "cpanfile", e.g. "2014_06_23_15_29_22"
-    local CPANFILE_SHA1=$(TZ=UTC $DATE --date "@$(git log -1 --format=%at cpanfile)" "+%F-%T" | tr -s ' :-' '_')
+    local CPANFILE_DATE=$(TZ=UTC $DATE --date "@$(git log -1 --format=%at cpanfile)" "+%F-%T" | tr -s ' :-' '_')
+    if [ -z "$CPANFILE_DATE" ]; then
+        echo "Unable to determine 'cpanfile' author date."
+        exit 1
+    fi
 
     # Author date (GMT) of current version of "cpanfile.snapshot", e.g. "2014_06_23_14_50_51"
-    local CPANFILE_SNAPSHOT_SHA1=$(TZ=UTC $DATE --date "@$(git log -1 --format=%at cpanfile.snapshot)" "+%F-%T" | tr -s ' :-' '_')
+    local CPANFILE_SNAPSHOT_DATE=$(TZ=UTC $DATE --date "@$(git log -1 --format=%at cpanfile.snapshot)" "+%F-%T" | tr -s ' :-' '_')
+    if [ -z "$CPANFILE_SNAPSHOT_DATE" ]; then
+        echo "Unable to determine 'cpanfile.snapshot' author date."
+        exit 1
+    fi
 
     # OS version
     local OS_VERSION="unknown"
     if [ -f /etc/lsb-release ]; then
         source /etc/lsb-release
-        OS_VERSION="ubuntu_$DISTRIB_RELEASE"
+        if [ -z "$DISTRIB_RELEASE" ]; then
+            echo "OS is Ubuntu, but DISTRIB_RELEASE is empty."
+            exit 1
+        fi
+        OS_VERSION="ubuntu_${DISTRIB_RELEASE}"
     elif [ `uname` == 'Darwin' ]; then
         MAC_OS_X_VERSION=`sw_vers -productVersion`
+        if [ -z "$MAC_OS_X_VERSION" ]; then
+            echo "OS is OS X, but productVersion is empty."
+            exit 1
+        fi
         OS_VERSION="osx_${MAC_OS_X_VERSION}"
     fi
 
-    local BUNDLE_ID="${OS_VERSION}-${OS_ARCH}-perl_${PERL_VERSION}-cpanfile_${CPANFILE_SHA1}-snapshot_${CPANFILE_SNAPSHOT_SHA1}"
+    local BUNDLE_ID="${OS_VERSION}-${OS_ARCH}-perl_${PERL_VERSION}-cpanfile_${CPANFILE_DATE}-snapshot_${CPANFILE_SNAPSHOT_DATE}"
     echo "$BUNDLE_ID"
 }
 
