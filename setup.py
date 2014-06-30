81c8cd967dfb837ee55e28b333308cf7de78b419
Rahul Bhargava <rahulb@media.mit.edu>
2014-06-30 11:19:19 -0400

    version bump to 2.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mediacloud',
-  version='2.6',
+  version='2.7',
   description='MediaCloud API Client Library',
   author='Rahul Bhargava',
   author_email='rahulb@media.mit.edu',
