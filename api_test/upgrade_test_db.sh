6c64adff5b8ec1d50e04f8465cd3a80fbc4eca53
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-08-13 17:10:53 -0400

    Add message so that we know restore is finished.

--- api_test/upgrade_test_db.sh
+++ api_test/upgrade_test_db.sh
@@ -10,6 +10,7 @@ echo "setting up test database"
 source  /tmp/test_db_$$
 echo "running pg_restore"
 pg_restore --clean -d $PGDATABASE data/db_dumps/cc_blogs_mc_db.dump
+echo "running mediawords_upgrade_db.pl --import"
 MEDIAWORDS_FORCE_USING_TEST_DATABASE=1 ./script/run_with_carton.sh ./script/mediawords_upgrade_db.pl --import
 echo "dumping"
 pg_dump --format=custom $PGDATABASE >  data/db_dumps/cc_blogs_mc_db.dump
