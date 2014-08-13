a188e36c76d56af1f2646d46b04c48edc794bfef
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2014-08-13 16:40:08 -0400

    Exit on error.

--- api_test/upgrade_test_db.sh
+++ api_test/upgrade_test_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -u
+set -o  errexit
+
 cd `dirname $0`/../
 
 echo "setting up test database"
@@ -8,4 +11,5 @@ source  /tmp/test_db_$$
 echo "running pg_restore"
 pg_restore --clean -d $PGDATABASE data/db_dumps/cc_blogs_mc_db.dump
 MEDIAWORDS_FORCE_USING_TEST_DATABASE=1 ./script/run_with_carton.sh ./script/mediawords_upgrade_db.pl --import
+echo "dumping"
 pg_dump --format=custom $PGDATABASE >  data/db_dumps/cc_blogs_mc_db.dump
