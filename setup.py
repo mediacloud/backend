da3455197a78ea40b034630d189c100bdc05aa14
Edward L Platt <ed@elplatt.com>
2014-09-22 15:27:02 -0400

    Fix version number in setup script.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mediacloud',
-  version='2.10',
+  version='2.11',
   description='MediaCloud API Client Library',
   author='Rahul Bhargava',
   author_email='rahulb@media.mit.edu',
