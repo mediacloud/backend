59f62f2a427c2e7aec07ee2c4f5e79f7b92a211d
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-10-02 18:42:13 +0300

    Use "carton update" for updating modules instead of reinstalling everything [ci skip]

--- script/update_modules_with_carton.sh
+++ script/update_modules_with_carton.sh
@@ -2,9 +2,6 @@
 #
 # Update Perl module dependencies to their latest versions.
 #
-# This script removes cpanfile.snapshot, Carton's cache and modules themselves so Carton is then
-# forced to check for new versions of modules.
-#
 
 if [ ! -f cpanfile ]; then
     echo "Run this script from the root of the Media Cloud repository."
@@ -15,11 +12,6 @@ fi
 source ./script/set_perl_brew_environment.sh
 cpanm Carton
 
-# Remove modules and Carton's cache, then reinstall the modules
-rm -rf .carton/
-rm -rf local/
-rm cpanfile.snapshot
-
 # Gearman::XS, the dependency of Gearman::JobScheduler, depends on
 # Module::Install, but the author of the module (probably) forgot to add it so
 # the list of dependencies (https://rt.cpan.org/Ticket/Display.html?id=89690),
@@ -28,5 +20,4 @@ mkdir -p local/
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm -L local/ Module::Install
 
 source ./script/set_java_home.sh
-JAVA_HOME=$JAVA_HOME ./script/run_carton.sh install  # running with "--deployment" would not regenerate cpanfile.snapshot
-
+JAVA_HOME=$JAVA_HOME ./script/run_carton.sh update
