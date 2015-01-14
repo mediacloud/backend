85ae2385b04da6b952276fdb901e5f74ec774e4c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2015-01-14 05:16:41 +0200

    Make 'controversy_opt' work just like in mediawords_dump_controversy.pl to avoid confusion

--- script/mediawords_fetch_twitter_and_facebook_stats.pl
+++ script/mediawords_fetch_twitter_and_facebook_stats.pl
@@ -19,6 +19,7 @@ use MediaWords::CommonLibs;
 use Getopt::Long;
 
 use MediaWords::DB;
+use MediaWords::CM;
 use MediaWords::Util::Twitter;
 use MediaWords::Util::Facebook;
 
@@ -31,41 +32,49 @@ sub main
     die( "Usage: $0 --controversy < id >" ) unless ( $controversy_opt );
 
     my $db = MediaWords::DB::connect_to_db;
+    my $controversies = MediaWords::CM::require_controversies_by_opt( $db, $controversy_opt );
+    unless ( $controversies )
+    {
+        die "Unable to find controversies for option '$controversy_opt'";
+    }
 
-    my $stories = $db->query( <<END, $controversy_opt )->hashes;
-select s.stories_id, s.url
-    from stories s
-        join controversy_stories cs on ( cs.stories_id = s.stories_id )
-        join controversies c on ( cs.controversies_id = c.controversies_id )
-    where
-        c.name = ?
+    for my $controversy ( @{ $controversies } )
+    {
+        my $controversies_id = $controversy->{ controversies_id };
+
+        my $stories = $db->query( <<END, $controversies_id )->hashes;
+    select s.stories_id, s.url
+        from stories s
+            join controversy_stories cs on ( cs.stories_id = s.stories_id )
+        where
+            cs.controversies_id = ?
 END
 
-    if ( !@{ $stories } )
-    {
-        say STDERR "No stories found for controversy '$controversy_opt'";
-    }
-
-    for my $story ( @{ $stories } )
-    {
-
-        my $ss = $db->query( "select * from story_statistics where stories_id = ?", $story->{ stories_id } )->hash;
-
-        say STDERR "$story->{ url }";
-
-        if ( !$ss || $ss->{ twitter_url_tweet_count_error } || !defined( $ss->{ twitter_url_tweet_count } ) )
+        if ( !@{ $stories } )
         {
-            my $count = MediaWords::Util::Twitter::get_and_store_tweet_count( $db, $story );
-            say STDERR "url_tweet_count: $count";
+            say STDERR "No stories found for controversy '$controversy->{ name }' ('$controversy_opt')";
         }
 
-        # if ( !$ss || $ss->{ facebook_share_count_error} || !defined( $ss->{ facebook_share_count } ) )
-        # {
-        #     my $count = MediaWords::Util::Facebook::get_and_store_share_count( $db, $story );
-        #     say STDERR "facebook_share_count: $count";
-        # }
-    }
+        for my $story ( @{ $stories } )
+        {
 
+            my $ss = $db->query( "select * from story_statistics where stories_id = ?", $story->{ stories_id } )->hash;
+
+            say STDERR "$story->{ url }";
+
+            if ( !$ss || $ss->{ twitter_url_tweet_count_error } || !defined( $ss->{ twitter_url_tweet_count } ) )
+            {
+                my $count = MediaWords::Util::Twitter::get_and_store_tweet_count( $db, $story );
+                say STDERR "url_tweet_count: $count";
+            }
+
+            # if ( !$ss || $ss->{ facebook_share_count_error} || !defined( $ss->{ facebook_share_count } ) )
+            # {
+            #     my $count = MediaWords::Util::Facebook::get_and_store_share_count( $db, $story );
+            #     say STDERR "facebook_share_count: $count";
+            # }
+        }
+    }
 }
 
 main();
