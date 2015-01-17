6e6da4747a4e9c5c109a4100a44800e35164b5e1
Hal Roberts <hroberts@cyber.law.harvard.edu>
2015-01-17 02:46:09 -0500

    fix crawler test to work acros time zones

--- t/data/crawler_stories/inline_content/1.pl
+++ t/data/crawler_stories/inline_content/1.pl
@@ -1,9 +1,9 @@
 #<<<
 $VAR1 = {
-          'collect_date' => '2015-01-09 17:04:05.048629',
+          'collect_date' => '2015-01-17 02:33:38.011193',
           'content' => '<p>This is the first item.</p>
 ',
-          'db_row_last_updated' => '2015-01-09 17:04:09.982683-05',
+          'db_row_last_updated' => '2015-01-17 02:33:42.972309-05',
           'description' => 'RSS description of the first inline item.',
           'extracted_text' => 'First inline item
 ***
@@ -21,7 +21,7 @@ RSS description of the first inline item.
           'stories_id' => 1,
           'story_sentences' => [
                                  {
-                                   'db_row_last_updated' => '2015-01-09 17:04:09.982683-05',
+                                   'db_row_last_updated' => '2015-01-17 02:33:42.972309-05',
                                    'language' => 'en',
                                    'media_id' => 1,
                                    'publish_date' => '2009-06-11 06:57:00',
@@ -32,7 +32,8 @@ RSS description of the first inline item.
                                  }
                                ],
           'tags' => [],
+          'timezone' => 'America/New_York',
           'title' => 'First inline item',
-          'url' => 'http://amanda.law.harvard.edu:42400/inline_content/item_1.html'
+          'url' => 'http://amanda.law.harvard.edu:36893/inline_content/item_1.html'
         };
 #>>>
--- t/data/crawler_stories/inline_content/4.pl
+++ t/data/crawler_stories/inline_content/4.pl
@@ -1,13 +1,13 @@
 #<<<
 $VAR1 = {
-          'collect_date' => '2015-01-09 17:04:06.732865',
+          'collect_date' => '2015-01-17 02:33:39.741037',
           'content' => '<p>
     This RSS feed is used to test whether very short downloads (stored directly
     in the <code>downloads.path</code> as <code>"content:..."</code>) are being
     stored / extracted / fetched correctly.
 </p>
 ',
-          'db_row_last_updated' => '2015-01-09 17:04:06.809085-05',
+          'db_row_last_updated' => '2015-01-17 02:33:39.819881-05',
           'description' => undef,
           'extracted_text' => '(no title)
 ***
@@ -20,17 +20,17 @@ $VAR1 = {
 ',
           'feed_type' => 'web_page',
           'full_text_rss' => 0,
-          'guid' => '1420841046:http://amanda.law.harvard.edu:42400//inline_content/home.html',
+          'guid' => '1421480019:http://amanda.law.harvard.edu:36893//inline_content/home.html',
           'language' => 'en',
           'media_id' => 1,
-          'publish_date' => '2015-01-09 17:04:06',
+          'publish_date' => '2015-01-17 02:33:39',
           'stories_id' => 4,
           'story_sentences' => [
                                  {
-                                   'db_row_last_updated' => '2015-01-09 17:04:06.809085-05',
+                                   'db_row_last_updated' => '2015-01-17 02:33:39.819881-05',
                                    'language' => 'en',
                                    'media_id' => 1,
-                                   'publish_date' => '2015-01-09 17:04:06',
+                                   'publish_date' => '2015-01-17 02:33:39',
                                    'sentence' => 'This RSS feed is used to test whether very short downloads (stored directly stored / extracted / fetched correctly.',
                                    'sentence_number' => 0,
                                    'stories_id' => 4,
@@ -38,7 +38,8 @@ $VAR1 = {
                                  }
                                ],
           'tags' => [],
+          'timezone' => 'America/New_York',
           'title' => '(no title)',
-          'url' => 'http://amanda.law.harvard.edu:42400//inline_content/home.html'
+          'url' => 'http://amanda.law.harvard.edu:36893//inline_content/home.html'
         };
 #>>>
