633c90e09330d7681a3362bb82f999018871a3ad
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2015-02-27 18:56:27 -0500

    Check the number of extractAndVector jobs in the Gearman queue and only queue more for reextraction if this number is low.

--- script/mediawords_reextraction_daemon.pl
+++ script/mediawords_reextraction_daemon.pl
@@ -30,16 +30,31 @@ sub main
 
     my $db = MediaWords::DB::connect_to_db;
 
-    $db->dbh->{ AutoCommit } = 0;
-
     my $tags_id = MediaWords::DBI::Stories::get_current_extractor_version_tags_id( $db );
 
     my $last_processed_stories_id = 0;
+    my $story_batch_size          = 10;
+    my $gearman_queue_limit       = 8;
+    my $sleep_time                = 20;
 
-    my $story_batch_size = 10;
+    my $gearman_db = MediaWords::DB::connect_to_db( "gearman" );
 
     while ( 1 )
     {
+        my $gearman_queued_jobs = $gearman_db->query(
+            "SELECT count(*) from queue where function_name = 'MediaWords::GearmanFunction::ExtractAndVector' " )->flat()
+          ->[ 0 ];
+
+        say STDERR "Gearman queued jobs $gearman_queued_jobs";
+
+        if ( $gearman_queued_jobs > $gearman_queue_limit )
+        {
+            say STDERR
+"Gearman queue contains more then $gearman_queue_limit jobs ( $gearman_queued_jobs) sleeping $sleep_time seconds";
+            sleep 20;
+            next;
+        }
+
         my $rows = $db->query(
 "select ps.* from processed_stories ps where processed_stories_id > ? EXCEPT select ps.* from processed_stories ps, stories_tags_map stm where ps.stories_id = stm.stories_id AND processed_stories_id > ? AND tags_id = ? order by processed_stories_id asc limit ? ",
             $last_processed_stories_id, $last_processed_stories_id, $tags_id, $story_batch_size )->hashes;
@@ -52,21 +67,16 @@ sub main
 
         my $i = 0;
 
-        say Dumper( $stories_ids );
+        #say Dumper( $stories_ids );
 
         for my $stories_id ( @{ $stories_ids } )
         {
             MediaWords::GearmanFunction::ExtractAndVector->enqueue_on_gearman(
                 { stories_id => $stories_id, disable_story_triggers => 1 } );
 
-            if ( !( ++$i % 100 ) )
-            {
-                $db->commit;
-                print STDERR "[$i]\n";
-            }
         }
+
     }
-    $db->commit;
 }
 
 main();
