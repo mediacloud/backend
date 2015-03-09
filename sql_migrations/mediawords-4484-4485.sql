0766b94c94f5773f14b2495d540766b322b1c0e1
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2015-03-09 14:59:41 -0400

    Fake version bump.

---
+++ sql_migrations/mediawords-4484-4485.sql
@@ -0,0 +1,43 @@
+--
+-- This is a Media Cloud PostgreSQL schema difference file (a "diff") between schema
+-- versions 4484 and 4485.
+--
+-- If you are running Media Cloud with a database that was set up with a schema version
+-- 4484, and you would like to upgrade both the Media Cloud and the
+-- database to be at version 4485, import this SQL file:
+--
+--     psql mediacloud < mediawords-4484-4485.sql
+--
+-- You might need to import some additional schema diff files to reach the desired version.
+--
+
+--
+-- 1 of 2. Import the output of 'apgdiff':
+--
+
+SET search_path = public, pg_catalog;
+
+CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
+DECLARE
+    
+    -- Database schema version number (same as a SVN revision number)
+    -- Increase it by 1 if you make major database schema changes.
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4485;
+    
+BEGIN
+
+    -- Update / set database schema version
+    DELETE FROM database_variables WHERE name = 'database-schema-version';
+    INSERT INTO database_variables (name, value) VALUES ('database-schema-version', MEDIACLOUD_DATABASE_SCHEMA_VERSION::int);
+
+    return true;
+    
+END;
+$$
+LANGUAGE 'plpgsql';
+
+--
+-- 2 of 2. Reset the database version.
+--
+SELECT set_database_schema_version();
+
