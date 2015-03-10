5615ea75e76fe5446da2232fb242dd0f5716ae54
David <dlarochelle@cyber.law.harvard.edu>
2015-03-10 14:16:58 -0400

    Default db_row_last_updated to now() for tables with not null constraints.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -45,7 +45,7 @@ DECLARE
     
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4485;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4486;
     
 BEGIN
 
@@ -1044,7 +1044,7 @@ create table stories_tags_map
     stories_tags_map_id     serial  primary key,
     stories_id              int     not null references stories on delete cascade,
     tags_id                 int     not null references tags on delete cascade,
-    db_row_last_updated                timestamp with time zone not null
+    db_row_last_updated                timestamp with time zone not null default now()
 );
 
 DROP TRIGGER IF EXISTS stories_tags_map_last_updated_trigger on stories_tags_map CASCADE;
@@ -1330,7 +1330,7 @@ create table story_sentences_tags_map
     story_sentences_tags_map_id     bigserial  primary key,
     story_sentences_id              bigint     not null references story_sentences on delete cascade,
     tags_id                 int     not null references tags on delete cascade,
-    db_row_last_updated                timestamp with time zone not null
+    db_row_last_updated                timestamp with time zone not null default now()
 );
 
 DROP TRIGGER IF EXISTS story_sentences_tags_map_last_updated_trigger on story_sentences_tags_map CASCADE;
