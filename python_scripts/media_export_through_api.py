b86d7e2b1d48481f91dff417a5ec803b50afc289
David Larochelle <dlarochelle@cyber.law.harvard.edu>
2015-05-18 17:02:15 -0400

    Add script to export feed downloads using the API.

--- python_scripts/media_export_through_api.py
+++ python_scripts/media_export_through_api.py
@@ -23,29 +23,51 @@ def add_feed_download_with_api( mc_api_url, api_key, download, raw_content ):
 
     return r
 
-local_key = '2a4cebc31101a2d3d5e60456c23ae877c2d49944068f237e1134e2c75191a2af'
-local_key = '1161251f5de4f381a198eea4dc20350fd992f5eef7cb2fdc284c245ff3d4f3ca'
-source_media_cloud_api_url =  'http://localhost:8000/'
-dest_media_cloud_api_url = 'http://localhost:3000/'
-source_api_key = 'e07cf98dd0d457351354ee520635c226acd238ecf15ec9e853346e185343bf7b'
-dest_api_key = local_key
+def main( source_media_cloud_api_url, dest_media_cloud_api_url, source_api_key, dest_api_key, db_label ):
+    conn = mc_database.connect_to_database( db_label )
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-db_label =  "AWS backup crawler"
+    print "fetching feed downloads from postgresql"
+    cursor.execute( "SELECT downloads_id from downloads where type='feed' and state in ( 'success', 'feed_error') order by downloads_id limit 100" )
+    feed_downloads = cursor.fetchall()
 
-conn = mc_database.connect_to_database( db_label )
-cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    feed_downloads_processed = 0
 
-cursor.execute( "SELECT * from downloads where type='feed' and state in ( 'success', 'feed_error') order by downloads_id limit 10" )
-feed_downloads = cursor.fetchall()
+    print "exporting feed downloads with API"
 
-for feed_download in feed_downloads:
-    download = get_download_from_api( source_media_cloud_api_url, source_api_key, feed_download['downloads_id'] )
-    #print download
-    #break
-    raw_content = download['raw_content' ]
-    del download['raw_content']
+    for feed_download in feed_downloads:
+        download = get_download_from_api( source_media_cloud_api_url, source_api_key, feed_download['downloads_id'] )
+        #print download
+        #break
+        raw_content = download['raw_content' ]
+        del download['raw_content']
 
-    if download[ 'state' ] == 'feed_error':
-        download[ 'state' ]  = 'success'
-    add_feed_download_with_api( dest_media_cloud_api_url, dest_api_key, download, raw_content )
-    
+        if download[ 'state' ] == 'feed_error':
+            download[ 'state' ]  = 'success'
+        add_feed_download_with_api( dest_media_cloud_api_url, dest_api_key, download, raw_content )
+
+        feed_downloads_processed+= 1
+
+        if feed_downloads_processed % 10 == 0:
+            print "Processed " + str( feed_downloads_processed ) + " downloads "
+            print "last download ", feed_download
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Export feed downloads through API.')
+
+parser.add_argument( '--source-api-key', required=True )
+parser.add_argument( '--dest-api-key', required=True )
+parser.add_argument( '--source-media-cloud-api_url', required=True )
+parser.add_argument( '--dest-media-cloud-api_url', required=True )
+parser.add_argument( '--db-label', required=False, default=None )
+
+args = parser.parse_args()
+
+source_media_cloud_api_url = args.source_media_cloud_api_url
+dest_media_cloud_api_url = args.dest_media_cloud_api_url
+source_api_key = args.source_api_key
+dest_api_key = args.dest_api_key
+db_label = args.db_label
+
+main ( source_media_cloud_api_url, dest_media_cloud_api_url, source_api_key, dest_api_key, db_label )
