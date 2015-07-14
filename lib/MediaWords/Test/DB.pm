ea2fdcea85f7e12ceba1947e7916702c89ad42d7
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2015-07-14 19:12:11 +0300

    After running a test on a test database, reset "MEDIAWORDS_FORCE_USING_TEST_DATABASE" environment variable to the previous value

--- lib/MediaWords/Test/DB.pm
+++ lib/MediaWords/Test/DB.pm
@@ -63,14 +63,18 @@ sub test_on_test_database
 {
     my ( $sub ) = @_;
 
-    $ENV{ MEDIAWORDS_FORCE_USING_TEST_DATABASE } = 1;
-
     my $result = MediaWords::Pg::Schema::recreate_db( 'test' );
 
     die "Error recreating the test db" unless $result == 0;
 
     my $db = MediaWords::DB::connect_to_db( 'test' );
+
+    my $previous_force_using_test_db_value = $ENV{ MEDIAWORDS_FORCE_USING_TEST_DATABASE };
+    $ENV{ MEDIAWORDS_FORCE_USING_TEST_DATABASE } = 1;
+
     eval { $sub->( $db ); };
+
+    $ENV{ MEDIAWORDS_FORCE_USING_TEST_DATABASE } = $previous_force_using_test_db_value;
 
     if ( $@ )
     {
