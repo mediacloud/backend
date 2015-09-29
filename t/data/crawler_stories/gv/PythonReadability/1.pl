16adb4905f9f03f115e66826b0c875362ad793bd
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2015-09-29 14:45:15 +0300

    Add expected test output for PythonReadability extractor method

---
+++ t/data/crawler_stories/gv/PythonReadability/1.pl
@@ -0,0 +1,165 @@
+#<<<
+use utf8;
+
+$VAR1 = {
+		# This data file is only used in test_extractor.t so it has only a limited number of keys
+		'title' => 'Brazil: Amplified conversations to fight the Digital Crimes Bill',
+
+		# Note two extra spaces added after EOF
+		'extracted_text' => <<'EOF' . '  ',
+Brazil: Amplified conversations to fight the Digital Crimes Bill
+***
+
+
+***
+
+Brazil: Amplified conversations to fight the Digital Crimes Bill
+
+  
+
+ 
+
+ 
+					
+
+ 
+ 
+
+
+
+    
+
+ Art: Luciano Matsuzaki and Mariana Lettis 
+
+ 
+
+
+
+
+ On May 14th a  protest  [pt] against the  Digital Crimes Bill  proposed by Senator Azeredo to typify crimes on the Internet took place in São Paulo to promote debate. On May 25th, it was time for a  demonstration  in Porto Alegre. On June 1st, a similar protest happened in Minas Gerais and a  new one is being planned  now in Rio de Janeiro. 
+
+
+
+
+ These protests have been called “Against the Digital AI-5” after the Brazilian dictatorship's Institutional Act Number Five or “ AI-5 “, the fifth, and considered the most cruel, of seventeen decrees issued by the military dictatorship in the years following the 1964 coup d'état in Brazil. Issued in 1968, AI-5 abolished freedom of expression by introducing the preliminary censorship of music, films, theater and television. Any work considered subversive to the political and moral values of the country was censored and artists jailed. AI-5 marked the transition to the toughest period  of human rights violations in Brazilian history. 
+
+
+
+
+ All over Brazil, mobilization against the proposed bill has been organized by a group of bloggers, who have put together a manifesto, a  Twitter account  and  Mega Não!  [Mega No!, pt], a blog created with the single purpose of gathering information and contributing to clarify what this bill might mean for Internet users in the country: 
+
+
+
+
+ 
+
+ A proposta do Mega Não, é ser um meta manifesto, um agregador de informações e de diversas manifestações na Internet e fora dela, com o objetivo de combater o vigilantismo. Diversos núcleos ciberativistas estão surgindo e aumentando o discurso e a pressão popular contra o vigilantismo, tenta agregar, fomentar e ajudar a divulgar estes eventos é a nossa proposta, nos informe de seus movimentos, vamos juntar forças!!! 
+
+ 
+
+
+
+
+ Mega Não's aim is to be a meta manifesto, a place to gather information about the various protests on the Internet and outside it, with the aim to fight surveillance. Many cyberactivist groups are emerging and increasing both the discussion of and popular pressure against vigilantism. To try to aggregate, foster and help promote those demonstrations is our main goal. Tell us about your demonstrations, let's join forces together! 
+
+
+
+
+ An  online petition  against the Digital Crimes Bill released back in 2008 has already gathered 146,627 signatures. Users of Orkut, one of the most popular online social networks in Brazil, have also entered this fight, since many of the demands of this bill are related to this specific social network. One of many related communities, “ Não ao projeto de Azeredo ” [No to the Azeredo Bill, pt], has already attracted 6,563 members. 
+
+
+
+
+ Activists are also using Twitter as an informative tool for their meetings and collaborative blogging about the subject: the hashtags  #meganao  and  #ai5digital  are usually used by bloggers and tweeters and represent a source of information for those seeking updates on how to contribute to the campaign. In addition to this, many of the demonstrations' organizers, supporters and enthusiasts can also be found on Twitter, such as  @caribe ,  @pauloteixeira13 ,  @samadeu ,  @arlesophia  and  @myris . To keep track with all the details on the repercussions of the demonstrations and the Azeredo Bill itself, people are also using  Delicious  tagged bookmarks to gather information in  one place . 
+
+
+
+
+    
+
+ "No, Azeredo", in São Paulo. Photo by Andre Deak, published under a Creative Commons license. 
+
+ 
+
+
+
+
+ Another interesting fact is that mainstream media has not been covering this situation satisfactorily. For many Brazilian bloggers and Internet users, it looks like the media outlets, in addition to Internet companies and the government, are willing to see the bill approved, so that they will have control over what people say and do all over the Web. Antonio Arles ( @arlesophia ) comments on this in his  blog  [pt] and stresses how the Internet is useful to gather information about the demonstrations: 
+
+
+
+
+ 
+
+ Participei (presencialmente ou via Internet) de pelo menos três atos este ano em que a Rede foi importante para a organização, difusão e transmissão: o Ato contra a “Ditabranda” da Folha, o Saia Gilmar e o Ato Contra o AI-5 Digital. E é justamente essa possibilidade de exercício da cidadania um dos motivos da adesão dos oligopólios midiáticos ao AI-5 Digital. A lógica é a mesma da dos órgãos de repressão do Império Soviético ou da Ditadura Civil-Militar Brasileira. Apesar de usaram a máscara de “democratas” - usando argumentos como o da liberdade quando seus interesses estão em jogo, tentando confundir “liberdade de imprensa” com “liberdade da empresa” -, os oligopólios midiáticos são fruto de uma ditadura. Em última análise, são uma das faces da Ditadura Civil-Militar que continuam vivas depois da redemocratização. A questão é que, para além dos interesses econômicos, a tentativa de criminalização e de aprofundar vigilantismo na Internet é uma tentativa de manutenção do monopólio da informação/opinião e, conseqüentemente, do poder. 
+
+ 
+
+
+
+
+ 
+
+ I have participated (in person or online) in at least three demonstrations this year for which the Internet was an important organizational, promotional and broadcasting tool: the action against the “ 
+
+ Ditabranda 
+
+ ” editorial of Folha de São Paulo newspaper, the Fora Gilmar [a protest calling for Chief Justice   
+
+ Gilmar Mendes 
+
+  to leave Brazil's Supreme Court] and the action against the Digital AI-5. And this possibility of exercising our citizenship is actually one of the reasons for the mass media's support of the Digital AI-5. The logic is the same as those of the suppression apparatus of the Soviet Empire or Brazilian Dictatorship. Although they wear the masks of democrats - arguing about freedom when actually their interests are at stake, and trying to confuse “press freedom” with “company freedom” - the mass media companies are a child of the dictatorship. In the last case, they are the faces of the civic-military dictatorship that live on after the re-democratization. The point is: beyond their economic interests, the attempt to criminalize and deepen vigilantism on the Web is actually an attempt to keep the monopoly of information/opinion and, consequently, of power. 
+
+ 
+
+
+
+
+    
+
+ Protest in São Paulo. Photo by Andre Deak, published under a Creative Commons license. 
+
+ 
+
+
+
+
+ Whether this bill will be passed or not, it seems that the main aim of the Brazilian authorities is to indulge international copyright companies. In an interview for the  Observatório do Direito à Comunicação  blog [Observatory of the Right for Communication, pt]  Paulo Teixeira, a PT [Labour Party] member of parliament for São Paulo, stresses: 
+
+
+
+
+ 
+
+ Ele pretende (o Projeto de Lei Azeredo), em primeiro lugar, introduzir no Brasil uma coisa que alguns países estão introduzindo e que no Brasil, na minha opinião, nós não deveríamos permitir que se introduzisse. Ele está querendo introduzir os mecanismos de controle da internet para a garantia do direito autoral. 
+
+ 
+
+
+
+
+ It (the Azeredo Cybercrimes Bill) intends, in the first place, to introduce in Brazil a practice that many countries are adopting, and that in our country, in my opinion, we should not let happen. It aims to introduce strict Internet control mechanisms to guarantee copyright perpetuation. 
+
+
+
+
+  
+
+ Signs read "Against the Azeredo Law Project" by @marioamaya 
+
+ 
+
+
+
+
+ The Digital Crimes Bill has been discussed in  several previous posts on Global Voices Online . Now it is up to activists, cyberactivists and Internet users to fight against the Digital Crimes Bill with even more strength. Otherwise, the Brazilian Internet might be turned into a place where people are guilty before they have even realized what they are being charged with. 
+
+
+				 
+
+
+				 
+
+EOF
+};
+#>>>
