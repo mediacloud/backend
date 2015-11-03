607b4537ea8bed2102c7ca02849576a8fb78cc96
Hal Roberts <hroberts@cyber.law.harvard.edu>
2015-11-03 14:53:15 -0600

    added solr imported stories to daily stats

---
+++ sql_migrations/mediawords-4514-4515.sql
@@ -0,0 +1,63 @@
+--
+-- This is a Media Cloud PostgreSQL schema difference file (a "diff") between schema
+-- versions 4514 and 4515.
+--
+-- If you are running Media Cloud with a database that was set up with a schema version
+-- 4514, and you would like to upgrade both the Media Cloud and the
+-- database to be at version 4515, import this SQL file:
+--
+--     psql mediacloud < mediawords-4514-4515.sql
+--
+-- You might need to import some additional schema diff files to reach the desired version.
+--
+--
+-- 1 of 2. Import the output of 'apgdiff':
+--
+
+CREATE OR REPLACE VIEW daily_stats AS
+    SELECT *
+    FROM (
+            SELECT COUNT(*) AS daily_downloads
+            FROM downloads_in_past_day
+         ) AS dd,
+         (
+            SELECT COUNT(*) AS daily_stories
+            FROM stories_collected_in_past_day
+         ) AS ds,
+         (
+            SELECT COUNT(*) AS downloads_to_be_extracted
+            FROM downloads_to_be_extracted
+         ) AS dex,
+         (
+            SELECT COUNT(*) AS download_errors
+            FROM downloads_with_error_in_past_day
+         ) AS er,
+         (
+            SELECT SUM( num_stories ) AS solr_stories
+            FROM solr_imports WHERE import_date > now() - interval '1 day'
+         ) AS si;
+
+--
+-- 2 of 2. Reset the database version.
+--
+
+CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
+DECLARE
+
+    -- Database schema version number (same as a SVN revision number)
+    -- Increase it by 1 if you make major database schema changes.
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4515;
+
+BEGIN
+
+    -- Update / set database schema version
+    DELETE FROM database_variables WHERE name = 'database-schema-version';
+    INSERT INTO database_variables (name, value) VALUES ('database-schema-version', MEDIACLOUD_DATABASE_SCHEMA_VERSION::int);
+
+    return true;
+
+END;
+$$
+LANGUAGE 'plpgsql';
+
+SELECT set_database_schema_version();
