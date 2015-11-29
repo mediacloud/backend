d3c03c392899f6868e5f888df7714fc9bf354d87
Hal Roberts <hroberts@cyber.law.harvard.edu>
2015-11-29 09:33:30 -0600

    fix undef warning

--- lib/MediaWords/DBI/Downloads.pm
+++ lib/MediaWords/DBI/Downloads.pm
@@ -356,7 +356,7 @@ sub _download_stores_for_reading($)
     # Overrides:
 
     # GridFS downloads have to be fetched from S3?
-    if ( lc( get_config->{ mediawords }->{ read_all_downloads_from_s3 } eq 'yes' ) )
+    if ( lc( get_config->{ mediawords }->{ read_all_downloads_from_s3 } || '' ) eq 'yes' )
     {
         $download_store = 'amazon_s3';
     }
