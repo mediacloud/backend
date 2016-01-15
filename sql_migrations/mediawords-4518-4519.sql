565b1520af3ce95dcac9529c4b9e28b39ec05384
Hal Roberts <hroberts@cyber.law.harvard.edu>
2016-01-15 13:30:55 -0600

    fix bug in initial story_sentences_dup creation

--- sql_migrations/mediawords-4518-4519.sql
+++ sql_migrations/mediawords-4518-4519.sql
@@ -16,6 +16,8 @@
 
 alter table story_sentences add is_dup boolean null;
 
+-- we have to do this in a function to create the partial index on a constant value,
+-- which you cannot do with a simple 'create index ... where publish_date > now()'
 create or replace function create_initial_story_sentences_dup() RETURNS boolean as $$
 declare
     one_month_ago date;
@@ -25,13 +27,13 @@ begin
     raise notice 'date: %', one_month_ago;
 
     execute 'create index story_sentences_dup on story_sentences( md5( sentence ) ) ' ||
-        'where week_start_date( publish_date::date ) > $1::date'
-        using one_month_ago;
+        'where week_start_date( publish_date::date ) > ''' || one_month_ago || '''::date';
 
     return true;
 END;
 $$ LANGUAGE plpgsql;
 
+select create_initial_story_sentences_dup();
 
 --
 -- 2 of 2. Reset the database version.
