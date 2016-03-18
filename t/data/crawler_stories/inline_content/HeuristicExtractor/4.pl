dbb50b47fb641c87cc9373b7769712e8f58c258a
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-03-18 21:52:27 +0200

    Update crawler test data
    * Use integer story_sentences_id
    * Strip HTML from story description

--- t/data/crawler_stories/inline_content/HeuristicExtractor/4.pl
+++ t/data/crawler_stories/inline_content/HeuristicExtractor/4.pl
@@ -1,13 +1,13 @@
 #<<<
 $VAR1 = {
-          'collect_date' => '2016-03-08 22:44:07.102651',
+          'collect_date' => '2016-03-18 21:39:24.640135',
           'content' => '<p>
     This RSS feed is used to test whether very short downloads (stored directly
     in the <code>downloads.path</code> as <code>"content:..."</code>) are being
     stored / extracted / fetched correctly.
 </p>
 ',
-          'db_row_last_updated' => '2016-03-08 22:44:07.337469-06',
+          'db_row_last_updated' => '2016-03-18 21:39:24.739785+02',
           'description' => undef,
           'disable_triggers' => undef,
           'extracted_text' => '(no title)
@@ -21,28 +21,28 @@ $VAR1 = {
 ',
           'feed_type' => 'web_page',
           'full_text_rss' => 0,
-          'guid' => '1457498647:http://raven.local:58903//inline_content/home.html',
+          'guid' => '1458329964:http://tundra.local:65016//inline_content/home.html',
           'language' => 'en',
           'media_id' => 1,
-          'publish_date' => '2016-03-08 21:44:07',
+          'publish_date' => '2016-03-18 21:39:24',
           'stories_id' => 4,
           'story_sentences' => [
                                  {
-                                   'db_row_last_updated' => '2016-03-08 22:44:07.337469-06',
+                                   'db_row_last_updated' => '2016-03-18 21:39:24.739785+02',
                                    'disable_triggers' => 0,
                                    'is_dup' => undef,
                                    'language' => 'en',
                                    'media_id' => 1,
-                                   'publish_date' => '2016-03-08 21:44:07',
+                                   'publish_date' => '2016-03-18 21:39:24',
                                    'sentence' => 'This RSS feed is used to test whether very short downloads (stored directly stored / extracted / fetched correctly.',
                                    'sentence_number' => 0,
                                    'stories_id' => 4,
-                                   'story_sentences_id' => '1'
+                                   'story_sentences_id' => 1
                                  }
                                ],
           'tags' => [],
-          'timezone' => 'America/Denver',
+          'timezone' => 'Europe/Vilnius',
           'title' => '(no title)',
-          'url' => 'http://raven.local:58903//inline_content/home.html'
+          'url' => 'http://tundra.local:65016//inline_content/home.html'
         };
 #>>>
