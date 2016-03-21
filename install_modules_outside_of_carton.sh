b2ae955cade0662b097bd1411ee1c138048d5707
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-03-21 15:49:13 +0200

    Upgrade Carton

--- install_modules_outside_of_carton.sh
+++ install_modules_outside_of_carton.sh
@@ -27,7 +27,7 @@ $CPANM CPAN~2.10
 # (if the install of Carton 1.0.9 fails because of CPAN::Meta failure,
 # purge Perl with ./install_scripts/purge_mc_perl_brew.sh and
 # ./install_scripts/purge_carton_install.sh)
-$CPANM Carton~1.0.9
+$CPANM Carton~1.0.22
 
 $CPANM List::MoreUtils
 
