bb1cba36582c83aa0872dd065db41ed6ccd3aa08
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-04-19 07:13:00 +0300

    Reorder migration file so that dropped objects don't depend on each other

--- sql_migrations/mediawords-4530-4531.sql
+++ sql_migrations/mediawords-4530-4531.sql
@@ -18,14 +18,15 @@
 SET search_path = public, pg_catalog;
 
 
-DROP TABLE extractor_training_lines;
-DROP TABLE extracted_lines;
-
+DROP VIEW IF EXISTS media_adjusted_extractor_training_downloads_count;
+DROP VIEW IF EXISTS media_extractor_training_downloads_count_adjustments;
 DROP VIEW IF EXISTS media_extractor_training_downloads_count;
 DROP VIEW IF EXISTS yahoo_top_political_2008_media;
 DROP VIEW IF EXISTS technorati_top_political_2008_media;
-DROP VIEW IF EXISTS media_extractor_training_downloads_count_adjustments;
-DROP VIEW IF EXISTS media_adjusted_extractor_training_downloads_count;
+
+
+DROP TABLE extractor_training_lines;
+DROP TABLE extracted_lines;
 
 
 CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
