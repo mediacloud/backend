21f76cfa2dc23d15072fb0d22b75519d403e1c35
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-04-19 06:22:22 +0300

    Remove unused configuration parameter "disable_json_top_500_words_cache"

--- script/mediawords_query_config.pl
+++ script/mediawords_query_config.pl
@@ -87,7 +87,6 @@ use XML::LibXML;
 #     <controversy_model_reps>25</controversy_model_reps>
 #     <data_dir>/Users/pypt/Dropbox/etc-MediaCloud/trunk/script/../lib/MediaWords/Util/../../../data</data_dir>
 #     <default_home_page>admin/media/list</default_home_page>
-#     <disable_json_top_500_words_cache>no</disable_json_top_500_words_cache>
 #     <download_storage_locations>postgresql</download_storage_locations>
 #     <language>en_US_and_ru_RU</language>
 #     <owner>mediawords@cyber.law.harvard.edu</owner>
