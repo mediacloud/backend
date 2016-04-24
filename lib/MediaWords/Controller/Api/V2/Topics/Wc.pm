c1bc15d5a76e00d08a0b0bf981139c38c6432c3d
Sean Wheeler <swheeler@media.mit.edu>
2016-04-24 19:45:22 -0400

    Add sort rank to wordcount

--- lib/MediaWords/Controller/Api/V2/Topics/Wc.pm
+++ lib/MediaWords/Controller/Api/V2/Topics/Wc.pm
@@ -19,13 +19,13 @@ BEGIN { extends 'MediaWords::Controller::Api::V2::MC_Controller_REST' }
 
 __PACKAGE__->config( action => { list_GET => { Does => [ qw( ) ] }, } );
 
-sub apibase: Chained('/') : PathPart('api/v2/topics') : CaptureArgs(1)
+sub apibase : Chained('/') : PathPart('api/v2/topics') : CaptureArgs(1)
 {
-  my ($self, $c, $topic_id) = @_;
-  $c->stash->{topic_id} = $topic_id;
+    my ( $self, $c, $topic_id ) = @_;
+    $c->stash->{ topic_id } = $topic_id;
 }
 
-sub wc: Chained('apibase') : PathPart('wc') : CaptureArgs(0)
+sub wc : Chained('apibase') : PathPart('wc') : CaptureArgs(0)
 {
 
 }
@@ -37,21 +37,33 @@ sub list : Chained('wc') : Args(0) : ActionClass('REST')
 
 sub list_GET
 {
-  my ( $self, $c ) = @_;
+    my ( $self, $c ) = @_;
 
-  if ( $c->req->params->{ sample_size } && ( $c->req->params->{ sample_size } > 100_000 ) )
-  {
-      $c->req->params->{ sample_size } = 100_000;
-  }
-  my $cdts = MediaWords::CM::get_time_slice_for_controversy($c->dbis, $c->stash->{ topic_id }, $c->req->params->{ timeslice }, $c->req->params->{ snapshot });
-  if ($cdts) {
-    my $query = "{~ controversy_dump_time_slice:$cdts->{ controversy_dump_time_slices_id } }";
-    my $wc = MediaWords::Solr::WordCounts->new( { db => $c->dbis, q=> $query } );
-    my $words = $wc->get_words;
-    $self->status_ok( $c, entity => $words );
-  } else {
-    $self->status_bad_request( $c, message=> "could not retrieve word counts")
-  }
+    if ( $c->req->params->{ sample_size } && ( $c->req->params->{ sample_size } > 100_000 ) )
+    {
+        $c->req->params->{ sample_size } = 100_000;
+    }
+    my $cdts = MediaWords::CM::get_time_slice_for_controversy(
+        $c->dbis,
+        $c->stash->{ topic_id },
+        $c->req->params->{ timeslice },
+        $c->req->params->{ snapshot }
+    );
+    if ( $cdts )
+    {
+        my $query  = "{~ controversy_dump_time_slice:$cdts->{ controversy_dump_time_slices_id } }";
+        my $wc     = MediaWords::Solr::WordCounts->new( { db => $c->dbis, q => $query } );
+        my $entity = {};
+        my $words  = $wc->get_words;
+        map { $words->[ $_ ]->{ rank } = $_ + 1 } ( 0 .. $#{ $words } );
+        $entity->{ timeslice } = $cdts;
+        $entity->{ words }     = $words;
+        $self->status_ok( $c, entity => $entity );
+    }
+    else
+    {
+        $self->status_bad_request( $c, message => "could not retrieve word counts" );
+    }
 
 }
 
