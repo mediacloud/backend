9d6bf4e9b4d054c7eb488f6e9b5424112a7199d9
Sean Wheeler <swheeler@media.mit.edu>
2016-04-24 20:29:57 -0400

    Wrap stories list into hash in Stories endpoint

--- lib/MediaWords/Controller/Api/V2/Topics/Stories.pm
+++ lib/MediaWords/Controller/Api/V2/Topics/Stories.pm
@@ -18,13 +18,13 @@ BEGIN { extends 'MediaWords::Controller::Api::V2::MC_Controller_REST' }
 
 __PACKAGE__->config( action => { list_GET => { Does => [ qw( ) ] }, } );
 
-sub apibase: Chained('/') : PathPart('api/v2/topics') : CaptureArgs(1)
+sub apibase : Chained('/') : PathPart('api/v2/topics') : CaptureArgs(1)
 {
-    my ($self, $c, $topic_id) = @_;
-    $c->stash->{topic_id} = $topic_id;
+    my ( $self, $c, $topic_id ) = @_;
+    $c->stash->{ topic_id } = $topic_id;
 }
 
-sub stories: Chained('apibase') : PathPart('stories') : CaptureArgs(0)
+sub stories : Chained('apibase') : PathPart('stories') : CaptureArgs(0)
 {
 
 }
@@ -36,21 +36,29 @@ sub list : Chained('stories') : Args(0) : ActionClass('REST')
 
 sub list_GET
 {
-  my ( $self, $c ) = @_;
-  my $db = $c->dbis;
-  my $cdts = MediaWords::CM::get_time_slice_for_controversy($c->dbis, $c->stash->{ topic_id }, $c->req->params->{ timeslice }, $c->req->params->{ snapshot });
-  my $stories = undef;
-  if ($cdts) {
-    my $stories = $db->query( <<SQL, $cdts->{ controversy_dump_time_slices_id } )->hashes;
+    my ( $self, $c ) = @_;
+    my $db   = $c->dbis;
+    my $cdts = MediaWords::CM::get_time_slice_for_controversy(
+        $c->dbis,
+        $c->stash->{ topic_id },
+        $c->req->params->{ timeslice },
+        $c->req->params->{ snapshot }
+    );
+    my $entity = {};
+    $entity->{ timeslice } = $cdts;
+    if ( $cdts )
+    {
+        $entity->{ stories } = $db->query( <<SQL, $cdts->{ controversy_dump_time_slices_id } )->hashes;
 select * from cd.story_link_counts slc
   join cd.stories s on slc.stories_id = s.stories_id
   where slc.controversy_dump_time_slices_id = \$1
 SQL
-    print Dumper($cdts);
-    $self->status_ok( $c, entity => $stories );
-  } else {
-    $self->status_bad_request( $c, message=> "could not retrieve word counts")
-  }
+        $self->status_ok( $c, entity => $entity );
+    }
+    else
+    {
+        $self->status_bad_request( $c, message => "could not retrieve word counts" );
+    }
 }
 
 1;
