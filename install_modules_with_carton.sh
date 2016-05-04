c9c2e20b3b4e95a33c927032f6e36d5aefeb4b58
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-05-04 04:53:47 +0300

    Upgrade to MediaCloud::JobManager 0.19 with RabbitMQ + Celery support
    On OS X, install Net::AMQP::RabbitMQ separately because it needs a custom
    CCFLAGS/LDFLAGS/LD set.

--- install_modules_with_carton.sh
+++ install_modules_with_carton.sh
@@ -22,6 +22,20 @@ else
     OPENSSL_PREFIX="/usr"
 fi
 
+# OS X no longer provides OpenSSL headers and Net::AMQP::RabbitMQ doesn't care
+# about OPENSSL_PREFIX so we need to set CCFLAGS and install the module
+# separately.
+#
+# Additionally, we reset LD to just "cc" in order to remove
+# MACOSX_DEPLOYMENT_TARGET parameter.
+if [ `uname` == 'Darwin' ]; then
+    ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
+        --configure-args="CCFLAGS=\"-I${OPENSSL_PREFIX}/include\" LDFLAGS=\"-L${OPENSSL_PREFIX}/lib\" LD=\"env cc\"" \
+        --local-lib-contained local/ \
+        --verbose \
+        --notest \
+        Net::AMQP::RabbitMQ~2.000000
+fi
 
 # Install dependency modules; run the command twice because the first
 # attempt might fail
