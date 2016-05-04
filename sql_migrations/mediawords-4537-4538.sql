1d3bf66466804af630477fae2cee9b51e8b31c03
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-05-04 10:09:53 +0300

    DROP tables / functions only if they exist

--- sql_migrations/mediawords-4537-4538.sql
+++ sql_migrations/mediawords-4537-4538.sql
@@ -19,8 +19,8 @@
 SET search_path = public, pg_catalog;
 
 
-DROP TABLE gearman_job_queue;
-DROP FUNCTION gearman_job_queue_sync_lastmod();
+DROP TABLE IF EXISTS gearman_job_queue;
+DROP FUNCTION IF EXISTS gearman_job_queue_sync_lastmod();
 
 COMMENT ON COLUMN controversies.process_with_bitly
   IS 'Enable processing controversy''s stories with Bit.ly; add all new controversy stories to Bit.ly processing queue';
