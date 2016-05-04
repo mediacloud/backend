5b2af1968528129357d9cb18b10b9308538262a9
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-05-04 08:25:26 +0300

    Increase the max. open file limit to 65536
    This is the recommended min. open file limit for RabbitMQ in production
    environments: https://www.rabbitmq.com/install-debian.html#kernel-resource-limits

--- install_scripts/set_kernel_parameters.sh
+++ install_scripts/set_kernel_parameters.sh
@@ -35,3 +35,38 @@ EOF
 fi
 
 echo "Done setting required kernel parameters via sysctl."
+
+# ---
+
+echo "Setting required kernel parameters via limits.conf..."
+
+if [ `uname` == 'Darwin' ]; then
+    # Mac OS X -- nothing to do (not likely to be running production environment on OS X)
+    :
+else
+
+    LIMITS_FILE=/etc/limits.d/50-mediacloud.conf
+
+    if [ -f "$LIMITS_FILE" ]; then
+        echo "Limits file $LIMITS_FILE already exists, please either remove it or add parameters manually."
+        exit 1
+    fi
+
+    # Overwrite
+    sudo tee "$LIMITS_FILE" <<EOF
+#
+# Media Cloud limits
+#
+
+# Each process is limited up to ~34 GB of memory
+mediacloud      hard    as            33554432
+
+# Increase the max. open files limit
+mediacloud      soft    nofile        65536
+mediacloud      hard    nofile        65536
+EOF
+
+fi
+
+echo "Done setting required kernel parameters via limits.conf."
+
