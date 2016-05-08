d99afcd4cbe89c2b3f7299d13618acd26a979f0a
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-05-09 01:59:16 +0300

    Set limits for current user, assuming that's the Media Cloud user
    [ci skip]

--- install_scripts/set_kernel_parameters.sh
+++ install_scripts/set_kernel_parameters.sh
@@ -38,7 +38,8 @@ echo "Done setting required kernel parameters via sysctl."
 
 # ---
 
-echo "Setting required kernel parameters via limits.conf..."
+MEDIACLOUD_USER=`id -un`
+echo "Setting required kernel parameters via limits.conf for user '$MEDIACLOUD_USER'..."
 
 if [ `uname` == 'Darwin' ]; then
     # Mac OS X -- nothing to do (not likely to be running production environment on OS X)
@@ -59,11 +60,11 @@ else
 #
 
 # Each process is limited up to ~34 GB of memory
-mediacloud      hard    as            33554432
+$MEDIACLOUD_USER      hard    as            33554432
 
 # Increase the max. open files limit
-mediacloud      soft    nofile        65536
-mediacloud      hard    nofile        65536
+$MEDIACLOUD_USER      soft    nofile        65536
+$MEDIACLOUD_USER      hard    nofile        65536
 EOF
 
 fi
