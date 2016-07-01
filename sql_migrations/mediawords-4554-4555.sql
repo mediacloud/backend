e81fd1d91b53771afb14da987d50c5c5dbfae22d
Hal Roberts <hroberts@cyber.law.harvard.edu>
2016-07-01 11:13:08 -0500

    fix sql migration

--- sql_migrations/mediawords-4554-4555.sql
+++ sql_migrations/mediawords-4554-4555.sql
@@ -19,7 +19,19 @@ alter table cd.story_link_counts add facebook_share_count int null;
 update cd.story_link_counts slc set facebook_share_count = ss.facebook_share_count
     from story_statistics ss where ss.stories_id = slc.stories_id;
 
+drop view controversies_with_dates;
 alter table controversies drop column process_with_bitly;
+create view controversies_with_dates as
+    select c.*,
+            to_char( cd.start_date, 'YYYY-MM-DD' ) start_date,
+            to_char( cd.end_date, 'YYYY-MM-DD' ) end_date
+        from
+            controversies c
+            join controversy_dates cd on ( c.controversies_id = cd.controversies_id )
+        where
+            cd.boundary;
+
+
 
 --
 -- 2 of 2. Reset the database version.
