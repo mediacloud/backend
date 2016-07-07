069f02915d7ebb29a8aa25b221c1cc15b23515c3
Hal Roberts <hroberts@cyber.law.harvard.edu>
2016-07-07 15:45:23 -0500

    add media_inlink metrics

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -45,7 +45,7 @@ DECLARE
 
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4555;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4556;
 
 BEGIN
 
@@ -1911,6 +1911,8 @@ create table cd.story_link_counts (
     controversy_dump_time_slices_id         int not null
                                             references controversy_dump_time_slices on delete cascade,
     stories_id                              int not null,
+    media_inlink_count                      int not null,
+    sum_media_inlink_count                  int not null,
     inlink_count                            int not null,
     outlink_count                           int not null,
 
@@ -1929,6 +1931,7 @@ create table cd.medium_link_counts (
     controversy_dump_time_slices_id int not null
                                     references controversy_dump_time_slices on delete cascade,
     media_id                        int not null,
+    media_inlink_count              int not null,
     inlink_count                    int not null,
     outlink_count                   int not null,
     story_count                     int not null,
