41fb8925f19570f0aae8743d087d46d22cbe7347
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-07-11 04:48:21 +0300

    Remove old partial "story_sentences_dup" index and a maintaining script
    There's now a table-wide "story_sentences_sentence_half_md5" index to replace it.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -45,7 +45,7 @@ DECLARE
 
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4561;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4562;
 
 BEGIN
 
@@ -1042,26 +1042,6 @@ SELECT create_index_if_not_exists(
     'story_sentences_sentence_half_md5',
     '(half_md5(sentence))'
 );
-
--- we have to do this in a function to create the partial index on a constant value,
--- which you cannot do with a simple 'create index ... where publish_date > now()'
-create or replace function create_initial_story_sentences_dup() RETURNS boolean as $$
-declare
-    one_month_ago date;
-begin
-    select now() - interval '1 month' into one_month_ago;
-
-    raise notice 'date: %', one_month_ago;
-
-    execute 'create index story_sentences_dup on story_sentences( md5( sentence ) ) ' ||
-        'where week_start_date( publish_date::date ) > ''' || one_month_ago || '''::date';
-
-    return true;
-END;
-$$ LANGUAGE plpgsql;
-
-select create_initial_story_sentences_dup();
-
 
 ALTER TABLE story_sentences
     ADD CONSTRAINT story_sentences_media_id_fkey
