be3640486faa82c415da64bcfbb8ea0f08bea09b
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-07-19 10:25:19 +0300

    Rename "hosts" to "host"

--- solr/reload_solr_shards.py
+++ solr/reload_solr_shards.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
                                             "Run 'update_zookeeper_config.py' before.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-c", "--shard_count", type=int, required=True, help="Number of shards to expect on each host.")
-    parser.add_argument("-s", "--hosts", type=str, required=False, action="store", nargs="*", default=["localhost"],
-                        help="Hosts running Solr to reload.")
+    parser.add_argument("-s", "--host", type=str, required=False, action="store", nargs="*", default=["localhost"],
+                        help="Host(s) running Solr to reload.")
 
     args = parser.parse_args()
 
-    for host in args.hosts:
+    for host in args.host:
         reload_all_solr_shards(shard_count=args.shard_count, host=host)
