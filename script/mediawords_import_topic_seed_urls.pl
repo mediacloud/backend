02dae6f896fab94e2c626cc675e42dad2d6edae9
Hal Roberts <hroberts@cyber.law.harvard.edu>
2016-07-21 10:06:01 -0500

    refactor to topics language

--- script/mediawords_import_topic_seed_urls.pl
+++ script/mediawords_import_topic_seed_urls.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/env perl
 
-# given a csv of urls labeled by controversy, import each url into the
-# controversy_seed_urls table.
+# given a csv of urls labeled by topic, import each url into the
+# topic_seed_urls table.
 #
-# usage: mediawords_import_controversy_seed_urls.pl --file < csv file >
-#    [ --controversy < controversy id or name > ] [ --source < source description > ]
+# usage: mediawords_import_topic_seed_urls.pl --file < csv file >
+#    [ --topic < topic id or name > ] [ --source < source description > ]
 #
 # the csv must include these columns:
 # * url
 #
 # the csv must include these columns unless a default is specified on the command line:
-# * controversy
+# * topic
 # * source (text description of the source of the urls)
 #
 # the csv may include these columns, which will be directlly imported into the resulting story for each url:
@@ -18,7 +18,7 @@
 # * publish_date
 # * title
 # * guid
-# * assume_match - 1 or 0, should the controversy spider assume that the url matches the controversy?
+# * assume_match - 1 or 0, should the topic spider assume that the url matches the topic?
 
 use strict;
 use warnings;
@@ -35,39 +35,39 @@ use Getopt::Long;
 use MediaWords::DB;
 use MediaWords::Util::CSV;
 
-# find the controversy with the given controversies_id as either an id or a name.
-# die if no controversy is found.
-sub get_controversy
+# find the topic with the given topics_id as either an id or a name.
+# die if no topic is found.
+sub get_topic
 {
-    my ( $db, $csv_url, $default_controversy ) = @_;
+    my ( $db, $csv_url, $default_topic ) = @_;
 
-    return $default_controversy if ( $default_controversy );
+    return $default_topic if ( $default_topic );
 
-    my $controversies_id = $csv_url->{ controversy };
+    my $topics_id = $csv_url->{ topic };
 
-    die( "no controversy specified for url '$csv_url->{ url }'" ) unless ( $default_controversy || $controversies_id );
+    die( "no topic specified for url '$csv_url->{ url }'" ) unless ( $default_topic || $topics_id );
 
-    if ( $controversies_id =~ /^\d+$/ )
+    if ( $topics_id =~ /^\d+$/ )
     {
-        return $db->find_by_id( 'controversies', $controversies_id );
+        return $db->find_by_id( 'topics', $topics_id );
     }
-    elsif ( $controversies_id )
+    elsif ( $topics_id )
     {
-        return $db->query( "select * from controversies where name = ?", $controversies_id )->hash;
-    }
-
-    if ( $controversies_id )
-    {
-        die( "unable to find controversy '$controversies_id'" ) if ( $controversies_id );
+        return $db->query( "select * from topics where name = ?", $topics_id )->hash;
     }
 
-    return $default_controversy;
+    if ( $topics_id )
+    {
+        die( "unable to find topic '$topics_id'" ) if ( $topics_id );
+    }
+
+    return $default_topic;
 }
 
 # store the given seed url in the database if it does not already exist
 sub import_seed_url
 {
-    my ( $db, $default_controversy, $default_source, $csv_url ) = @_;
+    my ( $db, $default_topic, $default_source, $csv_url ) = @_;
 
     # print STDERR "$csv_url->{ url }...\n";
 
@@ -76,67 +76,67 @@ sub import_seed_url
     $csv_url->{ source } ||= $default_source;
     die( "No source for url '$csv_url->{ url }'" ) unless ( $csv_url->{ source } );
 
-    my $controversy = get_controversy( $db, $csv_url, $default_controversy );
+    my $topic = get_topic( $db, $csv_url, $default_topic );
 
-    my $existing_seed_url = $db->query( <<END, $csv_url->{ url }, $controversy->{ controversies_id } )->hash;
-select * from controversy_seed_urls where url = ? and controversies_id = ?
+    my $existing_seed_url = $db->query( <<END, $csv_url->{ url }, $topic->{ topics_id } )->hash;
+select * from topic_seed_urls where url = ? and topics_id = ?
 END
 
     if ( $existing_seed_url )
     {
         if ( $existing_seed_url->{ assume_match } != $csv_url->{ assume_match } )
         {
-            $db->query( <<END, $csv_url->{ assume_match }, $existing_seed_url->{ controversy_seed_urls_id } );
-update controversy_seed_urls set assume_match = ? where controversy_seed_urls_id = ?
+            $db->query( <<END, $csv_url->{ assume_match }, $existing_seed_url->{ topic_seed_urls_id } );
+update topic_seed_urls set assume_match = ? where topic_seed_urls_id = ?
 END
         }
 
         return;
     }
 
-    my $controversy_seed_url = {
-        url              => $csv_url->{ url },
-        controversies_id => $controversy->{ controversies_id },
-        source           => $csv_url->{ source },
-        assume_match     => $csv_url->{ assume_match },
-        content          => $csv_url->{ content },
-        publish_date     => $csv_url->{ publish_date },
-        guid             => $csv_url->{ guid },
-        title            => $csv_url->{ title }
+    my $topic_seed_url = {
+        url          => $csv_url->{ url },
+        topics_id    => $topic->{ topics_id },
+        source       => $csv_url->{ source },
+        assume_match => $csv_url->{ assume_match },
+        content      => $csv_url->{ content },
+        publish_date => $csv_url->{ publish_date },
+        guid         => $csv_url->{ guid },
+        title        => $csv_url->{ title }
     };
 
-    $db->create( 'controversy_seed_urls', $controversy_seed_url );
+    $db->create( 'topic_seed_urls', $topic_seed_url );
 }
 
 sub main
 {
-    my ( $controversies_id, $file, $source );
+    my ( $topics_id, $file, $source );
 
     binmode( STDOUT, 'utf8' );
     binmode( STDERR, 'utf8' );
 
     Getopt::Long::GetOptions(
-        "controversy=s" => \$controversies_id,
-        "file=s"        => \$file,
-        "source=s"      => \$source
+        "topic=s"  => \$topics_id,
+        "file=s"   => \$file,
+        "source=s" => \$source
     ) || return;
 
-    die( "usage: $0 --file < csv file > [ --controversy < controversy id or name > ] [ --source < source description > ]" )
+    die( "usage: $0 --file < csv file > [ --topic < topic id or name > ] [ --source < source description > ]" )
       unless ( $file );
 
     my $db = MediaWords::DB::connect_to_db;
 
-    my $default_controversy;
-    if ( $controversies_id )
+    my $default_topic;
+    if ( $topics_id )
     {
-        $default_controversy = get_controversy( $db, { controversy => $controversies_id, url => 'default' } );
+        $default_topic = get_topic( $db, { topic => $topics_id, url => 'default' } );
     }
 
     my $seed_urls = MediaWords::Util::CSV::get_csv_as_hashes( $file );
 
     $db->begin;
 
-    map { import_seed_url( $db, $default_controversy, $source, $_ ) } @{ $seed_urls };
+    map { import_seed_url( $db, $default_topic, $source, $_ ) } @{ $seed_urls };
 
     $db->commit;
 
