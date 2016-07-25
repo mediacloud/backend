59b4f84363e79c671e3da2988d1951654bc43423
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-07-25 15:35:34 +0300

    Create partitions in bitly_clicks_total_create_partitions() helper
    Creating partitions in INSERT trigger might lead to a race condition and is slow.
    
    References #40.

--- script/mediawords.sql
+++ script/mediawords.sql
@@ -20,7 +20,7 @@ DECLARE
 
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4568;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4569;
 
 BEGIN
 
@@ -62,6 +62,21 @@ LANGUAGE 'plpgsql' IMMUTABLE
 CREATE OR REPLACE FUNCTION half_md5(string TEXT) RETURNS bytea AS $$
     SELECT SUBSTRING(digest(string, 'md5'::text), 0, 9);
 $$ LANGUAGE SQL;
+
+
+-- Returns true if table exists (and user has access to it)
+CREATE OR REPLACE FUNCTION table_exists(target_table_name VARCHAR)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = CURRENT_SCHEMA()
+          AND table_name = target_table_name
+    );
+END;
+$$
+LANGUAGE plpgsql;
 
 
  -- Returns true if the date is greater than the latest import date in solr_imports
@@ -1515,70 +1530,14 @@ END;
 $$
 LANGUAGE plpgsql;
 
--- Automatic Bit.ly total click count partitioning to stories_id chunks of 1m rows
+-- Insert row into correct partition
 CREATE OR REPLACE FUNCTION bitly_clicks_total_partition_by_stories_id_insert_trigger()
 RETURNS TRIGGER AS $$
 DECLARE
     target_table_name TEXT;       -- partition table name (e.g. "bitly_clicks_total_000001")
-    target_table_owner TEXT;      -- partition table owner (e.g. "mediaclouduser")
-
-    stories_id_start INT;         -- stories_id chunk lower limit, inclusive (e.g. 30,000,000)
-    stories_id_end INT;           -- stories_id chunk upper limit, exclusive (e.g. 31,000,000)
 BEGIN
-
-    select bitly_get_partition_name( NEW.stories_id, 'bitly_clicks_total' ) into target_table_name;
-
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_schema = current_schema()
-          AND table_name = target_table_name
-    ) THEN
-
-        SELECT (NEW.stories_id / bitly_partition_chunk_size() ) * bitly_partition_chunk_size() INTO stories_id_start;
-        SELECT ((NEW.stories_id / bitly_partition_chunk_size()) + 1) * bitly_partition_chunk_size() INTO stories_id_end;
-
-        EXECUTE '
-            CREATE TABLE ' || target_table_name || ' (
-
-                -- Primary key
-                CONSTRAINT ' || target_table_name || '_pkey
-                    PRIMARY KEY (bitly_clicks_id),
-
-                -- Partition by stories_id
-                CONSTRAINT ' || target_table_name || '_stories_id CHECK (
-                    stories_id >= ''' || stories_id_start || '''
-                AND stories_id <  ''' || stories_id_end   || '''),
-
-                -- Foreign key to stories.stories_id
-                CONSTRAINT ' || target_table_name || '_stories_id_fkey
-                    FOREIGN KEY (stories_id) REFERENCES stories (stories_id) MATCH FULL,
-
-                -- Unique duplets
-                CONSTRAINT ' || target_table_name || '_stories_id_unique
-                    UNIQUE (stories_id)
-
-            ) INHERITS (bitly_clicks_total);
-        ';
-
-        -- Update owner
-        SELECT u.usename AS owner
-        FROM information_schema.tables AS t
-            JOIN pg_catalog.pg_class AS c ON t.table_name = c.relname
-            JOIN pg_catalog.pg_user AS u ON c.relowner = u.usesysid
-        WHERE t.table_name = 'bitly_clicks_total'
-          AND t.table_schema = 'public'
-        INTO target_table_owner;
-
-        EXECUTE 'ALTER TABLE ' || target_table_name || ' OWNER TO ' || target_table_owner || ';';
-
-    END IF;
-
-    EXECUTE '
-        INSERT INTO ' || target_table_name || '
-            SELECT $1.*;
-    ' USING NEW;
-
+    SELECT bitly_get_partition_name( NEW.stories_id, 'bitly_clicks_total' ) INTO target_table_name;
+    EXECUTE 'INSERT INTO ' || target_table_name || ' SELECT $1.*;' USING NEW;
     RETURN NULL;
 END;
 $$
@@ -1587,6 +1546,82 @@ LANGUAGE plpgsql;
 CREATE TRIGGER bitly_clicks_total_partition_by_stories_id_insert_trigger
     BEFORE INSERT ON bitly_clicks_total
     FOR EACH ROW EXECUTE PROCEDURE bitly_clicks_total_partition_by_stories_id_insert_trigger();
+
+
+-- Create missing Bit.ly partitions
+CREATE OR REPLACE FUNCTION bitly_clicks_total_create_partitions()
+RETURNS VOID AS
+$$
+DECLARE
+    chunk_size INT;
+    max_stories_id BIGINT;
+    partition_stories_id BIGINT;
+
+    target_table_name TEXT;       -- partition table name (e.g. "bitly_clicks_total_000001")
+    target_table_owner TEXT;      -- partition table owner (e.g. "mediaclouduser")
+
+    stories_id_start INT;         -- stories_id chunk lower limit, inclusive (e.g. 30,000,000)
+    stories_id_end INT;           -- stories_id chunk upper limit, exclusive (e.g. 31,000,000)
+BEGIN
+
+    SELECT bitly_partition_chunk_size() INTO chunk_size;
+
+    -- Create +1 partition for future insertions
+    SELECT MAX(stories_id) + chunk_size FROM stories INTO max_stories_id;
+
+    FOR partition_stories_id IN 1..max_stories_id BY chunk_size LOOP
+        SELECT bitly_get_partition_name( partition_stories_id, 'bitly_clicks_total' ) INTO target_table_name;
+        IF table_exists(target_table_name) THEN
+            RAISE NOTICE 'Partition "%" for story ID % already exists.', target_table_name, partition_stories_id;
+        ELSE
+            RAISE NOTICE 'Creating partition "%" for story ID %', target_table_name, partition_stories_id;
+
+            SELECT (partition_stories_id / chunk_size) * chunk_size INTO stories_id_start;
+            SELECT ((partition_stories_id / chunk_size) + 1) * chunk_size INTO stories_id_end;
+
+            EXECUTE '
+                CREATE TABLE ' || target_table_name || ' (
+
+                    -- Primary key
+                    CONSTRAINT ' || target_table_name || '_pkey
+                        PRIMARY KEY (bitly_clicks_id),
+
+                    -- Partition by stories_id
+                    CONSTRAINT ' || target_table_name || '_stories_id CHECK (
+                        stories_id >= ''' || stories_id_start || '''
+                    AND stories_id <  ''' || stories_id_end   || '''),
+
+                    -- Foreign key to stories.stories_id
+                    CONSTRAINT ' || target_table_name || '_stories_id_fkey
+                        FOREIGN KEY (stories_id) REFERENCES stories (stories_id) MATCH FULL,
+
+                    -- Unique duplets
+                    CONSTRAINT ' || target_table_name || '_stories_id_unique
+                        UNIQUE (stories_id)
+
+                ) INHERITS (bitly_clicks_total);
+            ';
+
+            -- Update owner
+            SELECT u.usename AS owner
+            FROM information_schema.tables AS t
+                JOIN pg_catalog.pg_class AS c ON t.table_name = c.relname
+                JOIN pg_catalog.pg_user AS u ON c.relowner = u.usesysid
+            WHERE t.table_name = 'bitly_clicks_total'
+              AND t.table_schema = 'public'
+            INTO target_table_owner;
+
+            EXECUTE 'ALTER TABLE ' || target_table_name || ' OWNER TO ' || target_table_owner || ';';
+
+        END IF;
+    END LOOP;
+
+END;
+$$
+LANGUAGE plpgsql;
+
+-- Create initial partitions for empty database
+SELECT bitly_clicks_total_create_partitions();
 
 
 -- Helper to INSERT / UPDATE story's Bit.ly statistics
