ac4f7a64dc010525c1624dfd26397b12b18a82f6
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-09-06 18:07:16 +0300

    Update Vagrant to 1.8.5

--- install_scripts/install_mediacloud_system_package_dependencies.sh
+++ install_scripts/install_mediacloud_system_package_dependencies.sh
@@ -4,7 +4,7 @@ set -u
 set -o errexit
 
 
-VAGRANT_URL_DEBIAN="https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb"
+VAGRANT_URL_DEBIAN="https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5_x86_64.deb"
 ERLANG_APT_GPG_KEY_URL="http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc"
 ERLANG_APT_REPOSITORY_URL="http://packages.erlang-solutions.com/ubuntu"
 RABBITMQ_PACKAGECLOUD_SCRIPT="https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh"
