50bf887cdeb81582c343e8b98498b3f0f2ba4919
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-10-11 16:19:34 +0800

    Use DBIx::Simple's prepare_cached() alias to avoid using internal handle

--- lib/DBIx/Simple/MediaWords.pm
+++ lib/DBIx/Simple/MediaWords.pm
@@ -657,6 +657,13 @@ sub quote_timestamp
     return $self->quote_varchar( $value ) . '::timestamp';
 }
 
+# Alias for DBD::Pg's prepare_cached()
+sub prepare_cached
+{
+    my $self = shift;
+    return $self->dbh->prepare_cached( @_ );
+}
+
 # for each row in $data, attach all results in the child query that match a join with the $id_column field in each
 # row of $data.  attach to $row->{ $child_field } for each row in $data
 sub attach_child_query ($$$$$)
