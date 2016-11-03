c6faa5016825f3792bd4d0a8e8f0d0d9c0c80aa5
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-11-03 14:37:34 -0400

    Revert "Prepend '_' to private subroutine"
    This reverts commit 47a7d27255d91aa2e4f372cc99067767b9f57b63.

--- lib/DBIx/Simple/MediaWords.pm
+++ lib/DBIx/Simple/MediaWords.pm
@@ -202,7 +202,7 @@ sub query
     return $ret;
 }
 
-sub _get_current_work_mem
+sub get_current_work_mem
 {
     my $self = shift @_;
 
@@ -221,19 +221,10 @@ sub _get_large_work_mem
 
     if ( !defined( $ret ) )
     {
-        $ret = $self->_get_current_work_mem();
+        $ret = $self->get_current_work_mem();
     }
 
     return $ret;
-}
-
-sub _set_work_mem
-{
-    my ( $self, $new_work_mem ) = @_;
-
-    $self->query( "SET work_mem = ? ", $new_work_mem );
-
-    return;
 }
 
 sub run_block_with_large_work_mem($&)
@@ -253,7 +244,7 @@ sub run_block_with_large_work_mem($&)
 
     my $large_work_mem = $self->_get_large_work_mem();
 
-    my $old_work_mem = $self->_get_current_work_mem();
+    my $old_work_mem = $self->get_current_work_mem();
 
     $self->_set_work_mem( $large_work_mem );
 
@@ -271,6 +262,15 @@ sub run_block_with_large_work_mem($&)
     $self->_set_work_mem( $old_work_mem );
 
     TRACE "exiting run_block_with_large_work_mem";
+}
+
+sub _set_work_mem
+{
+    my ( $self, $new_work_mem ) = @_;
+
+    $self->query( "SET work_mem = ? ", $new_work_mem );
+
+    return;
 }
 
 sub query_with_large_work_mem
