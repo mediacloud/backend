61edc6813c465402dca0f536ad40993d64ef6c06
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-11-03 14:37:24 -0400

    Revert "Fix how arguments are being passed around"
    This reverts commit 5b7bab418fa34e5edbb8ca5e80f57bad89cc7a65.

--- lib/DBIx/Simple/MediaWords.pm
+++ lib/DBIx/Simple/MediaWords.pm
@@ -259,7 +259,7 @@ sub run_block_with_large_work_mem($&)
 
     try
     {
-        $block->( $self );
+        $block->();
     }
     catch
     {
@@ -277,11 +277,10 @@ sub query_with_large_work_mem
 {
     my $self = shift @_;
 
-    my @args = @_;
     my $ret;
     $self->run_block_with_large_work_mem(
         sub {
-            $ret = $self->query( @args );
+            $ret = $self->query( @_ );
         }
     );
 
