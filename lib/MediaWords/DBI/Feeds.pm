c548e5f796a32d7bea7df7c78e0a28fc437e9719
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-11-21 23:06:52 +0800

    Remove unused subroutine

--- lib/MediaWords/DBI/Feeds.pm
+++ lib/MediaWords/DBI/Feeds.pm
@@ -13,44 +13,12 @@ use warnings;
 
 =head1 FUNCTIONS
 
-=head2 delete_feed_and_stories( $db, $feeds_id )
-
-Delete a feed, making sure to delete any stories belonging to that feed that are not associated with another feed
-
-=cut
-sub delete_feed_and_stories
-{
-    my ( $db, $feeds_id ) = @_;
-
-    $db->query( <<END, $feeds_id );
-delete from stories s using feeds_stories_map fsm
-    where s.stories_id = fsm.stories_id and fsm.feeds_id = ?
-        and not exists
-            ( select 1 from feeds_stories_map fsm_b
-                  where fsm_b.stories_id = s.stories_id and fsm_b.feeds_id <> fsm.feeds_id )
-END
-
-    $db->query( <<END, $feeds_id );
-delete from downloads d
-    where d.feeds_id = ? and not exists ( select 1 from stories s where s.stories_id = d.stories_id )
-END
-
-    $db->query( <<END, $feeds_id );
-update downloads d set feeds_id = fsm.feeds_id
-    from feeds_stories_map fsm
-    where d.feeds_id = ? and d.stories_id = fsm.stories_id  and fsm.feeds_id <> d.feeds_id
-END
-
-    $db->query( "delete from downloads where stories_id is null and feeds_id = ?", $feeds_id );
-
-    $db->query( "delete from feeds where feeds_id = ?", $feeds_id );
-}
-
 =head2 disable_feed( $db, $feeds_id )
 
 (Temporarily) disable feed
 
 =cut
+
 sub disable_feed($$)
 {
     my ( $db, $feeds_id ) = @_;
