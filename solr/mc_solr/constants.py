55ad475c1ff60630e861c045e32b4aba45611ecd
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-12-06 19:12:10 +0800

    Upgrade Solr to 6.3.0

--- solr/mc_solr/constants.py
+++ solr/mc_solr/constants.py
@@ -1,8 +1,8 @@
 # Solr version to install and use
-MC_SOLR_VERSION = "6.1.0"
+MC_SOLR_VERSION = "6.3.0"
 
 # <luceneMatchVersion> value
-MC_SOLR_LUCENEMATCHVERSION = "6.1.0"
+MC_SOLR_LUCENEMATCHVERSION = "6.3.0"
 
 # Solr home directory (solr.home; relative to solr/; must already exist)
 MC_SOLR_HOME_DIR = "mediacloud/"
