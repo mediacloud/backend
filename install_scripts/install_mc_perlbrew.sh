a9ece093ac026ca824f0309d024336c7a7ff2b2b
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-12-13 22:33:09 +0800

    If system's Perl is up-to-date enough, use it instead of compiling Perl
    Reasons:
    
    1) It takes a lot of time to build Perl with Perlbrew.
    2) Ubuntu 16.04 already comes with Perl 5.22.

--- install_scripts/install_mc_perlbrew.sh
+++ install_scripts/install_mc_perlbrew.sh
@@ -8,22 +8,19 @@ source "$PWD/set_mc_root_dir.inc.sh"
 
 cd "$MC_ROOT_DIR"
 
-# Perl version to install
-PERL_INSTALL_VERSION="5.22.1"
-
-# Works on both Ubuntu and OS X
-CPU_CORE_COUNT=`getconf _NPROCESSORS_ONLN`
-
-# Apparently Perlbrew's scripts contain a lot of uninitialized variables
-set +u
 
 if [ `getconf LONG_BIT` != '64' ]; then
     echo "Install failed, you must have a 64 bit OS."
     exit 1
 fi
 
-echo "Installing Perlbrew..."
-curl -LsS http://install.perlbrew.pl | bash
+# Apparently Perlbrew's scripts contain a lot of uninitialized variables
+set +u
+
+if [ ! -f ~/perl5/perlbrew/bin/perlbrew ]; then
+    echo "Installing Perlbrew..."
+    curl -LsS http://install.perlbrew.pl | bash
+fi
 
 echo "Loading Perlbrew environment variables..."
 source ~/perl5/perlbrew/etc/bashrc
@@ -31,31 +28,57 @@ source ~/perl5/perlbrew/etc/bashrc
 echo "Running 'perlbrew init'..."
 perlbrew init
 
-echo "Running 'perlbrew install'..."
-nice perlbrew install \
-	-j $CPU_CORE_COUNT \
-	--verbose `# Perlbrew output should be chatty so that Vagrant provision script does not timeout` \
-	"perl-${PERL_INSTALL_VERSION}" \
-	-Duseithreads \
-	-Dusemultiplicity \
-	-Duse64bitint \
-	-Duse64bitall \
-	-Duseposix \
-	-Dusethreads \
-	-Duselargefiles \
-	-Dccflags=-DDEBIAN
+PERLBREW_ROOT="${PERLBREW_ROOT:-~/perl5/perlbrew}"
+
+if [ ! -d "$PERLBREW_ROOT/perls/perl-system" ]; then
+    SYSTEM_PERL_BIN=`command -v perl`   # `whereis` usually gives looney results
+    echo "System Perl binary was found at: $SYSTEM_PERL_BIN"
+    SYSTEM_PERL_DIR=`dirname "$SYSTEM_PERL_BIN"`
+
+    echo "Setting up system's Perl in Perlbrew from $SYSTEM_PERL_DIR..."
+    mkdir -p "$PERLBREW_ROOT/perls/perl-system"
+    ln -s "$SYSTEM_PERL_DIR" "$PERLBREW_ROOT/perls/perl-system/bin"
+fi
+
+echo "System's Perl version: `perl -e 'print substr($^V, 1)'`"
+
+MC_PERL_VERSION=`perl $MC_ROOT_DIR/install_scripts/perl_version_to_use.pl`
+echo "Will set up Media Cloud to use Perl version: $MC_PERL_VERSION"
+
+# "perl-system" should already exist, so if some other version gets chosen, compile and install it
+if [ ! -d "$PERLBREW_ROOT/perls/perl-${MC_PERL_VERSION}" ]; then
+
+    # Works on both Ubuntu and OS X
+    CPU_CORE_COUNT=`getconf _NPROCESSORS_ONLN`
+
+    echo "Installing Perl ${MC_PERL_VERSION}..."
+    nice perlbrew install \
+        -j $CPU_CORE_COUNT \
+        --verbose `# Perlbrew output should be chatty so that Vagrant provision script does not timeout` \
+        "perl-${MC_PERL_VERSION}" \
+        -Duseithreads \
+        -Dusemultiplicity \
+        -Duse64bitint \
+        -Duse64bitall \
+        -Duseposix \
+        -Dusethreads \
+        -Duselargefiles \
+        -Dccflags=-DDEBIAN
+fi
 
 echo "Switching to installed Perl..."
-perlbrew switch "perl-${PERL_INSTALL_VERSION}"
+perlbrew switch "perl-${MC_PERL_VERSION}"
 
-echo "Installing cpanm..."
-perlbrew install-cpanm
+if [ ! -f "$PERLBREW_ROOT/bin/cpanm" ]; then
+    echo "Installing cpanm..."
+    perlbrew install-cpanm
+fi
 
 echo "Creating 'mediacloud' library..."
-perlbrew lib create mediacloud
+perlbrew lib create "perl-${MC_PERL_VERSION}@mediacloud"
 
 echo "Switching to 'mediacloud' library..."
-perlbrew switch "perl-${PERL_INSTALL_VERSION}@mediacloud"
+perlbrew switch "perl-${MC_PERL_VERSION}@mediacloud"
 
 echo "Done installing Perl with Perlbrew."
 
