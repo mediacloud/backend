556e25d8579ae1853af43b07c7cd5ddebbf236b1
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-12-15 02:04:04 +0800

    Don't run Python installation commands with sudo on OS X

--- install_scripts/install_python_dependencies.sh
+++ install_scripts/install_python_dependencies.sh
@@ -12,35 +12,43 @@ function verlt() {
     [ "$1" = "$2" ] && return 1 || verlte "$1" "$2"
 }
 
-# Install / upgrade Setuptools (easy_install) to both Python versions before installing dependencies
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python2.7 -
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3.5 -
+if [ `uname` == 'Darwin' ]; then
+    # Mac OS X
+    COMMAND_PREFIX=""    # doesn't need sudo as Python gets installed via Homebrew
+else
+    # assume Ubuntu
+    COMMAND_PREFIX="sudo"
+fi
 
-# Install / upgrade Pip to both Python versions before installing dependencies
-sudo easy_install-2.7 pip
-sudo easy_install-3.5 pip
+echo "Installing (upgrading) Setuptools..."
+wget https://bootstrap.pypa.io/ez_setup.py -O - | $COMMAND_PREFIX python2.7 -
+wget https://bootstrap.pypa.io/ez_setup.py -O - | $COMMAND_PREFIX python3.5 -
 
-# Install (upgrade) Supervisor:
+echo "Installing (upgrading) Pip..."
+$COMMAND_PREFIX easy_install-2.7 pip
+$COMMAND_PREFIX easy_install-3.5 pip
+
+echo "Installing (upgrading) Supervisor..."
 # * change dir, otherwise the installer might think we're trying to install from the supervisor/ directory
 # * also, Supervisor only supports Python 2.7 at the moment
-( cd /tmp; sudo pip2.7 install --upgrade supervisor )
+( cd /tmp; $COMMAND_PREFIX pip2.7 install --upgrade supervisor )
 
-# Install (upgrade Virtualenv)
-sudo pip2.7 install --upgrade virtualenv
-sudo pip3.5 install --upgrade virtualenv
+echo "Installing (upgrading) Virtualenv..."
+$COMMAND_PREFIX pip2.7 install --upgrade virtualenv
+$COMMAND_PREFIX pip3.5 install --upgrade virtualenv
 
-# Install Python 2 dependencies
-sudo pip2.7 install --upgrade -r python_scripts/requirements.txt || {
+echo "Installing Python 2.7 dependencies..."
+$COMMAND_PREFIX pip2.7 install --upgrade -r python_scripts/requirements.txt || {
     # Sometimes fails with some sort of Setuptools error
     echo "'pip2.7 install' failed the first time, retrying..."
-    sudo pip2.7 install --upgrade -r python_scripts/requirements.txt
+    $COMMAND_PREFIX pip2.7 install --upgrade -r python_scripts/requirements.txt
 }
 
-# Create virtualenv and activate it
+echo "Creating mc-venv virtualenv..."
 virtualenv --python=python3.5 mc-venv
 source mc-venv/bin/activate
 
-# Install Python 3 dependencies (no sudo because we're in virtualenv)
+echo "Installing Python 3.5 dependencies..."
 pip3.5 install --upgrade -r mediacloud/requirements.txt || {
     # Sometimes fails with some sort of Setuptools error
     echo "'pip3.5 install' failed the first time, retrying..."
