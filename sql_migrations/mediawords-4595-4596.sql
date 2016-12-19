2ca9ae7171594769d5afb05fa1c124f3000ac004
Hal Roberts <hroberts@cyber.law.harvard.edu>
2016-12-19 08:21:30 -0500

    prepare sql updates for master merge

--- sql_migrations/mediawords-4595-4596.sql
+++ sql_migrations/mediawords-4595-4596.sql
@@ -1,12 +1,12 @@
 --
 -- This is a Media Cloud PostgreSQL schema difference file (a "diff") between schema
--- versions 4594 and 4595.
+-- versions 4595 and 4596.
 --
 -- If you are running Media Cloud with a database that was set up with a schema version
--- 4594, and you would like to upgrade both the Media Cloud and the
--- database to be at version 4595, import this SQL file:
+-- 4595, and you would like to upgrade both the Media Cloud and the
+-- database to be at version 4596, import this SQL file:
 --
---     psql mediacloud < mediawords-4594-4595.sql
+--     psql mediacloud < mediawords-4595-4596.sql
 --
 -- You might need to import some additional schema diff files to reach the desired version.
 --
@@ -50,7 +50,7 @@ DECLARE
 
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4595;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4596;
 
 BEGIN
 
