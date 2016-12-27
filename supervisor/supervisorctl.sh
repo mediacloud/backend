c3576ee49e3a9dc3e7fd8e973012f50e339202c3
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-12-27 21:04:02 +0200

    Don't validate Supervisor's version
    Script no longer works with 3.3*.

--- supervisor/supervisorctl.sh
+++ supervisor/supervisorctl.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PWD="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$PWD/supervisor_is_up_to_date.inc.sh"
-validate_supervisor_version
 
 /usr/local/bin/supervisorctl \
     --configuration `dirname "$0"`/supervisord.conf \
