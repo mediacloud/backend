c4e3ae49d789b6da07e23b1fa6b55fc7d542cea3
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-01-10 02:51:47 +0200

    Migrate distribution / real configuration file unit tests to Python

--- mediacloud/mediawords/util/test_config.py
+++ mediacloud/mediawords/util/test_config.py
@@ -12,6 +12,25 @@ def test_get_config():
     assert 'data_dir' in config['mediawords']
 
 
+# FIXME unit test not really stateless
+def test_set_config_file():
+    root_path = mc_root_path()
+
+    # Test with .yml.dist
+    mediawords_yml_dist_path = os.path.join(root_path, 'mediawords.yml.dist')
+    assert os.path.isfile(mediawords_yml_dist_path)
+    old_config = get_config()
+    set_config_file(mediawords_yml_dist_path)
+    set_config(old_config)
+
+    # Test with .yml
+    mediawords_yml_path = os.path.join(root_path, 'mediawords.yml')
+    assert os.path.isfile(mediawords_yml_path)
+    old_config = get_config()
+    set_config_file(mediawords_yml_path)
+    set_config(old_config)
+
+
 def test_set_config_file_nonexistent():
     tempdir = tempfile.mkdtemp()
     nonexistent_config = os.path.join(tempdir, 'nonexistent_configuration.yml')
