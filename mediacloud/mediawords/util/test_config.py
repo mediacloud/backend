e9823699d30b84295217672fe3d5cb9588abc39b
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-01-10 02:37:53 +0200

    Migrate empty database list configuration file unit test to Python

--- mediacloud/mediawords/util/test_config.py
+++ mediacloud/mediawords/util/test_config.py
@@ -17,3 +17,8 @@ def test_set_config_nonexistent_file():
     nonexistent_config = os.path.join(tempdir, 'nonexistent_configuration.yml')
     assert os.path.exists(nonexistent_config) is False
     assert_raises(McConfigException, set_config_file, nonexistent_config)
+
+
+def test_set_config_no_database_connections():
+    assert_raises(McConfigException, set_config, {'name': 'MediaWords'})
+    assert_raises(McConfigException, set_config, {'name': 'MediaWords', 'database': None})
