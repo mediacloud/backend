18eb0c585c95408812e5326be054132c6df3b23b
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-01-19 05:52:16 +0200

    Implement own deep copy helper to allow more flexibility

--- lib/MediaWords/Util/Python.pm
+++ lib/MediaWords/Util/Python.pm
@@ -13,26 +13,74 @@ our @ISA    = qw(Exporter);
 our @EXPORT = qw(make_python_variable_writable);
 
 use Carp;
-use Clone qw(clone);
+use Inline::Python;
 
 # Python function return values proxied through Inline::Python become read-only so attempts to modify them afterwards fail with:
 #
 #     Modification of non-creatable hash value attempted, subscript "language"
 #
 # To make the return values writable, we simply clone them.
-sub make_python_variable_writable($)
+sub make_python_variable_writable
 {
     my $variable = shift;
 
-    my $cloned_variable;
-    eval { $cloned_variable = clone( $variable ); };
-    if ( $@ )
+    my $copy;
+
+    # Neither clone() from Clone nor dclone() from Storable flattens
+    # Inline::Python's booleans as we want, so here goes our own deep copy
+    if ( ref( $variable ) eq ref( [] ) )
     {
-        # Print stack trace which led to this mishap
-        confess "Cloning variable failed: $@; variable: $variable";
+        # Arrayref
+        $copy = [];
+        foreach my $value ( @{ $variable } )
+        {
+            my $writable_value = make_python_variable_writable( $value );
+            push( @{ $copy }, $writable_value );
+        }
+
+    }
+    elsif ( ref( $variable ) eq ref( {} ) )
+    {
+        # Hashref
+        $copy = {};
+        foreach my $key ( keys %{ $variable } )
+        {
+            my $value = $variable->{ $key };
+
+            my $writable_key   = make_python_variable_writable( $key );
+            my $writable_value = make_python_variable_writable( $value );
+            $copy->{ $writable_key } = $writable_value;
+        }
+
+    }
+    elsif ( ref( $variable ) eq 'Inline::Python::Boolean' )
+    {
+        # Inline::Python booleans
+
+        $copy = int( "$variable" );
+        if ( $copy )
+        {
+            $copy = $Inline::Python::Boolean::true;
+        }
+        else
+        {
+            $copy = $Inline::Python::Boolean::false;
+        }
+
+    }
+    elsif ( ref( $variable ) )
+    {
+        # Some other object
+        $copy = scalar( $variable );
+
+    }
+    else
+    {
+        $copy = $variable;
+
     }
 
-    return $cloned_variable;
+    return $copy;
 }
 
 1;
