3162917148939c922be7094a94a04817fdad40f0
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-01-19 02:14:39 +0200

    Revert "Raise McIntInsteadOfBooleanException when int instead of bool is being passed"
    This reverts commit 30c38dca72e439477d2b79f614ecf2bbcda9e436.

--- mediacloud/mediawords/db/exceptions/result.py
+++ mediacloud/mediawords/db/exceptions/result.py
@@ -3,16 +3,6 @@ class McDatabaseResultException(Exception):
     pass
 
 
-class McIntInsteadOfBooleanException(McDatabaseResultException):
-    """Exception raised on 'column "..." is of type boolean but expression is of type integer'."""
-
-    affected_column = None
-
-    def __init__(self, message, affected_column):
-        McDatabaseResultException.__init__(self, message)
-        self.affected_column = affected_column
-
-
 class McDatabaseResultTextException(McDatabaseResultException):
     """text() exception."""
     pass
