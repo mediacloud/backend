d7a80144d565b82163e98892763975cc5fb57cee
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-02-03 22:00:51 +0800

    Cast “show_on_media” and “show_on_stories” in tags/create

--- lib/MediaWords/Controller/Api/V2/Tags.pm
+++ lib/MediaWords/Controller/Api/V2/Tags.pm
@@ -176,6 +176,9 @@ sub create_GET
     my $fields = [ 'tag_sets_id', @{ $self->get_update_fields } ];
     my $input = { map { $_ => $data->{ $_ } } grep { exists( $data->{ $_ } ) } @{ $fields } };
 
+    $input->{ show_on_media }   = normalize_boolean_for_db( $input->{ show_on_media } );
+    $input->{ show_on_stories } = normalize_boolean_for_db( $input->{ show_on_stories } );
+
     my $created_tag = $c->dbis->create( 'tags', $input );
 
     return $self->status_ok( $c, entity => { tag => $created_tag } );
