91e69371f504e363b2674a4aeb254bdaf408aa7c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-03-09 15:27:06 -0500

    Add run_alone() helper that will make sure only a single instance of argument function is running
    Similar to Perl’s Sys::RunAlone.

--- mediacloud/mediawords/util/process.py
+++ mediacloud/mediawords/util/process.py
@@ -1,11 +1,15 @@
+import atexit
+import hashlib
+import inspect
 import os
 import signal
 import sys
 import subprocess
 import time
-from typing import List
+from typing import List, Callable, Any
 
 from mediawords.util.log import create_logger
+from mediawords.util.paths import lock_file, McLockFileException, unlock_file, McUnlockFileException
 from mediawords.util.perl import decode_object_from_bytes_if_needed
 
 l = create_logger(__name__)
@@ -94,3 +98,115 @@ def gracefully_kill_child_process(child_pid: int, sigkill_timeout: int = 60) -> 
 
         if process_with_pid_is_running(pid=child_pid):
             l.warning("Even SIGKILL didn't do anything, kill child process with PID %d manually!" % child_pid)
+
+
+class McEnsureThatIAmRunningAloneException(Exception):
+    """Exception on ensure_that_i_am_running_alone()."""
+    pass
+
+
+class McUnableToDetermineCaller(McEnsureThatIAmRunningAloneException):
+    """Exception thrown when ensure_that_i_am_running_alone() can not determine caller."""
+    pass
+
+
+class McScriptInstanceIsAlreadyRunning(McEnsureThatIAmRunningAloneException):
+    """Exception thrown when another instance of the caller script is already running."""
+    pass
+
+
+__run_alone_function_lock_file = None
+
+
+def run_alone(function: Callable, *args, **kwargs) -> Any:
+    """Run function while making sure that only a single instance of it is running."""
+
+    global __run_alone_function_lock_file
+
+    def __function_unique_id(func: Callable) -> str:
+        """Return unique signature of the function, consisting of its absolute path and a name.
+
+        Retry locking for 5 seconds before giving up."""
+
+        module = inspect.getmodule(func)
+        signature = inspect.signature(func)
+
+        module_path = module.__file__
+        if not os.path.isfile(module_path):
+            raise Exception("Module '%s' for function '%s' does not exist." % (module_path, str(func)))
+
+        function_name = func.__qualname__
+        if function_name is None or len(function_name) == 0:
+            raise Exception("Unable to determine function name: %s" % str(func))
+
+        parameters_type = str(signature.parameters)
+        return_value_type = str(signature.return_annotation)
+
+        unique_id = '%(module_path)s:%(function_name)s:%(parameters)s:%(return_value)s' % {
+            'module_path': module_path,
+            'function_name': function_name,
+            'parameters': parameters_type,
+            'return_value': return_value_type,
+        }
+        return unique_id
+
+    # noinspection PyUnusedLocal
+    def __remove_run_alone_lock_file(signum: int = None, frame: int = None) -> None:
+        global __run_alone_function_lock_file
+
+        if __run_alone_function_lock_file is not None:
+            l.info("Caught SIGTERM, unlocking '%s'..." % __run_alone_function_lock_file)
+            try:
+                unlock_file(__run_alone_function_lock_file)
+            except McUnlockFileException as exception:
+                # Not critical, the lock file might have been removed by some other process
+                l.warning("Unlocking file failed: %s" % str(exception))
+        else:
+            l.debug("Nothing to unlock.")
+
+        sys.exit(signum)
+
+    try:
+        function_unique_id = __function_unique_id(function)
+    except Exception as ex:
+        raise McUnableToDetermineCaller("Unable to determine caller script: %s" % str(ex))
+
+    timeout = 5
+
+    l.debug("Function unique ID: %s" % function_unique_id)
+
+    function_unique_id_hash = hashlib.sha256(bytes(function_unique_id, 'utf-8')).hexdigest()
+    l.debug("Function unique ID hash: %s" % function_unique_id_hash)
+
+    # Catch SIGINTs and SIGTERMs while running the function to be able to remove lock file afterwards
+    original_sigint_handler = signal.getsignal(signal.SIGINT)
+    original_sigterm_handler = signal.getsignal(signal.SIGTERM)
+    signal.signal(signal.SIGINT, __remove_run_alone_lock_file)
+    signal.signal(signal.SIGTERM, __remove_run_alone_lock_file)
+    atexit.register(__remove_run_alone_lock_file)
+
+    function_lock_file = os.path.join("/var", "tmp", function_unique_id_hash)
+
+    try:
+        lock_file(path=function_lock_file, timeout=timeout)
+        __run_alone_function_lock_file = function_lock_file
+    except McLockFileException as ex:
+        raise McScriptInstanceIsAlreadyRunning("Instance of %s is already running: %s" % (str(function), str(ex)))
+
+    # noinspection PyCallingNonCallable
+    return_value = function(*args, **kwargs)
+
+    try:
+        unlock_file(__run_alone_function_lock_file)
+    except McUnlockFileException as exc:
+        # Not critical, the lock file might have been removed by some other process
+        l.warning("Unlocking file failed: %s" % str(exc))
+
+    # Reset signal handlers
+    atexit.unregister(__remove_run_alone_lock_file)
+    signal.signal(signal.SIGINT, original_sigint_handler)
+    signal.signal(signal.SIGTERM, original_sigterm_handler)
+
+    __run_alone_function_lock_file = None
+
+    return return_value
