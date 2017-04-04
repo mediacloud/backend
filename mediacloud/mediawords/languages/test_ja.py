5ede5628cd564ca998dea166a4ac735c4033db44
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-04-04 18:01:49 +0800

    First set returned result to variable, then compare
    Easier to set breakpoint and see the the returned result in case of failure.

--- mediacloud/mediawords/languages/test_ja.py
+++ mediacloud/mediawords/languages/test_ja.py
@@ -21,67 +21,73 @@ class TestJapaneseTokenizer(TestCase):
         assert self.__tokenizer.tokenize_text_to_sentences(".") == ["."]
 
         # English-only punctuation
-        assert self.__tokenizer.tokenize_text_to_sentences(
+        sentences = self.__tokenizer.tokenize_text_to_sentences(
             "Hello. How do you do? I'm doing okay."
-        ) == [
-                   "Hello.",
-                   "How do you do?",
-                   "I'm doing okay.",
-               ]
+        )
+        assert sentences == [
+            "Hello.",
+            "How do you do?",
+            "I'm doing okay.",
+        ]
 
         # English-only punctuation, no period at the end of sentence
-        assert self.__tokenizer.tokenize_text_to_sentences(
+        sentences = self.__tokenizer.tokenize_text_to_sentences(
             "Hello. How do you do? I'm doing okay"
-        ) == [
-                   "Hello.",
-                   "How do you do?",
-                   "I'm doing okay",
-               ]
+        )
+        assert sentences == [
+            "Hello.",
+            "How do you do?",
+            "I'm doing okay",
+        ]
 
         # Japanese-only punctuation
-        assert self.__tokenizer.tokenize_text_to_sentences(
+        sentences = self.__tokenizer.tokenize_text_to_sentences(
             "ジアゼパムはてんかんや興奮の治療に用いられる。"
             "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。"
             "鎮静作用を生かし手術などの前投薬にも用いられる。"
             "アルコールやドラッグによる離脱症状の治療にも用いられる。"
-        ) == [
-                   "ジアゼパムはてんかんや興奮の治療に用いられる。",
-                   "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
-                   "鎮静作用を生かし手術などの前投薬にも用いられる。",
-                   "アルコールやドラッグによる離脱症状の治療にも用いられる。",
-               ]
+        )
+        assert sentences == [
+            "ジアゼパムはてんかんや興奮の治療に用いられる。",
+            "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
+            "鎮静作用を生かし手術などの前投薬にも用いられる。",
+            "アルコールやドラッグによる離脱症状の治療にも用いられる。",
+        ]
 
         # Japanese-only punctuation, no EOS at the end of the sentence
-        assert self.__tokenizer.tokenize_text_to_sentences(
+        sentences = self.__tokenizer.tokenize_text_to_sentences(
             "ジアゼパムはてんかんや興奮の治療に用いられる。"
             "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。"
             "鎮静作用を生かし手術などの前投薬にも用いられる。"
             "アルコールやドラッグによる離脱症状の治療にも用いられる"
-        ) == [
-                   "ジアゼパムはてんかんや興奮の治療に用いられる。",
-                   "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
-                   "鎮静作用を生かし手術などの前投薬にも用いられる。",
-                   "アルコールやドラッグによる離脱症状の治療にも用いられる",
-               ]
+        )
+        assert sentences == [
+            "ジアゼパムはてんかんや興奮の治療に用いられる。",
+            "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
+            "鎮静作用を生かし手術などの前投薬にも用いられる。",
+            "アルコールやドラッグによる離脱症状の治療にも用いられる",
+        ]
 
         # Japanese and English punctuation
-        assert self.__tokenizer.tokenize_text_to_sentences(
+        sentences = self.__tokenizer.tokenize_text_to_sentences(
             "ジアゼパムはてんかんや興奮の治療に用いられる。"
             "This is some English text out of the blue. "
             "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。"
             "This is some more English text."
-        ) == [
-                   "ジアゼパムはてんかんや興奮の治療に用いられる。",
-                   "This is some English text out of the blue.",
-                   "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
-                   "This is some more English text.",
-               ]
+        )
+        assert sentences == [
+            "ジアゼパムはてんかんや興奮の治療に用いられる。",
+            "This is some English text out of the blue.",
+            "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
+            "This is some more English text.",
+        ]
 
         # Japanese and English punctuation (with newlines)
-        assert self.__tokenizer.tokenize_text_to_sentences("""ジアゼパムはてんかんや興奮の治療に用いられる。
+        sentences = self.__tokenizer.tokenize_text_to_sentences("""ジアゼパムはてんかんや興奮の治療に用いられる。
 This is some English text out of the blue. 
 また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。
-This is some more English text.""") == [
+This is some more English text.""")
+        assert sentences == [
             "ジアゼパムはてんかんや興奮の治療に用いられる。",
             "This is some English text out of the blue.",
             "また、有痛性筋痙攣（いわゆる“こむらがえり”）などの筋痙攣の治療にはベンゾジアゼピン類の中で最も有用であるとされている。",
@@ -96,7 +102,8 @@ This is some more English text.""") == [
         assert self.__tokenizer.tokenize_sentence_to_words(".") == []
 
         # English sentence
-        assert self.__tokenizer.tokenize_sentence_to_words("How do you do?") == [
+        words = self.__tokenizer.tokenize_sentence_to_words("How do you do?")
+        assert words == [
             "How",
             "do",
             "you",
@@ -104,7 +111,8 @@ This is some more English text.""") == [
         ]
 
         # English sentence, no period at the end of the sentence
-        assert self.__tokenizer.tokenize_sentence_to_words("How do you do") == [
+        words = self.__tokenizer.tokenize_sentence_to_words("How do you do")
+        assert words == [
             "How",
             "do",
             "you",
@@ -112,7 +120,8 @@ This is some more English text.""") == [
         ]
 
         # English sentence, literal string "EOS"
-        assert self.__tokenizer.tokenize_sentence_to_words("EOS this, EOS that.") == [
+        words = self.__tokenizer.tokenize_sentence_to_words("EOS this, EOS that.")
+        assert words == [
             "EOS",
             "this",
             "EOS",
@@ -120,46 +129,49 @@ This is some more English text.""") == [
         ]
 
         # English sentence; tab, newline and comma characters
-        assert self.__tokenizer.tokenize_sentence_to_words(
+        words = self.__tokenizer.tokenize_sentence_to_words(
             "Something\tSomething else\nSomething, completely, different."
-        ) == [
-                   "Something",
-                   "Something else",  # curiously, mecab-ipadic-neologd treats "something else" as a single token
-                   "Something",
-                   "completely",
-                   "different",
-               ]
+        )
+        assert words == [
+            "Something",
+            "Something else",  # curiously, mecab-ipadic-neologd treats "something else" as a single token
+            "Something",
+            "completely",
+            "different",
+        ]
 
         # Japanese sentence
-        assert self.__tokenizer.tokenize_sentence_to_words(
+        words = self.__tokenizer.tokenize_sentence_to_words(
             "10日放送の「中居正広のミになる図書館」（テレビ朝日系）で、SMAPの中居正広が、篠原信一の過去の勘違いを明かす一幕があった。"
-        ) == [
-                   "10日",
-                   "放送",
-                   "の",
-                   "中居正広のミになる図書館",
-                   "テレビ朝日",
-                   "系",
-                   "で",
-                   "SMAP",
-                   "の",
-                   "中居正広",
-                   "が",
-                   "篠原信一",
-                   "の",
-                   "過去",
-                   "の",
-                   "勘違い",
-                   "を",
-                   "明かす",
-                   "一幕",
-                   "が",
-                   "あっ",
-                   "た",
-               ]
+        )
+        assert words == [
+            "10日",
+            "放送",
+            "の",
+            "中居正広のミになる図書館",
+            "テレビ朝日",
+            "系",
+            "で",
+            "SMAP",
+            "の",
+            "中居正広",
+            "が",
+            "篠原信一",
+            "の",
+            "過去",
+            "の",
+            "勘違い",
+            "を",
+            "明かす",
+            "一幕",
+            "が",
+            "あっ",
+            "た",
+        ]
 
         # Japanese + English sentence
-        assert self.__tokenizer.tokenize_sentence_to_words("pythonが大好きです") == [
+        words = self.__tokenizer.tokenize_sentence_to_words("pythonが大好きです")
+        assert words == [
             "python",
             "が",
             "大好き",
@@ -167,10 +179,11 @@ This is some more English text.""") == [
         ]
 
         # Japanese punctuation
-        assert self.__tokenizer.tokenize_sentence_to_words(
+        words = self.__tokenizer.tokenize_sentence_to_words(
             "Badger、badger。Badger・Badger『badger』badger？Badger！Badger！？"
             "Badger【badger】Badger～badger▽badger（badger）"
-        ) == [
-                   'Badger', 'badger', 'Badger', 'Badger', 'badger', 'badger', 'Badger', 'Badger', 'Badger', 'badger',
-                   'Badger', 'badger', 'badger', 'badger',
-               ]
+        )
+        assert words == [
+            'Badger', 'badger', 'Badger', 'Badger', 'badger', 'badger', 'Badger',
+            'Badger', 'Badger', 'badger', 'Badger', 'badger', 'badger', 'badger',
+        ]
