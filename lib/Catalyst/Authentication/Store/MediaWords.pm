393ef71908df0715571c38243f27c0734f4d062f
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-05-23 06:09:46 +0300

    Re-add roles to Catalyst's object

--- lib/Catalyst/Authentication/Store/MediaWords.pm
+++ lib/Catalyst/Authentication/Store/MediaWords.pm
@@ -63,6 +63,13 @@ sub find_user
         'id'       => $userauth->id(),
         'username' => $userauth->email(),
         'password' => $userauth->password_hash(),
+
+        # List of roles get hashed into the user object and are refetched from the
+        # database each and every time the user tries to access a page (via the
+        # from_session() subroutine). This is done because a list of roles might
+        # change while the user is still logged in.
+        'roles' => [ map { $_->role() } @{ $userauth->roles() } ],
+
     );
 }
 
