3f9712e1fb626ee8e9a7bc38ca8b3eb276ed0d7c
Hal Roberts <hroberts@cyber.law.harvard.edu>
2017-06-12 17:42:14 -0500

    fidget with parameters to make more reasonable layout

--- mediacloud/mediawords/tm/snapshot/graph_layout.py
+++ mediacloud/mediawords/tm/snapshot/graph_layout.py
@@ -1,10 +1,8 @@
 from scipy.sparse import spdiags, coo_matrix
 
-import forceatlas2
 import io
 import networkx as nx
 import numpy as np
-import scipy as sp
 import typing
 
 from mediawords.util.log import create_logger
@@ -13,18 +11,20 @@ from mediawords.util.perl import decode_object_from_bytes_if_needed
 l = create_logger(__name__)
 
 # forceatlas2_layout function copied from https://github.com/tpoisot/nxfa2
-def forceatlas2_layout(G, iterations=10, linlog=False, pos=None, nohubs=False,
-                       kr=0.001, k=None, dim=2):
+def forceatlas2_layout(G, iterations: int = 10, linlog: bool = False, pos: int = None, nohubs: bool = False,
+                       dim: int = 2, scale: float = 1) -> typing.Dict:
     """
     Options values are
-    g                The graph to layout
+    G                The graph to layout
     iterations       Number of iterations to do
     linlog           Whether to use linear or log repulsion
-    random_init      Start with a random position
-                     If false, start with FR
-    avoidoverlap     Whether to avoid overlap of points
-    degreebased      Degree based repulsion
+    nohubs           Wheter to use hub repulsion
+    dim              Num of dimensions
+    scale            Scaling factor for size of map
     """
+
+    min_length = 0.001
+
     # We add attributes to store the current and previous convergence speed
     for n in G:
         G.node[n]['prevcs'] = 0
@@ -38,16 +38,18 @@ def forceatlas2_layout(G, iterations=10, linlog=False, pos=None, nohubs=False,
         A = A.tolil()
     except Exception as e:
         A = (coo_matrix(A)).tolil()
+
     if pos is None:
         pos = np.asarray(np.random.random((nnodes, dim)), dtype=A.dtype)
     else:
         pos = pos.astype(A.dtype)
-    if k is None:
-        k = np.sqrt(1.0 / nnodes)
-        # Iterations
+
+    k = np.sqrt(1.0 / nnodes)
+
     # the initial "temperature" is about .1 of domain area (=1x1)
     # this is the largest step allowed in the dynamics.
     t = 0.1
+
     # simple cooling scheme.
     # linearly step down by dt on each iteration so last iteration is size dt.
     dt = t / float(iterations + 1)
@@ -60,12 +62,12 @@ def forceatlas2_layout(G, iterations=10, linlog=False, pos=None, nohubs=False,
             delta = (pos[i] - pos).T
             # distance between points
             distance = np.sqrt((delta ** 2).sum(axis=0))
-            # enforce minimum distance of 0.01
-            distance = np.where(distance < 0.01, 0.01, distance)
+            # enforce minimum distance
+            distance = np.where(distance < min_length, min_length, distance)
             # the adjacency matrix row
             Ai = np.asarray(A.getrowview(i).toarray())
             # displacement "force"
-            Dist = k * k / distance ** 2
+            Dist = ( k * k / distance ** 2 ) * scale
             if nohubs:
                 Dist = Dist / float(Ai.sum(axis=1) + 1)
             if linlog:
@@ -74,11 +76,12 @@ def forceatlas2_layout(G, iterations=10, linlog=False, pos=None, nohubs=False,
                 (delta * (Dist - Ai * distance / k)).sum(axis=1)
             # update positions
         length = np.sqrt((displacement ** 2).sum(axis=0))
-        length = np.where(length < 0.01, 0.01, length)
+        length = np.where(length < min_length, min_length, length)
         pos += (displacement * t / length).T
         # cool temperature
         t -= dt
-        # Return the layout
+
+    # Return the layout
     return dict(zip(G, pos))
 
 #def postgres_regex_match(db: DatabaseHandler, strings: List[str], regex: str) -> bool:
@@ -88,22 +91,13 @@ def layout_gexf(gexf: str) -> typing.Dict:
     in_fh = io.StringIO(gexf)
     graph = nx.read_gexf(in_fh)
 
-    print("START LAYOUT")
-    layout = forceatlas2_layout(G=graph, iterations=500 )
-    print("END LAYOUT")
+    layout = forceatlas2_layout(G=graph, iterations=100, scale=1 )
 
-    print(layout)
-    #
-    # out_fh = io.StringIO()
-    # fa_gexf = networkx.write_gexf(fa_graph, io)
-
-    scale = 10000
+    scale = 5000
 
     int_layout = dict()
     for id in layout:
         pos = layout[id]
         int_layout[id] = (int(pos[0] * scale), int(pos[1] * scale))
-
-    print(int_layout)
 
     return int_layout
