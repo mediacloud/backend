b2587f0cf91e3ee157999347501acdd349607bed
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-06-26 22:35:46 +0300

    Try installing IO::Socket::SSL separately

--- install/install_modules_with_carton.sh
+++ install/install_modules_with_carton.sh
@@ -38,13 +38,20 @@ fi
 
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
     --verbose \
-    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay LWP::UserAgent IO::Socket::SSL Net::SSL
+    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay LWP::UserAgent Net::SSL
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
     --local-lib-contained local/ \
     --verbose \
-    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay LWP::UserAgent IO::Socket::SSL Net::SSL
+    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay LWP::UserAgent Net::SSL
 
-# needed to install the https github links below
+./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
+    --verbose \
+    IO::Socket::SSL
+./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
+    --local-lib-contained local/ \
+    --verbose \
+    IO::Socket::SSL
+
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
     --verbose \
     Test::RequiresInternet
