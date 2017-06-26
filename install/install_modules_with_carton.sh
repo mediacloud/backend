b10ddb72c5168149fc04cdf07de5496008dc0141
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-06-26 20:59:24 +0300

    Add LWP::UserAgent

--- install/install_modules_with_carton.sh
+++ install/install_modules_with_carton.sh
@@ -22,11 +22,11 @@ fi
 
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
     --verbose \
-    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay
+    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay LWP::UserAgent
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
     --local-lib-contained local/ \
     --verbose \
-    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay
+    Test::Fatal File::Listing HTTP::Cookies HTTP::Daemon HTTP::Negotiate Net::HTTP WWW::RobotRules Net::SSLeay LWP::UserAgent
 
 # needed to install the https github links below
 ./script/run_with_carton.sh ~/perl5/perlbrew/bin/cpanm \
