7ce27ccd82edbb71592a6d48a8b60ea857f4175c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-07-27 12:58:51 +0300

    Revert "added COMMAND_PREFIX to use sudo on linux"
    This reverts commit db1c5848f6089320589a75eeb7c303acb535505a.

--- install/install_python_dependencies.sh
+++ install/install_python_dependencies.sh
@@ -58,6 +58,13 @@ else
     NLTK_DATA_PATH=/usr/share/nltk_data
 fi
 
+echo "  Download data with wget, this may take a while"
+wget -nv https://github.com/nltk/nltk_data/archive/gh-pages.zip
+echo "  Unzip data with unzip -n, preventing overwriting existing files this may take a while"
+unzip -n gh-pages.zip
+echo "  Move data to ideal directory"
+mv nltk_data-gh-pages/ $NLTK_DATA_PATH
+
 $COMMAND_PREFIX python$PYTHON3_MAJOR_VERSION \
     -m nltk.downloader \
     -u https://s3.amazonaws.com/mediacloud-nltk-data/nltk_data/index.xml \
