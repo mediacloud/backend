e4815f4d99c955c01d546f78dcc100bfc0eb9bcd
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-08-11 01:44:19 +0300

    Install Perlbrew using Ansible

--- install/install_mc_perlbrew_and_modules.sh
+++ install/install_mc_perlbrew_and_modules.sh
@@ -8,8 +8,6 @@ source "$PWD/set_mc_root_dir.inc.sh"
 
 cd "$MC_ROOT_DIR"
 
-echo "starting perlbrew install "
-./install/install_mc_perlbrew.sh
 echo "starting non-carton-based modules install "
 ./install/install_modules_outside_of_carton.sh
 echo "starting carton-based modules install "
