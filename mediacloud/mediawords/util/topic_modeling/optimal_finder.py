d1129a6b9d4443b35cee767c7273de0accefccfe
Alan32Liu <alan32.liu@gmail.com>
2017-08-13 16:34:33 +1000

    a finder that can identify the max/min points of a polynomial computed based on a few points

---
+++ mediacloud/mediawords/util/topic_modeling/optimal_finder.py
@@ -0,0 +1,56 @@
+import numpy as np
+from typing import List
+from numpy.polynomial import polynomial
+
+
+class OptimalFinder:
+    """Given a list of data points,
+    identify the best fit polynomial equation,
+    and find the root point(s) which is the max/min value"""
+
+    def _identify_equation(self,
+                           x: List[int],
+                           y: List[float],
+                           degree: int=2,
+                           accuracy: int=10) -> List[int]:
+        """
+        Identify the polynomial equation of x and y
+        :param x: a list of x values
+        :param y: a list of y values
+        :param degree:c
+        :param accuracy: the number of decimal places to keep
+        :return: coefficient of polynomials, params[i] * x^(degree-i)
+        """
+        params = [round(number=param, ndigits=accuracy)
+                  for param in np.polyfit(x=x, y=y, deg=degree)][::-1]
+        return params
+
+    def _find_roots(self,
+                    params: List[int]=None,
+                    accuracy: int=10) -> List[int]:
+        """
+        Find the root of a polynomial equation
+        :param params: parameters of polynomial equation, params[i] * x^(degree-i)
+        :param accuracy: the number of decimal places to keep
+        :return: the list of roots
+        """
+        roots = [round(number=root, ndigits=accuracy)
+                 for root in np.roots(params)]
+
+        return roots
+
+    def find_extreme(self,
+                     x: List[int],
+                     y: List[float],
+                     degree: int=2) -> List[int]:
+        """
+        Find out the extreme value of the polynomial via derivative
+        :param x: a list of x values
+        :param y: a list of y values
+        :param degree: max power of x
+        :return: the list of extreme values
+        """
+        params = self._identify_equation(x=x, y=y, degree=degree)
+        first_der_params = [param for param in polynomial.polyder(params)]
+        roots = self._find_roots(params=first_der_params)
+        return roots
