f4e912dcc187c64fd6752b809b2470baf132ef92
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-08-15 15:26:04 +0300

    "perlbrew use" full path to Perlbrew library

--- script/set_perlbrew_environment.sh
+++ script/set_perlbrew_environment.sh
@@ -12,7 +12,7 @@ unset MANPATH
 source ${PERLBREW_ROOT}/etc/bashrc
 
 # Switch to whatever version has the "mediacloud" library
-perlbrew use @mediacloud
+perlbrew use "perl-system@mediacloud"
 
 # NOTE: We filter the useless MANPATH warning message this way because there was no good way to get rid of it.
 # perlbrew use will fail unless $MANPATH is set but then it generates this warning.
