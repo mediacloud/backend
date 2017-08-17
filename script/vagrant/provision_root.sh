314e46ff5ee75481019351cb3763f783081ddc86
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-08-18 01:57:04 +0300

    Generate and set locale using Ansible

--- script/vagrant/provision_root.sh
+++ script/vagrant/provision_root.sh
@@ -6,8 +6,6 @@
 # Tested on Ubuntu (64 bit)
 #
 
-MC_LOCALE_LANG="en_US"
-MC_LOCALE_LANG_VARIANT="UTF-8"
 MC_TIMEZONE="America/New_York"
 
 
@@ -22,18 +20,6 @@ echo "Setting timezone to ${MC_TIMEZONE}..."
 ln -sf "/usr/share/zoneinfo/${MC_TIMEZONE}" /etc/localtime
 echo -n "$MC_TIMEZONE" > /etc/timezone
 dpkg-reconfigure tzdata
-
-echo "Setting default locale (or else Perl's test locale.t will fail)..."
-locale-gen $MC_LOCALE_LANG
-locale-gen $MC_LOCALE_LANG.$MC_LOCALE_LANG_VARIANT
-update-locale LANG=$MC_LOCALE_LANG.$MC_LOCALE_LANG_VARIANT LANGUAGE=$MC_LOCALE_LANG
-dpkg-reconfigure locales
-
-# Export locale settings manually for this session; later on, it will be set
-# automatically upon logging in
-export LANG=$MC_LOCALE_LANG.$MC_LOCALE_LANG_VARIANT
-export LANGUAGE=$MC_LOCALE_LANG
-locale
 
 if [ -b /dev/sda ]; then
     echo "Installing GRUB so that APT doesn't complain..."
