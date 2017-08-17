0ce449cf74fa710cafb71681a13b320af45f4d34
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-08-18 02:08:09 +0300

    Remove redundant commands

--- script/vagrant/provision_root.sh
+++ script/vagrant/provision_root.sh
@@ -19,12 +19,3 @@ if [ -b /dev/sda ]; then
     grub-install /dev/sda
     update-grub
 fi
-
-echo "Fetching a list of APT updates and new repository listings..."
-apt-get update
-
-echo "Upgrading packages with APT..."
-apt-get -y upgrade
-
-echo "Installing some basic utilities..."
-apt-get -y install vim git screen mc zip unzip links htop
