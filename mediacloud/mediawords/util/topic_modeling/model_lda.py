f8193661cf1f928081ad01d91d6c06c287494769
Alan32Liu <alan32.liu@gmail.com>
2017-08-20 00:43:00 +1000

    improved polynomial tuning algorithm

--- mediacloud/mediawords/util/topic_modeling/model_lda.py
+++ mediacloud/mediawords/util/topic_modeling/model_lda.py
@@ -32,7 +32,7 @@ class ModelLDA(BaseTopicModel):
         self._token_matrix = np.empty
         self._stories_number = 0
         self._random_state = 1
-        logging.getLogger("lda").setLevel(logging.WARNING)
+        logging.getLogger("lda").setLevel(logging.WARN)
 
     def add_stories(self, stories: Dict[int, List[List[str]]]) -> None:
         """
@@ -59,13 +59,13 @@ class ModelLDA(BaseTopicModel):
         """
         dictionary = corpora.Dictionary(new_stories_tokens)
 
-        self.vocab = list(dictionary.token2id.keys())
+        self._vocab = list(dictionary.token2id.keys())
 
         token_count = []
         for story_tokens in self._stories_tokens:
-            token_count.append([story_tokens.count(token) for token in self.vocab])
+            token_count.append([story_tokens.count(token) for token in self._vocab])
 
-        self.token_matrix = np.array(token_count)
+        self._token_matrix = np.array(token_count)
 
     def summarize_topic(self, total_topic_num: int = 0,
                         topic_word_num: int = 4,
@@ -76,21 +76,23 @@ class ModelLDA(BaseTopicModel):
         :rtype: list
         and corresponding list of TOPIC_NUMBER topics (each topic contains WORD_NUMBER words)
         """
+        # logging.warning(msg="total_topic_num={}".format(total_topic_num))
         total_topic_num = total_topic_num if total_topic_num else self._stories_number
+        logging.warning(msg="total_topic_num={}".format(total_topic_num))
 
         # turn our token documents into a id <-> term dictionary
         self._model = lda.LDA(n_topics=total_topic_num,
                               n_iter=iteration_num,
                               random_state=self._random_state)
 
-        self._model.fit(self.token_matrix)
+        self._model.fit(self._token_matrix)
         topic_word = self._model.topic_word_
         n_top_words = topic_word_num
 
         topic_words_list = []
         for i, topic_dist in enumerate(topic_word):
             topic_words_list.append(
-                np.array(self.vocab)[np.argsort(topic_dist)][:-(n_top_words + 1):-1])
+                np.array(self._vocab)[np.argsort(topic_dist)][:-(n_top_words + 1):-1])
 
         doc_topic = self._model.doc_topic_
 
@@ -110,14 +112,18 @@ class ModelLDA(BaseTopicModel):
             topic_num = self._stories_number
 
         if not self._model:
-            self.summarize_topic()
+            logging.warning(msg="Model does not exist, "
+                                "train a new one with topic_num = {}".format(topic_num))
+            self._train(topic_num=topic_num)
 
         if self._model.n_topics != topic_num:
-            self.train(topic_num=topic_num)
+            logging.warning(msg="model.n_topics({}) != desired topic_num ({})"
+                            .format(self._model.n_topics, topic_num))
+            self._train(topic_num=topic_num)
 
         return [self._model.n_topics, self._model.loglikelihood()]
 
-    def train(self, topic_num: int, word_num: int = 4, unit_iteration_num: int = 1) -> float:
+    def _train(self, topic_num: int, word_num: int = 4, unit_iteration_num: int = 10000) -> float:
         """
         train the model iteratively until the result is stable
         :param topic_num: total number of topics
@@ -125,18 +131,28 @@ class ModelLDA(BaseTopicModel):
         :param unit_iteration_num: number of iteration for each time
         :return: the final log likelihood value
         """
-        prev_likelihood = None
-
-        while True:
-            self.summarize_topic(
+        self.summarize_topic(
                 total_topic_num=topic_num,
                 topic_word_num=word_num,
                 iteration_num=unit_iteration_num)
-            if (type(prev_likelihood) == float) \
-                    and (prev_likelihood == self._model.loglikelihood()):
-                return prev_likelihood
 
-            prev_likelihood = self._model.loglikelihood()
+        return self._model.loglikelihood()
+
+        # prev_likelihood = None
+        # self._model = None
+        #
+        # while True:
+        #     logging.warning(msg="topic_num={}, prev_likelihood={}"
+        #                     .format(topic_num, prev_likelihood))
+        #     self.summarize_topic(
+        #         total_topic_num=topic_num,
+        #         topic_word_num=word_num,
+        #         iteration_num=unit_iteration_num)
+        #     if (type(prev_likelihood) == float) \
+        #             and (prev_likelihood == self._model.loglikelihood()):
+        #         return prev_likelihood
+        #
+        #     prev_likelihood = self._model.loglikelihood()
 
     def tune_with_iteration(self, topic_word_num: int = 4,
                             topic_num_range: List[int] = None,
@@ -165,7 +181,7 @@ class ModelLDA(BaseTopicModel):
 
         for topic_num in iter(topic_num_range):
             if topic_num not in score_dict.values():
-                likelihood = self.train(topic_num=topic_num, word_num=topic_word_num)
+                likelihood = self._train(topic_num=topic_num, word_num=topic_word_num)
                 score_dict[likelihood] = topic_num
 
         sorted_scores = sorted(score_dict.keys())[::-1]
@@ -185,13 +201,18 @@ class ModelLDA(BaseTopicModel):
 
         if not topic_num_samples:
             # TODO: Find better initial sample values here
-            topic_num_samples = [1, len(self._stories_ids) + 10, len(self._stories_ids) + 20]
+            topic_num_samples = [1,
+                                 # int(self._stories_number/4),
+                                 int(self._stories_number/2),
+                                 self._stories_number,
+                                 # int(self._stories_number * 1.5),
+                                 self._stories_number * 2]
 
         score_dict = {}
 
         for topic_num in iter(topic_num_samples):
             if topic_num not in score_dict.values():
-                likelihood = self.train(topic_num=topic_num, word_num=topic_word_num)
+                likelihood = self._train(topic_num=topic_num, word_num=topic_word_num)
                 score_dict[likelihood] = topic_num
 
         optimal_topic_nums = OptimalFinder().find_extreme(
@@ -204,7 +225,7 @@ class ModelLDA(BaseTopicModel):
             if num in score_dict.values():
                 continue
 
-            likelihood = self.train(topic_num=num, word_num=topic_word_num)
+            likelihood = self._train(topic_num=num, word_num=topic_word_num)
             score_dict[likelihood] = num
 
         optimal_topic_num = score_dict.get(max(score_dict.keys()))
@@ -216,12 +237,32 @@ if __name__ == '__main__':
     model = ModelLDA()
 
     # pool = TokenPool(connect_to_db())
-
     pool = TokenPool(SampleHandler())
 
-    model.add_stories(pool.output_tokens())
+    all_tokens = pool.output_tokens()
+    # print(tokens)
+    model.add_stories(all_tokens)
     topic_number = model.tune_with_polynomial()
     print(topic_number)
+
     evaluation = model.evaluate(topic_num=topic_number)
-    print("Number of Topics = {}; Likelihood = {}".format(evaluation[0], evaluation[1]))
-    print(model.summarize_topic(total_topic_num=2))
+    print(evaluation)
+
+    for x in range(topic_number-2, topic_number+2):
+        evaluation = model.evaluate(topic_num=x)
+        print(evaluation)
+
+    evaluation = model.evaluate()
+    print(evaluation)
+
+    # evaluation = model.evaluate(topic_num=6)
+    # logging.warning(msg="Number of Topics = {}; Likelihood = {}"
+    #                     .format(evaluation[0], evaluation[1]))
+    # evaluation = model.evaluate(topic_num=1)
+    # logging.warning(msg="Number of Topics = {}; Likelihood = {}"
+    #                     .format(evaluation[0], evaluation[1]))
+    # evaluation = model.evaluate(topic_num=0)
+    # logging.warning(msg="Number of Topics = {}; Likelihood = {}"
+    #                     .format(evaluation[0], evaluation[1]))
+
+    # print(model.summarize_topic(total_topic_num=topic_number))
