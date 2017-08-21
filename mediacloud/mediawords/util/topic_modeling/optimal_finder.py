e39415b83b5079105c72f25a028ccb60778f003c
Alan32Liu <alan32.liu@gmail.com>
2017-08-21 23:51:16 +1000

    removed unnecessary code to keep higher level of accuracy

--- mediacloud/mediawords/util/topic_modeling/optimal_finder.py
+++ mediacloud/mediawords/util/topic_modeling/optimal_finder.py
@@ -13,32 +13,27 @@ class OptimalFinder:
     @staticmethod
     def _identify_equation(x: List[int],
                            y: List[float],
-                           degree: int=2,
-                           accuracy: int=10) -> List[int]:
+                           degree: int=2) -> List[int]:
         """
         Identify the polynomial equation of x and y
         :param x: a list of x values
         :param y: a list of y values
         :param degree:c
-        :param accuracy: the number of decimal places to keep
         :return: coefficient of polynomials, params[i] * x^(degree-i)
         """
-        params = [round(number=param, ndigits=accuracy)
-                  for param in np.polyfit(x=x, y=y, deg=degree)][::-1]
+
+        params = list(np.polyfit(x=x, y=y, deg=degree)[::-1])
         logging.warning(msg="Equation params = {}".format(params))
         return params
 
     @staticmethod
-    def _find_roots(params: List[int]=None,
-                    accuracy: int=10) -> List[int]:
+    def _find_roots(params: List[int]=None) -> List[int]:
         """
         Find the root of a polynomial equation
         :param params: parameters of polynomial equation, params[i] * x^(degree-i)
-        :param accuracy: the number of decimal places to keep
         :return: the list of roots
         """
-        roots = [round(number=root, ndigits=accuracy)
-                 for root in np.roots(params)]
+        roots = list(np.roots(params))
         logging.warning(msg="Equation roots = {}".format(roots))
         return roots
 
@@ -53,6 +48,8 @@ class OptimalFinder:
         :param degree: max power of x
         :return: the list of extreme values
         """
+        if len(x) < 3:
+            return [x[y.index(max(y))]]
         params = self._identify_equation(x=x, y=y, degree=degree)
         first_der_params = [param for param in polynomial.polyder(params)][::-1]
         logging.warning(msg="First Derivative Parameters = {}".format(first_der_params))
