cf0ea0a212a52c9ceb0fd39f20664784aeb4f879
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-08-24 05:53:46 +0300

    Try running Travis builds on Docker using dockermediacloud/mediacloud-travis:20170824 image

--- script/run_test_suite_for_devel_cover.sh
+++ script/run_test_suite_for_devel_cover.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 #
-# Run the test suite with Devel::Cover, generate coverage report
+# Run the test suite with Devel::Cover
 #
 # Usage:
 #
-#     # create test coverage database, generate HTML report:
+#     # create test coverage database:
 #     ./script/run_test_suite_for_devel_cover.sh
-#
-# or
-#
-#     # create test coverage database, generate report of type "report_type"
-#     # (e.g. "html" or "coveralls"):
-#     ./script/run_test_suite_for_devel_cover.sh [report_type]
 #
 
 set -e
@@ -21,34 +15,17 @@ set -o errexit
 cd `dirname $0`/../
 
 DESTROY_SOLR="0"
-REPORT="html"
-
-if [[ $# == 2 && -n "$2" ]]; then
-    if [ '--destroy-solr' = "$2" ]
-    then
-	echo "set destroy solr"
-	DESTROY_SOLR=1
-    fi;
-    REPORT="$1";
-fi
 
 if [[ $# == 1 && -n "$1" ]]; then
-	REPORT="$1";
+    if [ '--destroy-solr' = "$1" ]; then
+        echo "set destroy solr"
+        DESTROY_SOLR=1
+    fi
 fi
-
-# Devel::Cover options
-HARNESS_PERL_SWITCHES='-MDevel::Cover=+ignore,local/,+ignore,^foreign_modules/,+ignore,t/,+ignore,\.t$'
-
-echo "Will generate test coverage report: $REPORT"
 
 echo "Removing old test coverage database..." 1>&2
 rm -rf cover_db/
 
 echo "Running full test suite..." 1>&2
-HARNESS_PERL_SWITCHES="$HARNESS_PERL_SWITCHES" ./script/run_test_suite.sh
-
-
-echo "Generating '$REPORT' report..." 1>&2
-./script/run_in_env.sh cover --nogcov -report "$REPORT"
-
-echo "Done." 1>&2
+HARNESS_PERL_SWITCHES='-MDevel::Cover=+ignore,t/,+ignore,\.t$' \
+    ./script/run_test_suite.sh
