2c7968c5be8bdb2d83a928d2363e160e63a00c39
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-09-26 16:52:14 +0300

    Remove orphan tags
    References #197.

--- schema/migrations/mediawords-4631-4632.sql
+++ schema/migrations/mediawords-4631-4632.sql
@@ -20,6 +20,14 @@ SET search_path = public, pg_catalog;
 
 DROP TABLE IF EXISTS story_sentences_tags_map;
 
+-- Remove orphan tags
+DELETE FROM tags
+WHERE NOT EXISTS (SELECT 1 FROM feeds_tags_map WHERE tags.tags_id = feeds_tags_map.tags_id)
+  AND NOT EXISTS (SELECT 1 FROM media_tags_map WHERE tags.tags_id = media_tags_map.tags_id)
+  AND NOT EXISTS (SELECT 1 FROM stories_tags_map WHERE tags.tags_id = stories_tags_map.tags_id)
+  AND NOT EXISTS (SELECT 1 FROM media_suggestions_tags_map WHERE tags.tags_id = media_suggestions_tags_map.tags_id)
+;
+
 
 CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
