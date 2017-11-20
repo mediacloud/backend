09da08dbad0b2f3cb761aee5a5736a983139cf2d
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-11-20 19:33:54 +0800

    Rewrite MediaWords::Util::Compress::* to Python

--- mediacloud/mediawords/util/compress.py
+++ mediacloud/mediawords/util/compress.py
@@ -1,4 +1,7 @@
+import bz2
+import gzip as gzip_lib
 import os
+from typing import Union
 
 from mediawords.util.log import create_logger
 from mediawords.util.paths import file_extension
@@ -8,7 +11,38 @@ from mediawords.util.process import run_command_in_foreground, McRunCommandInFor
 log = create_logger(__name__)
 
 
-class McExtractTarballToDirectoryException(Exception):
+class McCompressException(Exception):
+    """Exception raised when compressing or decompressing data."""
+    pass
+
+
+class McExtractTarballToDirectoryException(McCompressException):
+    """extract_tarball_to_directory() exception."""
+    pass
+
+
+class McExtractZipToDirectoryException(McCompressException):
+    """extract_zip_to_directory() exception."""
+    pass
+
+
+class McBzip2Exception(McCompressException):
+    """bzip2() exception."""
+    pass
+
+
+class McBunzip2Exception(McCompressException):
+    """bunzip2() exception."""
+    pass
+
+
+class McGzipException(McCompressException):
+    """gzip() exception."""
+    pass
+
+
+class McGunzipException(McCompressException):
+    """gunzip() exception."""
     pass
 
 
@@ -43,10 +77,6 @@ def extract_tarball_to_directory(archive_file: str, dest_directory: str, strip_r
         raise McExtractTarballToDirectoryException("Error while extracting archive '%s': %s" % (archive_file, str(ex)))
 
 
-class McExtractZipToDirectoryException(Exception):
-    pass
-
-
 def extract_zip_to_directory(archive_file: str, dest_directory: str) -> None:
     """Extract ZIP archive (.zip or .war) to destination directory."""
 
@@ -67,3 +97,107 @@ def extract_zip_to_directory(archive_file: str, dest_directory: str) -> None:
         run_command_in_foreground(args)
     except McRunCommandInForegroundException as ex:
         raise McExtractZipToDirectoryException("Error while extracting archive '%s': %s" % (archive_file, str(ex)))
+
+
+def bzip2(data: Union[str, bytes]) -> bytes:
+    """Bzip2 data."""
+
+    if data is None:
+        raise McBzip2Exception("Data is None.")
+
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+
+    if not isinstance(data, bytes):
+        raise McBzip2Exception("Data is not str or bytes: %s" % str(data))
+
+    try:
+        bzipped2_data = bz2.compress(data, compresslevel=9)
+    except Exception as ex:
+        raise McBzip2Exception("Unable to bzip2 data: %s" % str(ex))
+
+    if bzipped2_data is None:
+        raise McBzip2Exception("Bzipped data is None.")
+
+    if not isinstance(bzipped2_data, bytes):
+        raise McBzip2Exception("Bzipped data is not bytes.")
+
+    return bzipped2_data
+
+
+def bunzip2(data: bytes) -> bytes:
+    """Bunzip2 data."""
+
+    if data is None:
+        raise McBunzip2Exception("Data is None.")
+
+    if not isinstance(data, bytes):
+        raise McBunzip2Exception("Data is not bytes: %s" % str(data))
+
+    if len(data) == 0:
+        raise McBunzip2Exception("Data is empty (no way an empty string is a valid Bzip2 archive).")
+
+    try:
+        bunzipped2_data = bz2.decompress(data)
+    except Exception as ex:
+        raise McBunzip2Exception("Unable to bunzip2 data: %s" % str(ex))
+
+    if bunzipped2_data is None:
+        raise McBunzip2Exception("Bunzipped data is None.")
+
+    if not isinstance(bunzipped2_data, bytes):
+        raise McBunzip2Exception("Bunzipped data is not bytes.")
+
+    return bunzipped2_data
+
+
+def gzip(data: Union[str, bytes]) -> bytes:
+    """Gzip data."""
+
+    if data is None:
+        raise McGzipException("Data is None.")
+
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+
+    if not isinstance(data, bytes):
+        raise McGzipException("Data is not str or bytes: %s" % str(data))
+
+    try:
+        gzipped_data = gzip_lib.compress(data, compresslevel=9)
+    except Exception as ex:
+        raise McGzipException("Unable to gzip data: %s" % str(ex))
+
+    if gzipped_data is None:
+        raise McGzipException("Gzipped data is None.")
+
+    if not isinstance(gzipped_data, bytes):
+        raise McGzipException("Gzipped data is not bytes.")
+
+    return gzipped_data
+
+
+def gunzip(data: bytes) -> bytes:
+    """Gunzip data."""
+
+    if data is None:
+        raise McGunzipException("Data is None.")
+
+    if not isinstance(data, bytes):
+        raise McGunzipException("Data is not bytes: %s" % str(data))
+
+    if len(data) == 0:
+        raise McGunzipException("Data is empty (no way an empty string is a valid Gzip archive).")
+
+    try:
+        gunzipped_data = gzip_lib.decompress(data)
+    except Exception as ex:
+        raise McGunzipException("Unable to gunzip data: %s" % str(ex))
+
+    if gunzipped_data is None:
+        raise McGunzipException("Gunzipped data is None.")
+
+    if not isinstance(gunzipped_data, bytes):
+        raise McGunzipException("Gunzipped data is not bytes.")
+
+    return gunzipped_data
