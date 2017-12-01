cd93fc3b82b4610926a6bfe4346a93ef2c99c32e
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-12-01 21:58:55 +0800

    Remove Univision and Superglue enum hacks
    All deployments have been migrated already.

--- lib/MediaWords/DB/Schema.pm
+++ lib/MediaWords/DB/Schema.pm
@@ -29,54 +29,6 @@ sub upgrade_db($;$)
         $db = MediaWords::DB::connect_to_db( $label, $do_not_check_schema_version );
     }
 
-    # Add 'univision' option to "feed_feed_type" enum
-    # (adding new enum values don't work in transactions or multi-line queries
-    # thus a migration wouldn't have worked)
-    my ( $feed_type_has_univision_value ) = $db->query(
-        <<SQL
-        SELECT 1
-        FROM pg_type AS t
-            JOIN pg_enum AS e ON t.oid = e.enumtypid
-            JOIN pg_catalog.pg_namespace AS n ON n.oid = t.typnamespace
-        WHERE n.nspname = CURRENT_SCHEMA()
-          AND t.typname = 'feed_feed_type'
-          AND e.enumlabel = 'univision'
-SQL
-    )->flat;
-    unless ( $feed_type_has_univision_value )
-    {
-        DEBUG( "Adding 'univision' value to 'feed_feed_type' enum..." );
-        $db->query( "ALTER TYPE feed_feed_type ADD VALUE 'univision'" );
-    }
-    else
-    {
-        DEBUG( "'feed_feed_type' already has 'univision' value" );
-    }
-
-    # Add 'superglue' option to "feed_feed_type" enum
-    # (adding new enum values don't work in transactions or multi-line queries
-    # thus a migration wouldn't have worked)
-    my ( $feed_type_has_superglue_value ) = $db->query(
-        <<SQL
-        SELECT 1
-        FROM pg_type AS t
-            JOIN pg_enum AS e ON t.oid = e.enumtypid
-            JOIN pg_catalog.pg_namespace AS n ON n.oid = t.typnamespace
-        WHERE n.nspname = CURRENT_SCHEMA()
-          AND t.typname = 'feed_feed_type'
-          AND e.enumlabel = 'superglue'
-SQL
-    )->flat;
-    unless ( $feed_type_has_superglue_value )
-    {
-        DEBUG( "Adding 'superglue' value to 'feed_feed_type' enum..." );
-        $db->query( "ALTER TYPE feed_feed_type ADD VALUE 'superglue'" );
-    }
-    else
-    {
-        DEBUG( "'feed_feed_type' already has 'superglue' value" );
-    }
-
     # Current schema version
     my $schema_version_query = <<EOF;
         SELECT value AS schema_version
