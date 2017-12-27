1b12e8ddb30fe19ff04be43b6bac8c2dd6aa95bd
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2017-12-27 18:58:03 +0200

    Add mix-in for text -> sentence tokenization using "sentence_splitter" module
    SentenceSplitterMixIn implements split_text_to_sentences() for languages that are supported by the "sentence_splitter" module.

--- mediacloud/mediawords/languages/__init__.py
+++ mediacloud/mediawords/languages/__init__.py
@@ -96,3 +96,94 @@ class SpaceSeparatedWordsMixIn(AbstractLanguage, metaclass=abc.ABCMeta):
         tokens = [token for token in tokens if is_word(token)]
 
         return tokens
+
+
+class SentenceSplitterMixIn(AbstractLanguage, metaclass=abc.ABCMeta):
+    """Language which is supported by "sentence_splitter" Python module."""
+
+    # Max. text length to try to split into sentences
+    __MAX_TEXT_LENGTH = 1024 * 1024
+
+    def __init__(self):
+        """Constructor."""
+        super().__init__()
+
+        # SentenceSplitter instance (lazy initialized)
+        self.__sentence_splitter = None
+
+    def split_text_to_sentences(self, text: str) -> List[str]:
+        """Splits text into sentences with "sentence_splitter" module.
+
+        Language code will be read from language_code() method."""
+        text = decode_object_from_bytes_if_needed(text)
+
+        language_code = self.language_code()
+
+        if self.__sentence_splitter is None:
+            try:
+                self.__sentence_splitter = SentenceSplitter(language=language_code)
+            except Exception as ex:
+                raise McLanguageException(
+                    "Unable to initialize sentence splitter for language '%s': %s" % (language_code, str(ex),)
+                )
+
+        if text is None:
+            log.warning("Text is None.")
+            return []
+
+        # Sentence tokenizer can hang for a very long on very long text, and anything greater than 1 MB is more likely
+        # to be an artifact than actual text
+        if len(text) > self.__MAX_TEXT_LENGTH:
+            text = text[:self.__MAX_TEXT_LENGTH]
+
+        # Only "\n\n" (not a single "\n") denotes the end of sentence, so remove single line breaks
+        text = re.sub('([^\n])\n([^\n])', r"\1 \2", text, flags=re.DOTALL)
+
+        # Remove asterisks from lists
+        text = re.sub(r" {2}\*", " ", text, flags=re.DOTALL)
+
+        text = re.sub(r"\n\s\*\n", "\n\n", text, flags=re.DOTALL)
+        text = re.sub(r"\n\n\n\*", "\n\n", text, flags=re.DOTALL)
+        text = re.sub(r"\n\n", "\n", text, flags=re.DOTALL)
+
+        # Replace tabs with spaces
+        text = re.sub(r"\t", " ", text, flags=re.DOTALL)
+
+        # Replace non-breaking spaces with normal spaces
+        text = re.sub(r"\xa0", " ", text, flags=re.DOTALL)
+
+        # Replace multiple spaces with a single space
+        text = re.sub(" +", " ", text, flags=re.DOTALL)
+
+        # The above regexp and HTML stripping often leave a space before the period at the end of a sentence
+        text = re.sub(r" +\.", ".", text, flags=re.DOTALL)
+
+        # We see lots of cases of missing spaces after sentence ending periods (has a hardcoded lower limit of
+        # characters because otherwise it breaks Portuguese "a.C.." abbreviations and such)
+        text = re.sub(r"([a-z]{2,})\.([A-Z][a-z]+)", r"\1. \2", text, flags=re.DOTALL)
+
+        # Replace Unicode's "…" with "..."
+        text = text.replace("…", "...")
+
+        # Trim whitespace from start / end of the whole string
+        text = text.strip()
+
+        # FIXME: fix "bla bla... yada yada"? is it two sentences?
+        # FIXME: fix "text . . some more text."?
+
+        if len(text) == 0:
+            log.debug("Text is empty after processing it.")
+            return []
+
+        # Split to sentences
+        sentences = self.__sentence_splitter.split(text=text)
+
+        non_empty_sentences = []
+        # Trim whitespace from start / end of each of the sentences
+        for sentence in sentences:
+            sentence = sentence.strip()
+
+            if len(sentence) > 0:
+                non_empty_sentences.append(sentence)
+
+        return non_empty_sentences
