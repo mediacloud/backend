12459c7f33e7e3cc194671051c3584a042f0bddc
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-02-20 01:31:26 +0800

    Make table_exists() support table names with schema

--- schema/mediawords.sql
+++ schema/mediawords.sql
@@ -24,7 +24,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4640;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4641;
 
 BEGIN
 
@@ -69,15 +69,31 @@ $$ LANGUAGE SQL;
 
 
 -- Returns true if table exists (and user has access to it)
+-- Table name might be with ("public.stories") or without ("stories") schema.
 CREATE OR REPLACE FUNCTION table_exists(target_table_name VARCHAR)
 RETURNS BOOLEAN AS $$
+DECLARE
+    schema_position INT;
+    schema VARCHAR;
 BEGIN
+
+    SELECT POSITION('.' IN target_table_name) INTO schema_position;
+
+    -- "." at string index 0 would return position 1
+    IF schema_position = 0 THEN
+        schema := CURRENT_SCHEMA();
+    ELSE
+        schema := SUBSTRING(target_table_name FROM 1 FOR schema_position - 1);
+        target_table_name := SUBSTRING(target_table_name FROM schema_position + 1);
+    END IF;
+
     RETURN EXISTS (
         SELECT 1
         FROM information_schema.tables
-        WHERE table_schema = CURRENT_SCHEMA()
+        WHERE table_schema = schema
           AND table_name = target_table_name
     );
+
 END;
 $$
 LANGUAGE plpgsql;
