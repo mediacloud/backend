35d7c3a4efafdaeb8ad776d1b29b5274317a0986
Hal Roberts <hroberts@cyber.law.harvard.edu>
2018-02-21 14:27:17 -0600

    group schema changes into one version

--- schema/migrations/mediawords-4642-4643.sql
+++ schema/migrations/mediawords-4642-4643.sql
@@ -13,6 +13,34 @@
 --
 -- 1 of 2. Import the output of 'apgdiff':
 --
+
+
+-- update media stats table for deleted story sentence
+CREATE FUNCTION update_media_db_row_last_updated() RETURNS trigger AS $$
+BEGIN
+    NEW.db_row_last_updated = now();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+create trigger update_media_db_row_last_updated before update or insert
+    on media for each row execute procedure update_media_db_row_last_updated();
+
+--- allow lookup of media by mediawords.util.url.normalized_url_lossy.
+-- the data in this table is accessed and kept up to date by mediawords.tm.media.lookup_medium_by_url
+create table media_normalized_urls (
+    media_normalized_urls_id        serial primary key,
+    media_id                        int not null references media,
+    normalized_url                  varchar(1024) not null,
+    db_row_last_updated             timestamp not null default now(),
+
+    -- assigned the value of mediawords.util.url.normalized_url_lossy_version()
+    normalize_url_lossy_version    int not null
+);
+
+create unique index media_normalized_urls_medium on media_normalized_urls(normalize_url_lossy_version, media_id);
+create index media_normalized_urls_url on media_normalized_urls(normalized_url);
+
 create table topic_fetch_urls(
     topic_fetch_urls_id         bigserial primary key,
     topics_id                   int not null references topics on delete cascade,
@@ -25,6 +53,7 @@ create table topic_fetch_urls(
     assume_match                boolean not null default false
 );
 
+create index topic_fetch_urls_pending on topic_fetch_urls(topics_id) where state = 'pending';
 
 --
 -- 2 of 2. Reset the database version.
