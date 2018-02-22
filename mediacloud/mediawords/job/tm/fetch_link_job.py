dfb2b3329202192be9a90f24d028f2406f9e7e2a
Hal Roberts <hroberts@cyber.law.harvard.edu>
2018-02-21 18:22:22 -0600

    reduce REQUEUES_UNTIL_SLEEP

--- mediacloud/mediawords/job/tm/fetch_link_job.py
+++ mediacloud/mediawords/job/tm/fetch_link_job.py
@@ -18,7 +18,7 @@ log = create_logger(__name__)
 
 
 # after requeueing this many times in a rows, start sleeping one second before each requeue
-REQUEUES_UNTIL_SLEEP = 100
+REQUEUES_UNTIL_SLEEP = 20
 
 
 class McFetchLinkJobException(McAbstractJobException):
