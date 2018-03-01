f0229543ed8cc7c01dac329a6bd3a8484d68cbba
Hal Roberts <hroberts@cyber.law.harvard.edu>
2018-02-28 22:07:57 -0600

    fix backward update

--- schema/migrations/mediawords-4646-4647.sql
+++ schema/migrations/mediawords-4646-4647.sql
@@ -18,7 +18,7 @@ alter table topic_fetch_urls add topic_links_id int references topic_links on de
 
 create index topic_fetch_urls_url on topic_fetch_urls(url);
 
-update topic_links set link_spidered = 't' where ref_stories_id is null;
+update topic_links set link_spidered = 't' where ref_stories_id is not null;
 
 --
 -- 2 of 2. Reset the database version.
