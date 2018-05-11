0692d8827e4dbed78fb08f04dd19af6603a91ebf
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-05-11 20:29:10 +0300

    Extract functions to be used by all tables partitioned by "stories_id"
    * stories_partition_chunk_size() -- return chunk size of stories stored in a single partition
    * stories_partition_name() -- return partition name for a given partition base name and "stories_id"
    * stories_create_partitions() -- create missing partitions for a given partition base name, return list of created partitions

--- schema/mediawords.sql
+++ schema/mediawords.sql
@@ -24,7 +24,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4658;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4659;
 
 BEGIN
 
@@ -919,15 +919,126 @@ create index feeds_stories_map_story on feeds_stories_map (stories_id);
 
 
 --
--- Story -> tag map
+-- Partitioning tools
 --
 
-CREATE OR REPLACE FUNCTION stories_tags_map_partition_chunk_size()
+-- Return partition size for every table that is partitioned by "stories_id"
+CREATE OR REPLACE FUNCTION stories_partition_chunk_size()
 RETURNS BIGINT AS $$
 BEGIN
     RETURN 100 * 1000 * 1000;   -- 100m stories in each partition
 END; $$
 LANGUAGE plpgsql IMMUTABLE;
+
+
+-- Return partition table name for a given base table name and "stories_id"
+CREATE OR REPLACE FUNCTION stories_partition_name(base_table_name TEXT, stories_id BIGINT)
+RETURNS TEXT AS $$
+DECLARE
+
+    -- Up to 100 partitions, suffixed as "_00", "_01" ..., "_99"
+    -- (having more of them is not feasible)
+    to_char_format CONSTANT TEXT := '00';
+
+    -- Partition table name (e.g. "stories_tags_map_01")
+    table_name TEXT;
+
+    stories_id_chunk_number INT;
+
+BEGIN
+    SELECT stories_id / stories_partition_chunk_size() INTO stories_id_chunk_number;
+
+    SELECT base_table_name || '_' || TRIM(leading ' ' FROM TO_CHAR(stories_id_chunk_number, to_char_format))
+        INTO table_name;
+
+    RETURN table_name;
+END;
+$$
+LANGUAGE plpgsql IMMUTABLE;
+
+
+-- Create missing partitions for tables partitioned by "stories_id", returning
+-- a list of created partition tables
+CREATE OR REPLACE FUNCTION stories_create_partitions(base_table_name TEXT)
+RETURNS SETOF TEXT AS
+$$
+DECLARE
+    chunk_size INT;
+    max_stories_id BIGINT;
+    partition_stories_id BIGINT;
+
+    -- Partition table name (e.g. "stories_tags_map_01")
+    target_table_name TEXT;
+
+    -- Partition table owner (e.g. "mediaclouduser")
+    target_table_owner TEXT;
+
+    -- "stories_id" chunk lower limit, inclusive (e.g. 30,000,000)
+    stories_id_start BIGINT;
+
+    -- stories_id chunk upper limit, exclusive (e.g. 31,000,000)
+    stories_id_end BIGINT;
+BEGIN
+
+    SELECT stories_partition_chunk_size() INTO chunk_size;
+
+    -- Create +1 partition for future insertions
+    SELECT COALESCE(MAX(stories_id), 0) + chunk_size FROM stories INTO max_stories_id;
+
+    FOR partition_stories_id IN 1..max_stories_id BY chunk_size LOOP
+        SELECT stories_partition_name( base_table_name, partition_stories_id ) INTO target_table_name;
+        IF table_exists(target_table_name) THEN
+            RAISE NOTICE 'Partition "%" for story ID % already exists.', target_table_name, partition_stories_id;
+        ELSE
+            RAISE NOTICE 'Creating partition "%" for story ID %', target_table_name, partition_stories_id;
+
+            SELECT (partition_stories_id / chunk_size) * chunk_size INTO stories_id_start;
+            SELECT ((partition_stories_id / chunk_size) + 1) * chunk_size INTO stories_id_end;
+
+            EXECUTE '
+                CREATE TABLE ' || target_table_name || ' (
+
+                    PRIMARY KEY (' || base_table_name || '_id),
+
+                    -- Partition by stories_id
+                    CONSTRAINT ' || REPLACE(target_table_name, '.', '_') || '_stories_id CHECK (
+                        stories_id >= ''' || stories_id_start || '''
+                    AND stories_id <  ''' || stories_id_end   || '''),
+
+                    -- Foreign key to stories.stories_id
+                    CONSTRAINT ' || REPLACE(target_table_name, '.', '_') || '_stories_id_fkey
+                        FOREIGN KEY (stories_id) REFERENCES stories (stories_id) MATCH FULL ON DELETE CASCADE
+
+                ) INHERITS (' || base_table_name || ');
+            ';
+
+            -- Update owner
+            SELECT u.usename AS owner
+            FROM information_schema.tables AS t
+                JOIN pg_catalog.pg_class AS c ON t.table_name = c.relname
+                JOIN pg_catalog.pg_user AS u ON c.relowner = u.usesysid
+            WHERE t.table_name = base_table_name
+              AND t.table_schema = 'public'
+            INTO target_table_owner;
+
+            EXECUTE 'ALTER TABLE ' || target_table_name || ' OWNER TO ' || target_table_owner || ';';
+
+            -- Add created partition name to the list of returned partition names
+            RETURN NEXT target_table_name;
+
+        END IF;
+    END LOOP;
+
+    RETURN;
+
+END;
+$$
+LANGUAGE plpgsql;
+
+
+--
+-- Story -> tag map
+--
 
 -- "Master" table (no indexes, no foreign keys as they'll be ineffective)
 CREATE TABLE stories_tags_map (
@@ -949,101 +1060,41 @@ CREATE TRIGGER stories_tags_map_update_stories_last_updated_trigger
     FOR EACH ROW EXECUTE PROCEDURE update_stories_updated_time_by_stories_id_trigger();
 
 
-CREATE OR REPLACE FUNCTION stories_tags_map_get_partition_name(stories_id BIGINT, table_name TEXT)
-RETURNS TEXT AS $$
-DECLARE
-    to_char_format CONSTANT TEXT := '00';     -- Up to 100 partitions, suffixed as "_00", "_01" ..., "_99"
-                                              -- (having more of them is not feasible)
-    stories_id_chunk_number INT;
-
-    target_table_name TEXT;       -- partition table name (e.g. "stories_tags_map_01")
-BEGIN
-    SELECT stories_id / stories_tags_map_partition_chunk_size() INTO stories_id_chunk_number;
-
-    SELECT table_name || '_' || trim(leading ' ' FROM to_char(stories_id_chunk_number, to_char_format))
-        INTO target_table_name;
-
-    RETURN target_table_name;
-END;
-$$
-LANGUAGE plpgsql;
-
-
--- Create missing stories_tags_map partitions
+-- Create missing "stories_tags_map" partitions
 CREATE OR REPLACE FUNCTION stories_tags_map_create_partitions()
 RETURNS VOID AS
 $$
 DECLARE
-    chunk_size INT;
-    max_stories_id BIGINT;
-    partition_stories_id BIGINT;
-
-    target_table_name TEXT;       -- partition table name (e.g. "stories_tags_map_01")
-    target_table_owner TEXT;      -- partition table owner (e.g. "mediaclouduser")
-
-    stories_id_start INT;         -- stories_id chunk lower limit, inclusive (e.g. 30,000,000)
-    stories_id_end INT;           -- stories_id chunk upper limit, exclusive (e.g. 31,000,000)
+    created_partitions TEXT[];
+    partition TEXT;
 BEGIN
 
-    SELECT stories_tags_map_partition_chunk_size() INTO chunk_size;
+    created_partitions := ARRAY(SELECT stories_create_partitions('stories_tags_map'));
 
-    -- Create +1 partition for future insertions
-    SELECT COALESCE(MAX(stories_id), 0) + chunk_size FROM stories INTO max_stories_id;
+    FOREACH partition IN ARRAY created_partitions LOOP
 
-    FOR partition_stories_id IN 1..max_stories_id BY chunk_size LOOP
-        SELECT stories_tags_map_get_partition_name( partition_stories_id, 'stories_tags_map' ) INTO target_table_name;
-        IF table_exists(target_table_name) THEN
-            RAISE NOTICE 'Partition "%" for story ID % already exists.', target_table_name, partition_stories_id;
-        ELSE
-            RAISE NOTICE 'Creating partition "%" for story ID %', target_table_name, partition_stories_id;
+        RAISE NOTICE 'Altering created partition "%"...', partition;
+        
+        -- Add extra foreign keys / constraints to the newly created partitions
+        EXECUTE '
+            ALTER TABLE ' || partition || '
 
-            SELECT (partition_stories_id / chunk_size) * chunk_size INTO stories_id_start;
-            SELECT ((partition_stories_id / chunk_size) + 1) * chunk_size INTO stories_id_end;
+                -- Foreign key to tags.tags_id
+                ADD CONSTRAINT ' || REPLACE(partition, '.', '_') || '_tags_id_fkey
+                    FOREIGN KEY (tags_id) REFERENCES tags (tags_id) MATCH FULL ON DELETE CASCADE,
 
-            EXECUTE '
-                CREATE TABLE ' || target_table_name || ' (
+                -- Unique duplets
+                ADD CONSTRAINT ' || REPLACE(partition, '.', '_') || '_stories_id_tags_id_unique
+                    UNIQUE (stories_id, tags_id);
+        ';
 
-                    PRIMARY KEY (stories_tags_map_id),
-
-                    -- Partition by stories_id
-                    CONSTRAINT ' || REPLACE(target_table_name, '.', '_') || '_stories_id CHECK (
-                        stories_id >= ''' || stories_id_start || '''
-                    AND stories_id <  ''' || stories_id_end   || '''),
-
-                    -- Foreign key to stories.stories_id
-                    CONSTRAINT ' || REPLACE(target_table_name, '.', '_') || '_stories_id_fkey
-                        FOREIGN KEY (stories_id) REFERENCES stories (stories_id) MATCH FULL ON DELETE CASCADE,
-
-                    -- Foreign key to tags.tags_id
-                    CONSTRAINT ' || REPLACE(target_table_name, '.', '_') || '_tags_id_fkey
-                        FOREIGN KEY (tags_id) REFERENCES tags (tags_id) MATCH FULL ON DELETE CASCADE,
-
-                    -- Unique duplets
-                    CONSTRAINT ' || REPLACE(target_table_name, '.', '_') || '_stories_id_tags_id_unique
-                        UNIQUE (stories_id, tags_id)
-
-                ) INHERITS (stories_tags_map);
-            ';
-
-            -- Update owner
-            SELECT u.usename AS owner
-            FROM information_schema.tables AS t
-                JOIN pg_catalog.pg_class AS c ON t.table_name = c.relname
-                JOIN pg_catalog.pg_user AS u ON c.relowner = u.usesysid
-            WHERE t.table_name = 'stories_tags_map'
-              AND t.table_schema = 'public'
-            INTO target_table_owner;
-
-            EXECUTE 'ALTER TABLE ' || target_table_name || ' OWNER TO ' || target_table_owner || ';';
-
-        END IF;
     END LOOP;
 
 END;
 $$
 LANGUAGE plpgsql;
 
--- Create initial partitions for empty database
+-- Create initial "stories_tags_map" partitions for empty database
 SELECT stories_tags_map_create_partitions();
 
 
@@ -1053,7 +1104,7 @@ RETURNS TRIGGER AS $$
 DECLARE
     target_table_name TEXT;       -- partition table name (e.g. "stories_tags_map_01")
 BEGIN
-    SELECT stories_tags_map_get_partition_name( NEW.stories_id, 'stories_tags_map' ) INTO target_table_name;
+    SELECT stories_partition_name( 'stories_tags_map', NEW.stories_id ) INTO target_table_name;
     EXECUTE '
         INSERT INTO ' || target_table_name || '
             SELECT $1.*
