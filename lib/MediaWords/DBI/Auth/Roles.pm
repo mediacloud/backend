f76fdc3cfef7585a0f7ee30e43d267a429518661
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-06-08 20:05:55 +0300

    Revert "Make ::DBI::Auth::Roles use Python implementation"
    This reverts commit 5676078de76fa728f5abe09e46194d7305e9df7e.

--- lib/MediaWords/DBI/Auth/Roles.pm
+++ lib/MediaWords/DBI/Auth/Roles.pm
@@ -1,11 +1,84 @@
 package MediaWords::DBI::Auth::Roles;
 
+#
+# Authentication role helpers
+#
+
 use strict;
 use warnings;
 
 use Modern::Perl "2015";
 use MediaWords::CommonLibs;
 
-import_python_module( __PACKAGE__, 'mediawords.dbi.auth.roles' );
+use Readonly;
+
+use MediaWords::DBI::Auth::Roles::List;
+
+# Fetch a list of available user roles
+sub all_user_roles($)
+{
+    my ( $db ) = @_;
+
+    my $roles = $db->query(
+        <<"SQL"
+        SELECT auth_roles_id,
+               role,
+               description
+        FROM auth_roles
+        ORDER BY auth_roles_id
+SQL
+    )->hashes;
+
+    return $roles;
+}
+
+# Fetch a user role's ID for a role; die()s if no such role was found
+sub role_id_for_role($$)
+{
+    my ( $db, $role ) = @_;
+
+    if ( !$role )
+    {
+        LOGCONFESS "Role is empty.";
+    }
+
+    my $auth_roles_id = $db->query(
+        <<"SQL",
+        SELECT auth_roles_id
+        FROM auth_roles
+        WHERE role = ?
+        LIMIT 1
+SQL
+        $role
+    )->hash;
+    if ( !( ref( $auth_roles_id ) eq ref( {} ) and $auth_roles_id->{ auth_roles_id } ) )
+    {
+        LOGCONFESS "Role '$role' was not found.";
+    }
+
+    return $auth_roles_id->{ auth_roles_id };
+}
+
+# List of role IDs to apply to new users
+sub default_role_ids($)
+{
+    my ( $db ) = @_;
+
+    my $default_role_ids = $db->query(
+        <<SQL,
+        SELECT auth_roles_id
+        FROM auth_roles
+        WHERE role = ?
+SQL
+        $MediaWords::DBI::Auth::Roles::List::SEARCH
+    )->flat;
+
+    unless ( scalar( @{ $default_role_ids } ) )
+    {
+        die 'Unable to find default role IDs';
+    }
+
+    return $default_role_ids;
+}
 
 1;
