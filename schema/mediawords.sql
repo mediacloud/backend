ae8050c120146a6d240d47dbafa33956e7398a64
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-06-13 17:44:17 +0300

    Remove "stories_without_readability_tag" table
    References #39.

--- schema/mediawords.sql
+++ schema/mediawords.sql
@@ -24,7 +24,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4676;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4677;
 
 BEGIN
 
@@ -2940,30 +2940,6 @@ END;
 $$
 LANGUAGE 'plpgsql';
 
-
---
--- Stories without Readability tag
---
-CREATE TABLE IF NOT EXISTS stories_without_readability_tag (
-    stories_id BIGINT NOT NULL REFERENCES stories (stories_id)
-);
-CREATE INDEX stories_without_readability_tag_stories_id
-    ON stories_without_readability_tag (stories_id);
-
--- Fill in the table manually with:
---
--- INSERT INTO scratch.stories_without_readability_tag (stories_id)
---     SELECT stories.stories_id
---     FROM stories
---         LEFT JOIN stories_tags_map
---             ON stories.stories_id = stories_tags_map.stories_id
-
---             -- "extractor_version:readability-lxml-0.3.0.5"
---             AND stories_tags_map.tags_id = 8929188
-
---     -- No Readability tag
---     WHERE stories_tags_map.tags_id IS NULL
---     ;
 
 -- implements link_id as documented in the topics api spec
 create table api_links (
