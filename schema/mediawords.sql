424fd03dac09783d988ff71638abc7be0014ab22
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-06-19 02:51:12 +0300

    Remove "mediawords_find_corrupted_sequences.pl" and find_corrupted_sequences() function
    Sequence monitoring is now done by Munin.

--- schema/mediawords.sql
+++ schema/mediawords.sql
@@ -24,7 +24,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4677;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4678;
 
 BEGIN
 
@@ -2632,76 +2632,6 @@ BEGIN
 
 END;
 $$
-LANGUAGE 'plpgsql';
-
-
--- Helper to find corrupted sequences (the ones in which the primary key's sequence value > MAX(primary_key))
-CREATE OR REPLACE FUNCTION find_corrupted_sequences()
-RETURNS TABLE(tablename VARCHAR, maxid BIGINT, sequenceval BIGINT)
-AS $BODY$
-DECLARE
-    r RECORD;
-BEGIN
-
-    SET client_min_messages TO WARNING;
-    DROP TABLE IF EXISTS temp_corrupted_sequences;
-    CREATE TEMPORARY TABLE temp_corrupted_sequences (
-        tablename VARCHAR NOT NULL UNIQUE,
-        maxid BIGINT,
-        sequenceval BIGINT
-    ) ON COMMIT DROP;
-    SET client_min_messages TO NOTICE;
-
-    FOR r IN (
-
-        -- Get all tables, their primary keys and serial sequence names
-        SELECT t.relname AS tablename,
-               primarykey AS idcolumn,
-               pg_get_serial_sequence(t.relname, primarykey) AS serialsequence
-        FROM pg_constraint AS c
-            JOIN pg_class AS t ON c.conrelid = t.oid
-            JOIN pg_namespace nsp ON nsp.oid = t.relnamespace
-            JOIN (
-                SELECT a.attname AS primarykey,
-                       i.indrelid
-                FROM pg_index AS i
-                    JOIN pg_attribute AS a
-                        ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
-                WHERE i.indisprimary
-            ) AS pkey ON pkey.indrelid = t.relname::regclass
-        WHERE conname LIKE '%_pkey'
-          AND nsp.nspname = CURRENT_SCHEMA()
-          AND t.relname NOT IN (
-            'story_similarities_100_short',
-            'url_discovery_counts'
-          )
-        ORDER BY t.relname
-
-    )
-    LOOP
-
-        -- Filter out the tables that have their max ID bigger than the last
-        -- sequence value
-        EXECUTE '
-            INSERT INTO temp_corrupted_sequences
-                SELECT tablename,
-                       maxid,
-                       sequenceval
-                FROM (
-                    SELECT ''' || r.tablename || ''' AS tablename,
-                           MAX(' || r.idcolumn || ') AS maxid,
-                           ( SELECT last_value FROM ' || r.serialsequence || ') AS sequenceval
-                    FROM ' || r.tablename || '
-                ) AS id_and_sequence
-                WHERE maxid > sequenceval
-        ';
-
-    END LOOP;
-
-    RETURN QUERY SELECT * FROM temp_corrupted_sequences ORDER BY tablename;
-
-END
-$BODY$
 LANGUAGE 'plpgsql';
 
 
