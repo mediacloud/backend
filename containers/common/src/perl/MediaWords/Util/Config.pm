c104f328388b1b3fc27f877471e72b631fc5bf00
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-07-16 16:16:47 -0400

    Remove some boilerplate

--- containers/common/src/perl/MediaWords/Util/Config.pm
+++ containers/common/src/perl/MediaWords/Util/Config.pm
@@ -9,23 +9,6 @@ use Modern::Perl "2015";
 
 use MediaWords::Util::Python;
 
-BEGIN
-{
-
-    use File::Basename;
-    use File::Spec;
-    use Cwd qw( realpath );
-    use File::Spec;
-
-    sub get_mc_python_dir()
-    {
-        return realpath( File::Spec->canonpath( dirname( __FILE__ ) . "/../../../" . 'mediacloud/' ) );
-    }
-
-    # ::CommonLibs doesn't set PYTHONPATH
-    $ENV{ PYTHONPATH } = get_mc_python_dir();
-}
-
 MediaWords::Util::Python::import_python_module( __PACKAGE__, 'mediawords.util.config' );
 
 1;
