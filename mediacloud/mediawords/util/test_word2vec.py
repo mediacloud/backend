14b31faa84981e7aa3d335b10f578ef71fdbddf2
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2018-09-28 13:08:38 +0300

    Fetch story sentences in chunks (1000 stories_id at once) for word2vec
    Depending on the snapshot size, all attempts to join story_sentences to
    snap.stories (with WHERE or INNER JOIN, and with or without a temporary
    table) might lead to sequential scan on story_sentences.
    
    Instead, fetch sentences in chunks (sentences for up to 1000 stories per
    chunk) and feed them to word2vec.
    
    References #348.
    
    References #481.

--- mediacloud/mediawords/util/test_word2vec.py
+++ mediacloud/mediawords/util/test_word2vec.py
@@ -14,6 +14,12 @@ from mediawords.util.word2vec.sentence_iterators import SnapshotSentenceIterator
 
 
 class TestWord2vec(TestDatabaseWithSchemaTestCase):
+    TEST_STORY_COUNT = 20
+    TEST_SENTENCE_PER_STORY_COUNT = 20
+
+    # Use very small stories_id chunk size to test out whether all stories and their sentences are being read
+    TEST_STORIES_ID_CHUNK_SIZE = 3
+
     __slots__ = [
         'topics_id',
         'snapshots_id',
@@ -25,16 +31,15 @@ class TestWord2vec(TestDatabaseWithSchemaTestCase):
         medium = create_test_medium(db=self.db(), label='test')
         feed = create_test_feed(db=self.db(), label='feed', medium=medium)
 
-        # Test stories
-        for story_num in range(10):
+        for story_num in range(self.TEST_STORY_COUNT):
             story = create_test_story(db=self.db(), label='story-%d' % story_num, feed=feed)
-            for sentence_number in range(1, 10):
+            for sentence_number in range(1, self.TEST_SENTENCE_PER_STORY_COUNT + 1):
                 self.db().create(table='story_sentences', insert_hash={
                     'stories_id': story['stories_id'],
                     'media_id': medium['media_id'],
                     'publish_date': story['publish_date'],
                     'sentence_number': sentence_number,
-                    'sentence': 'One, two, three, four, five, six, seven, eight, nine, ten.',
+                    'sentence': 'story {}, sentence {}'.format(story['stories_id'], sentence_number),
                 })
 
         # Test topic
@@ -59,13 +64,43 @@ class TestWord2vec(TestDatabaseWithSchemaTestCase):
         """, {'snapshots_id': self.snapshots_id})
 
     def test_snapshot_sentence_iterator(self):
-        # Nonexistent snapshot
+        """Ensure that all of the sentences get returned"""
+
+        sentence_iterator = SnapshotSentenceIterator(
+            db=self.db(),
+            snapshots_id=self.snapshots_id,
+            stories_id_chunk_size=self.TEST_STORIES_ID_CHUNK_SIZE,
+        )
+        returned_sentence_count = 0
+        seen_sentences = set()
+
+        for sentence_words in sentence_iterator:
+            assert sentence_words, "Sentence words should be set."
+
+            sentence = ' '.join(sentence_words)
+            assert sentence not in seen_sentences, "Every sentence should be unique."
+
+            returned_sentence_count += 1
+            seen_sentences.add(sentence)
+
+        assert returned_sentence_count == self.TEST_STORY_COUNT * self.TEST_SENTENCE_PER_STORY_COUNT, \
+            "All of the sentences should have been returned."
+
+    def test_snapshot_sentence_iterator_nonexistent_snapshot(self):
         with pytest.raises(McWord2vecException):
-            SnapshotSentenceIterator(db=self.db(), snapshots_id=123456)
+            SnapshotSentenceIterator(
+                db=self.db(),
+                snapshots_id=123456,
+                stories_id_chunk_size=self.TEST_STORIES_ID_CHUNK_SIZE,
+            )
 
     def test_train_word2vec_model(self):
 
-        sentence_iterator = SnapshotSentenceIterator(db=self.db(), snapshots_id=self.snapshots_id)
+        sentence_iterator = SnapshotSentenceIterator(
+            db=self.db(),
+            snapshots_id=self.snapshots_id,
+            stories_id_chunk_size=self.TEST_STORIES_ID_CHUNK_SIZE,
+        )
         model_store = SnapshotDatabaseModelStore(db=self.db(), snapshots_id=self.snapshots_id)
 
         models_id = train_word2vec_model(sentence_iterator=sentence_iterator,
@@ -84,8 +119,8 @@ class TestWord2vec(TestDatabaseWithSchemaTestCase):
         word_vectors = KeyedVectors.load(temp_model_path)
 
         assert word_vectors is not None
-        assert word_vectors['one'] is not None
-        assert word_vectors['two'] is not None
+        assert word_vectors['story'] is not None
+        assert word_vectors['sentence'] is not None
 
         assert 'badger' not in word_vectors
 
