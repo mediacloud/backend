26095f0804410af7586bb93cce1364fd47cf7f71
Linas Valiukas <linas@media.mit.edu>
2019-01-29 20:08:47 +0000

    Fix up S3 test credentials

--- containers/common/src/python/mediawords/key_value_store/test_amazon_s3.py
+++ containers/common/src/python/mediawords/key_value_store/test_amazon_s3.py
@@ -1,5 +1,3 @@
-import pytest
-
 from mediawords.key_value_store.amazon_s3 import AmazonS3Store
 from mediawords.key_value_store.test_amazon_s3_credentials import (
     TestAmazonS3CredentialsTestCase,
@@ -8,19 +6,15 @@ from mediawords.key_value_store.test_amazon_s3_credentials import (
 
 test_credentials = get_test_s3_credentials()
 
-pytest_amazon_s3_credentials_set = pytest.mark.skipif(
-    test_credentials is None,
-    reason="Amazon S3 test credentials are not set in environment / configuration"
-)
 
-
-@pytest_amazon_s3_credentials_set
 class TestAmazonS3StoreTestCase(TestAmazonS3CredentialsTestCase):
     def _initialize_store(self) -> AmazonS3Store:
-        return AmazonS3Store(access_key_id=test_credentials['access_key_id'],
-                             secret_access_key=test_credentials['secret_access_key'],
-                             bucket_name=test_credentials['bucket_name'],
-                             directory_name=test_credentials['directory_name'])
+        return AmazonS3Store(
+            access_key_id=test_credentials.access_key_id(),
+            secret_access_key=test_credentials.secret_access_key(),
+            bucket_name=test_credentials.bucket_name(),
+            directory_name=test_credentials.directory_name(),
+        )
 
     def _expected_path_prefix(self) -> str:
         return 's3:'
--- containers/common/src/python/mediawords/key_value_store/test_multiple_stores.py
+++ containers/common/src/python/mediawords/key_value_store/test_multiple_stores.py
@@ -1,5 +1,3 @@
-import pytest
-
 from mediawords.key_value_store.amazon_s3 import AmazonS3Store
 from mediawords.key_value_store.multiple_stores import MultipleStoresStore
 from mediawords.key_value_store.postgresql import PostgreSQLStore
@@ -11,24 +9,22 @@ from mediawords.key_value_store.test_mock_download import TestMockDownloadTestCa
 
 test_credentials = get_test_s3_credentials()
 
-pytest_amazon_s3_credentials_set = pytest.mark.skipif(
-    test_credentials is None,
-    reason="Amazon S3 test credentials are not set in environment / configuration"
-)
 
-
-@pytest_amazon_s3_credentials_set
 class TestMultipleStoresStoreTestCase(TestAmazonS3CredentialsTestCase, TestMockDownloadTestCase):
     def _initialize_store(self) -> MultipleStoresStore:
         postgresql_store = PostgreSQLStore(table='raw_downloads')
 
-        amazon_s3_store = AmazonS3Store(access_key_id=test_credentials['access_key_id'],
-                                        secret_access_key=test_credentials['secret_access_key'],
-                                        bucket_name=test_credentials['bucket_name'],
-                                        directory_name=test_credentials['directory_name'])
+        amazon_s3_store = AmazonS3Store(
+            access_key_id=test_credentials.access_key_id(),
+            secret_access_key=test_credentials.secret_access_key(),
+            bucket_name=test_credentials.bucket_name(),
+            directory_name=test_credentials.directory_name(),
+        )
 
-        return MultipleStoresStore(stores_for_reading=[postgresql_store, amazon_s3_store],
-                                   stores_for_writing=[postgresql_store, amazon_s3_store])
+        return MultipleStoresStore(
+            stores_for_reading=[postgresql_store, amazon_s3_store],
+            stores_for_writing=[postgresql_store, amazon_s3_store],
+        )
 
     def _expected_path_prefix(self) -> str:
         # Object gets prefix from last store written to
