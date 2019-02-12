2b25992688c2eb4a97726e3a6bdd305e341eb5b0
Linas Valiukas <linas@media.mit.edu>
2019-02-12 17:04:42 +0800

    Rewrite create_temporary_snapshot_views() to Python

--- mediacloud/mediawords/tm/snapshot/snapshot.py
+++ mediacloud/mediawords/tm/snapshot/snapshot.py
@@ -1,5 +1,65 @@
 from mediawords.db import DatabaseHandler
 
+# All tables that get stored as snapshot_* for each snapshot
+__SNAPSHOT_TABLES = [
+    'topic_stories',
+    'topic_links_cross_media',
+    'topic_media_codes',
+    'stories media',
+    'stories_tags_map',
+    'media_tags_map',
+    'tags',
+    'tag_sets',
+    'tweet_stories',
+]
+
+# All tables that get stories as snapshot_* for each timespan
+__TIMESPAN_TABLES = [
+    'story_link_counts',
+    'story_links',
+    'medium_link_counts',
+    'medium_links',
+    'timespan_tweets',
+]
+
+
+def create_temporary_snapshot_views(db: DatabaseHandler, timespan: dict) -> None:
+    """Create temporary view of all the snapshot_* tables that call into the snap.* tables.
+
+    This is useful for writing queries on the snap.* tables without lots of ugly joins and clauses to cd and timespan.
+    It also provides the same set of snapshot_* tables as provided by write_story_link_counts_snapshot_tables, so that
+    the same set of queries can run against either."""
+
+    # PostgreSQL prints lots of 'NOTICE's when deleting temporary tables
+    db.set_print_warn(False)
+
+    for table in __SNAPSHOT_TABLES:
+        db.query(f"""
+            CREATE TEMPORARY VIEW snapshot_{table} AS
+                SELECT *
+                FROM snap.$t
+                WHERE snapshots_id = %(snapshots_id)s
+        """, {'snapshots_id': timespan['snapshots_id']})
+
+    for table in __TIMESPAN_TABLES:
+        db.query(f"""
+            CREATE TEMPORARY VIEW snapshot_{table} AS
+                SELECT *
+                FROM snap.$t
+                WHERE timespans_id = %(timespans_id)s
+        """, {'timespans_id': timespan['timespans_id']})
+
+    db.query("""
+        CREATE TEMPORARY VIEW snapshot_period_stories AS
+            SELECT stories_id
+            FROM snapshot_story_link_counts
+    """)
+
+    add_media_type_views(db)
+
+    # Set the warnings back on
+    db.set_print_warn(True)
+
 
 def add_media_type_views(db: DatabaseHandler) -> None:
     # noinspection SqlResolve
