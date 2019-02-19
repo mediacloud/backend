eab899ffe5639e0f4b51deb6d84028288c7c9b63
Linas Valiukas <linas@media.mit.edu>
2019-02-20 00:18:40 +0800

    Fix up some queries to avoid sequential scans on "download_texts"

--- schema/mediawords.sql
+++ schema/mediawords.sql
@@ -24,7 +24,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4707;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4708;
 BEGIN
 
     -- Update / set database schema version
@@ -1944,28 +1944,23 @@ SELECT setval(
 -- Proxy view to join partitioned and non-partitioned "download_texts" tables
 CREATE OR REPLACE VIEW download_texts AS
 
-    SELECT *
-    FROM (
+    -- Non-partitioned table
+    SELECT
+        download_texts_np_id::bigint AS download_texts_id,
+        downloads_id::bigint,
+        download_text,
+        download_text_length
+    FROM download_texts_np
 
-        -- Non-partitioned table
-        SELECT
-            download_texts_np_id::bigint AS download_texts_id,
-            downloads_id::bigint,
-            download_text,
-            download_text_length
-        FROM download_texts_np
+    UNION ALL
 
-        UNION ALL
-
-        -- Partitioned table
-        SELECT
-            download_texts_p_id AS download_texts_id,
-            downloads_id,
-            download_text,
-            download_text_length
-        FROM download_texts_p
-
-    ) AS dt;
+    -- Partitioned table
+    SELECT
+        download_texts_p_id AS download_texts_id,
+        downloads_id,
+        download_text,
+        download_text_length
+    FROM download_texts_p;
 
 -- Make RETURNING work with partitioned tables
 -- (https://wiki.postgresql.org/wiki/INSERT_RETURNING_vs_Partitioning)
