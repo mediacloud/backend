3c5ca56e162e16701964350db1d113e1993de523
Linas Valiukas <linas@media.mit.edu>
2019-04-04 01:38:41 +0800

    Add PyCharm projects to Cron jobs, fix typos

--- containers/cron-print-long-running-job-states/bin/find_domain_media_dups.py
+++ containers/cron-print-long-running-job-states/bin/find_domain_media_dups.py
@@ -1,39 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 generate a csv report of all media with active feeds that would be duped into other media by the
 domain_media switch.
 """
 
-import csv
 import sys
 
-import mediawords.db
-import mediawords.util.log
-import mediawords.util.url
+from mediawords.db import connect_to_db, DatabaseHandler
+from mediawords.util.log import create_logger
+from mediawords.util.url import get_url_distinctive_domain
 
-log = mediawords.util.log.create_logger(__name__)
+log = create_logger(__name__)
 
-def main():
-    limit = sys.argv[1] if len(sys.argv) > 1 else 10000000
 
-    db = mediawords.db.connect_to_db()
-
+def print_long_running_job_states(db: DatabaseHandler, limit: int):
     media = db.query("""
         select m.*, mh.*
-            from media m
-                join media_health mh using ( media_id ) 
-            where
-                dup_media_id is null
-            order by m.media_id asc limit %(a)s
-    """,
-    {'a': limit}).hashes()
+        from media m
+            join media_health mh using ( media_id ) 
+        where dup_media_id is null
+        order by m.media_id asc limit %(a)s
+    """, {'a': limit}).hashes()
 
     media_groups = {}
 
     num_media = len(media)
     for i, medium in enumerate(media):
-        domain = mediawords.util.url.get_url_distinctive_domain(medium['url']) 
+        domain = get_url_distinctive_domain(medium['url'])
         log.warning("%s [%d/%d]" % (domain, i, num_media))
 
         if domain not in media_groups:
@@ -52,11 +46,11 @@ def main():
         media_groups[domain].extend(dup_media)
 
         for dup_medium in dup_media:
-            dup_domain = mediawords.util.url.get_url_distinctive_domain(dup_medium['url'])
+            dup_domain = get_url_distinctive_domain(dup_medium['url'])
             medium['medium_domain'] = dup_domain
             medium['dup_domain_matches'] = domain == dup_domain
 
-    db.query("drop table if exists media_dups");
+    db.query("drop table if exists media_dups")
     db.query(
         """
         create table media_dups (
@@ -65,24 +59,21 @@ def main():
             )
         """)
 
-    fieldnames = \
-        'domain media_id dup_media_id name url medium_domain dup_domain_matches num_stories num_sentences'.split()
-
     db.begin()
-    media_dups_rows = []
     for i, domain in enumerate(media_groups.keys()):
-        log.warning("domain %s [%d/%d]" %(domain, i, len(media_groups.keys())))
+        log.warning("domain %s [%d/%d]" % (domain, i, len(media_groups.keys())))
         media = media_groups[domain]
         if len(media) > 1:
             for m in media:
-                db.query(
-                    """
+                db.query("""
                     insert into media_dups (domain, media_id) values (%(a)s, %(b)s)
-                    """,
-                    {'a': domain, 'b': m['media_id']})
+                """, {'a': domain, 'b': m['media_id']})
     db.commit()
 
 
+if __name__ == '__main__':
+    limit_ = sys.argv[1] if len(sys.argv) > 1 else 10000000
 
-        
-main()
+    db_ = connect_to_db()
+
+    print_long_running_job_states(db=db_, limit=limit_)
