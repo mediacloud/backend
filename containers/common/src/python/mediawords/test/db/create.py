91ed4722f94b2bc5d1c64b75143404c96b2ad403
Linas Valiukas <linas@media.mit.edu>
2019-04-19 05:00:01 +0800

    Fix typo

--- containers/common/src/python/mediawords/test/db/create.py
+++ containers/common/src/python/mediawords/test/db/create.py
@@ -375,7 +375,7 @@ def add_content_to_test_story(db: DatabaseHandler, story: dict, feed: dict) -> d
         content = _get_test_content()
 
     content_language_code = language_code_for_text(content)
-    assert content_language_code, f"Unknown language for content: {content}".
+    assert content_language_code, f"Unknown language for content: {content}"
 
     if story.get('full_text_rss', None):
         story['full_text_rss'] = False
