1f4a1036d9c1ef7971b18bf342307e88b5f6730b
Linas Valiukas <linas@media.mit.edu>
2019-05-25 19:49:56 +0300

    Add SSL-terminating proxy with LE autorenew cron job

---
+++ containers/proxy-cron-certbot/bin/renew_le_certs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -u
+set -e
+
+# Make sure credentials are set
+source /opt/mediacloud/bin/credentials.inc.sh
+
+# Write credentials
+rm -f /var/tmp/cloudflare.ini
+touch /var/tmp/cloudflare.ini
+echo "dns_cloudflare_email = \"${MC_PROXY_CERTBOT_CLOUDFLARE_EMAIL}\"" >> /var/tmp/cloudflare.ini
+echo "dns_cloudflare_api_key = \"${MC_PROXY_CERTBOT_CLOUDFLARE_GLOBAL_API_KEY}\"" >> /var/tmp/cloudflare.ini
+
+# Try to renew certificates
+certbot certonly \
+    --dns-cloudflare \
+    --dns-cloudflare-credentials /var/tmp/cloudflare.ini \
+    -d "${MC_PROXY_CERTBOT_DOMAIN},*.${MC_PROXY_CERTBOT_DOMAIN}" \
+    --preferred-challenges dns-01 \
+    --agree-tos \
+    -m "${MC_PROXY_CERTBOT_LETSENCRYPT_EMAIL}" \
+    -n
+
+# Update certificate for lighttpd / nginx
+cat \
+    "/etc/letsencrypt/live/${MC_PROXY_CERTBOT_DOMAIN}/privkey.pem" \
+    "/etc/letsencrypt/live/${MC_PROXY_CERTBOT_DOMAIN}/fullchain.pem" \
+    > "/etc/letsencrypt/live/${MC_PROXY_CERTBOT_DOMAIN}/ssl.pem"
---
+++ containers/proxy-httpd/bin/nginx.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -u
+set -e
+
+set +u
+if [ -z "$MC_PROXY_HTTPD_AUTH_USERS" ]; then
+    echo "MC_PROXY_HTTPD_AUTH_USERS (user credentials for restricted webapps) is not set."
+    exit 1
+fi
+set -u
+
+# Create htpasswd
+rm -f /var/lib/mediacloud-htpasswd
+touch /var/lib/mediacloud-htpasswd
+while IFS=';' read -ra USERS; do
+    for USER in "${USERS[@]}"; do
+
+        while IFS=':' read -ra USER_CREDENTIALS; do
+            USERNAME="${USER_CREDENTIALS[0]}"
+            PASSWORD="${USER_CREDENTIALS[1]}"
+            
+            htpasswd -bB /var/lib/mediacloud-htpasswd "${USERNAME}" "${PASSWORD}"
+
+        done <<< "$USER"
+
+    done
+done <<< "${MC_PROXY_HTTPD_AUTH_USERS}"
+
+# Wait for proxy_cron_certbot to generate us a SSL certificate
+while true; do
+    echo "Waiting for Let's Encrypt certificate to appear..."
+    if [ -e /etc/letsencrypt/live/testmediacloud.ml/privkey.pem ]; then
+        break
+    else
+        sleep 1
+    fi
+done
+
+# Start nginx
+echo "Starting nginx..."
+exec nginx
