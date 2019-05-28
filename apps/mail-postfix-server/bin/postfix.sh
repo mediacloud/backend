14615c096ba1c63a4a4928574c44d75200c1045d
Linas Valiukas <linas@media.mit.edu>
2019-05-28 15:31:49 +0300

    Fix up Postfix / OpenDKIM startup, add volumes

--- apps/mail-postfix-server/bin/postfix.sh
+++ apps/mail-postfix-server/bin/postfix.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-set -u
 set -e
+
+if [ -z "$MC_MAIL_POSTFIX_DOMAIN" ]; then
+    echo "MC_MAIL_POSTFIX_DOMAIN (top-level domain to use for sending email) is not set."
+    exit 1
+fi
+
+if [ -z "$MC_MAIL_POSTFIX_HOSTNAME" ]; then
+    echo "MC_MAIL_POSTFIX_HOSTNAME (mail server hostname) is not set."
+    exit 1
+fi
+
+set -u
 
 # Set up rsyslog for logging
 source /rsyslog.inc.sh
+
+# Configure Postfix
+postconf -e hostname="${MC_MAIL_POSTFIX_HOSTNAME}.${MC_MAIL_POSTFIX_DOMAIN}"
 
 # Start Postfix
 exec /usr/lib/postfix/sbin/master -c /etc/postfix -d
