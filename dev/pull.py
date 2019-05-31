eae1d3c0e91bf8bedc9d253cb5b4b4e1d4207a24
Linas Valiukas <linas@media.mit.edu>
2019-05-31 17:25:20 +0300

    Tag the pulled images as 'latest' locally

--- dev/pull.py
+++ dev/pull.py
@@ -30,12 +30,37 @@ if __name__ == '__main__':
     branch = current_git_branch_name()
 
     for image in _docker_images_to_pull(all_apps_dir=args.all_apps_dir(), conf=conf_):
-        # First try to pull the image for the current branch, and if that fails (e.g. the branch
-        # is new and it hasn't yet been built and tagged on Docker Hub), pull builds for "master"
-        # and tag them as if they were of the current branch
+
+        # 1) First try to pull the image for the current branch
+        # 2) if that fails (e.g. the branch is new and it hasn't yet been built
+        #    and tagged on Docker Hub), pull builds for "master" and tag them
+        #    as if they were built from the current branch
+        # 3) Tag the branch image (which at this point was either built from
+        #    the branch or from "master") as "latest", i.e. mark them as the
+        #    latest local build to be later used for rebuilding and running
+        #    tests
+
+        pull_branch = 'docker pull {image}:{branch}'.format(
+            image=image,
+            branch=branch,
+        )
+
+        pull_master = 'docker pull {image}:master'.format(
+            image=image,
+        )
+
+        tag_master_as_branch = 'docker tag {image}:master {image}:{branch}'.format(
+            image=image,
+            branch=branch,
+        )
+
+        tag_branch_as_latest = 'docker tag {image}:{branch} {image}:latest'.format(
+            image=image,
+            branch=branch,
+        )
+
         print(
-            'docker pull {image}:{branch} || {{ docker pull {image}:master && docker tag {image}:master {image}:{branch} }}'.format(
-                image=image,
-                branch=branch,
-            )
+            pull_branch + ' || ' +
+            '{{' + pull_master + ' && ' + tag_master_as_branch + '; }}' +
+            ' && ' + tag_branch_as_latest
         )
