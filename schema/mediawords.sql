3c8e70bda34c4a9d6bcd5f8c9d4442a3b60859fd
Linas Valiukas <linas@media.mit.edu>
2019-08-15 18:24:25 +0300

    Add function + script to move non-partitioned download_texts rows to partitioned table
    References #566.

--- schema/mediawords.sql
+++ schema/mediawords.sql
@@ -24,7 +24,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4725;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4726;
 BEGIN
 
     -- Update / set database schema version
@@ -1677,6 +1677,76 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER download_texts_view_insert_update_delete_trigger
     INSTEAD OF INSERT OR UPDATE OR DELETE ON download_texts
     FOR EACH ROW EXECUTE PROCEDURE download_texts_view_insert_update_delete();
+
+
+--
+-- Move a chunk of download texts from a non-partitioned "download_texts_np" to a
+-- partitioned "download_texts_p".
+--
+-- Expects starting and ending "download_texts_id" instead of a chunk size in order
+-- to avoid index bloat that would happen when reading rows in sequential
+-- chunks.
+--
+-- Returns number of rows that were moved.
+--
+-- Call this repeatedly to migrate all the data to the partitioned table.
+CREATE OR REPLACE FUNCTION move_chunk_of_nonpartitioned_download_texts_to_partitions(
+    start_download_texts_id INT,
+    end_download_texts_id INT
+)
+RETURNS INT AS $$
+
+DECLARE
+    moved_row_count INT;
+
+BEGIN
+
+    IF NOT (start_download_texts_id < end_download_texts_id) THEN
+        RAISE EXCEPTION '"end_download_texts_id" must be bigger than "start_download_texts_id".';
+    END IF;
+
+
+    PERFORM pid
+    FROM pg_stat_activity, LATERAL pg_cancel_backend(pid) f
+    WHERE backend_type = 'autovacuum worker'
+      AND query ~ 'download_texts';
+
+    RAISE NOTICE
+        'Moving download texts of download_texts_id BETWEEN % AND % to the partitioned table...',
+        start_download_texts_id, end_download_texts_id;
+
+    -- Fetch and delete download texts within bounds
+    WITH deleted_rows AS (
+        DELETE FROM download_texts_np
+        WHERE download_texts_np_id BETWEEN start_download_texts_id AND end_download_texts_id
+        RETURNING download_texts_np.*
+    )
+
+    -- Insert rows to the partitioned table
+    INSERT INTO download_texts_p (
+        download_texts_p_id,
+        downloads_id,
+        download_text,
+        download_text_length
+    )
+    SELECT
+        download_texts_np_id,
+        downloads_id,
+        download_text,
+        download_text_length
+    FROM deleted_rows;
+
+    GET DIAGNOSTICS moved_row_count = ROW_COUNT;
+
+    RAISE NOTICE
+        'Done moving download texts of download_texts_id BETWEEN % AND % to the partitioned table, moved % rows.',
+        start_download_texts_id, end_download_texts_id, moved_row_count;
+
+    RETURN moved_row_count;
+
+END;
+$$
+LANGUAGE plpgsql;
 
 
 --
