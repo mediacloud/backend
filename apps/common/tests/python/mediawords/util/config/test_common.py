4f0665aafe90e856fcef994344b81c2dcfeec18a
Linas Valiukas <linas@media.mit.edu>
2019-10-09 16:07:50 +0800

    Pass authenticated domains as JSON

--- apps/common/tests/python/mediawords/util/config/test_common.py
+++ apps/common/tests/python/mediawords/util/config/test_common.py
@@ -1,38 +1,75 @@
 import pytest
 
 from mediawords.util.config.common import (
-    _authenticated_domains_from_string,
+    _authenticated_domains_from_json,
     AuthenticatedDomain,
     McConfigAuthenticatedDomainsException,
 )
 
 
-def test_authenticated_domains_from_string():
+def test_authenticated_domains_from_json():
     # noinspection PyTypeChecker
-    assert _authenticated_domains_from_string(None) == []
-    assert _authenticated_domains_from_string('') == []
-    assert _authenticated_domains_from_string('user:pass@domain') == [
+    assert _authenticated_domains_from_json(None) == []
+    assert _authenticated_domains_from_json('') == []
+    assert _authenticated_domains_from_json('  ') == []
+
+    assert _authenticated_domains_from_json("""
+        [
+            {"domain": "domain", "username": "user", "password": "pass"}
+        ]
+    """) == [
         AuthenticatedDomain(domain='domain', username='user', password='pass'),
     ]
-    assert _authenticated_domains_from_string('user:pass@domain;user2:pass2@domain2') == [
+    assert _authenticated_domains_from_json("""
+        [
+            {"domain": "domain", "username": "user", "password": "pass"},
+            {"domain": "domain2", "username": "user2", "password": "pass2"}
+        ]
+    """) == [
         AuthenticatedDomain(domain='domain', username='user', password='pass'),
         AuthenticatedDomain(domain='domain2', username='user2', password='pass2'),
     ]
 
     with pytest.raises(McConfigAuthenticatedDomainsException):
-        _authenticated_domains_from_string('blergh')
+        # Invalid JSON
+        _authenticated_domains_from_json('blergh')
 
     with pytest.raises(McConfigAuthenticatedDomainsException):
-        _authenticated_domains_from_string('username:password')
+        # No domain
+        _authenticated_domains_from_json("""
+            [
+                {"username": "user", "password": "pass"}
+            ]
+        """)
 
     with pytest.raises(McConfigAuthenticatedDomainsException):
-        _authenticated_domains_from_string('username@domain')
+        # No password
+        _authenticated_domains_from_json("""
+            [
+                {"domain": "domain", "username": "user"}
+            ]
+        """)
 
     with pytest.raises(McConfigAuthenticatedDomainsException):
-        _authenticated_domains_from_string('username:password@domain;username')
+        # List within a list
+        _authenticated_domains_from_json("""
+            [
+                [
+                    {"domain": "domain", "username": "user", "password": "pass"}
+                ]
+            ]
+        """)
 
     with pytest.raises(McConfigAuthenticatedDomainsException):
-        _authenticated_domains_from_string('username:password:password2@domain')
+        # Just a dictionary without a list
+        _authenticated_domains_from_json("""
+            {"domain": "domain", "username": "user", "password": "pass"}
+        """)
 
     with pytest.raises(McConfigAuthenticatedDomainsException):
-        _authenticated_domains_from_string('username:password@domain@domain2')
+        # Single quotes instead of double ones (invalid JSON)
+        _authenticated_domains_from_json("""
+            [
+                {'domain': 'domain', 'username': 'user', 'password': 'pass'}
+            ]
+        """)
