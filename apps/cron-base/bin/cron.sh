09c858eca838acb35567d89c2d81b9c6b1467d33
Linas Valiukas <linas@media.mit.edu>
2019-10-16 17:03:40 +0800

    Copy environment variables to /etc/environment before starting Cron

--- apps/cron-base/bin/cron.sh
+++ apps/cron-base/bin/cron.sh
@@ -6,5 +6,8 @@ set -e
 # Set up rsyslog for logging
 source /rsyslog.inc.sh
 
+# Copy environment variables to /etc/environment for the Cron jobs to be able to use them
+printenv | grep -v '^HOME=' >> /etc/environment
+
 # Run Cron in foreground
 exec cron -f
