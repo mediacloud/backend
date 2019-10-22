333adba80be439d360b63d6eccadc52314cd2874
Linas Valiukas <linas@media.mit.edu>
2019-10-22 20:04:55 +0800

    Print a nice JSON response when root ('/') gets requested

--- apps/webapp-api/src/perl/MediaWords/Controller/Root.pm
+++ apps/webapp-api/src/perl/MediaWords/Controller/Root.pm
@@ -8,11 +8,39 @@ use MediaWords::CommonLibs;
 
 use base 'Catalyst::Controller';
 
+use Encode;
 use HTTP::Status qw(:constants);
+
+use MediaWords::Util::ParseJSON;
 
 # Sets the actions in this controller to be registered with no prefix
 # so they function identically to actions created in MyApp.pm
 __PACKAGE__->config->{ namespace } = '';
+
+sub index : Path : Args(0)
+{
+    my ( $self, $c ) = @_;
+
+    my $message = <<EOF;
+Welcome to Media Cloud API!
+
+To get your API key and see the spec, head to:
+
+https://github.com/berkmancenter/mediacloud/blob/master/doc/api_2_0_spec/api_2_0_spec.md
+
+This particular API endpoint ('/') is not authenticated and does nothing.
+EOF
+
+    my $response = { 'error' => $message };
+
+    # Catalyst expects bytes
+    my $json = encode_utf8( MediaWords::Util::ParseJSON::encode_json( $response ) );
+
+    $c->response->status( HTTP_UNAUTHORIZED );
+    $c->response->content_type( 'application/json; charset=UTF-8' );
+    $c->response->content_length( bytes::length( $json ) );
+    $c->response->body( $json );
+}
 
 sub default : Private
 {
