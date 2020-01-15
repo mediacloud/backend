bead97463d5e696dedbedc670ad7ab63af3d006e
Linas Valiukas <linas@media.mit.edu>
2020-01-16 00:48:18 +0200

    Test .media_file with an invalid media file

--- apps/podcast-fetch-episode/tests/test_media_file.py
+++ apps/podcast-fetch-episode/tests/test_media_file.py
@@ -23,21 +23,29 @@ assert not [f for f in SAMPLE_FILENAMES if '/' in f], f"There can't be any paths
 
 def test_media_file_info():
     for filename in SAMPLE_FILENAMES:
-        input_file_path = os.path.join(MEDIA_SAMPLES_PATH, filename)
-        media_info = media_file_info(media_file_path=input_file_path)
-        assert isinstance(media_info, MediaFileInfo)
-        if '.mp3' in filename:
-            assert not media_info.has_video_streams, f"MP3 file '{filename}' is not expected to have video streams."
-        if '.mkv' in filename:
-            assert media_info.has_video_streams, f"MKV file '{filename}' is expected to have video streams."
-        if 'noaudio' in filename:
-            assert not media_info.audio_streams, f"File '{filename}' is not expected to have any audio streams."
-        else:
-            assert media_info.audio_streams, f"File '{filename}' is expected to have audio streams."
 
-        if media_info.audio_streams:
-            for stream in media_info.audio_streams:
-                assert stream.duration > 0, f"File's '{filename}' stream's {stream} duration should be positive."
+        input_file_path = os.path.join(MEDIA_SAMPLES_PATH, filename)
+
+        if '-invalid' in filename:
+            with pytest.raises(McPodcastFileIsInvalidException, message="Invalid media file"):
+                media_file_info(media_file_path=input_file_path)
+
+        else:
+
+            media_info = media_file_info(media_file_path=input_file_path)
+            assert isinstance(media_info, MediaFileInfo)
+            if '.mp3' in filename:
+                assert not media_info.has_video_streams, f"MP3 file '{filename}' is not expected to have video streams."
+            if '.mkv' in filename:
+                assert media_info.has_video_streams, f"MKV file '{filename}' is expected to have video streams."
+            if 'noaudio' in filename:
+                assert not media_info.audio_streams, f"File '{filename}' is not expected to have any audio streams."
+            else:
+                assert media_info.audio_streams, f"File '{filename}' is expected to have audio streams."
+
+            if media_info.audio_streams:
+                for stream in media_info.audio_streams:
+                    assert stream.duration > 0, f"File's '{filename}' stream's {stream} duration should be positive."
 
 
 def _file_sha1_hash(file_path: str) -> str:
@@ -66,16 +74,16 @@ def test_transcode_media_file_if_needed():
 
         input_media_file = TranscodeTempDirAndFile(temp_dir=MEDIA_SAMPLES_PATH, filename=filename)
 
-        if 'noaudio' in filename:
-            with pytest.raises(McPodcastFileIsInvalidException):
+        if '-noaudio' in filename:
+            with pytest.raises(McPodcastFileIsInvalidException, message="Media file with no audio"):
+                transcode_media_file_if_needed(input_media_file=input_media_file)
+
+        elif '-invalid' in filename:
+            with pytest.raises(McPodcastFileIsInvalidException, message="Invalid media file"):
                 transcode_media_file_if_needed(input_media_file=input_media_file)
 
         else:
             output_media_file = transcode_media_file_if_needed(input_media_file=input_media_file)
-
-            after_sha1_hash = _file_sha1_hash(input_file_path)
-
-            assert before_sha1_hash == after_sha1_hash, f"Input file '{filename}' shouldn't have been modified."
 
             assert output_media_file, "Output media file was set."
 
@@ -87,3 +95,7 @@ def test_transcode_media_file_if_needed():
                 output_file_info.audio_streams[0].audio_codec_class,
                 AbstractAudioCodec,
             ), f"Processed '{filename}' should be in one of the supported codecs."
+
+        after_sha1_hash = _file_sha1_hash(input_file_path)
+
+        assert before_sha1_hash == after_sha1_hash, f"Input file '{filename}' shouldn't have been modified."
