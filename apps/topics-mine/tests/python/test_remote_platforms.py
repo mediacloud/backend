ea3a96dd8de959eed101ddec9e8f48dcb49db893
Hal Roberts <hroberts@cyber.law.harvard.edu>
2020-02-09 22:33:24 -0500

    run test individually in test_remote_platforms

--- apps/topics-mine/tests/python/test_remote_platforms.py
+++ apps/topics-mine/tests/python/test_remote_platforms.py
@@ -15,36 +15,15 @@ from mediawords.util.log import create_logger
 
 log = create_logger(__name__)
 
-TEST_DEFINITIONS =\
-    [
-        {
-            'class': CrimsonHexagonTwitterPostFetcher,
-            'query': 32780805819,
-            'pattern': '.*', # CH searches through non-content metadata for pattern matches
-            'day': '2017-08-17',
-            'min_posts': 400,
-            'max_posts': 500
-        },
-        {
-            'class': PushshiftRedditPostFetcher,
-            'query': 'trump',
-            'pattern': 'trump',
-            'day': '2020-01-01',
-            'min_posts': 1000,
-            'max_posts': 3000
-        }
-    ]
 
-def run_single_platform_test(test: dict) -> None:
-    """Run test for a single platform / source as definite in TEST_DEFINITIONS."""
-    fetcher = test['class']()
-
-    start_date = dateutil.parser.parse(test['day'])
+def run_single_platform_test(fetcher, query, pattern, day, min_posts, max_posts) -> None:
+    """Run test for a single platform / source.""" 
+    start_date = dateutil.parser.parse(day)
     end_date = start_date + datetime.timedelta(days=1) - datetime.timedelta(seconds=1) 
 
-    got_posts = fetcher.fetch_posts(query=test['query'], start_date=start_date, end_date=end_date)
+    got_posts = fetcher.fetch_posts(query=query, start_date=start_date, end_date=end_date)
 
-    assert len(got_posts) >= test['min_posts'] and len(got_posts) <= test['max_posts'], test['class']
+    assert len(got_posts) >= min_posts and len(got_posts) <= max_posts
 
     # allow for timezone skew from source
     got_start_date = (start_date - datetime.timedelta(hours=12)).replace(tzinfo=None)
@@ -53,7 +32,7 @@ def run_single_platform_test(test: dict) -> None:
     for got_post in got_posts:
         publish_date = dateutil.parser.parse(got_post['publish_date']).replace(tzinfo=None)
         assert publish_date >= got_start_date and publish_date <= got_end_date
-        assert re.search(test['pattern'], got_post['content'], re.I)
+        assert re.search(pattern, got_post['content'], re.I)
         assert len(got_post['content']) > 0
         assert len(got_post['author']) > 0
         assert len(got_post['channel']) > 0
@@ -65,9 +44,23 @@ def run_single_platform_test(test: dict) -> None:
 
     assert statistics.mean([len(p['content']) for p in got_posts]) > 80
 
-def test_remote_platforms() -> None:
-    """Test each remote platform / source combination."""
+def test_crimson_hexagon_twitter() -> None:
+    run_single_platform_test(
+            fetcher=CrimsonHexagonTwitterPostFetcher(),
+            query=32780805819,
+            pattern='.*',
+            day='2017-08-17',
+            min_posts=400,
+            max_posts=500
+        )
 
-    for test in TEST_DEFINITIONS:
-        log.info("testing %s" % test['class'])
-        run_single_platform_test(test)
+
+def test_pushshift_reddit() -> None:
+    run_single_platform_test(
+            fetcher=PushshiftRedditPostFetcher(),
+            query='trump',
+            pattern='trump',
+            day='2020-01-01',
+            min_posts=1000,
+            max_posts=3000
+        )
