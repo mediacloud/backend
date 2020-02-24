b0a2b1ac9852069b3a2b7bbc810ae1aab1e18cb5
Linas Valiukas <linas@media.mit.edu>
2020-02-24 18:58:17 +0200

    Clean up temporary directory after unsuccessful transcode

--- apps/podcast-fetch-episode/src/python/podcast_fetch_episode/media_file.py
+++ apps/podcast-fetch-episode/src/python/podcast_fetch_episode/media_file.py
@@ -2,6 +2,7 @@ import dataclasses
 import subprocess
 import math
 import os
+import shutil
 import tempfile
 from typing import Type, Optional, List
 
@@ -282,6 +283,8 @@ def transcode_media_file_if_needed(input_media_file: TranscodeTempDirAndFile) ->
             log.info(f"Done transcoding {input_media_file.temp_full_path} to {temp_file_path}")
 
         except Exception as ex:
+
+            shutil.rmtree(temp_dir)
 
             raise McPodcastFileIsInvalidException(f"Unable to transcode {input_media_file.temp_full_path}: {ex}")
 
