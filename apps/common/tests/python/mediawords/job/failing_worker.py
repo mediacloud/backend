290b1a2efffdfce8e63ebb322f7aeecb946d3201
Linas Valiukas <linas@media.mit.edu>
2020-03-03 10:15:57 -0500

    Kill process tree properly

--- apps/common/tests/python/mediawords/job/failing_worker.py
+++ apps/common/tests/python/mediawords/job/failing_worker.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Worker that fails right away.
+Worker that fails pretty much right away.
 """
+
+import subprocess
+import time
 
 from mediawords.job import JobBroker
 from mediawords.util.log import create_logger
@@ -12,6 +15,13 @@ log = create_logger(__name__)
 
 
 def run_failing_worker() -> None:
+    # Start some background processes to see if they get killed properly
+    # noinspection PyUnusedLocal
+    bg_process_1 = subprocess.Popen(["sleep", "30"])
+    # noinspection PyUnusedLocal
+    bg_process_2 = subprocess.Popen(["sleep", "30"])
+    time.sleep(0.5)
+
     fatal_error(f"Failing worker.")
 
 
