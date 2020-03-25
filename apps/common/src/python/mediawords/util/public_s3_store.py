e5c28dce501d7a2936346b4fb828282a8e55ceb2
Hal Roberts <hroberts@cyber.law.harvard.edu>
2020-03-25 16:43:06 -0400

    update public_s3_store to assign s3 directories in a test safe way

--- apps/common/src/python/mediawords/util/public_s3_store.py
+++ apps/common/src/python/mediawords/util/public_s3_store.py
@@ -6,18 +6,18 @@ is also not compressed.
 """
 
 import hashlib
+import uuid
 
 from mediawords.db import DatabaseHandler
 import mediawords.key_value_store
 from mediawords.key_value_store.amazon_s3 import AmazonS3Store
-from mediawords.util.config import env_value
+from mediawords.util.config import env_value, McConfigEnvironmentVariableUnsetException
 from mediawords.util.log import create_logger
 
 log = create_logger(__name__)
 
 TIMESPAN_MAPS_TYPE = 'timespan_maps'
 TOPIC_DUMPS_TYPE = 'topic_dumps'
-TEST_TYPE = 'test'
 
 def get_object_hash(object_id: int) -> int:
     """Hash the object_id with a salt so that it is not discoverable."""
@@ -28,11 +28,67 @@ def get_object_hash(object_id: int) -> int:
     return int(hashlib.md5(key.encode('utf-8')).hexdigest(), 16)
 
 
-def _get_s3_store(directory_name: str) -> None:
+def _get_test_directory_name_from_db(db) -> str:
+    """Get or generate an s3 directory name from the database.
+
+    We want each instance of containers to use the same directory but not conflict with one another, 
+    so we generate the directory name and then store it in the database.
+    """
+    name = 'public_s3_store_directory'
+
+    max_stories = 100000
+    has_max_stories = db.query("select 1 from stories offset %(a)s", {'a': max_stories}).hash()
+    if has_max_stories:
+        error = "cowardly refusing to create test directory in database with at least %d stories" % max_stories
+        raise McConfigEnvironmentVariableUnsetException(error)
+
+    directory_names = db.query(
+        "select value from database_variables where name = %(a)s",
+        {'a': name }).flat()
+
+    if directory_names:
+        return directory_names[0]
+
+    db.begin();
+    db.query("lock table database_variables")
+    
+    directory_names = db.query(
+        "select value from database_variables where name = %(a)s",
+        {'a': name }).flat()
+    if directory_names:
+        directory_name = directory_names[0]
+    else:
+        log.warning("generating test directory name for public s3 store")
+        directory_name = "test/%d" % uuid.uuid4().int
+        db.query(
+            "insert into database_variables (name, value) values(%(a)s, %(b)s)",
+            {'a': name, 'b': directory_name })
+
+    db.commit()
+
+    return directory_name
+
+
+def _get_directory_name(db, object_type: str) -> str:
+    """Get the directory name either from the env var or from the database."""
+    # MC_PUBLIC_AMAZON_S3_DIRECTORY_NAME should be unique for production to prevent overwriting
+    try:
+        directory_name = env_value("MC_PUBLIC_AMAZON_S3_DIRECTORY_NAME")
+    except McConfigEnvironmentVariableUnsetException:
+        directory_name = _get_test_directory_name_from_db(db)
+
+    full_path = f'{directory_name}/{object_type}'
+
+    return full_path
+
+
+def _get_s3_store(db: DatabaseHandler, object_type: str) -> None:
     """Get the amazon s3 store."""
     access_key_id = env_value("MC_PUBLIC_AMAZON_S3_ACCESS_KEY_ID")
     secret_access_key = env_value("MC_PUBLIC_AMAZON_S3_SECRET_ACCESS_KEY")
     bucket_name = env_value("MC_PUBLIC_AMAZON_S3_BUCKET_NAME")
+
+    directory_name = _get_directory_name(db, object_type)
 
     store = AmazonS3Store(
             access_key_id=access_key_id,
@@ -45,7 +101,7 @@ def _get_s3_store(directory_name: str) -> None:
 
 def store_content(db: DatabaseHandler, object_type: str, object_id: int, content: bytes, content_type:str) -> None:
     """Store the content on S3."""
-    s3 = _get_s3_store(object_type)
+    s3 = _get_s3_store(db, object_type)
 
     hash_id = get_object_hash(object_id)
 
@@ -54,15 +110,16 @@ def store_content(db: DatabaseHandler, object_type: str, object_id: int, content
 
 def fetch_content(db: DatabaseHandler, object_type: str, object_id: int) -> None:
     """Fetch the map content from S3."""
-    s3 = _get_s3_store(object_type)
+    s3 = _get_s3_store(db, object_type)
 
     hash_id = get_object_hash(object_id)
 
     return s3.fetch_content(db, hash_id)
 
 
-def get_content_url(object_type: str, object_id: int) -> str:
+def get_content_url(db: DatabaseHandler, object_type: str, object_id: int) -> str:
     """Return the public url for the content."""
     hash_id = get_object_hash(object_id)
+    directory_name = _get_directory_name(db, object_type)
 
-    return "https://mediacloud-public.s3.amazonaws.com/%s/%d" % (object_type, hash_id);
+    return "https://mediacloud-public.s3.amazonaws.com/%s/%d" % (directory_name, hash_id);
--- apps/common/tests/python/mediawords/util/test_public_s3_store.py
+++ apps/common/tests/python/mediawords/util/test_public_s3_store.py
@@ -10,15 +10,15 @@ def test_store_fetch() -> None:
     test_content_id = 123456
     test_content_type = 'text/plain'
 
-    store_content(db, TEST_TYPE, test_content_id, test_content, test_content_type)
+    store_content(db, TIMESPAN_MAPS_TYPE, test_content_id, test_content, test_content_type)
 
-    got_content = fetch_content(db, TEST_TYPE, test_content_id)
+    got_content = fetch_content(db, TIMESPAN_MAPS_TYPE, test_content_id)
 
     assert got_content == test_content
 
-    url = get_content_url(TEST_TYPE, test_content_id)
+    url = get_content_url(db, TIMESPAN_MAPS_TYPE, test_content_id)
     response = UserAgent().get(url)
 
     assert response.is_success
-    assert response.content_type() == test_content_type
     assert response.decoded_content() == test_content.decode('utf-8')
+    assert response.content_type() == test_content_type
