3d3ecd8fb969c4042642264822c3cae1ead7ca6a
Hal Roberts <hroberts@cyber.law.harvard.edu>
2020-04-02 18:10:43 -0400

    aborted attempt to deal gracefully with throttled 429s

--- apps/common/src/python/mediawords/util/web/user_agent/throttled.py
+++ apps/common/src/python/mediawords/util/web/user_agent/throttled.py
@@ -1,5 +1,6 @@
 """Implement ThrottledUserAgent as a sub class of mediawords.util.web.UserAgent with per domain throttling."""
 
+import time
 import typing
 
 import mediawords.db
@@ -13,8 +14,17 @@ from mediawords.util.log import create_logger
 
 log = create_logger(__name__)
 
+# wait this long to retry a throttled domain if 429 is returned but retry-after is not specified
+_DEFAULT_RETRY_AFTER = 60
+
+# if retry-after is greater than this, just return the failed response with the 429
+_MAX_RETRY_AFTER = 120
+
 # default amount of time in between requests
 _DEFAULT_DOMAIN_TIMEOUT = 10
+
+# timeout for accelerate domains and shorteners
+_ACCELERATED_TIMEOUT = 0.1
 
 # Domains (in addition to all shortened URLs) for which the throttling will be less intense
 _ACCELERATED_DOMAINS = {
@@ -29,14 +39,11 @@ _ACCELERATED_DOMAINS = {
     'doi.org',
     'archive.org',
     'reddit.com',
-    'youtube.com',
+    # youtube was returning 429s on about 10% of requests when accelerated
+    #'youtube.com', 
     'instagram.com',
     'yahoo.com'
 }
-
-# Divide the normal domain timeout by this for accelerated URLs
-_ACCELERATED_DOMAIN_SPEEDUP_FACTOR = 10
-
 
 class McThrottledDomainException(Exception):
     """Exception raised when a ThrottledUserAgent request fails to get a domain request lock."""
@@ -70,6 +77,44 @@ class ThrottledUserAgent(UserAgent):
 
         super().__init__()
 
+    def _handle_too_many_requests(self, response: Response, domain: str) -> Response:
+        """Handle a 429 response by waiting to retry unless the retry is too long."""
+        retry_after = response.headers().get('retry-after', _DEFAULT_RETRY_AFTER)
+
+        log.info("retry-after %d for domain %s" % (retry_after, domain))
+
+        if retry_after > _MAX_RETRY_AFTER:
+            log.info("retry-after is too large, returning 429")
+            return response
+
+        # lock all the other requests so that no one else tries to request while this one is waiting
+        db.begin()
+        db.query("lock table domain_web_requests")
+        db.query("delete from domain_web_requests where domain = %(a)s", {'a': domain})
+        db.query(
+            """
+            insert into domain_web_requests (domain, request_time)
+                values (%(a)s,  now() + interval %(b)s || ' seconds')
+            """
+            ['a': domain, 'b' retry_after])
+        db.commit()
+
+        # FIXME - giving up here.  we need some way to give a max retry time for a given reqeust
+        # so that we don't end up looking forever if a server is not allowing us back in.  we also need a way
+        # to figure out whether a site is not sanely giving out 429s so that we don't want 5 minutes for each
+        # individual url to time out (especially because domains that are giving us 429s are more likely than
+        # others to be domains that we have lots of urls to fetch for)
+        time.sleep(retry_after)
+
+        response = super().request(request)
+
+        if response.code() == 429:
+            log.info("domain %s failed after waiting for 429, failing all future 429s" % domain)
+            self.fail_429_domains[domain] = True
+
+        return repsonse
+
+
     def request(self, request: Request) -> Response:
         """
         Execute domain throttled version of mediawords.util.web.user_agent.UserAgent.request.
@@ -83,30 +128,36 @@ class ThrottledUserAgent(UserAgent):
         other followup requests to succeed.  To ensure proper throttling, a new object should be create for each
         top level request.
 
-        Accelerated domains and shortened links (eg. http://bit.ly/EFGDfrTg) get their timeout divided by
-        _ACCELERATED_DOMAIN_SPEEDUP_FACTOR.
+        Accelerated domains and shortened links (eg. http://bit.ly/EFGDfrTg) use ACCELERATED_TIMEOUT.
         """
-        if self._use_throttling:
-            domain = mediawords.util.url.get_url_distinctive_domain(request.url())
-
-            domain_timeout = self.domain_timeout
-            if domain_timeout > 1 and (is_shortened_url(request.url()) or domain in _ACCELERATED_DOMAINS):
-                domain_timeout = max(1, int(self.domain_timeout / _ACCELERATED_DOMAIN_SPEEDUP_FACTOR))
-
-            # this postgres function returns true if we are allowed to make the request and false otherwise. this
-            # function does not use a table lock, so some extra requests might sneak through, but thats better than
-            # dealing with a lock.  we use a postgres function to make the the race condition as rare as possible.
-            got_domain_lock = self.db.query(
-                "select get_domain_web_requests_lock(%s, %s)",
-                (domain, domain_timeout)).flat()[0]
-
-            log.debug("domain lock obtained for %s: %s" % (str(request.url()), str(got_domain_lock)))
-
-            if not got_domain_lock:
-                raise McThrottledDomainException("domain " + str(domain) + " is locked.")
-        else:
+        if not self._use_throttling:
             log.debug("domain lock obtained for %s: skipped" % str(request.url()))
+            return super().request(request)
 
         self._use_throttling = False
 
-        return super().request(request)
+        domain = mediawords.util.url.get_url_distinctive_domain(request.url())
+
+        domain_timeout = self.domain_timeout
+        if is_shortened_url(request.url()) or domain in _ACCELERATED_DOMAINS:
+            domain_timeout = ACCELERATED_TIMEOUT
+
+        # this postgres function returns true if we are allowed to make the request and false otherwise. this
+        # function does not use a table lock, so some extra requests might sneak through, but thats better than
+        # dealing with a lock.  we use a postgres function to make the the race condition as rare as possible.
+        got_domain_lock = self.db.query(
+            "select get_domain_web_requests_lock(%s, %s)",
+            (domain, domain_timeout)).flat()[0]
+
+        log.debug("domain lock obtained for %s: %s" % (str(request.url()), str(got_domain_lock)))
+
+        if not got_domain_lock:
+            raise McThrottledDomainException("domain " + str(domain) + " is locked.")
+
+        response = super().request(request)
+
+        # too many requests 
+        if response.code() == 429:
+            return self._handle_too_many_requests(response, domain)
+
+        return response
