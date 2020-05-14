01acbd2190b61dad82dc09053192a50d2343b5b2
Hal Roberts <hroberts@cyber.law.harvard.edu>
2020-05-14 16:20:11 -0400

    add migration for null publish_dates

--- apps/postgresql-server/schema/mediawords.sql
+++ apps/postgresql-server/schema/mediawords.sql
@@ -26,7 +26,7 @@ CREATE OR REPLACE FUNCTION set_database_schema_version() RETURNS boolean AS $$
 DECLARE
     -- Database schema version number (same as a SVN revision number)
     -- Increase it by 1 if you make major database schema changes.
-    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4754;
+    MEDIACLOUD_DATABASE_SCHEMA_VERSION CONSTANT INT := 4755;
 BEGIN
 
     -- Update / set database schema version
