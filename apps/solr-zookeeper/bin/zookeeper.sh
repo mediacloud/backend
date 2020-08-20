b481da86a2eea10600dfc14fe58c4df175b2a075
Linas Valiukas <linas@media.mit.edu>
2020-08-20 20:00:06 +0300

    Reduce sun.net.inetaddr.ttl and sun.net.inetaddr.negative.ttl
    Seemingly Java caches DNS entries indefinitely by default, so if one of
    Solr shards or ZooKeeper goes down (due to, say, OOM), it gets restarted
    by Docker but the remaining shards and ZooKeeper are then no longer able
    to resolve it due to old host being cached.

--- apps/solr-zookeeper/bin/zookeeper.sh
+++ apps/solr-zookeeper/bin/zookeeper.sh
@@ -7,7 +7,22 @@ set -e
 export ZOOCFGDIR=/opt/zookeeper/conf    # no slash at the end
 export ZOOCFG=zoo.cfg
 export ZOO_LOG_DIR=/var/lib/zookeeper   # no slash at the end
-export SERVER_JVMFLAGS="-Dlog4j.configuration=file:///opt/zookeeper/conf/log4j.properties"
+
+export SERVER_JVMFLAGS=""
+
+# Custom logging configuration
+export SERVER_JVMFLAGS="${SERVER_JVMFLAGS} -Dlog4j.configuration=file:///opt/zookeeper/conf/log4j.properties"
+
+#
+# Cache DNS only for a second to be able to resolve new IPs of shards and
+# ZooKeeper if they change
+#
+# FIXME: replace with networkaddress.cache.ttl and
+# networkaddress.cache.negative.ttl properties
+#
+export SERVER_JVMFLAGS="${SERVER_JVMFLAGS} -Dsun.net.inetaddr.ttl=10"
+export SERVER_JVMFLAGS="${SERVER_JVMFLAGS} -Dsun.net.inetaddr.negative.ttl=10"
+
 
 if [ ! -d /var/lib/zookeeper-template/ ]; then
     echo "ZooKeeper template data directory does not exist."
