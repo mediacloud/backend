594afd329191f51412c0c8c409ff8c89b2044692
Linas Valiukas <linas@mediacloud.org>
2020-12-03 23:27:38 +0200

    Update some properties for newer Kibana version

--- apps/elk-kibana/bin/kibana.sh
+++ apps/elk-kibana/bin/kibana.sh
@@ -3,7 +3,6 @@
 set -u
 set -e
 
-
 exec /opt/kibana/bin/kibana \
-    --cpu.cgroup.path.override=/ \
-    --cpuacct.cgroup.path.override=/
+	--ops.cGroupOverrides.cpuPath=/ \
+	--ops.cGroupOverrides.cpuAcctPath=/
