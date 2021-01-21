d846c016d43d8ff36e19b672bd73509d49e533c9
Linas Valiukas <linas@mediacloud.org>
2021-01-21 20:53:56 +0200

    Load word2vec using shelve instead of gensim; simplify some things

--- apps/nytlabels-annotator/src/crappy-predict-news-labels/nytlabels.py
+++ apps/nytlabels-annotator/src/crappy-predict-news-labels/nytlabels.py
@@ -1,204 +1,110 @@
-"""
-Prediction models.
-"""
-
-import abc
+import dataclasses
 import os
+import shelve
 from typing import List
 
-import gensim
-import onnxruntime
-from nltk.tokenize import word_tokenize
+from nltk.data import load as load_nltk_data
+from nltk.tokenize.destructive import NLTKWordTokenizer
 import numpy as np
-
-DEFAULT_MAX_PREDICTIONS = 30
-"""Max. predictions to come up with."""
+import onnxruntime
 
 
-class MissingModelsException(Exception):
-    """Exception that's thrown when the models are missing."""
-    pass
-
-
-def _default_models_dir() -> str:
-    """
-    Return default path to directory with models.
-    :return: Path to directory with models.
-    """
-    pwd = os.path.dirname(os.path.abspath(__file__))
-    models_dir = os.path.join(pwd, 'models')
-    assert os.path.isdir(models_dir), f"Models path should be directory: {models_dir}"
-    return models_dir
-
-
-class Prediction(object):
-    """Single prediction."""
+class _ShelveWord2vecModel(object):
+    """Google News word2vec model stored with "shelve" module."""
 
     __slots__ = [
-        'label',
-        'score',
+        '_word2vec',
     ]
 
-    def __init__(self, label: str, score: float):
-        self.label = label
-        self.score = score
+    def __init__(self, word2vec_shelve_path: str) -> None:
+        self._word2vec = shelve.open(word2vec_shelve_path, flag='r', writeback=False)
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._word2vec
+
+    def __getitem__(self, item: str) -> np.ndarray:
+        vectors = np.frombuffer(self._word2vec[item], dtype=np.float32)
+        return vectors
 
 
-class _BaseLoader(object, metaclass=abc.ABCMeta):
-
-    @abc.abstractmethod
-    def _initialize_model(self, models_dir: str) -> None:
-        """
-        Initialize model and get it ready for prediction.
-        :param models_dir: Directory to where models are to be found.
-        """
-        raise NotImplemented("Abstract method.")
-
-    def __init__(self, models_dir: str = None):
-        """
-        Initialize model.
-        :param models_dir: (optional) Directory to where models are to be found.
-        """
-        if not models_dir:
-            models_dir = _default_models_dir()
-        if not os.path.isdir(models_dir):
-            raise MissingModelsException(f"Models directory does not exist at {models_dir}")
-
-        print(f"Loading model {self.__class__.__name__}...")
-        self._initialize_model(models_dir=models_dir)
-        print(f"Loaded model {self.__class__.__name__}.")
-
-
-class _BaseModel(_BaseLoader, metaclass=abc.ABCMeta):
-    """Base model class."""
-
-    @abc.abstractmethod
-    def predict(self, text: str, max_predictions: int = DEFAULT_MAX_PREDICTIONS) -> List[Prediction]:
-        """
-        Predict text.
-        :param text: Text to run predictions against.
-        :param max_predictions: Max. predictions to come up with.
-        :return: Predictions.
-        """
-        raise NotImplemented("Abstract method.")
-
-
-class Word2vecModel(_BaseModel):
-    """Google News word2vec model."""
-
+class Text2ScaledVectors(object):
     __slots__ = [
-        '_model',
-    ]
-
-    _BASE_NAME = "GoogleNews-vectors-negative300.keyedvectors"
-
-    def _initialize_model(self, models_dir: str) -> None:
-        vectors_bin_path = os.path.join(models_dir, self._BASE_NAME)
-        vectors_npy_path = os.path.join(models_dir, self._BASE_NAME + '.vectors.npy')
-
-        if not os.path.isfile(vectors_bin_path):
-            raise MissingModelsException(f"Vectors file does not exist at {vectors_bin_path}")
-        if not os.path.isfile(vectors_npy_path):
-            raise MissingModelsException(f"Vectors .npy file does not exist at {vectors_npy_path}")
-        gensim.models.keyedvectors.Word2VecKeyedVectors
-        self._model = gensim.models.KeyedVectors.load(vectors_bin_path)
-
-    def predict(self, text: str, max_predictions: int = DEFAULT_MAX_PREDICTIONS) -> List[Prediction]:
-        raw_predictions = self._model.predict(text)
-        predictions = [Prediction(label=x[0], score=x[1]) for x in raw_predictions[:max_predictions]]
-        return predictions
-
-    def raw_word2vec_model(self):
-        """
-        Return raw KeyedVectors model.
-        :return: Raw KeyedVectorsModel.
-        """
-        return self._model
-
-
-class Scaler(_BaseLoader):
-    __slots__ = [
+        '_word2vec',
         '_scaler',
-    ]
-
-    def _initialize_model(self, models_dir: str) -> None:
-        """
-        Initialize model and get it ready for prediction.
-        :param models_dir: Directory to where models are to be found.
-        """
-        # Load pre-trained scaler used by all the models
-        scaler_path = os.path.join(models_dir, 'scaler.onnx')
-        if not os.path.isfile(scaler_path):
-            raise MissingModelsException(f"Scaler was not found in {scaler_path}")
-
-        self._scaler = onnxruntime.InferenceSession(scaler_path)
-
-    def raw_scaler(self):
-        return self._scaler
-
-
-class _TopicDetectionBaseModel(_BaseModel, metaclass=abc.ABCMeta):
-    """Base topic detection model."""
-
-    __slots__ = [
-        '_raw_word2vec_model',
-        '_raw_scaler',
-        '_model',
-        '_labels',
+        '_sentence_tokenizer',
+        '_word_tokenizer',
     ]
 
     _PUNCTUATION = '.,:;!?()/\"-<>[]{}|\\@#`$%^&*'
 
-    @staticmethod
-    @abc.abstractmethod
-    def _model_basename() -> str:
-        """
-        Return file basename (without extension) of model to load
-        :return: File basename of model to load, e.g. 'descriptors_600'.
-        """
-        raise NotImplemented("Abstract method")
+    def __init__(self, word2vec_shelve_path: str, scaler_path: str):
 
-    def _initialize_model(self, models_dir: str) -> None:
+        self._sentence_tokenizer = load_nltk_data("tokenizers/punkt/english.pickle")
+        self._word_tokenizer = NLTKWordTokenizer()
 
-        assert self._raw_word2vec_model, "Raw word2vec model is unset."
-        assert self._raw_scaler, "Scaler is unset."
+        if not os.path.isfile(word2vec_shelve_path):
+            raise RuntimeError(f"word2vec shelved file was not found in {word2vec_shelve_path}")
+        if not os.path.isfile(scaler_path):
+            raise RuntimeError(f"Scaler was not found in {scaler_path}")
 
-        model_basename = self._model_basename()
-        assert model_basename, "Model basename is empty."
+        self._word2vec = _ShelveWord2vecModel(word2vec_shelve_path=word2vec_shelve_path)
+        self._scaler = onnxruntime.InferenceSession(scaler_path)
 
-        model_path = os.path.join(models_dir, f'{model_basename}.onnx')
-        model_labels = os.path.join(models_dir, f'{model_basename}.txt')
+    def _word_tokenize(self, text: str) -> List[str]:
+        sentences = self._sentence_tokenizer.tokenize(text)
+        return [
+            token for sent in sentences for token in self._word_tokenizer.tokenize(sent)
+        ]
 
-        if not os.path.isfile(model_path):
-            raise MissingModelsException(f"Model was not found in {model_path}")
-        if not os.path.isfile(model_labels):
-            raise MissingModelsException(f"Model labels were not found in {model_labels}")
+    def transform(self, text: str, sample_length: int, embedding_size: int) -> np.ndarray:
 
-        self._model = onnxruntime.InferenceSession(model_path)
-        self._labels = open(model_labels, 'r').read().splitlines()
+        assert embedding_size == self._scaler.get_inputs()[0].shape[1]
 
-    def __init__(self, word2vec_model: Word2vecModel, scaler, models_dir: str = None):
-        assert word2vec_model, "word2vec model is unset."
-        assert scaler, "Scaler is unset."
-
-        self._raw_word2vec_model = word2vec_model.raw_word2vec_model()
-        self._raw_scaler = scaler.raw_scaler()
-
-        super().__init__(models_dir=models_dir)
-
-    def predict(self, text: str, max_predictions: int = DEFAULT_MAX_PREDICTIONS) -> List[Prediction]:
-
-        _, sample_length, embedding_size = self._model.get_inputs()[0].shape
-        assert embedding_size == self._raw_scaler.get_inputs()[0].shape[1]
-
-        words = [w.lower() for w in word_tokenize(text) if w not in self._PUNCTUATION][:sample_length]
+        words = [w.lower() for w in self._word_tokenize(text) if w not in self._PUNCTUATION][:sample_length]
         x_matrix = np.zeros((1, sample_length, embedding_size))
 
         for i, w in enumerate(words):
-            if w in self._raw_word2vec_model:
-                word_vector = self._raw_word2vec_model[w].reshape(1, -1)
-                scaled_vector = self._raw_scaler.run(None, {self._raw_scaler.get_inputs()[0].name: word_vector})[0][0]
+            if w in self._word2vec:
+                word_vector = self._word2vec[w].reshape(1, -1)
+                scaled_vector = self._scaler.run(None, {self._scaler.get_inputs()[0].name: word_vector})[0][0]
                 x_matrix[0][i] = scaled_vector
+
+        return x_matrix
+
+
+@dataclasses.dataclass
+class Prediction(object):
+    """Single prediction."""
+    label: str
+    score: float
+
+
+class MultiLabelPredict(object):
+    __slots__ = [
+        '_model',
+        '_labels',
+        '_sample_length',
+        '_embedding_size',
+    ]
+
+    def __init__(self, model_path: str, labels_path: str):
+        if not os.path.isfile(model_path):
+            raise RuntimeError(f"Model was not found in {model_path}")
+        if not os.path.isfile(labels_path):
+            raise RuntimeError(f"Model labels were not found in {labels_path}")
+
+        self._model = onnxruntime.InferenceSession(model_path)
+        self._labels = open(labels_path, 'r').read().splitlines()
+
+        _, self._sample_length, self._embedding_size = self._model.get_inputs()[0].shape
+
+    def sample_length(self) -> int:
+        return self._sample_length
+
+    def embedding_size(self) -> int:
+        return self._embedding_size
+
+    def predict(self, x_matrix: np.ndarray, max_predictions: int = 30) -> List[Prediction]:
 
         x = {node.name: x_matrix.astype(np.float32) for node in self._model.get_inputs()}
 
@@ -211,38 +117,3 @@ class _TopicDetectionBaseModel(_BaseModel, metaclass=abc.ABCMeta):
         predictions = [Prediction(label=x[0], score=x[1]) for x in raw_predictions[:max_predictions]]
 
         return predictions
-
-
-class Descriptors600Model(_TopicDetectionBaseModel):
-
-    @staticmethod
-    def _model_basename() -> str:
-        return 'descriptors_600'
-
-
-class Descriptors3000Model(_TopicDetectionBaseModel):
-
-    @staticmethod
-    def _model_basename() -> str:
-        return 'descriptors_3000'
-
-
-class DescriptorsAllModel(_TopicDetectionBaseModel):
-
-    @staticmethod
-    def _model_basename() -> str:
-        return 'all_descriptors'
-
-
-class DescriptorsWithTaxonomiesModel(_TopicDetectionBaseModel):
-
-    @staticmethod
-    def _model_basename() -> str:
-        return 'descriptors_with_taxonomies'
-
-
-class JustTaxonomiesModel(_TopicDetectionBaseModel):
-
-    @staticmethod
-    def _model_basename() -> str:
-        return 'just_taxonomies'
---
+++ apps/nytlabels-annotator/src/crappy-predict-news-labels/word2vec_to_shelve.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+Convert Google News model to shelve (https://docs.python.org/3/library/shelve.html) format:
+
+gensim.models.KeyedVectors insists on loading the full model into RAM which takes a lot of resources, so it's cheaper
+to store the vocabulary and associated vectors in a disk-backed dictionary such as shelve.
+
+Usage:
+
+    import shelve
+    word2vec = shelve.open(...)
+    vectors = numpy.frombuffer(word2vec['hello'], dtype=numpy.float32)
+
+"""
+
+import os
+import shelve
+
+from gensim.models import KeyedVectors
+
+if __name__ == '__main__':
+
+    sh_vectors_path = 'GoogleNews-vectors-negative300.shelve'
+
+    if os.path.isfile(sh_vectors_path):
+        os.unlink(sh_vectors_path)
+
+    vectors = shelve.open(sh_vectors_path)
+
+    model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    word2vec = model.wv
+
+    for term in word2vec.vocab.keys():
+        vectors[term] = word2vec.vectors[word2vec.vocab[term].index].tobytes()
+
+    vectors.sync()
+
+    vectors.close()
