d12985b47112959281b91cfc0bff9e7985fb936a
jtotoole <jtotoole@gmail.com>
2021-02-11 17:19:15 -0400

    remove some tests

--- apps/crawler-ap/tests/python/test_extract_url_parameters.py
+++
@@ -1,8 +0,0 @@
-# noinspection PyProtectedMember
-from crawler_ap.ap import _extract_url_parameters
-
-
-def test_extract_url_parameters() -> None:
-    """Test parameter extraction from url"""
-    url = 'https://www.google.com/page?a=5&b=abc'
-    assert _extract_url_parameters(url) == {'a': '5', 'b': 'abc'}
