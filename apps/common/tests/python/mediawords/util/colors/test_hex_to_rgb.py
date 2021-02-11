9df3331166820c6c1842db64fdd40de0336559b3
jtotoole <jtotoole@gmail.com>
2021-02-11 17:28:00 -0400

    remove a bunch of tests

--- apps/common/tests/python/mediawords/util/colors/test_hex_to_rgb.py
+++
@@ -1,8 +0,0 @@
-from mediawords.util.colors import hex_to_rgb
-
-
-def test_hex_to_rgb():
-    assert hex_to_rgb('ff0000') == (255, 0, 0,)
-    assert hex_to_rgb('FFFFFF') == (255, 255, 255,)
-    assert hex_to_rgb('#ff0000') == (255, 0, 0,)
-    assert hex_to_rgb('#FFFFFF') == (255, 255, 255,)
