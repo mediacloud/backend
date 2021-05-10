21ae1e6023ecfaac53a059e7767f814ff0bf5578
Linas Valiukas <linas@mediacloud.org>
2021-05-10 10:26:14 +0300

    Remove some huge directories before proceeding with the build

--- .github/free-up-disk-space.sh
+++ .github/free-up-disk-space.sh
@@ -25,6 +25,12 @@ sudo rm -f /swapfile
 echo "Cleaning APT cache..."
 sudo apt clean
 
+echo "Removing some directories..."
+sudo rm -rf /usr/local/lib/android/
+sudo rm -rf /usr/local/lib/node_modules/
+sudo rm -rf /usr/local/share/chromium/
+sudo rm -rf "${AGENT_TOOLSDIRECTORY}"
+
 echo "Removing docker images..."
 docker rmi $(docker image ls -aq)
 
