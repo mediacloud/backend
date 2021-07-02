2e95753505d7ebbef6ddad0dd2b36670579d961f
Linas Valiukas <shirshegsm@gmail.com>
2021-07-03 00:48:25 +0300

    Make snapshot_media_with_types view work

--- apps/common/src/perl/MediaWords/TM/Snapshot/Views.pm
+++ apps/common/src/perl/MediaWords/TM/Snapshot/Views.pm
@@ -129,47 +129,51 @@ sub add_media_type_views
     my ( $db ) = @_;
 
     $db->query( <<SQL
-
         CREATE OR REPLACE TEMPORARY VIEW snapshot_media_with_types AS
+
             WITH topics_id AS (
                 SELECT topics_id
                 FROM snapshot_topic_stories
                 LIMIT 1
+            ),
+
+            umtm AS (
+                SELECT
+                    *,
+                    ut.label AS tag_label
+                FROM tags AS ut
+                    INNER JOIN tag_sets AS uts ON
+                        ut.tag_sets_id = uts.tag_sets_id AND
+                        uts.name = 'media_type'
+                    INNER JOIN snapshot_media_tags_map AS umtm ON
+                        umtm.tags_id = ut.tags_id
+            ),
+
+            cmtm AS (
+                SELECT
+                    *,
+                    ct.label AS tag_label
+                FROM tags AS ct
+                    INNER JOIN snapshot_media_tags_map AS cmtm ON
+                        cmtm.tags_id = ct.tags_id
+                    INNER JOIN topics AS c ON
+                        c.media_type_tag_sets_id = ct.tag_sets_id
+                    INNER JOIN topics_id AS cid ON
+                        c.topics_id = cid.topics_id
             )
 
             SELECT
                 m.*,
                 CASE
-                    WHEN (ct.label != 'Not Typed') THEN ct.label
-                    WHEN (ut.label IS NOT NULL) THEN ut.label
+                    WHEN (cmtm.tag_label != 'Not Typed') THEN cmtm.tag_label
+                    WHEN (umtm.tag_label IS NOT NULL) THEN umtm.tag_label
                     ELSE 'Not Typed'
                 END AS media_type
-
             FROM snapshot_media AS m
-
-                LEFT JOIN (
-                    tags AS ut
-
-                        JOIN tag_sets AS uts
-                            ON ut.tag_sets_id = uts.tag_sets_id
-                           AND uts.name = 'media_type'
-
-                        JOIN snapshot_media_tags_map AS umtm
-                            on umtm.tags_id = ut.tags_id
-
-                ) ON m.media_id = umtm.media_id
-
-                LEFT JOIN (
-                    tags AS ct
-
-                        JOIN snapshot_media_tags_map AS cmtm
-                            ON cmtm.tags_id = ct.tags_id
-                        JOIN topics AS c
-                            ON c.media_type_tag_sets_id = ct.tag_sets_id
-                        JOIN topics_id AS cid
-                            ON c.topics_id = cid.topics_id
-                ) ON m.media_id = cmtm.media_id
-
+                LEFT JOIN umtm ON
+                    m.media_id = umtm.media_id
+                LEFT JOIN cmtm ON
+                    m.media_id = cmtm.media_id
 SQL
     );
 
@@ -179,8 +183,8 @@ SQL
                 s.*,
                 m.media_type
             FROM snapshot_stories AS s
-                JOIN snapshot_media_with_types AS m
-                    ON s.media_id = m.media_id
+                INNER JOIN snapshot_media_with_types AS m ON
+                    s.media_id = m.media_id
 SQL
 
 }
