3895b6d2244fed88aedf15f78cca8243a1073b2c
Linas Valiukas <shirshegsm@gmail.com>
2021-07-20 16:17:08 +0300

    Reformat queries

--- apps/topics-base/tests/python/topics_base/stories/test_add_missing_normalized_title_hashes.py
+++ apps/topics-base/tests/python/topics_base/stories/test_add_missing_normalized_title_hashes.py
@@ -12,16 +12,16 @@ from topics_base.stories import add_to_topic_stories, _add_missing_normalized_ti
 
 def __count_null_title_stories(db: DatabaseHandler, topic: dict) -> int:
     """Count the stories in the topic with a null normalized_title_hash."""
-    null_count = db.query(
-        """
-        select count(*)
-            from stories s
-                join topic_stories ts using ( stories_id )
-            where
-                ts.topics_id = %(a)s and
-                s.normalized_title_hash is null
-        """,
-        {'a': topic['topics_id']}).flat()[0]
+    null_count = db.query("""
+        SELECT COUNT(*)
+        FROM stories AS s
+            INNER JOIN topic_stories AS ts USING (stories_id)
+        WHERE
+            ts.topics_id = %(topics_id)s AND
+            s.normalized_title_hash IS NULL
+    """, {
+        'topics_id': topic['topics_id'],
+    }).flat()[0]
 
     return null_count
 
@@ -39,10 +39,10 @@ def test_add_missing_normalized_title_hashes():
         add_to_topic_stories(db, story, topic)
 
     # disable trigger so that we can actually set normalized_title_hash to null
-    db.query("alter table stories disable trigger stories_add_normalized_title")
+    db.query("ALTER TABLE stories DISABLE TRIGGER stories_add_normalized_title")
     # noinspection SqlWithoutWhere
-    db.query("update stories set normalized_title_hash = null")
-    db.query("alter table stories enable trigger stories_add_normalized_title")
+    db.query("UPDATE stories SET normalized_title_hash = NULL")
+    db.query("ALTER TABLE stories ENABLE TRIGGER stories_add_normalized_title")
 
     assert __count_null_title_stories(db=db, topic=topic) == num_stories
 
