9f29982eb8b35385712a6490a95cfcd975a6a07e
Linas Valiukas <shirshegsm@gmail.com>
2021-07-21 19:38:23 +0300

    Remove unused function table_exists()

--- apps/postgresql-server/schema/mediawords.sql
+++ apps/postgresql-server/schema/mediawords.sql
@@ -112,38 +112,6 @@ SELECT SUBSTRING(public.digest(string, 'md5'::text), 0, 9);
 $$ LANGUAGE SQL IMMUTABLE;
 
 
--- Returns true if table exists (and user has access to it)
--- Table name might be with ("public.stories") or without ("stories") schema.
-CREATE OR REPLACE FUNCTION table_exists(target_table_name VARCHAR)
-    RETURNS BOOLEAN AS
-$$
-DECLARE
-    schema_position INT;
-    schema          VARCHAR;
-BEGIN
-
-    SELECT POSITION('.' IN target_table_name) INTO schema_position;
-
-    -- "." at string index 0 would return position 1
-    IF schema_position = 0 THEN
-        schema := CURRENT_SCHEMA();
-    ELSE
-        schema := SUBSTRING(target_table_name FROM 1 FOR schema_position - 1);
-        target_table_name := SUBSTRING(target_table_name FROM schema_position + 1);
-    END IF;
-
-    RETURN EXISTS(
-            SELECT 1
-            FROM information_schema.tables
-            WHERE table_schema = schema
-              AND table_name = target_table_name
-        );
-
-END;
-$$ LANGUAGE plpgsql;
-
-
-
 CREATE TABLE media
 (
     media_id          BIGSERIAL PRIMARY KEY,
