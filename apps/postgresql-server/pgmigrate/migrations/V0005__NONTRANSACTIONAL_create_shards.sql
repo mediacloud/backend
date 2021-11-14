dc4c55486af02f7166444aa6b6c4c879f6759516
Linas Valiukas <shirshegsm@gmail.com>
2021-11-14 18:14:20 +0000

    Add FIXME

--- apps/postgresql-server/pgmigrate/migrations/V0005__NONTRANSACTIONAL_create_shards.sql
+++ apps/postgresql-server/pgmigrate/migrations/V0005__NONTRANSACTIONAL_create_shards.sql
@@ -20,6 +20,8 @@
 -- FIXME move very small tables in a migration
 -- FIXME re-add length constraints: https://brandur.org/text
 -- FIXME add media_id to its own separate colocation group
+-- FIXME drop the foreign keys on unsharded tables, then drop the tables the
+--     contents of which have been moved
 
 
 -- Rename the unsharded schema created in previous migrations
