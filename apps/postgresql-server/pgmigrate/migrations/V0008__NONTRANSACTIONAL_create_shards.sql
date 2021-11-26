bb002c2a860527c3b95b9c1e1a9ac6479460cbba
Linas Valiukas <shirshegsm@gmail.com>
2021-11-26 23:37:59 +0000

    Don't generate stories.normalized_title_hash if caller says they know better

--- apps/postgresql-server/pgmigrate/migrations/V0008__NONTRANSACTIONAL_create_shards.sql
+++ apps/postgresql-server/pgmigrate/migrations/V0008__NONTRANSACTIONAL_create_shards.sql
@@ -654,7 +654,9 @@ BEGIN
         END IF;
     END IF;
 
-    SELECT INTO NEW.normalized_title_hash MD5(get_normalized_title(NEW.title, NEW.media_id))::uuid;
+    IF (NEW.normalized_title_hash IS NULL) THEN
+        SELECT INTO NEW.normalized_title_hash MD5(get_normalized_title(NEW.title, NEW.media_id))::uuid;
+    END IF;
 
     RETURN NEW;
 
