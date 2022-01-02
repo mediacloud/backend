6f5e3d4f64456cdee003e368aaa4fab1dcec5782
Linas Valiukas <shirshegsm@gmail.com>
2022-01-02 23:43:51 +0200

    Run workflow and activities as different services
    https://github.com/firdaus/temporal-python-sdk/issues/15

--- apps/move-rows-to-shards/bin/activities_worker.py
+++ apps/move-rows-to-shards/bin/activities_worker.py
@@ -8,21 +8,24 @@ from temporal.workerfactory import WorkerFactory
 from mediawords.util.log import create_logger
 from mediawords.workflow.client import workflow_client
 
-from move_rows_to_shards.workflow import MoveRowsToShardsWorkflowImpl
-from move_rows_to_shards.workflow_interface import TASK_QUEUE
+from move_rows_to_shards.workflow import MoveRowsToShardsActivitiesImpl
+from move_rows_to_shards.workflow_interface import TASK_QUEUE, MoveRowsToShardsActivities
 
 log = create_logger(__name__)
 
 
-async def _start_workflow():
+async def _start_activities():
     client = workflow_client()
     factory = WorkerFactory(client=client, namespace=client.namespace)
     worker = factory.new_worker(task_queue=TASK_QUEUE)
-    worker.register_workflow_implementation_type(impl_cls=MoveRowsToShardsWorkflowImpl)
+    worker.register_activities_implementation(
+        activities_instance=MoveRowsToShardsActivitiesImpl(),
+        activities_cls_name=MoveRowsToShardsActivities.__name__,
+    )
     factory.start()
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    asyncio.ensure_future(_start_workflow())
+    asyncio.ensure_future(_start_activities())
     loop.run_forever()
