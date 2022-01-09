9e3438126241c808c86ffd8046d6d39b492123c1
Linas Valiukas <shirshegsm@gmail.com>
2022-01-10 01:11:34 +0200

    Revert "Add temporary exceptions for some story_urls chunks"
    This reverts commit 96081d9d2261775a6997d2a021892b886311558f.

--- apps/move-rows-to-shards/src/main/java/org/mediacloud/mrts/Database.java
+++ apps/move-rows-to-shards/src/main/java/org/mediacloud/mrts/Database.java
@@ -2,7 +2,6 @@ package org.mediacloud.mrts;
 
 import javax.annotation.Nullable;
 import java.sql.*;
-import java.util.Arrays;
 import java.util.List;
 
 public class Database {
@@ -14,60 +13,9 @@ public class Database {
     }
 
     public void query(List<String> sqlQueries) throws SQLException {
-
-        // FIXME story_urls exceptions
-        List<String> storyUrlsCopiedChunkSqlExcerpts = Arrays.asList(
-                "WHERE story_urls_id BETWEEN 1 AND 50000000",
-                "WHERE story_urls_id BETWEEN 50000001 AND 100000000",
-                "WHERE story_urls_id BETWEEN 100000001 AND 150000000",
-                "WHERE story_urls_id BETWEEN 150000001 AND 200000000",
-                "WHERE story_urls_id BETWEEN 200000001 AND 250000000",
-                "WHERE story_urls_id BETWEEN 250000001 AND 300000000",
-                "WHERE story_urls_id BETWEEN 300000001 AND 350000000",
-                "WHERE story_urls_id BETWEEN 350000001 AND 400000000",
-                "WHERE story_urls_id BETWEEN 400000001 AND 450000000",
-                "WHERE story_urls_id BETWEEN 450000001 AND 500000000",
-                "WHERE story_urls_id BETWEEN 500000001 AND 550000000",
-                "WHERE story_urls_id BETWEEN 600000001 AND 650000000",
-                "WHERE story_urls_id BETWEEN 650000001 AND 700000000",
-                "WHERE story_urls_id BETWEEN 700000001 AND 750000000",
-                "WHERE story_urls_id BETWEEN 750000001 AND 800000000",
-                "WHERE story_urls_id BETWEEN 800000001 AND 850000000",
-                "WHERE story_urls_id BETWEEN 850000001 AND 900000000",
-                "WHERE story_urls_id BETWEEN 900000001 AND 950000000",
-                "WHERE story_urls_id BETWEEN 950000001 AND 1000000000",
-                "WHERE story_urls_id BETWEEN 1000000001 AND 1050000000",
-                "WHERE story_urls_id BETWEEN 1050000001 AND 1100000000",
-                "WHERE story_urls_id BETWEEN 1100000001 AND 1150000000",
-                "WHERE story_urls_id BETWEEN 1150000001 AND 1200000000",
-                "WHERE story_urls_id BETWEEN 1200000001 AND 1250000000",
-                "WHERE story_urls_id BETWEEN 1250000001 AND 1300000000",
-                "WHERE story_urls_id BETWEEN 1300000001 AND 1350000000",
-                "WHERE story_urls_id BETWEEN 1350000001 AND 1400000000",
-                "WHERE story_urls_id BETWEEN 1400000001 AND 1450000000",
-                "WHERE story_urls_id BETWEEN 2050000001 AND 2100000000",
-                "WHERE story_urls_id BETWEEN 2100000001 AND 2150000000",
-                "WHERE story_urls_id BETWEEN 2150000001 AND 2200000000",
-                "WHERE story_urls_id BETWEEN 2300000001 AND 2350000000"
-        );
-
-        boolean sqlQueryIsStoryUrlsException = false;
-        if (sqlQueries.size() == 1) {
-            String sqlQuery = sqlQueries.get(0);
-
-            for (String excerpt : storyUrlsCopiedChunkSqlExcerpts) {
-                if (sqlQuery.contains(excerpt)) {
-                    sqlQueryIsStoryUrlsException = true;
-                    break;
-                }
-            }
-        }
-
-        if (!sqlQueryIsStoryUrlsException) {
-            Statement stmt = this.conn.createStatement();
-            for (String query : sqlQueries) {
-                stmt.executeUpdate(query);
-            }
+        Statement stmt = this.conn.createStatement();
+        for (String query : sqlQueries) {
+            stmt.executeUpdate(query);
         }
     }
 
